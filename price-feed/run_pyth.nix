64301439516ae8d22afba505c3399fff8b4743be
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2021-08-05 11:05:20 +0200

    fix missing keystore setup in nix script for pyth

--- price-feed/run_pyth.nix
+++ price-feed/run_pyth.nix
@@ -20,28 +20,48 @@ let
     '';
     installPhase = ''
       mkdir -p $out/bin
+      mv pyth $out/bin
       mv pythd $out/bin
       mv pyth_tx $out/bin
+      mv ../pctest/init_key_store.sh $out/bin
     '';
   };
 in mkShell {
-  packages = [ pythd ];
+  buildInputs = [ pythd ];
   SOLANA_ENV = "devnet";
   shellHook = ''
-    echo "Running up pyth_tx & pythd"
-    export PYTH_TX_LOG=$(mktemp)
-    pyth_tx -l $PYTH_TX_LOG -d -r api.$SOLANA_ENV.solana.com &
-    export PYTH_TX_PID=$!
-    export PYTHD_LOG=$(mktemp)
-    pythd -l $PYTHD_LOG -d -r api.$SOLANA_ENV.solana.com &
-    export PYTHD_PID=$!
-    teardown() {
-      echo "Shuting down pyth_tx & pythd";
-      kill -2 $PYTHD_PID
-      kill -2 $PYTH_TX_PID
-      rm $PYTH_TX_LOG
-      rm $PYTHD_LOG
+    export PYTHD_KEYSTORE=$HOME/.pythd
+
+    function init_keystore() {
+      echo "Creating key store"
+      rm -rf $PYTHD_KEYSTORE || true
+      mkdir -m 600 -p $PYTHD_KEYSTORE
+      echo "Initializing key store"
+      ${pythd}/bin/pyth init_key -k $PYTHD_KEYSTORE
+      echo "Populating key store"
+      ${pythd}/bin/init_key_store.sh $SOLANA_ENV $PYTHD_KEYSTORE
     }
-    trap teardown EXIT
+
+    function run() {
+      echo "Running pyth_tx"
+      export PYTH_TX_LOG=$(mktemp)
+      ${pythd}/bin/pyth_tx -l $PYTH_TX_LOG -d -r api.$SOLANA_ENV.solana.com &
+      export PYTH_TX_PID=$!
+
+      echo "Running pythd"
+      export PYTHD_LOG=$(mktemp)
+      ${pythd}/bin/pythd -k $PYTHD_KEYSTORE -l $PYTHD_LOG -d -r api.$SOLANA_ENV.solana.com &
+      export PYTHD_PID=$!
+
+      function teardown() {
+        echo "Shuting down pyth_tx & pythd";
+        kill -2 $PYTHD_PID
+        kill -2 $PYTH_TX_PID
+        rm $PYTH_TX_LOG
+        rm $PYTHD_LOG
+      }
+
+      trap teardown EXIT
+    }
   '';
 }
