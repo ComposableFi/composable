b6ad03f4ed771ea112c1e8613e51d809c5256f44
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2021-08-19 12:42:28 +0200

    multi-currency based vault

--- frame/vault/src/models.rs
+++ frame/vault/src/models.rs
@@ -3,23 +3,24 @@ use sp_runtime::Perquintill;
 use sp_std::collections::btree_map::BTreeMap;
 
 #[derive(Encode, Decode, Default, Debug, PartialEq)]
-pub struct VaultConfig<AccountId, AssetId>
+pub struct VaultConfig<AccountId, CurrencyId>
 where
     AccountId: core::cmp::Ord + core::hash::Hash,
 {
-    pub asset_id: AssetId,
+    pub asset_id: CurrencyId,
     pub reserved: Perquintill,
     pub strategies: BTreeMap<AccountId, Perquintill>,
 }
 
 #[derive(Encode, Decode, Default, Debug, PartialEq)]
-pub struct Vault<AssetId, Balance> {
-    pub asset_id: AssetId,
-    pub lp_token_id: AssetId,
+pub struct Vault<CurrencyId, Balance> {
+    pub asset_id: CurrencyId,
+    pub lp_token_id: CurrencyId,
     pub assets_under_management: Balance,
 }
 
 #[derive(Encode, Decode, Default, Debug, PartialEq)]
-pub struct StrategyOverview<Balance> {
+pub struct StrategyOverview<Balance, Report> {
     pub withdrawn: Balance,
+    pub report: Report,
 }
