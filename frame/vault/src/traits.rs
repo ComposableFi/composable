a4b041dc97ab35b16f7c9ffcdd3c54eb9929bfa5
kaiserkarel <k.l.kubat@gmail.com>
2021-08-23 09:42:25 +0200

    fmt

--- frame/vault/src/traits.rs
+++ frame/vault/src/traits.rs
@@ -1,4 +1,4 @@
 //! Traits on which this pallet relies
 
 pub use composable_traits::currency::CurrencyFactory;
-pub use composable_traits::vault::{ReportableStrategicVault, StrategicVault, FundsAvailability};
+pub use composable_traits::vault::{FundsAvailability, ReportableStrategicVault, StrategicVault};
