53e9f7f6886bfab82a724fccb92a28e1ec098c4d
Seun Lanlege <seunlanlege@gmail.com>
2021-08-24 10:23:11 +0100

    adjust blocktime, correct SS58 format

--- runtime/picasso/src/lib.rs
+++ runtime/picasso/src/lib.rs
@@ -133,8 +133,8 @@ parameter_types! {
         })
         .avg_block_initialization(AVERAGE_ON_INITIALIZE_RATIO)
         .build_or_panic();
-	// TODO: update
-    pub const SS58Prefix: u8 = 42;
+
+    pub const SS58Prefix: u8 = 49;
 }
 
 // Configure FRAME pallets to include in runtime.
