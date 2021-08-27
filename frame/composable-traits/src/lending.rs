c1dbef3d42b523335dca3f1a6ddcc171b5037c3f
dzmitry.lahoda <dzmitry@lahoda.pro>
2021-08-27 20:09:06 +0300

    lending trait

--- frame/composable-traits/src/lending.rs
+++ frame/composable-traits/src/lending.rs
@@ -1,37 +1,92 @@
+use codec::Codec;
+use frame_support::{pallet_prelude::*, sp_std::fmt::Debug};
 
+use crate::vault::Deposit;
 
+#[derive(Clone, Encode, Decode, Default, Debug)]
+pub struct AccountConfig<AccountId, AssetId>
+where
+	AccountId: core::cmp::Ord,
+{
+	pub deposit: AssetId,
+	pub collateral: AssetId,
+	pub manager: AccountId,
+}
 
-
+pub trait Composable {
+	type Error;
+	type Balance;
+	type BlockNumber;
+	type AccountId: core::cmp::Ord;
+}
 
 /// basic lending
-pub trait Lending  {
-	type AccountId: core::cmp::Ord;
+// assumption that user will deposit borrow and collateral assets via vault
+// does not have wrapped tokens, so no need to proxy vault calls
+// liquidation is other trait
+// ASK: not clear how Vault will prevent withdrawing collateral?
+// based on Blacksmith (Warp v2) IBSLendingPair.sol, but without wrapper token and proxy methods
+pub trait Lending: Composable {
 	type AssetId;
-	type Error;
-	type PairId : AccountId;
-	type Balance;
+	type VaultId: Clone + Codec + Debug + PartialEq;
+	type PairId: Clone + Codec + Debug + PartialEq;
 
-	fn take_loan(
-		pair: PairId,
-		to: &Self::AccountId,
-		borrowed_amount: &Self::Balance
+	/// creates market for new pair in specified vault
+	fn create(
+		vault: Self::VaultId,
+		deposit: Deposit<Self::Balance, Self::BlockNumber>,
+		config: AccountConfig<Self::AccountId, Self::AssetId>,
+	) -> Result<Self::PairId, Self::Error>;
+
+	fn get_pair_in_vault(vault: Self::VaultId) -> Result<Vec<Self::PairId>, Self::Error>;
+
+	fn get_pairs_all() -> Result<Vec<Self::PairId>, Self::Error>;
+
+	fn borrow(
+		pair: Self::PairId,
+		debt_owner: &Self::AccountId,
+		amount_to_borrow: Self::Balance,
 	) -> Result<(), Self::Error>;
 
-	fn pay_back_loan(
-		pair: PairId,
+	fn repay(
+		pair: Self::PairId,
 		from: &Self::AccountId,
-		to: &Self::AccountId,
+		beneficiary: &Self::AccountId,
+		repay_amount: Self::Balance,
 	) -> Result<(), Self::Error>;
 
-	/// gets per year interest rate
-	fn get_interest_rate(
-		pair: PairId,
-	) ->
+	/// part or whole deposited assets + interest to account
+	fn redeem(
+		pair: Self::PairId,
+		to: &Self::AccountId,
+		redeem_amount: Self::Balance,
+	) -> Result<(), Self::Error>;
 
-	// assumption that user will deposit borrow and collateral assets via vault
-	// fn add_collateral(
-	// 	pair: PairId,
-	// 	from: &Self::AccountId,
-	// 	borrowed_amount: &Self::Balance
-	// ) -> Result<(), Self::Error>;
+	fn calculate_liquidation_fee(amount: Self::Balance) -> Self::Balance;
+
+	fn total_borrows(pair: Self::PairId) -> Result<Self::Balance, Self::Error>;
+
+	fn accrue_interest(pair: Self::PairId) -> Result<(), Self::Error>;
+
+	fn borrow_balance_current(
+		pair: Self::PairId,
+		account: &Self::AccountId,
+	) -> Result<Self::Balance, Self::Error>;
+
+	fn withdraw_fees(to_withdraw: Self::Balance) -> Result<(), Self::Error>;
+
+	fn collateral_of_account(
+		pair: Self::PairId,
+		account: &Self::AccountId,
+	) -> Result<Self::Balance, Self::Error>;
+
+	fn collateral_required(
+		pair: Self::PairId,
+		borrow_amount: Self::Balance,
+	) -> Result<Self::Balance, Self::Error>;
+
+	fn get_borrow_limit(
+		pair: Self::PairId,
+		account: Self::AccountId,
+	) -> Result<Self::Balance, Self::Error>;
 }
--- frame/composable-traits/src/lib.rs
+++ frame/composable-traits/src/lib.rs
@@ -1,5 +1,5 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 pub mod currency;
-pub mod vault;
 pub mod lending;
+pub mod vault;
