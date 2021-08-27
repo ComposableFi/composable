35afc4ad24b4b4063f727c6c630fbef2dc6bf04c
dzmitry.lahoda <dzmitry@lahoda.pro>
2021-08-27 16:21:55 +0300

    more  modelling

--- frame/composable-traits/src/lending.rs
+++ frame/composable-traits/src/lending.rs
@@ -1,7 +1,37 @@
 
+
+
+
+
+/// basic lending
 pub trait Lending  {
 	type AccountId: core::cmp::Ord;
 	type AssetId;
 	type Error;
-	type Vault: crate::vault::Vault,
+	type PairId : AccountId;
+	type Balance;
+
+	fn take_loan(
+		pair: PairId,
+		to: &Self::AccountId,
+		borrowed_amount: &Self::Balance
+	) -> Result<(), Self::Error>;
+
+	fn pay_back_loan(
+		pair: PairId,
+		from: &Self::AccountId,
+		to: &Self::AccountId,
+	) -> Result<(), Self::Error>;
+
+	/// gets per year interest rate
+	fn get_interest_rate(
+		pair: PairId,
+	) ->
+
+	// assumption that user will deposit borrow and collateral assets via vault
+	// fn add_collateral(
+	// 	pair: PairId,
+	// 	from: &Self::AccountId,
+	// 	borrowed_amount: &Self::Balance
+	// ) -> Result<(), Self::Error>;
 }
