85494b73b0298af71f405fd847f2f2a6e95cba7e
Andrey Orlov <aaorlov1@gmail.com>
2021-09-17 11:28:56 +0000

    Include benchmarks of pallet-lending in runtime

--- runtime/picasso/src/lib.rs
+++ runtime/picasso/src/lib.rs
@@ -1015,6 +1015,7 @@ impl_runtime_apis! {
 			add_benchmark!(params, batches, scheduler, Scheduler);
 			add_benchmark!(params, batches, democracy, Democracy);
 			add_benchmark!(params, batches, collective, Council);
+			add_benchmark!(params, batches, lending, Lending);
 
 			if batches.is_empty() { return Err("Benchmark not found for this pallet.".into()) }
 			Ok(batches)
