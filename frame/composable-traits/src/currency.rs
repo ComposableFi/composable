58149b3eb37eeb5425a811cb790ff7515afb847e
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2021-09-21 15:00:10 +0200

    introduce DynamicCurrency for sub-ranges that can be generated

--- frame/composable-traits/src/currency.rs
+++ frame/composable-traits/src/currency.rs
@@ -1,12 +1,21 @@
-use codec::FullCodec;
-use core::fmt::Debug;
 use frame_support::pallet_prelude::*;
+
+/// A currency we can generate given that we have a previous currency.
+/// I initially added a generic type to index into the generatable sub-range but realised it was
+/// overkill. Perhaps it will be required later if we want to differentiate multiple sub-ranges
+/// (possibly making a sub-range constant, e.g. using a constant currency id for a pallet expecting
+/// currency ids to be generated).
+/// The implementor should ensure that a new `DynamicCurrency` is created and collisions are
+/// avoided.
+pub trait DynamicCurrency
+where
+	Self: Sized,
+{
+	fn next(self) -> Result<Self, DispatchError>;
+}
 
 /// Creates a new asset, compatible with [`MultiCurrency`](https://docs.rs/orml-traits/0.4.0/orml_traits/currency/trait.MultiCurrency.html).
 /// The implementor should ensure that a new `CurrencyId` is created and collisions are avoided.
-pub trait CurrencyFactory<CurrencyId>
-where
-	CurrencyId: FullCodec + Eq + PartialEq + Copy + MaybeSerializeDeserialize + Debug,
-{
+pub trait CurrencyFactory<CurrencyId> {
 	fn create() -> Result<CurrencyId, DispatchError>;
 }
