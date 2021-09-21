4eb2ca7761ac2958933416fbe7f504642b1b535b
dzmitry.lahoda <dzmitry@lahoda.pro>
2021-09-21 15:39:14 +0300

    repaced std usage with sp

--- frame/composable-traits/src/rate_model.rs
+++ frame/composable-traits/src/rate_model.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::convert::TryInto;
+use sp_std::convert::TryInto;
 
 use codec::{Decode, Encode};
 
