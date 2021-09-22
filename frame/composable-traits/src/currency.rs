ca48d9907c75236f1eafa409d0b7bb5d417fcbc6
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2021-09-22 10:26:38 +0200

    move note to normal comment

--- frame/composable-traits/src/currency.rs
+++ frame/composable-traits/src/currency.rs
@@ -1,12 +1,14 @@
 use frame_support::pallet_prelude::*;
 
+/* NOTE(hussein-aitlahcen):
+ I initially added a generic type to index into the generatable sub-range but realised it was
+ overkill. Perhaps it will be required later if we want to differentiate multiple sub-ranges
+ (possibly making a sub-range constant, e.g. using a constant currency id for a pallet expecting
+ currency ids to be generated).
+ The implementor should ensure that a new `DynamicCurrency` is created and collisions are
+ avoided.
+*/
 /// A currency we can generate given that we have a previous currency.
-/// I initially added a generic type to index into the generatable sub-range but realised it was
-/// overkill. Perhaps it will be required later if we want to differentiate multiple sub-ranges
-/// (possibly making a sub-range constant, e.g. using a constant currency id for a pallet expecting
-/// currency ids to be generated).
-/// The implementor should ensure that a new `DynamicCurrency` is created and collisions are
-/// avoided.
 pub trait DynamicCurrency
 where
 	Self: Sized,
