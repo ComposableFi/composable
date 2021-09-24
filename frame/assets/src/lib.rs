5f0d539c3f0ce35708a4f91758a9bdde7750eb0a
kaiserkarel <k.l.kubat@gmail.com>
2021-09-24 11:40:52 +0200

    define and implement MultiCurrency trait
    clippy

--- frame/assets/src/lib.rs
+++ frame/assets/src/lib.rs
@@ -4,43 +4,13 @@ pub use pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use codec::FullCodec;
-	use frame_support::{
-		dispatch::Codec,
-		pallet_prelude::*,
-		sp_runtime::traits::{AtLeast32BitUnsigned, CheckedAdd, CheckedMul, CheckedSub},
-		sp_std::fmt::Debug,
-	};
-	use sp_std::ops::AddAssign;
-
-	pub trait AssetId: FullCodec + Copy + Eq + PartialEq + Debug {}
-	impl<T: FullCodec + Copy + Eq + PartialEq + Debug> AssetId for T {}
-	pub trait Balance:
-		AtLeast32BitUnsigned
-		+ FullCodec
-		+ Copy
-		+ Default
-		+ Debug
-		+ MaybeSerializeDeserialize
-		+ MaxEncodedLen
-	{
-	}
-	impl<
-			T: AtLeast32BitUnsigned
-				+ FullCodec
-				+ Copy
-				+ Default
-				+ Debug
-				+ MaybeSerializeDeserialize
-				+ MaxEncodedLen,
-		> Balance for T
-	{
-	}
+	use composable_traits::currency::{AssetId, Balance};
+	use frame_support::pallet_prelude::*;
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		Placeholder
+		Placeholder,
 	}
 
 	#[pallet::config]
@@ -65,18 +35,24 @@ pub mod pallet {
 
 	mod currency {
 		use super::*;
-		use frame_support::traits::{Currency, ExistenceRequirement, SignedImbalance, WithdrawReasons, ReservableCurrency, BalanceStatus};
+		use frame_support::traits::{
+			BalanceStatus, Currency, ExistenceRequirement, ReservableCurrency, SignedImbalance,
+			WithdrawReasons,
+		};
 
 		impl<T: Config> ReservableCurrency<T::AccountId> for Pallet<T>
-			where
-				<T as Config>::Currency: Currency<T::AccountId, Balance = T::Balance>,
-				<T as Config>::Currency: ReservableCurrency<T::AccountId, Balance = T::Balance>,
+		where
+			<T as Config>::Currency: Currency<T::AccountId, Balance = T::Balance>,
+			<T as Config>::Currency: ReservableCurrency<T::AccountId, Balance = T::Balance>,
 		{
 			fn can_reserve(who: &T::AccountId, value: Self::Balance) -> bool {
 				<<T as Config>::Currency>::can_reserve(who, value)
 			}
 
-			fn slash_reserved(who: &T::AccountId, value: Self::Balance) -> (Self::NegativeImbalance, Self::Balance) {
+			fn slash_reserved(
+				who: &T::AccountId,
+				value: Self::Balance,
+			) -> (Self::NegativeImbalance, Self::Balance) {
 				<<T as Config>::Currency>::slash_reserved(who, value)
 			}
 
@@ -92,7 +68,12 @@ pub mod pallet {
 				<<T as Config>::Currency>::unreserve(who, value)
 			}
 
-			fn repatriate_reserved(slashed: &T::AccountId, beneficiary: &T::AccountId, value: Self::Balance, status: BalanceStatus) -> Result<Self::Balance, DispatchError> {
+			fn repatriate_reserved(
+				slashed: &T::AccountId,
+				beneficiary: &T::AccountId,
+				value: Self::Balance,
+				status: BalanceStatus,
+			) -> Result<Self::Balance, DispatchError> {
 				<<T as Config>::Currency>::repatriate_reserved(slashed, beneficiary, value, status)
 			}
 		}
@@ -219,14 +200,9 @@ pub mod pallet {
 	mod fungible {
 		use super::*;
 
-		use frame_support::traits::{
-			fungible::{CreditOf, DebtOf},
-			tokens::{
-				fungible::{
-					BalancedHold, Inspect, InspectHold, Mutate, MutateHold, Transfer, Unbalanced,
-				},
-				DepositConsequence, WithdrawConsequence,
-			},
+		use frame_support::traits::tokens::{
+			fungible::{Inspect, InspectHold, Mutate, MutateHold, Transfer, Unbalanced},
+			DepositConsequence, WithdrawConsequence,
 		};
 
 		impl<T: Config> MutateHold<T::AccountId> for Pallet<T>
