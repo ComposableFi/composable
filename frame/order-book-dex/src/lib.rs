19a69b2315f3e7bd2e8e6a014856ed0772842283
dzmitry.lahoda <dzmitry@lahoda.pro>
2021-09-25 16:56:04 +0300

    fixed traits, satisfy dex

--- frame/order-book-dex/src/lib.rs
+++ frame/order-book-dex/src/lib.rs
@@ -23,9 +23,9 @@
 #[frame_support::pallet]
 pub mod pallet {
 
-	use codec::Codec;
-use composable_traits::rate_model::LiftedFixedBalance;
-use frame_support::{Parameter, traits::UnixTime};
+	use codec::{Codec, FullCodec};
+	use composable_traits::{dex::SimpleExchange, rate_model::LiftedFixedBalance};
+	use frame_support::{Parameter, pallet_prelude::MaybeSerializeDeserialize, traits::UnixTime};
 	use frame_system::pallet_prelude::*;
 	use num_traits::{CheckedDiv, SaturatingSub};
 	use sp_runtime::{
@@ -35,60 +35,88 @@ use frame_support::{Parameter, traits::UnixTime};
 		},
 		ArithmeticError, FixedPointNumber, FixedPointOperand, FixedU128, Percent, Perquintill,
 	};
-
+	pub trait DeFiComposablePallet {
+		type AssetId: FullCodec
+		+ Eq
+		+ PartialEq
+		+ Copy
+		+ MaybeSerializeDeserialize
+		+ From<u128>
+		+ Default;
+	}
 
 	#[pallet::config]
-	pub trait Config : frame_system::Config  {
+	pub trait Config: frame_system::Config + DeFiComposablePallet {
 		type Balance: Default
-		+ Parameter
-		+ Codec
-		+ Copy
-		+ Ord
-		+ CheckedAdd
-		+ CheckedSub
-		+ CheckedMul
-		+ SaturatingSub
-		+ AtLeast32BitUnsigned
-		+ From<u64> // at least 64 bit
-		+ Zero
-		+ FixedPointOperand
-		+ Into<LiftedFixedBalance> // integer part not more than bits in this
-		+ Into<u128>; // cannot do From<u128>, until LiftedFixedBalance integer part is larger than 128
-		  // bit
+			+ Parameter
+			+ Codec
+			+ Copy
+			+ Ord
+			+ CheckedAdd
+			+ CheckedSub
+			+ CheckedMul
+			+ SaturatingSub
+			+ AtLeast32BitUnsigned
+			+ From<u64> // at least 64 bit
+			+ Zero
+			+ FixedPointOperand
+			+ Into<LiftedFixedBalance> // integer part not more than bits in this
+			+ Into<u128>; // cannot do From<u128>, until LiftedFixedBalance integer part is larger than 128
+			  // bit
 		type UnixTime: UnixTime;
 	}
 
-	pub struct DexInitialization {
-
-	}
+	pub struct DexInitialization {}
 
 	/// allows order to be diminished in requested price
-	pub struct DutchAuctionsConfig
-	{
+	pub struct DutchAuctionsConfig {}
 
-	}
-
-	pub enum OrderPrice<T:Config> {
+	pub enum OrderPrice<T: Config> {
 		ExactPrice(T::Balance),
 		Dutch(T::Balance, DutchAuctionsConfig),
 	}
 
-	pub enum OrderStatus {
-
-	}
+	pub enum OrderStatus {}
 
 	/// Store on chain multi dictionary key (from, to, account) , dictionary per buy and sell
-	pub struct Order<T:Config>
-	{
+	pub struct Order<T: Config> {
 		pub amount: T::Balance,
-		pub price : OrderPrice<T>,
-		pub time_stamp : T::UnixTime,
-		pub trader : T::AccountId,
+		pub price: OrderPrice<T>,
+		pub time_stamp: T::UnixTime,
+		pub trader: T::AccountId,
 		pub status: OrderStatus,
 	}
+	#[pallet::error]
+	pub enum Error<T> {}
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
-}
 
+
+
+	impl<T: Config> SimpleExchange for Pallet<T> {
+		type AssetId = T::AssetId;
+
+		type Balance = T::Balance;
+
+		type AccountId = T::Balance;
+
+		type Error = Error<T>;
+
+		fn price(asset_id: Self::AssetId) -> Option<Self::Balance> {
+			todo!()
+		}
+
+		fn exchange(
+			from: Self::AssetId,
+			from_account: Self::AccountId,
+			to: Self::AssetId,
+			to_account: Self::AccountId,
+			from_amount: Self::Balance,
+			slippage: sp_runtime::Perbill,
+		) -> Result<Self::Balance, Self::Error> {
+			todo!()
+		}
+	}
+}
