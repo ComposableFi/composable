39693deab1030c0117f490d7977d1def2271a3ba
Filip <flipchan@riseup.net>
2021-09-27 11:41:01 +0300

    weights file

---
+++ frame/assets/src/weights.rs
@@ -0,0 +1,301 @@
+// Executed Command:
+// target/release/substrate
+// benchmark
+// --chain=dev
+// --steps=50
+// --repeat=20
+// --pallet=pallet_assets
+// --extrinsic=*
+// --execution=wasm
+// --wasm-execution=compiled
+// --heap-pages=4096
+// --output=./frame/assets/src/weights.rs
+// --template=./.maintain/frame-weight-template.hbs
+
+
+#![allow(unused_parens)]
+#![allow(unused_imports)]
+
+use frame_support::{traits::Get, weights::{Weight, constants::RocksDbWeight}};
+use sp_std::marker::PhantomData;
+
+pub trait WeightInfo {
+	fn transfer() -> Weight;
+	fn transfer_native() -> Weight;
+	fn force_transfer() -> Weight;
+	fn force_transfer_native() -> Weight;
+	fn transfer_all() -> Weight;
+	fn transfer_native() -> Weight;
+/*
+      fn can_reserve() -> Weights;
+      fn slash_reserved() -> Weights;                
+      fn reserved_balance() -> Weights;
+      fn reserve() -> Weights;
+      fn unreserve() -> Weights;
+      fn repatriate_reserved() -> Weights;           
+	fn destroy(c: u32, s: u32, a: u32, ) -> Weight;
+	fn mint() -> Weight;
+	fn burn() -> Weight;
+	fn force_transfer() -> Weight;
+	fn freeze() -> Weight;
+	fn thaw() -> Weight;
+	fn freeze_asset() -> Weight;
+	fn thaw_asset() -> Weight;
+	fn transfer_ownership() -> Weight;
+	fn set_team() -> Weight;
+	fn set_metadata(n: u32, s: u32, ) -> Weight;
+	fn clear_metadata() -> Weight;
+	fn force_set_metadata(n: u32, s: u32, ) -> Weight;
+	fn force_clear_metadata() -> Weight;
+	fn force_asset_status() -> Weight;
+	fn approve_transfer() -> Weight;
+	fn transfer_approved() -> Weight;
+	fn cancel_approval() -> Weight;
+	fn force_cancel_approval() -> Weight;
+*/
+}
+
+
+pub struct SubstrateWeight<T>(PhantomData<T>);
+impl<T: frame_system::Config> WeightInfo for SubstrateWeight<T> {
+	// Storage: Assets Asset (r:1 w:1)
+	fn force_create() -> Weight {
+		(21_378_000 as Weight)
+			.saturating_add(T::DbWeight::get().reads(1 as Weight))
+			.saturating_add(T::DbWeight::get().writes(1 as Weight))
+	}
+	// Storage: Assets Asset (r:1 w:1)
+	// Storage: Assets Account (r:1 w:1)
+	fn mint() -> Weight {
+		(47_913_000 as Weight)
+			.saturating_add(T::DbWeight::get().reads(2 as Weight))
+			.saturating_add(T::DbWeight::get().writes(2 as Weight))
+	}
+	// Storage: Assets Asset (r:1 w:1)
+	fn burn() -> Weight {
+		(55_759_000 as Weight)
+			.saturating_add(T::DbWeight::get().reads(2 as Weight))
+			.saturating_add(T::DbWeight::get().writes(2 as Weight))
+	}
+	// Storage: Assets Asset (r:1 w:1)
+	fn transfer() -> Weight {
+		(83_205_000 as Weight)
+			.saturating_add(T::DbWeight::get().reads(4 as Weight))
+			.saturating_add(T::DbWeight::get().writes(4 as Weight))
+	}
+	// Storage: Assets Asset (r:1 w:1)
+	fn transfer_keep_alive() -> Weight {
+		(70_665_000 as Weight)
+			.saturating_add(T::DbWeight::get().reads(4 as Weight))
+			.saturating_add(T::DbWeight::get().writes(4 as Weight))
+	}
+	// Storage: System Account (r:1 w:1)
+	fn force_transfer() -> Weight {
+		(81_458_000 as Weight)
+			.saturating_add(T::DbWeight::get().reads(4 as Weight))
+			.saturating_add(T::DbWeight::get().writes(4 as Weight))
+	}
+
+	fn force_transfer_native() -> Weight {
+		(81_458_000 as Weight)
+			.saturating_add(T::DbWeight::get().reads(4 as Weight))
+			.saturating_add(T::DbWeight::get().writes(4 as Weight))
+	}
+
+	fn approve_transfer() -> Weight {
+		(56_998_000 as Weight)
+			.saturating_add(T::DbWeight::get().reads(2 as Weight))
+			.saturating_add(T::DbWeight::get().writes(2 as Weight))
+	}
+	// Storage: System Account (r:1 w:1)
+	fn transfer_approved() -> Weight {
+		(107_171_000 as Weight)
+			.saturating_add(T::DbWeight::get().reads(5 as Weight))
+			.saturating_add(T::DbWeight::get().writes(5 as Weight))
+	}
+}
+
+// For backwards compatibility and tests
+impl WeightInfo for () {
+	// Storage: Assets Asset (r:1 w:1)
+	fn create() -> Weight {
+		(41_651_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(1 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(1 as Weight))
+	}
+	// Storage: Assets Asset (r:1 w:1)
+	fn force_create() -> Weight {
+		(21_378_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(1 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(1 as Weight))
+	}
+	// Storage: Assets Asset (r:1 w:1)
+	// Storage: Assets Account (r:5002 w:5001)
+	// Storage: System Account (r:5000 w:5000)
+	// Storage: Assets Metadata (r:1 w:0)
+	// Storage: Assets Approvals (r:501 w:500)
+	fn destroy(c: u32, s: u32, a: u32, ) -> Weight {
+		(0 as Weight)
+			// Standard Error: 32_000
+			.saturating_add((21_163_000 as Weight).saturating_mul(c as Weight))
+			// Standard Error: 32_000
+			.saturating_add((26_932_000 as Weight).saturating_mul(s as Weight))
+			// Standard Error: 329_000
+			.saturating_add((29_714_000 as Weight).saturating_mul(a as Weight))
+			.saturating_add(RocksDbWeight::get().reads(5 as Weight))
+			.saturating_add(RocksDbWeight::get().reads((2 as Weight).saturating_mul(c as Weight)))
+			.saturating_add(RocksDbWeight::get().reads((2 as Weight).saturating_mul(s as Weight)))
+			.saturating_add(RocksDbWeight::get().reads((1 as Weight).saturating_mul(a as Weight)))
+			.saturating_add(RocksDbWeight::get().writes(2 as Weight))
+			.saturating_add(RocksDbWeight::get().writes((2 as Weight).saturating_mul(c as Weight)))
+			.saturating_add(RocksDbWeight::get().writes((2 as Weight).saturating_mul(s as Weight)))
+			.saturating_add(RocksDbWeight::get().writes((1 as Weight).saturating_mul(a as Weight)))
+	}
+	// Storage: Assets Asset (r:1 w:1)
+	// Storage: Assets Account (r:1 w:1)
+	fn mint() -> Weight {
+		(47_913_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(2 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(2 as Weight))
+	}
+	// Storage: Assets Asset (r:1 w:1)
+	// Storage: Assets Account (r:1 w:1)
+	fn burn() -> Weight {
+		(55_759_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(2 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(2 as Weight))
+	}
+	// Storage: Assets Asset (r:1 w:1)
+	// Storage: Assets Account (r:2 w:2)
+	// Storage: System Account (r:1 w:1)
+	fn transfer() -> Weight {
+		(83_205_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(4 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(4 as Weight))
+	}
+	// Storage: Assets Asset (r:1 w:1)
+	// Storage: Assets Account (r:2 w:2)
+	// Storage: System Account (r:1 w:1)
+	fn transfer_keep_alive() -> Weight {
+		(70_665_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(4 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(4 as Weight))
+	}
+	// Storage: Assets Asset (r:1 w:1)
+	// Storage: Assets Account (r:2 w:2)
+	// Storage: System Account (r:1 w:1)
+	fn force_transfer() -> Weight {
+		(81_458_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(4 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(4 as Weight))
+	}
+	// Storage: Assets Asset (r:1 w:0)
+	// Storage: Assets Account (r:1 w:1)
+	fn freeze() -> Weight {
+		(32_845_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(2 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(1 as Weight))
+	}
+	// Storage: Assets Asset (r:1 w:0)
+	// Storage: Assets Account (r:1 w:1)
+	fn thaw() -> Weight {
+		(33_303_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(2 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(1 as Weight))
+	}
+	// Storage: Assets Asset (r:1 w:1)
+	fn freeze_asset() -> Weight {
+		(23_434_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(1 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(1 as Weight))
+	}
+	// Storage: Assets Asset (r:1 w:1)
+	fn thaw_asset() -> Weight {
+		(24_173_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(1 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(1 as Weight))
+	}
+	// Storage: Assets Asset (r:1 w:1)
+	// Storage: Assets Metadata (r:1 w:0)
+	fn transfer_ownership() -> Weight {
+		(27_466_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(2 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(1 as Weight))
+	}
+	// Storage: Assets Asset (r:1 w:1)
+	fn set_team() -> Weight {
+		(24_608_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(1 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(1 as Weight))
+	}
+	// Storage: Assets Asset (r:1 w:0)
+	// Storage: Assets Metadata (r:1 w:1)
+	fn set_metadata(n: u32, s: u32, ) -> Weight {
+		(49_515_000 as Weight)
+			// Standard Error: 1_000
+			.saturating_add((1_000 as Weight).saturating_mul(n as Weight))
+			// Standard Error: 1_000
+			.saturating_add((6_000 as Weight).saturating_mul(s as Weight))
+			.saturating_add(RocksDbWeight::get().reads(2 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(1 as Weight))
+	}
+	// Storage: Assets Asset (r:1 w:0)
+	// Storage: Assets Metadata (r:1 w:1)
+	fn clear_metadata() -> Weight {
+		(48_163_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(2 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(1 as Weight))
+	}
+	// Storage: Assets Asset (r:1 w:0)
+	// Storage: Assets Metadata (r:1 w:1)
+	fn force_set_metadata(_n: u32, s: u32, ) -> Weight {
+		(26_722_000 as Weight)
+			// Standard Error: 0
+			.saturating_add((5_000 as Weight).saturating_mul(s as Weight))
+			.saturating_add(RocksDbWeight::get().reads(2 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(1 as Weight))
+	}
+	// Storage: Assets Asset (r:1 w:0)
+	// Storage: Assets Metadata (r:1 w:1)
+	fn force_clear_metadata() -> Weight {
+		(47_923_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(2 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(1 as Weight))
+	}
+	// Storage: Assets Asset (r:1 w:1)
+	fn force_asset_status() -> Weight {
+		(23_081_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(1 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(1 as Weight))
+	}
+	// Storage: Assets Asset (r:1 w:1)
+	// Storage: Assets Approvals (r:1 w:1)
+	fn approve_transfer() -> Weight {
+		(56_998_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(2 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(2 as Weight))
+	}
+	// Storage: Assets Approvals (r:1 w:1)
+	// Storage: Assets Asset (r:1 w:1)
+	// Storage: Assets Account (r:2 w:2)
+	// Storage: System Account (r:1 w:1)
+	fn transfer_approved() -> Weight {
+		(107_171_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(5 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(5 as Weight))
+	}
+	// Storage: Assets Asset (r:1 w:1)
+	// Storage: Assets Approvals (r:1 w:1)
+	fn cancel_approval() -> Weight {
+		(57_358_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(2 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(2 as Weight))
+	}
+	// Storage: Assets Asset (r:1 w:1)
+	// Storage: Assets Approvals (r:1 w:1)
+	fn force_cancel_approval() -> Weight {
+		(58_330_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(2 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(2 as Weight))
+	}
+}
