dcee34d0a0d96db34d15be16df5091029236ff85
Oleksii Filonenko <oleksii@composable.finance>
2021-09-30 13:43:46 +0300

    Fix typo in democracy trait

--- frame/composable-traits/src/democracy.rs
+++ frame/composable-traits/src/democracy.rs
@@ -1,10 +1,9 @@
 pub trait Democracy {
 	type AccountId;
 	type Balance;
-	type Account;
 	type ReferendumIndex;
 	type Vote;
 
-	fn vote(account: Self::Account, ref_index: Self::ReferendumIndex, vote: Self::Vote);
+	fn vote(account: Self::AccountId, ref_index: Self::ReferendumIndex, vote: Self::Vote);
 	fn exists_and_is_ongoing(index: Self::ReferendumIndex) -> bool;
 }
