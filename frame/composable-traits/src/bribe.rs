5e9ab000130dd613dec39b318ca7153066e26955
Oleksii Filonenko <oleksii@composable.finance>
2021-09-30 13:08:15 +0300

    Initial bribe trait definition

--- frame/composable-traits/src/bribe.rs
+++ frame/composable-traits/src/bribe.rs
@@ -1,10 +1,49 @@
-/// BribeDAO interface
-pub trait Bribe {
-	fn create_request(request: BribeRequest) {}
+use frame_support::pallet_prelude::*;
 
-	fn take_bribe(request: TakeBribeRequest) {}
+pub trait Bribe {
+	type Balance;
+	type BribeIndex;
+	type Conviction;
+	type CurrencyId;
+	type ReferendumIndex;
+
+	fn create_bribe(
+		request: CreateBribeRequest<
+			Self::ReferendumIndex,
+			Self::Conviction,
+			Self::Balance,
+			Self::CurrencyId,
+		>,
+	) -> Result<Self::BribeIndex, DispatchError>;
+
+	fn take_bribe(
+		request: TakeBribeRequest<Self::BribeIndex, Self::Balance, Self::Conviction>,
+	) -> Result<bool, DispatchError>;
 }
 
-pub struct BribeRequest {}
+/// A request to create a bribe for an (ongoing) referendum.
+#[derive(Copy, Clone, Encode, Decode, Debug, PartialEq)]
+pub struct CreateBribeRequest<ReferendumIndex, Conviction, Balance, CurrencyId> {
+	/// Index of the referendum.
+	pub ref_index: ReferendumIndex,
+	/// Requested conviction (e.g. number of blocks to lock tokens for).
+	pub requested_conviction: Conviction,
+	/// How much to pay bribe-takers in total.
+	pub total_reward: Balance,
+	/// What asset to pay the bribes with.
+	pub asset_id: CurrencyId,
+	/// Whether the bribe is in favor of the proposal or against it.
+	pub is_aye: bool,
+}
 
-pub struct TakeBribeRequest {}
+/// A request to take a bribe and vote for the corresponding referendum.
+#[derive(Copy, Clone, Encode, Decode, Debug, PartialEq)]
+pub struct TakeBribeRequest<BribeIndex, Balance, Conviction> {
+	/// Index of the bribe.
+	pub bribe_index: BribeIndex,
+	/// How much tokens to lock up for the vote.
+	pub tokens: Balance,
+	/// How long to lock up the tokens for.
+	pub conviction: Conviction,
+	// REVIEW: we don't need Vote/aye fields as that will be handled by the pallet
+}
