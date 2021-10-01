35b8fc63b93346e74b1d4df1e03a324a44b3ef32
Oleksii Filonenko <brightone@protonmail.com>
2021-10-01 15:29:42 +0300

    pallet_bribedao: types, signatures and stubs (#131)
    * Add config stubs and BribeRequests storage
    
    * Add stubbed extrinsics and WIP events
    
    * Use Votes instead of Balance+Conviction
    
    Votes is implemented as pallet_democracy::Delegations.
    https://docs.rs/pallet-democracy/3.0.0/pallet_democracy/struct.Delegations.html
    
    * Remove deadcode and clarify TODOs
    
    * Add requests to emitted events

--- frame/bribedao/src/lib.rs
+++ frame/bribedao/src/lib.rs
@@ -1,116 +1,140 @@
 //Bribe DAO
 
 pub use pallet::*;
+use sp_runtime::DispatchError;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use super::*;
+	use codec::Codec;
+	use composable_traits::bribe::Bribe;
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
+	use num_traits::{CheckedAdd, CheckedMul, CheckedSub, SaturatingSub};
+	use sp_runtime::traits::{AtLeast32BitUnsigned, Zero};
+
+	pub type BribeIndex = u32;
+	pub type ReferendumIndex = pallet_democracy::ReferendumIndex;
+	pub type CreateBribeRequest<T> = composable_traits::bribe::CreateBribeRequest<
+		ReferendumIndex,
+		<T as Config>::Balance,
+		<T as Config>::CurrencyId,
+		<T as Config>::Votes,
+	>;
+	pub type TakeBribeRequest<T> =
+		composable_traits::bribe::TakeBribeRequest<BribeIndex, <T as Config>::Votes>;
+
 	#[pallet::config]
-	pub trait Config: pallet_balances::Config + frame_system::Config {
+	pub trait Config: frame_system::Config {
+		// REVIEW(oleksii): Balance traits; following are copied from pallet-vault
+		type Balance: Default
+			+ Parameter
+			+ Codec
+			+ Copy
+			+ Ord
+			+ CheckedAdd
+			+ CheckedSub
+			+ CheckedMul
+			+ SaturatingSub
+			+ AtLeast32BitUnsigned
+			+ Zero;
+
+		// TODO(oleksii): CurrencyId traits
+		type CurrencyId: Parameter;
+		// TODO(oleksii): Votes traits
+		type Votes: Parameter;
+
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-		/// Type representing the weight of this pallet
-		type WeightInfo: WeightInfo;
+		// TODO(oleksii): WeightInfo type
+		// type WeightInfo: WeightInfo;
 	}
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
-	#[pallet::hooks]
-	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
-		fn lorem(_n: T::BlockNumber) -> Weight {
-			0
-		}
-
-		fn on_finalize(_n: T::BlockNumber) {}
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		BribeCreated { id: BribeIndex, request: CreateBribeRequest<T> },
+		BribeTaken { id: BribeIndex, request: TakeBribeRequest<T> },
 	}
+
+	/// The number of bribes, also used to generate the next bribe identifier.
+	///
+	/// # Note
+	///
+	/// Cleaned up bribes do not decrement the counter.
+	#[pallet::storage]
+	#[pallet::getter(fn bribe_count)]
+	pub(super) type BribeCount<T: Config> = StorageValue<_, BribeIndex, ValueQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn bribe_requests)]
+	pub(super) type BribeRequests<T: Config> =
+		StorageMap<_, Blake2_128Concat, BribeIndex, CreateBribeRequest<T>>;
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		#[pallet::weight(
-			<T as pallet::Config>::WeightInfo::accumulate_dummy((*increase_by).saturated_into())
-		)]
-		pub fn accumulate_dummy(origin: OriginFor<T>, increase_by: T::Balance) -> DispatchResult {
-			let _sender = ensure_signed(origin)?;
-			<Dummy<T>>::mutate(|dummy| {
-				let new_dummy = dummy.map_or(increase_by, |d| d.saturating_add(increase_by));
-				*dummy = Some(new_dummy);
-			});
-
-			Self::deposit_event(Event::AccumulateDummy(increase_by));
-			Ok(())
+		#[pallet::weight(10_000)]
+		pub fn create_bribe(
+			origin: OriginFor<T>,
+			request: CreateBribeRequest<T>,
+		) -> DispatchResultWithPostInfo {
+			let _from = ensure_signed(origin)?;
+			let id = <Self as Bribe>::create_bribe(request.clone())?;
+			Self::deposit_event(Event::BribeCreated { id, request });
+			Ok(().into())
 		}
-	}
 
-	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	pub enum Event<T: Config> {
-		AccumulateDummy(BalanceOf<T>),
-		SetDummy(BalanceOf<T>),
-	}
-
-	#[pallet::storage]
-	#[pallet::getter(fn dummy)]
-	pub(super) type Dummy<T: Config> = StorageValue<_, T::Balance>;
-
-	#[pallet::storage]
-	#[pallet::getter(fn bar)]
-	pub(super) type Bar<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, T::Balance>;
-
-	#[pallet::storage]
-	#[pallet::getter(fn foo)]
-	pub(super) type Foo<T: Config> = StorageValue<_, T::Balance, ValueQuery>;
-
-	#[pallet::storage]
-	pub type CountedMap<T> = CountedStorageMap<_, Blake2_128Concat, u8, u16>;
-
-	// The genesis config type.
-	#[pallet::genesis_config]
-	pub struct GenesisConfig<T: Config> {
-		pub dummy: T::Balance,
-		pub bar: Vec<(T::AccountId, T::Balance)>,
-		pub foo: T::Balance,
-	}
-
-	#[cfg(feature = "std")]
-	impl<T: Config> Default for GenesisConfig<T> {
-		fn default() -> Self {
-			Self { dummy: Default::default(), bar: Default::default(), foo: Default::default() }
-		}
-	}
-
-	#[pallet::genesis_build]
-	impl<T: Config> GenesisBuild<T> for GenesisConfig<T> {
-		fn build(&self) {
-			<Dummy<T>>::put(&self.dummy);
-			for (a, b) in &self.bar {
-				<Bar<T>>::insert(a, b);
+		#[pallet::weight(10_000)]
+		pub fn take_bribe(
+			origin: OriginFor<T>,
+			request: TakeBribeRequest<T>,
+		) -> DispatchResultWithPostInfo {
+			let _from = ensure_signed(origin)?;
+			let bribe_index = request.bribe_index;
+			let bribe_taken = <Self as Bribe>::take_bribe(request.clone())?;
+			if bribe_taken {
+				Self::deposit_event(Event::BribeTaken { id: bribe_index, request });
 			}
-			<Foo<T>>::put(&self.foo);
+			Ok(().into())
 		}
 	}
-}
 
-impl<T: Config> Pallet<T> {
-	#[allow(dead_code)]
-	fn accumulate_foo(origin: T::Origin, increase_by: T::Balance) -> DispatchResult {
-		let _sender = ensure_signed(origin)?;
+	// TODO(oleksii): Errors (#[pallet::error])
 
-		let prev = <Foo<T>>::get();
-		let result = <Foo<T>>::mutate(|foo| {
-			*foo = foo.saturating_add(increase_by);
-			*foo
-		});
-		assert!(prev + increase_by == result);
-		Ok(())
+	impl<T: Config> Bribe for Pallet<T> {
+		type BribeIndex = BribeIndex;
+		type ReferendumIndex = ReferendumIndex;
+
+		type Balance = T::Balance;
+		type CurrencyId = T::CurrencyId;
+		type Votes = T::Votes;
+
+		fn create_bribe(request: CreateBribeRequest<T>) -> Result<Self::BribeIndex, DispatchError> {
+			Self::do_create_bribe(request)
+		}
+
+		fn take_bribe(request: TakeBribeRequest<T>) -> Result<bool, DispatchError> {
+			Self::do_take_bribe(request)
+		}
 	}
-}
 
-pub struct WatchDummy<T: Config + Send + Sync>(PhantomData<T>);
+	impl<T: Config> Pallet<T> {
+		fn do_create_bribe(request: CreateBribeRequest<T>) -> Result<BribeIndex, DispatchError> {
+			let id = BribeCount::<T>::mutate(|id| {
+				*id += 1;
+				*id
+			});
+			BribeRequests::<T>::insert(id, request);
 
-impl<T: Config + Send + Sync> sp_std::fmt::Debug for WatchDummy<T> {
-	fn fmt(&self, f: &mut sp_std::fmt::Formatter) -> sp_std::fmt::Result {
-		write!(f, "WatchDummy")
+			todo!("check that referendum exists");
+
+			Ok(id)
+		}
+
+		fn do_take_bribe(request: TakeBribeRequest<T>) -> Result<bool, DispatchError> {
+			todo!("enact vote through pallet_democracy");
+		}
 	}
 }
--- frame/composable-traits/src/bribe.rs
+++ frame/composable-traits/src/bribe.rs
@@ -1,49 +1,47 @@
 use frame_support::pallet_prelude::*;
+use sp_runtime::RuntimeDebug;
 
 pub trait Bribe {
 	type Balance;
 	type BribeIndex;
-	type Conviction;
 	type CurrencyId;
 	type ReferendumIndex;
+	type Votes;
 
 	fn create_bribe(
 		request: CreateBribeRequest<
 			Self::ReferendumIndex,
-			Self::Conviction,
 			Self::Balance,
 			Self::CurrencyId,
+			Self::Votes,
 		>,
 	) -> Result<Self::BribeIndex, DispatchError>;
 
 	fn take_bribe(
-		request: TakeBribeRequest<Self::BribeIndex, Self::Balance, Self::Conviction>,
+		request: TakeBribeRequest<Self::BribeIndex, Self::Votes>,
 	) -> Result<bool, DispatchError>;
 }
 
 /// A request to create a bribe for an (ongoing) referendum.
-#[derive(Copy, Clone, Encode, Decode, Debug, PartialEq)]
-pub struct CreateBribeRequest<ReferendumIndex, Conviction, Balance, CurrencyId> {
+#[derive(Copy, Clone, Encode, Decode, PartialEq, RuntimeDebug)]
+pub struct CreateBribeRequest<ReferendumIndex, Balance, CurrencyId, Votes> {
 	/// Index of the referendum.
 	pub ref_index: ReferendumIndex,
-	/// Requested conviction (e.g. number of blocks to lock tokens for).
-	pub requested_conviction: Conviction,
 	/// How much to pay bribe-takers in total.
 	pub total_reward: Balance,
 	/// What asset to pay the bribes with.
 	pub asset_id: CurrencyId,
+	/// Requested votes (e.g. number of tokens and the lock period).
+	pub requested_votes: Votes,
 	/// Whether the bribe is in favor of the proposal or against it.
 	pub is_aye: bool,
 }
 
 /// A request to take a bribe and vote for the corresponding referendum.
-#[derive(Copy, Clone, Encode, Decode, Debug, PartialEq)]
-pub struct TakeBribeRequest<BribeIndex, Balance, Conviction> {
+#[derive(Copy, Clone, Encode, Decode, PartialEq, RuntimeDebug)]
+pub struct TakeBribeRequest<BribeIndex, Votes> {
 	/// Index of the bribe.
 	pub bribe_index: BribeIndex,
-	/// How much tokens to lock up for the vote.
-	pub tokens: Balance,
-	/// How long to lock up the tokens for.
-	pub conviction: Conviction,
-	// REVIEW: we don't need Vote/aye fields as that will be handled by the pallet
+	/// A product of token amount and lock period.
+	pub votes: Votes,
 }
