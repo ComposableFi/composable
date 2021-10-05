1820bcbbdad18b5a809933ef5cb2e6e5d8998736
kaiserkarel <k.l.kubat@gmail.com>
2021-10-05 12:45:08 +0200

    remove mod tests

--- frame/democracy/src/lib.rs
+++ frame/democracy/src/lib.rs
@@ -185,9 +185,6 @@ pub use vote::{AccountVote, Vote, Voting};
 pub use vote_threshold::{Approved, VoteThreshold};
 pub use weights::WeightInfo;
 
-#[cfg(test)]
-mod tests;
-
 #[cfg(feature = "runtime-benchmarks")]
 pub mod benchmarking;
 
