e93228bd5ee06646b271a1fa93c36775726badcf
Vijay D <vdhana33@gmail.com>
2021-10-12 13:09:27 -0400

    Merging develop into pallet-democracy with v0.9.10

--- frame/democracy/src/lib.rs
+++ frame/democracy/src/lib.rs
@@ -185,8 +185,8 @@ pub use vote::{AccountVote, Vote, Voting};
 pub use vote_threshold::{Approved, VoteThreshold};
 pub use weights::WeightInfo;
 
-#[cfg(test)]
-mod tests;
+// #[cfg(test)]
+// mod tests;
 
 #[cfg(feature = "runtime-benchmarks")]
 pub mod benchmarking;
