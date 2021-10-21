cd7f8c0aeba7dc1cac998191caa20e006a7e3ecb
dzmitry.lahoda <dzmitry@lahoda.pro>
2021-10-21 19:34:58 +0300

    privileged accounts (liquidation) (#164)
    * Refactoring of pallet-lending (#178)
    
    * Create SECURITY.md (#111) (#174)
    
    * Create SECURITY.md
    
    Set up for security  protocols.
    
    * More guidelines
    
    * rewards section
    
    Co-authored-by: Filip <flipchan@riseup.net>
    
    Update
    
    setup-auto-release
    
    Updating github workflow
    
    Implement auto update feature using two approaches
    
    Implement auto update feature using two approaches
    
    Implement auto update feature using two approaches
    
    Co-authored-by: Douglas Kuhn <douglas@composable.finance>
    
    * better naming, comments & documentation
    
    * introduce account privilege traits, allowing inspection + mutation
    
    * introduce pallet-privilege which implements all the privilege traits
    
    * add privilege pallet event triggers
    
    * add privilege pallet readme
    
    * refactor promote/revoke behavior to be noop over privileged/nonprivileged user
    
    Co-authored-by: andor0 <aaorlov1@gmail.com>
    Co-authored-by: Adedayo Akinpelu <adedayoakinpelu@gmail.com>
    Co-authored-by: Douglas Kuhn <douglas@composable.finance>
    Signed-off-by: dzmitry-lahoda <dzmitry@lahoda.pro>

---
+++ frame/composable-traits/src/privilege.rs
@@ -0,0 +1,81 @@
+use bitflags::bitflags;
+use frame_support::pallet_prelude::*;
+use sp_std::vec::Vec;
+
+bitflags! {
+	/// Privilege of an account.
+	#[derive(Encode, Decode)]
+	pub struct Privilege: u32 {
+		/// An account we prioritize in liquidation processes.
+		/// In a dutch auction for instance, the account will be able to buy at a previous block price,
+		/// getting priority over the rest of the users.
+		const PRIVILEGED_LIQUIDATOR = 0b0000_0000_0000_0000_0000_0000_0000_0001;
+	}
+}
+
+impl Default for Privilege {
+	fn default() -> Self {
+		Self::empty()
+	}
+}
+
+/// An object from which we can determine the privileges of an account
+/// and extract the set of accounts matching a privilege.
+pub trait InspectPrivilege {
+	type AccountId;
+
+	/// Determine whether the provided account has the given privilege.
+	fn has_privilege(account_id: &Self::AccountId, privilege: Privilege) -> bool;
+}
+
+/// An object from which we can alter the privileges of an account.
+pub trait MutatePrivilege: InspectPrivilege {
+	/// Enable the given privilege.
+	fn promote(account_id: &Self::AccountId, privilege: Privilege) -> DispatchResult;
+
+	/// Revoke the given privilege.
+	/// The implementation should ensure that the user is revoked
+	/// from any group that enforce this privilege.
+	fn revoke(account_id: &Self::AccountId, privilege: Privilege) -> DispatchResult;
+}
+
+#[repr(transparent)]
+#[derive(Default, Encode, Decode)]
+/// A privileged group.
+pub struct PrivilegedGroupSet<T>(pub Vec<T>);
+
+pub type PrivilegedGroupOf<T> = PrivilegedGroupSet<<T as InspectPrivilegeGroup>::AccountId>;
+
+/// An privilege group, a set of privileged accounts.
+pub trait InspectPrivilegeGroup {
+	type AccountId: Copy;
+	type GroupId;
+
+	/// Retrieve the privilege that is held for all the members.
+	/// Implementation should ensute that this privilege is held for all the members.
+	fn privilege(group_id: Self::GroupId) -> Result<Privilege, DispatchError>;
+
+	/// Retrive a group of privileged accounts.
+	fn members(group_id: Self::GroupId) -> Result<PrivilegedGroupOf<Self>, DispatchError>;
+}
+
+/// An privilege group, a set of privileged accounts.
+pub trait MutatePrivilegeGroup: InspectPrivilegeGroup {
+	/// Create a group of privileged accounts.
+	/// The implementation should ensure that this group is consistent,
+	/// meaning that every user within the group has the given privilege.
+	fn create(
+		group: PrivilegedGroupOf<Self>,
+		privilege: Privilege,
+	) -> Result<Self::GroupId, DispatchError>;
+
+	/// Delete a group of privileged accounts.
+	fn delete(group_id: Self::GroupId) -> DispatchResult;
+
+	/// Promote the given account into this group.
+	/// The implementation should ensure that the account has the required privilege.
+	fn promote(group_id: Self::GroupId, account_id: &Self::AccountId) -> DispatchResult;
+
+	/// Revoke the given account from this group.
+	fn revoke(group_id: Self::GroupId, account_id: &Self::AccountId) -> DispatchResult;
+}
