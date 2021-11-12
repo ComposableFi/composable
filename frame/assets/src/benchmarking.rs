4e95a8639378a86d50e21ed62c8df67796dad461
Oleksii Filonenko <oleksii@composable.finance>
2021-11-12 17:14:55 +0200

    Add where_clause to benchmarks

--- frame/assets/src/benchmarking.rs
+++ frame/assets/src/benchmarking.rs
@@ -2,20 +2,39 @@ use super::*;
 
 use crate::Pallet as Assets;
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
+use frame_support::traits::{
+	fungible::{Inspect as NativeInspect, Mutate as NativeMutate, Transfer as NativeTransfer},
+	fungibles::{Inspect, Mutate, Transfer},
+};
 use frame_system::RawOrigin;
 use sp_std::prelude::*;
-
+use sp_runtime::traits::StaticLookup;
+use frame_system::Config as SystemConfig;
 const FROM_ACCOUNT: u64 = 1;
 const TO_ACCOUNT: u64 = 2;
-const ASSET_ID: u64 = 1;
+const ASSET_ID: u128 = 1;
 const INIT_AMOUNT: u64 = 1000;
-const TRANSFER_AMOUNT: u64 = 500;
+const TRANSFER_AMOUNT: u32 = 500;
 
 benchmarks! {
+	where_clause {
+		 where <T as Config>::Currency: NativeTransfer<T::AccountId, Balance = T::Balance>
+				   + NativeInspect<T::AccountId, Balance = T::Balance>
+				   + NativeMutate<T::AccountId, Balance = T::Balance>,
+			   <T as Config>::MultiCurrency: Inspect<T::AccountId, Balance = T::Balance, AssetId = T::AssetId>
+				   + Transfer<T::AccountId, Balance = T::Balance, AssetId = T::AssetId>
+				   + Mutate<T::AccountId, Balance = T::Balance, AssetId = T::AssetId>,
+				<T as Config>::AssetId: From<u64>,   
+				<T as SystemConfig>::AccountId: From<u64>,   
+
+	}
+
 	transfer {
 		let caller: T::AccountId = whitelisted_caller();
+		let dest = 2_u64.into();
+		let dest = T::Lookup::unlookup(dest);
 		// T::Currency::mint_into(T::AssetId::from(A), &caller, amount)
-	}: _(RawOrigin::Signed(caller), ASSET_ID, TO_ACCOUNT, TRANSFER_AMOUNT, true)
+	}: _(RawOrigin::Signed(caller), 1_u64.into(), dest, 1_u32.into(), true)
 
 	// transfer_native {
 	// 	let caller: T::AccountId = whitelisted_caller();
@@ -52,4 +71,4 @@ benchmarks! {
 
 }
 
-impl_benchmark_test_suite!(Assets, crate::mock::new_test_ext(), crate::mock::Test,);
+impl_benchmark_test_suite!(Assets, crate::mocks::new_test_ext(), crate::mocks::Test,);
