144db66f9f4a67b31e1ae8b291570546e4b3fccc
Seun Lanlege <seun@composable.finance>
2021-12-07 13:55:23 +0100

    bump spec, fix runtime (#346)

--- runtime/picasso/src/lib.rs
+++ runtime/picasso/src/lib.rs
@@ -101,7 +101,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
 	// This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
 	//   the compatible custom types.
-	spec_version: 200,
+	spec_version: 300,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
@@ -952,9 +952,7 @@ pub struct BaseCallFilter;
 
 impl Contains<Call> for BaseCallFilter {
 	fn contains(call: &Call) -> bool {
-		if call_filter::Pallet::<Runtime>::contains(call) {
-			return false
-		}
+		!call_filter::Pallet::<Runtime>::contains(call)
 	}
 }
 
