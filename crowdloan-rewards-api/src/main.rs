036d460d5ef1b10cee9f06c7646f9a38a40955bd
Ben Luelo <benluelo@hotmail.com>
2021-12-17 11:23:38 -0500

    Add API filter

--- crowdloan-rewards-api/src/main.rs
+++ crowdloan-rewards-api/src/main.rs
@@ -1,3 +1,5 @@
+use std::{sync::Arc, convert::Infallible};
+
 use pallet_crowdloan_rewards::models::Proof;
 use serde::{Deserialize, Serialize};
 use sp_runtime::{AccountId32, MultiSignature};
@@ -5,14 +7,29 @@ use warp::{hyper::StatusCode, Filter};
 
 #[tokio::main]
 async fn main() {
+	let api = Arc::new(
+		subxt::ClientBuilder::new()
+			.build()
+			.await
+			.unwrap()
+			.to_runtime_api::<PicassoApi>(),
+	);
+
 	let associate_filter = warp::path("associate")
 		.and(warp::post())
 		.and(warp::body::json::<AssociateOrigin>())
+		.and(with_subxt_api(api))
 		.and_then(associate);
-	// .map(|ao: AssociateOrigin| async move { associate(ao).await })
-	// .map(associate);
 
 	warp::serve(associate_filter).run(([127, 0, 0, 1], 3030)).await;
+}
+
+type PicassoApi = picasso::api::RuntimeApi<picasso::api::DefaultConfig>;
+
+fn with_subxt_api(
+	api: Arc<PicassoApi>,
+) -> impl Filter<Extract = (Arc<PicassoApi>,), Error = Infallible> + Clone {
+	warp::any().map(move || api.clone())
 }
 
 mod verify;
@@ -25,13 +42,9 @@ struct AssociateOrigin {
 // reward_account: RewardA
 }
 
-async fn associate(associate_origin: AssociateOrigin) -> Result<StatusCode, warp::Rejection> {
-	let api = match subxt::ClientBuilder::new().build().await.map_err(|why| why.to_string()) {
-		Ok(ok) => ok.to_runtime_api::<picasso::api::RuntimeApi<picasso::api::DefaultConfig>>(),
-		Err(why) => return Ok(StatusCode::INTERNAL_SERVER_ERROR),
-	};
-
-	
-
+async fn associate(
+	associate_origin: AssociateOrigin,
+	api: Arc<picasso::api::RuntimeApi<picasso::api::DefaultConfig>>,
+) -> Result<StatusCode, warp::Rejection> {
 	Ok(StatusCode::OK)
 }
