45c8f064114427861c304eb1a15851192c065f6e
Ben Luelo <benluelo@hotmail.com>
2021-12-17 11:36:59 -0500

    Reorganize and add comments

--- crowdloan-rewards-api/src/main.rs
+++ crowdloan-rewards-api/src/main.rs
@@ -1,9 +1,14 @@
-use std::{sync::Arc, convert::Infallible};
+use std::{convert::Infallible, sync::Arc};
 
 use pallet_crowdloan_rewards::models::Proof;
 use serde::{Deserialize, Serialize};
 use sp_runtime::{AccountId32, MultiSignature};
+use subxt_clients::picasso;
 use warp::{hyper::StatusCode, Filter};
+
+mod verify;
+
+type PicassoApi = picasso::api::RuntimeApi<picasso::api::DefaultConfig>;
 
 #[tokio::main]
 async fn main() {
@@ -24,27 +29,23 @@ async fn main() {
 	warp::serve(associate_filter).run(([127, 0, 0, 1], 3030)).await;
 }
 
-type PicassoApi = picasso::api::RuntimeApi<picasso::api::DefaultConfig>;
-
 fn with_subxt_api(
 	api: Arc<PicassoApi>,
 ) -> impl Filter<Extract = (Arc<PicassoApi>,), Error = Infallible> + Clone {
 	warp::any().map(move || api.clone())
 }
 
-mod verify;
-
-use subxt_clients::picasso;
-
 #[derive(Serialize, Deserialize)]
 struct AssociateOrigin {
-	// proof: Proof<[u8; 32]>,
-// reward_account: RewardA
+	// I'm not sure what this should be; should I use Encode/ Decode instead of serde? Or use Encode/ Decode in a custom serde implementation?
+// proof: Proof<[u8; 32]>,
+// reward_account: RewardAccount
 }
 
 async fn associate(
 	associate_origin: AssociateOrigin,
 	api: Arc<picasso::api::RuntimeApi<picasso::api::DefaultConfig>>,
 ) -> Result<StatusCode, warp::Rejection> {
+	api.tx(); // .what (?)
 	Ok(StatusCode::OK)
 }
