935e2fc2799f0745ce6cc13a9ac0f56f10fffbf0
dzmitry-lahoda <dzmitry@lahoda.pro>
2021-12-27 13:00:35 +0200

    fix fmt
    Signed-off-by: dzmitry-lahoda <dzmitry@lahoda.pro>

--- runtime/dali/src/lib.rs
+++ runtime/dali/src/lib.rs
@@ -19,9 +19,7 @@ use sp_api::impl_runtime_apis;
 use sp_core::{crypto::KeyTypeId, OpaqueMetadata};
 use sp_runtime::{
 	create_runtime_str, generic, impl_opaque_keys,
-	traits::{
-		AccountIdConversion, AccountIdLookup, BlakeTwo256, Block as BlockT, Zero,
-	},
+	traits::{AccountIdConversion, AccountIdLookup, BlakeTwo256, Block as BlockT, Zero},
 	transaction_validity::{TransactionSource, TransactionValidity},
 	ApplyExtrinsicResult,
 };
