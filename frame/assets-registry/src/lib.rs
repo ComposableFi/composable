fd5ea0f8ed25d041ae5cdb63ee68fbf0d204e92c
Ben Luelo <benluelo@hotmail.com>
2021-12-29 11:29:23 -0500

    rustfmt

--- frame/assets-registry/src/lib.rs
+++ frame/assets-registry/src/lib.rs
@@ -249,7 +249,7 @@ pub mod pallet {
 			let local_admin = <LocalAdmin<T>>::get();
 			let foreign_admin = <ForeignAdmin<T>>::get();
 			match current_candidate_status {
-				None => {
+				None =>
 					if who == local_admin {
 						<AssetsMappingCandidates<T>>::insert(
 							(local_asset_id, foreign_asset_id),
@@ -260,20 +260,17 @@ pub mod pallet {
 							(local_asset_id, foreign_asset_id),
 							CandidateStatus::ForeignAdminApproved,
 						);
-					}
-				}
-				Some(CandidateStatus::LocalAdminApproved) => {
+					},
+				Some(CandidateStatus::LocalAdminApproved) =>
 					if who == foreign_admin {
 						Self::set_location(local_asset_id, foreign_asset_id.clone())?;
 						<AssetsMappingCandidates<T>>::remove((local_asset_id, foreign_asset_id));
-					}
-				}
-				Some(CandidateStatus::ForeignAdminApproved) => {
+					},
+				Some(CandidateStatus::ForeignAdminApproved) =>
 					if who == local_admin {
 						Self::set_location(local_asset_id, foreign_asset_id.clone())?;
 						<AssetsMappingCandidates<T>>::remove((local_asset_id, foreign_asset_id));
-					}
-				}
+					},
 			};
 			Ok(().into())
 		}
@@ -284,9 +281,8 @@ pub mod pallet {
 		type Success = T::AccountId;
 		fn try_origin(o: T::Origin) -> Result<Self::Success, T::Origin> {
 			o.into().and_then(|o| match (o, LocalAdmin::<T>::try_get()) {
-				(frame_system::RawOrigin::Signed(ref who), Ok(ref f)) if who == f => {
-					Ok(who.clone())
-				}
+				(frame_system::RawOrigin::Signed(ref who), Ok(ref f)) if who == f =>
+					Ok(who.clone()),
 				(r, _) => Err(T::Origin::from(r)),
 			})
 		}
@@ -303,9 +299,8 @@ pub mod pallet {
 		type Success = T::AccountId;
 		fn try_origin(o: T::Origin) -> Result<Self::Success, T::Origin> {
 			o.into().and_then(|o| match (o, ForeignAdmin::<T>::try_get()) {
-				(frame_system::RawOrigin::Signed(ref who), Ok(ref f)) if who == f => {
-					Ok(who.clone())
-				}
+				(frame_system::RawOrigin::Signed(ref who), Ok(ref f)) if who == f =>
+					Ok(who.clone()),
 				(r, _) => Err(T::Origin::from(r)),
 			})
 		}
--- frame/curve-amm/src/lib.rs
+++ frame/curve-amm/src/lib.rs
@@ -899,7 +899,7 @@ pub mod pallet {
 			let n = FixedU128::saturating_from_integer(u128::try_from(xp_f.len()).ok()?);
 			let sum = xp_f.iter().try_fold(zero, |s, x| s.checked_add(x))?;
 			if sum == zero {
-				return Some(zero);
+				return Some(zero)
 			}
 			let mut d = sum;
 
@@ -922,10 +922,10 @@ pub mod pallet {
 
 				if d > d_prev {
 					if d.checked_sub(&d_prev)? <= prec {
-						return Some(d);
+						return Some(d)
 					}
 				} else if d_prev.checked_sub(&d)? <= prec {
-					return Some(d);
+					return Some(d)
 				}
 			}
 			None
@@ -961,14 +961,14 @@ pub mod pallet {
 
 			// Same coin
 			if i == j {
-				return None;
+				return None
 			}
 			// j above n
 			if j >= xp_f.len() {
-				return None;
+				return None
 			}
 			if i >= xp_f.len() {
-				return None;
+				return None
 			}
 			let d_f = Self::get_d(xp_f, ann_f)?;
 			let mut c = d_f;
@@ -984,7 +984,7 @@ pub mod pallet {
 				} else if k != j {
 					x_k = *xp_k;
 				} else {
-					continue;
+					continue
 				}
 				// s = s + x_k
 				s = s.checked_add(&x_k)?;
@@ -1014,10 +1014,10 @@ pub mod pallet {
 				// Equality with the specified precision
 				if y > y_prev {
 					if y.checked_sub(&y_prev)? <= prec {
-						return Some(y);
+						return Some(y)
 					}
 				} else if y_prev.checked_sub(&y)? <= prec {
-					return Some(y);
+					return Some(y)
 				}
 			}
 
@@ -1049,7 +1049,7 @@ pub mod pallet {
 			let n = FixedU128::try_from(xp_f.len() as u128).ok()?;
 
 			if i >= xp_f.len() {
-				return None;
+				return None
 			}
 
 			let mut c = d_f;
@@ -1057,7 +1057,7 @@ pub mod pallet {
 
 			for (k, xp_k) in xp_f.iter().enumerate() {
 				if k == i {
-					continue;
+					continue
 				}
 
 				let x = xp_k;
@@ -1083,10 +1083,10 @@ pub mod pallet {
 				// Equality with the specified precision
 				if y > y_prev {
 					if y.checked_sub(&y_prev)? <= prec {
-						return Some(y);
+						return Some(y)
 					}
 				} else if y_prev.checked_sub(&y)? <= prec {
-					return Some(y);
+					return Some(y)
 				}
 			}
 
