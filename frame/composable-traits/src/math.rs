b46e402444c263944e5a86e749b162a96d1ac6a5
Dzmitry Lahoda <dzmitry@lahoda.pro>
2021-12-30 17:49:44 +0200

    CU-1p8545r  dutch auction (#410)
    * dutch auction
    
    Signed-off-by: dzmitry-lahoda <dzmitry@lahoda.pro>
    
    * fixed tests
    
    Signed-off-by: dzmitry-lahoda <dzmitry@lahoda.pro>
    
    * fixed clippy
    
    Signed-off-by: dzmitry-lahoda <dzmitry@lahoda.pro>
    
    * fixed review comments
    
    Signed-off-by: dzmitry-lahoda <dzmitry@lahoda.pro>
    
    * adding tests and fixing bugs
    
    Signed-off-by: dzmitry-lahoda <dzmitry@lahoda.pro>
    
    * refactoring
    
    Signed-off-by: dzmitry-lahoda <dzmitry@lahoda.pro>
    
    * fix fmt
    
    Signed-off-by: dzmitry-lahoda <dzmitry@lahoda.pro>
    
    * fixed udeps
    
    Signed-off-by: dzmitry-lahoda <dzmitry@lahoda.pro>
    
    * fixed clippy
    
    Signed-off-by: dzmitry-lahoda <dzmitry@lahoda.pro>

--- frame/composable-traits/src/math.rs
+++ frame/composable-traits/src/math.rs
@@ -49,6 +49,8 @@ impl<T: CheckedAdd + CheckedMul + CheckedDiv + CheckedSub + Zero> SafeArithmetic
 /// This function cannot fail and might return the same object if a boundary is about to be crossed.
 // This kind of function is usually called an Endomorphism. But let's keep it simple.
 pub trait WrappingNext {
+	/// pallet must be coded that way that wrapping around does not do harm except of error
+	/// so additional check should be check on pallet level
 	fn next(&self) -> Self;
 }
 
@@ -57,6 +59,7 @@ where
 	T: Copy + One + Saturating,
 {
 	fn next(&self) -> Self {
+		// bug: this should WrappingAdd from num_traits.
 		self.saturating_add(T::one())
 	}
 }
