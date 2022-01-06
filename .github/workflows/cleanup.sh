118cfdeddc841967253e14ad7e7ea981561cd436
Ewere Diagboya <nimboya@gmail.com>
2022-01-06 23:07:24 +0100

    docker image cleanup

--- .github/workflows/cleanup.sh
+++ .github/workflows/cleanup.sh
@@ -2,3 +2,5 @@ rm -rf ./runtime/picasso/target/srtool/release/wbuild/picasso-runtime/picasso_ru
 rm -rf ./runtime/picasso/target/srtool/release/wbuild/picasso-runtime/picasso_runtime.compact.compressed.wasm
 rm -rf target/release/composable
 rm -rf target/release/parachain-utils
+docker image prune
+docker rmi $(docker images -a -q)
