7ec9e9a2e0dd1bfe5d0d3fc40a261230706b827f
Ewere Diagboya <nimboya@gmail.com>
2022-01-06 22:42:25 +0100

    add build cleanup

---
+++ .github/workflows/cleanup.sh
@@ -0,0 +1,4 @@
+rm -rf ./runtime/picasso/target/srtool/release/wbuild/picasso-runtime/picasso_runtime.compact.wasm
+rm -rf ./runtime/picasso/target/srtool/release/wbuild/picasso-runtime/picasso_runtime.compact.compressed.wasm
+rm -rf target/release/composable
+rm -rf target/release/parachain-utils
