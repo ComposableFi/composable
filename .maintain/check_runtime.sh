267ea38c7bfd02d54a679aae06379619845bcf93
haroldsphinx <adedayoakinpelu@gmail.com>
2022-01-12 17:41:40 +0100

    Update action

--- .maintain/check_runtime.sh
+++ .maintain/check_runtime.sh
@@ -17,7 +17,7 @@ declare -a VERSIONS_FILES=(
   "runtime/composable/src/lib.rs,composable,composable"
 )
 
-RELEASE_VERSION=$(git tag --sort=committerdate | grep -E '^v[0-9]' | tail -1 )
+RELEASE_VERSION=$(git tag --sort=committerdate | grep -E '^[0-9]' | tail -1 )
 
 boldprint () { printf "|\n| \033[1m%s\033[0m\n|\n" "${@}"; }
 boldcat () { printf "|\n"; while read -r l; do printf "| \033[1m%s\033[0m\n" "${l}"; done; printf "|\n" ; }
