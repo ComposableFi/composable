eccbe4022637a94dc1903a790ad25bd0f9c4dac7
haroldsphinx <adedayoakinpelu@gmail.com>
2022-01-16 16:13:59 +0100

    add runtime directory

--- .maintain/common/lib.sh
+++ .maintain/common/lib.sh
@@ -14,6 +14,12 @@ sanitised_git_logs(){
   sed 's/^/* /g'
 }
 
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+
 # Returns the last published release on github
 # Note: we can't just use /latest because that ignores prereleases
 # repo: 'organization/repo'
