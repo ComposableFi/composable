7f87587352d8e07387d218527bc38286398b6209
Seun Lanlege <seunlanlege@gmail.com>
2022-01-16 20:50:00 +0100

    move archive step into script

--- .maintain/client_release.sh
+++ .maintain/client_release.sh
@@ -21,4 +21,7 @@ boldcat () { printf "|\n"; while read -r l; do printf "| \033[1m%s\033[0m\n" "${
 if has_client_changes "${LATEST_TAG_NAME}" "${GITHUB_REF_NAME}"
 then
   cargo build --release -p composable
+  tar -czvf composable-${{ RELEASE_VERSION }}.tar.gz target/release/composable
+  tar -czvf picasso_runtime.compact.wasm-${RELEASE_VERSION}.tar.gz runtime/picasso/target/srtool/release/wbuild/picasso-runtime/picasso_runtime.compact.wasm
+  gsutil cp *.tar.gz gs://composable-binaries/community-releases/${{ RELEASE_VERSION }}/
 fi
