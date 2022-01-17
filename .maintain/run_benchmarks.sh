0d27b9be6619639fbcd1dd158529618b95bf944a
Seun Lanlege <seunlanlege@gmail.com>
2022-01-17 12:06:29 +0100

    Updates weights for picasso-dev

--- .maintain/run_benchmarks.sh
+++ .maintain/run_benchmarks.sh
@@ -8,9 +8,9 @@
 . "$(dirname "${0}")/./common/lib.sh"
 
 VERSIONS_FILES=(
-  "runtime/picasso/src/weights,picasso,picasso"
-  "runtime/dali/src/weights,dali-chachacha,dali"
-  "runtime/composable/src/weights,composable,composable"
+  "runtime/picasso/src/weights,picasso-dev,picasso"
+  "runtime/dali/src/weights,dali-dev,dali"
+  "runtime/composable/src/weights,composable-dev,composable"
 )
 
 steps=50
@@ -41,9 +41,9 @@ pallets=(
  git log -n1 "${LATEST_TAG_NAME}"
 
 
-/home/runner/.cargo/bin/rustup install nightly
-/home/runner/.cargo/bin/rustup target add wasm32-unknown-unknown --toolchain nightly
-/home/runner/.cargo/bin/cargo build --release -p composable --features=runtime-benchmarks
+rustup install nightly
+rustup target add wasm32-unknown-unknown --toolchain nightly
+cargo build --release -p composable --features=runtime-benchmarks
 
 run_benchmarks() {
   OUTPUT=$1
