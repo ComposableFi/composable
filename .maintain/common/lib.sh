0147e194503a9c9dc896ecd35a3c11c7c22fc615
Seun Lanlege <seunlanlege@gmail.com>
2022-01-19 11:58:45 +0100

    use commit sha

--- .maintain/common/lib.sh
+++ .maintain/common/lib.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 api_base="https://api.github.com/repos"
-GITHUB_REF_NAME=$(git branch --show-current)
+GITHUB_REF_NAME=$(git rev-parse HEAD)
 
 # Function to take 2 git tags/commits and get any lines from commit messages
 # that contain something that looks like a PR reference: e.g., (#1234)
