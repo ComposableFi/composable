76b4666acb1c47b54a049a3c63f2a249e8aef7c1
Seun Lanlege <seunlanlege@gmail.com>
2022-01-19 09:50:17 +0100

    reverts dali and picasso version for wasm override

--- runtime/dali/src/lib.rs
+++ runtime/dali/src/lib.rs
@@ -93,15 +93,15 @@ pub mod opaque {
 //   https://substrate.dev/docs/en/knowledgebase/runtime/upgrades#runtime-versioning
 #[sp_version::runtime_version]
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: create_runtime_str!("dali"),
-	impl_name: create_runtime_str!("dali"),
+	spec_name: create_runtime_str!("picasso"),
+	impl_name: create_runtime_str!("picasso"),
 	authoring_version: 1,
 	// The version of the runtime specification. A full node will not attempt to use its native
 	//   runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
 	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
 	// This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
 	//   the compatible custom types.
-	spec_version: 2000,
+	spec_version: 100,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
