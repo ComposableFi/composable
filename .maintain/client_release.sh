42e59129ce180b74ac18ac5489384b74ede5340b
Adedayo Akinpelu <adedayoakinpelu@gmail.com>
2022-01-19 09:27:47 +0100

    Adds Benchmarking & Simnode (#497)
    * check_runtime workflow
    
    * check_runtime workflow
    
    * Updates weights for picasso-dev
    
    * update benchmark script
    
    * update benchmark script
    
    * support -> frame_support
    
    * Updates weights for picasso-dev
    
    * Updates weights for dali-dev
    
    * fix conflict
    
    * Add cargo.lock
    
    * fix runtime.sh
    
    * replace boldprint with echo
    
    * adds set_changes_trie_config  to weights
    
    * adds set_changes_trie_config  to weights
    
    * Updates weights for picasso-dev
    
    * hotpatch substrate
    
    * fmt & clippy
    
    * Updates weights for picasso-dev
    
    * Update simnode script
    
    * Update github workflow
    
    * Update github workflow
    
    * Updates weights for dali-dev
    
    * Update github workflow
    
    * Update github workflow
    
    * Updates weights for composable-dev
    
    * Add condition to ignore check_runtime on draft PR
    
    * fmt
    
    * adds +nighly to checks
    
    * add git pull
    
    * Finalizing cleanup
    
    * Finalizing cleanup
    
    * fmt fixes
    
    * clippy fixes
    
    * use +nightly-2021-11-29 for fmt
    
    * Updates weights for picasso-dev
    
    Co-authored-by: Seun Lanlege <seunlanlege@gmail.com>
    Co-authored-by: haroldsphinx <haroldsphinx@gmail.com>

---
+++ .maintain/client_release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# check for any changes in the runtime/ and frame/*. if
+# there are any changes found, it should mark the PR breaksconsensus and
+# "auto-fail" the PR if there isn't a change in the runtime/src/lib.rs file
+# that alters the version.
+
+#set -e # fail on any error
+
+#shellcheck source=../common/lib.sh
+. "$(dirname "${0}")/./common/lib.sh"
+
+RELEASE_VERSION=$(git tag --sort=committerdate | grep -E '^v[0-9]' | tail -1 )
+
+
+if has_client_changes "${LATEST_TAG_NAME}" "${GITHUB_REF_NAME}"
+then
+  boldprint "Building new client binaries"
+  cargo build --release -p composable
+  tar -czvf composable-"${RELEASE_VERSION}".tar.gz target/release/composable
+  tar -czvf picasso_runtime.compact.wasm-"${RELEASE_VERSION}".tar.gz runtime/picasso/target/srtool/release/wbuild/picasso-runtime/picasso_runtime.compact.wasm
+  gsutil cp *.tar.gz gs://composable-binaries/community-releases/"${RELEASE_VERSION}"/
+fi
