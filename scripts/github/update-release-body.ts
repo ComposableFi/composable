a3264a2105d437114677df8e76cceebf4fc1477b
Seun Lanlege <seunlanlege@gmail.com>
2022-01-20 22:37:56 +0100

    use file args

--- scripts/github/update-release-body.ts
+++ scripts/github/update-release-body.ts
@@ -1,33 +1,44 @@
-import { Octokit } from "octokit";
+import {Octokit} from "octokit";
 import yargs from "yargs";
+import * as fs from "fs";
 
 async function main() {
-  const argv = yargs(process.argv.slice(2))
-    .usage("Usage: npm run update-release-body [args]")
-    .version("1.0.0")
-    .options({
-      id: {
-        type: "string",
-        describe: "id of the release to update",
-      },
-      body: {
-        type: "string",
-        describe: "newly updated body",
-      },
-    })
-    .demandOption(["id", "body", "repo"])
-    .help().argv;
+    const argv = yargs(process.argv.slice(2))
+        .usage("Usage: npm run update-release-body [args]")
+        .version("1.0.0")
+        .options({
+            id: {
+                type: "string",
+                describe: "id of the release to update",
+            },
+            changes: {
+                type: "string",
+                describe: "newly updated body",
+            },
+            repo: {
+                type: "string",
+                describe: "repo for the release",
+            },
+            metadata: {
+                type: "string",
+                describe: "runtime metadata in md format",
+            },
+        })
+        .demandOption(["id", "body", "repo", "metadata"])
+        .help().argv;
+    const metadata = fs.readFileSync(argv.metadata).toString();
 
-  const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN || undefined,
-  });
+    const body = `${metadata}\n\n${argv.body}`
+    const octokit = new Octokit({
+        auth: process.env.GITHUB_TOKEN || undefined,
+    });
 
-  await octokit.request("PATCH /repos/{owner}/{repo}/releases/{release_id}", {
-    owner: argv.repo.split("/")[0],
-    repo: argv.repo.split("/")[1],
-    release_id: argv.id,
-    body: argv.body,
-  });
+    await octokit.request("PATCH /repos/{owner}/{repo}/releases/{release_id}", {
+        owner: argv.repo.split("/")[0],
+        repo: argv.repo.split("/")[1],
+        release_id: argv.id,
+        body,
+    });
 }
 
 main();
