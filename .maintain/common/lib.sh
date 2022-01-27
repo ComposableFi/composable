6dd4fb07f289adb12036ef75a0da9d605e800986
haroldsphinx <adedayoakinpelu@gmail.com>
2022-01-27 10:59:22 +0100

    # This is a combination of 7 commits. # This is the 1st commit message:
    fetch depth
    
    # The commit message #2 will be skipped:
    
    # Update checkout action
    
    # The commit message #3 will be skipped:
    
    # Update checkout action
    
    # The commit message #4 will be skipped:
    
    # Update checkout action
    
    # The commit message #5 will be skipped:
    
    # Update checkout action
    
    # The commit message #6 will be skipped:
    
    # Update checkout action
    
    # The commit message #7 will be skipped:
    
    # Update checkout action

--- .maintain/common/lib.sh
+++ .maintain/common/lib.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+GITHUB_REF_NAME=$(git rev-parse --abbrev-ref HEAD | tr '/' '-')
+
 get_latest_release() {
   curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
     grep '"tag_name":' |                                            # Get tag line
