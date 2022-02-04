cdc634da3fb3b7f1111117e3672b35bfae4e03a5
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2022-02-04 16:26:38 +0100

    format

--- frame/assets-registry/src/lib.rs
+++ frame/assets-registry/src/lib.rs
@@ -291,7 +291,7 @@ pub mod pallet {
 			let local_admin = <LocalAdmin<T>>::get();
 			let foreign_admin = <ForeignAdmin<T>>::get();
 			match current_candidate_status {
-				None => {
+				None =>
 					if Some(who) == local_admin {
 						<AssetsMappingCandidates<T>>::insert(
 							(local_asset_id, foreign_asset_id),
@@ -302,20 +302,17 @@ pub mod pallet {
 							(local_asset_id, foreign_asset_id),
 							CandidateStatus::ForeignAdminApproved,
 						);
-					}
-				},
-				Some(CandidateStatus::LocalAdminApproved) => {
+					},
+				Some(CandidateStatus::LocalAdminApproved) =>
 					if Some(who) == foreign_admin {
 						Self::set_location(local_asset_id, foreign_asset_id.clone())?;
 						<AssetsMappingCandidates<T>>::remove((local_asset_id, foreign_asset_id));
-					}
-				},
-				Some(CandidateStatus::ForeignAdminApproved) => {
+					},
+				Some(CandidateStatus::ForeignAdminApproved) =>
 					if Some(who) == local_admin {
 						Self::set_location(local_asset_id, foreign_asset_id.clone())?;
 						<AssetsMappingCandidates<T>>::remove((local_asset_id, foreign_asset_id));
-					}
-				},
+					},
 			};
 			Ok(().into())
 		}
@@ -326,9 +323,8 @@ pub mod pallet {
 		type Success = T::AccountId;
 		fn try_origin(o: T::Origin) -> Result<Self::Success, T::Origin> {
 			o.into().and_then(|o| match (o, LocalAdmin::<T>::try_get()) {
-				(frame_system::RawOrigin::Signed(ref who), Ok(ref f)) if who == f => {
-					Ok(who.clone())
-				},
+				(frame_system::RawOrigin::Signed(ref who), Ok(ref f)) if who == f =>
+					Ok(who.clone()),
 				(r, _) => Err(T::Origin::from(r)),
 			})
 		}
@@ -345,9 +341,8 @@ pub mod pallet {
 		type Success = T::AccountId;
 		fn try_origin(o: T::Origin) -> Result<Self::Success, T::Origin> {
 			o.into().and_then(|o| match (o, ForeignAdmin::<T>::try_get()) {
-				(frame_system::RawOrigin::Signed(ref who), Ok(ref f)) if who == f => {
-					Ok(who.clone())
-				},
+				(frame_system::RawOrigin::Signed(ref who), Ok(ref f)) if who == f =>
+					Ok(who.clone()),
 				(r, _) => Err(T::Origin::from(r)),
 			})
 		}
