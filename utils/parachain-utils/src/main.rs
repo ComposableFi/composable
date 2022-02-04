f43f8207c211dd461864f460710742046f2e16dd
david <wizdave97@gmail.com>
2022-02-04 17:26:18 +0100

    parachain_utils: make upgrade_runtime generic

--- utils/parachain-utils/src/main.rs
+++ utils/parachain-utils/src/main.rs
@@ -1,12 +1,18 @@
 use codec::{Decode, Encode};
+use dali_runtime::{Runtime as DaliRuntime, SignedExtra as DaliSignedExtra};
 use jsonrpc_core_client::{futures::StreamExt, transports::ws, RpcChannel, RpcError};
+use parachain_system::Call as ParachainSystemCall;
+use picasso_runtime::{Runtime as PicaRuntime, SignedExtra as PicaSignedExtra};
 use sc_rpc::author::AuthorClient;
 use serde::Deserialize;
 use sp_core::{sr25519, Pair, H256};
 use sp_runtime::{traits::IdentifyAccount, MultiSigner};
 use std::str::FromStr;
 use structopt::StructOpt;
-use substrate_xt::Client;
+use substrate_xt::{Client, ConstructExt, ExtrinsicError, SubstrateXtError};
+mod events;
+use events::{match_event, AllRuntimeEvents};
+
 /// The command options
 #[derive(Debug, StructOpt, Clone)]
 pub enum Main {
@@ -18,6 +24,14 @@ pub enum Main {
 	},
 }
 
+/// The chain option
+#[derive(Debug, StructOpt, Clone)]
+pub struct Chain {
+	/// Chain id [`picasso` or `dali`]
+	#[structopt(long)]
+	chain_id: String,
+}
+
 #[derive(Deserialize, Debug)]
 struct Env {
 	/// Root key used to sign transactions
@@ -26,22 +40,78 @@ struct Env {
 	rpc_node: String,
 }
 
-struct State<T> {
+struct State<T: ConstructExt> {
 	/// Subxt api
 	api: Client<T>,
 	/// Pair signer
-	signer: sr25519::Pair,
+	signer: T::Pair,
 	/// Env variables
 	env: Env,
 }
 
-impl State<T> {
-	async fn new() -> Self {
+fn build_client(chain_id: &str, ws_url: &str) -> impl ConstructExt {
+	struct XtConstructor;
+	match chain_id {
+		"picasso" => {
+			impl ConstructExt for XtConstructor {
+				type Runtime = PicaRuntime;
+				type Pair = sp_core::sr25519::Pair;
+				type SignedExtra = PicaSignedExtra;
+
+				fn signed_extras(
+					account_id: <Self::Runtime as frame_system::Config>::AccountId,
+				) -> Self::SignedExtra {
+					let nonce = frame_system::Pallet::<Self::Runtime>::account_nonce(account_id);
+					(
+						frame_system::CheckSpecVersion::<Runtime>::new(),
+						frame_system::CheckTxVersion::<Runtime>::new(),
+						frame_system::CheckGenesis::<Runtime>::new(),
+						frame_system::CheckEra::<Runtime>::from(Era::Immortal),
+						frame_system::CheckNonce::<Runtime>::from(nonce),
+						frame_system::CheckWeight::<Runtime>::new(),
+						transaction_payment::ChargeTransactionPayment::<Runtime>::from(0),
+					)
+				}
+			}
+
+			Client::<XtConstructor>::new(ws_url).await.unwrap()
+		},
+
+		"dali" => {
+			impl ConstructExt for XtConstructor {
+				type Runtime = DaliRuntime;
+				type Pair = sp_core::sr25519::Pair;
+				type SignedExtra = DaliSignedExtra;
+
+				fn signed_extras(
+					account_id: <Self::Runtime as frame_system::Config>::AccountId,
+				) -> Self::SignedExtra {
+					let nonce = frame_system::Pallet::<Self::Runtime>::account_nonce(account_id);
+					(
+						frame_system::CheckSpecVersion::<Runtime>::new(),
+						frame_system::CheckTxVersion::<Runtime>::new(),
+						frame_system::CheckGenesis::<Runtime>::new(),
+						frame_system::CheckEra::<Runtime>::from(Era::Immortal),
+						frame_system::CheckNonce::<Runtime>::from(nonce),
+						frame_system::CheckWeight::<Runtime>::new(),
+						transaction_payment::ChargeTransactionPayment::<Runtime>::from(0),
+					)
+				}
+			}
+
+			Client::<XtConstructor>::new(ws_url).await.unwrap()
+		},
+		_ => panic!("Unsupported chain_id: {}", chain_id),
+	}
+}
+
+impl<T: ConstructExt> State<T> {
+	async fn new(chain_id: &str) -> Self {
 		let env = envy::from_env::<Env>().unwrap();
 		// create the signer
 		let signer = sr25519::Pair::from_string(&env.root_key, None).unwrap();
 
-		let api = Client::new(&env.rpc_node).await.unwrap();
+		let api = build_client(chain_id, &env.rpc_node);
 
 		State { api, signer, env }
 	}
@@ -49,16 +119,16 @@ impl State<T> {
 
 #[derive(derive_more::From, Debug)]
 enum Error {
-	Subxt(subxt::Error),
+	SubXt(SubstrateXtError),
 	Rpc(RpcError),
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
 	env_logger::init();
-
+	let chain = Chain::from_args();
 	let main = Main::from_args();
-	let state = State::new().await;
+	let state = State::new(chain.chain_id).await;
 
 	match main {
 		// Main::RotateKeys => rotate_keys(&state).await?,
@@ -114,7 +184,10 @@ async fn main() -> Result<(), Error> {
 // 	Ok(())
 // }
 
-async fn upgrade_runtime(code: Vec<u8>, state: &State<T>) -> Result<(), subxt::Error> {
+async fn upgrade_runtime<T: ConstructExt>(
+	code: Vec<u8>,
+	state: &State<T>,
+) -> Result<(), SubstrateXtError> {
 	let code_hash: H256 = sp_io::hashing::blake2_256(&code).into();
 
 	let call = ParachainSystemCall::authorize_upgrade { code_hash };
@@ -123,30 +196,40 @@ async fn upgrade_runtime(code: Vec<u8>, state: &State<T>) -> Result<(), subxt::E
 
 	let events =
 		state.api.with_rpc_externalities(Some(progress.wait_for_in_block().await?), || {
-			frame_system::Pallet::<Runtime>::events()
+			frame_system::Pallet::<<T as ConstructExt>::Runtime>::events()
 		});
 	let has_event = events.iter().any(|event| {
-		matches!(event.event, ParachainSystem(parachain_system::Event::UpgradeAuthorized))
+		match_event!(
+			event.event.into(),
+			ParachainSystem,
+			parachain_system::Event::UpgradeAuthorized
+		)
 	});
 
 	if !has_event {
-		return Err(subxt::Error::Other("Failed to authorize upgrade".into()));
+		return Err(ExtrinsicError::Custom("Failed to authorize upgrade".into()).into())
 	}
 
-	let call = Call::ParachainSystem(ParachainSystemCall::enact_authorized_upgrade { code });
+	let call = <<T as ConstructExt>::Runtime as frame_system::Config>::Call::ParachainSystem(
+		ParachainSystemCall::enact_authorized_upgrade { code },
+	);
 	let xt = state.api.construct_extrinsic(call.into(), state.signer.clone())?;
 	let progress = state.api.submit_and_watch(xt).await?;
 
 	let events =
 		state.api.with_rpc_externalities(Some(progress.wait_for_in_block().await?), || {
-			frame_system::Pallet::<Runtime>::events()
+			frame_system::Pallet::<<T as ConstructExt>::Runtime>::events()
 		});
 
 	let has_event = events.iter().any(|event| {
-		matches!(event.event, ParachainSystem(parachain_system::Event::ValidationFunctionStored))
+		match_event!(
+			event.event.into(),
+			ParachainSystem,
+			parachain_system::Event::ValidationFunctionStored
+		)
 	});
 	if !has_event {
-		return Err(subxt::Error::Other("Failed to enactg upgrade".into()));
+		return Err(ExtrinsicError::Custom("Failed to enact upgrade".into()).into())
 	}
 
 	log::info!("Runtime upgrade proposed, extrinsic hash: {}", result.extrinsic_hash());
