e4a1f246328fab6adb757c27d9a11a653642c379
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2022-02-07 09:26:13 +0100

    bump impl version for simnode

--- runtime/picasso/src/lib.rs
+++ runtime/picasso/src/lib.rs
@@ -100,7 +100,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	//   runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
 	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
 	spec_version: 1000,
-	impl_version: 3,
+	impl_version: 4,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
 	state_version: 0,
