a5a4163448ef60bd24bf303894b139522314e5c9
Seun Lanlege <seunlanlege@gmail.com>
2022-02-09 19:27:44 +0100

    CI Fixes (#613)
    * ci fixes
    
    * test release pipeline
    
    * remove release pipeline

--- .maintain/runtime_release.sh
+++ .maintain/runtime_release.sh
@@ -16,33 +16,25 @@ VERSIONS_FILES=(
   "runtime/dali/src/lib.rs,dali-chachacha,dali"
   "runtime/composable/src/lib.rs,composable,composable"
 )
-# Because this script runs when a tag has been published, the previous tag is the
-# last two tags
-PREV_TAG=$(gh release list -L=2 | sed -n '2 p' | awk '{print $(NF-1)}')
-CURRENT_TAG=$(gh release list -L=1 | sed -n '1 p' | awk '{print $(NF-1)}')
 
-# Install the neccessary tools needed for building
-cargo install --git https://github.com/chevdor/srtool-cli
-cargo install --locked --git https://github.com/chevdor/subwasm --tag v0.16.1
-
-build_runtime() {
+build_runtime () {
   chain=$3
   # srtool for reproducible builds
   srtool build --package "$chain"-runtime --profile release --runtime-dir runtime/"$chain"
   # subwasm for runtime metadata
-  echo "# $chain Runtime " >>release.md
-  subwasm info ./runtime/"$chain"/target/srtool/release/wbuild/"$chain"-runtime/"$chain"_runtime.compact.wasm >>release.md
+  printf "\n# %s Runtime\n\n" "$(echo $chain | awk '{ print toupper(substr($0, 1, 1)) substr($0, 2) }')" >> release.md
+  INFO=$(subwasm info ./runtime/"$chain"/target/srtool/release/wbuild/"$chain"-runtime/"$chain"_runtime.compact.wasm)
+  printf "\`\`\`\n%s\n\`\`\`\n" "$INFO" >> release.md
 }
 
 # Check which runtimes have changed and build them
 for i in "${VERSIONS_FILES[@]}"; do
   while IFS=',' read -r output chain folder; do
     echo "check if the wasm sources changed for $chain"
-    if has_runtime_changes "${PREV_TAG}" "${GITHUB_REF_NAME}" "$folder"; then
+    if has_runtime_changes "${PREV_TAG_OR_COMMIT}" "${GITHUB_REF_NAME}" "$folder"
+    then
       build_runtime $output $chain $folder
-      CHANGES=gh view release tag $CURRENT_TAG
-      echo $CHANGES | sed '1,/--/  d' >>release.md
-      echo "$chain-wasm=1" >>"$GITHUB_ENV"
+      echo ""$chain"_wasm=1" >> "$GITHUB_ENV"
     fi
-  done <<<"$i"
+  done <<< "$i"
 done
