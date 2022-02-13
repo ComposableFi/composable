74095f779fe4b4221bbe24061e86d8249ec73354
PoisonPhang <17688291+PoisonPhang@users.noreply.github.com>
2022-02-13 16:37:30 -0600

    explicitly defined Default for Validated

--- frame/composable-support/src/validation.rs
+++ frame/composable-support/src/validation.rs
@@ -70,10 +70,19 @@ use scale_info::TypeInfo;
 use sp_runtime::DispatchError;
 
 /// Black box that embbed the validated value.
-#[derive(Default, Copy, Clone)]
+#[derive(Copy, Clone)]
 pub struct Validated<T, U> {
 	value: T,
 	_marker: PhantomData<U>,
+}
+
+impl <T, U> Default for Validated<T, U>
+where
+    T: Default
+{
+    fn default() -> Self {
+        Validated { value: T::default(), _marker: PhantomData }
+    }
 }
 
 impl<T, U> TypeInfo for Validated<T, U>
