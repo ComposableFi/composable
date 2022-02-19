ead2e5ae20d09ad911ed4352990692247917f90c
cor <cor@pruijs.dev>
2022-02-19 17:15:54 +0100

    test(oracle): add_stake

--- frame/oracle/src/tests.rs
+++ frame/oracle/src/tests.rs
@@ -531,43 +531,108 @@ mod set_signer {
 
 }
 
-#[test]
-fn add_stake() {
-	new_test_ext().execute_with(|| {
-		let account_1 = get_account_1();
-		let account_2 = get_root_account();
-		// fails no controller set
-		assert_noop!(Oracle::add_stake(Origin::signed(account_1), 50), Error::<Test>::UnsetSigner);
+mod add_stake {
+    use super::*;
+    proptest! {
+        #![proptest_config(ProptestConfig::with_cases(10_000))]
 
-		assert_ok!(Oracle::set_signer(Origin::signed(account_1), account_2));
+        #[test]
+        fn cannot_add_stake_without_signer_account(
+            controller_account in account_id(),
+            // signer_account in account_id(),
+            stake in 0..Balance::MAX,
+        ) {
+            new_test_ext().execute_with(|| {
 
-		assert_eq!(Balances::free_balance(account_2), 100);
-		assert_eq!(Balances::free_balance(account_1), 99);
-		assert_ok!(Oracle::add_stake(Origin::signed(account_1), 50));
-		assert_eq!(Balances::free_balance(account_1), 49);
-		assert_eq!(Balances::total_balance(&account_1), 49);
-		// funds were transferred to signer and locked
-		assert_eq!(Balances::free_balance(account_2), 100);
-		assert_eq!(Balances::total_balance(&account_2), 151);
+                prop_assert_noop!(Oracle::add_stake(Origin::signed(controller_account), stake), Error::<Test>::UnsetSigner);
+                Ok(())
+            })?;
+        }
 
-		assert_eq!(Oracle::oracle_stake(account_2), Some(51));
-		assert_eq!(Oracle::oracle_stake(account_1), None);
+        #[test]
+        fn can_add_balance_to_stake(
+            controller_account in account_id(),
+            signer_account in account_id(),
+            controller_balance in (MinStake::get() + 1)..(Balance::MAX/2), // +1 so that the controller lives after setting signer
+            signer_balance in 0..(Balance::MAX/2),
+            stake in 0..(Balance::MAX/2),
+        ) {
+            prop_assume!(controller_account != signer_account);
 
-		assert_ok!(Oracle::add_stake(Origin::signed(account_1), 39));
-		assert_eq!(Balances::free_balance(account_1), 10);
-		assert_eq!(Balances::total_balance(&account_1), 10);
-		assert_eq!(Balances::free_balance(account_2), 100);
-		assert_eq!(Balances::total_balance(&account_2), 190);
+            // stake = stake.min
+            new_test_ext().execute_with(|| {
+                Balances::make_free_balance_be(&controller_account, controller_balance);
+                Balances::make_free_balance_be(&signer_account, signer_balance);
 
-		assert_eq!(Oracle::oracle_stake(account_2), Some(90));
-		assert_eq!(Oracle::oracle_stake(account_1), None);
+                prop_assert_ok!(Oracle::set_signer(Origin::signed(controller_account), signer_account));
 
-		assert_noop!(
-			Oracle::add_stake(Origin::signed(account_1), 10),
-			BalancesError::<Test>::KeepAlive
-		);
-	});
+                let new_controller_balance = controller_balance - MinStake::get();
+
+                prop_assert_eq!(Balances::free_balance(&controller_account), new_controller_balance);
+                prop_assert_eq!(Balances::free_balance(&signer_account), signer_balance);
+
+                let stake_to_add = stake.min(new_controller_balance - 1); // -1 so that the controller lives after adding stake
+
+                prop_assert_ok!(Oracle::add_stake(Origin::signed(controller_account), stake_to_add));
+
+                prop_assert_eq!(Balances::free_balance(controller_account), new_controller_balance - stake_to_add);
+
+                let amount_staked = Oracle::oracle_stake(signer_account).unwrap_or_else(|| 0_u32.into());
+                prop_assert_eq!(amount_staked, stake_to_add + MinStake::get());
+                // dbg!(Oracle::oracle_stake(signer_account));
+                // prop_assert_eq!(Oracle::oracle_stake(signer_account), signer_balance + stake_to_add + MinStake::get());
+
+
+                // prop_assert_ok!(Oracle::set_signer(Origin::signed(controller_account), signer_account));
+                //
+                //
+                // prop_assert_eq!(Balances::free_balance(controller_account), controller_balance + stake);
+                // prop_assert_eq!(Balances::free_balance(signer_account), 0);
+
+                Ok(())
+            })?;
+        }
+
+    }
 }
+
+// #[test]
+// fn add_stake() {
+// 	new_test_ext().execute_with(|| {
+// 		let account_1 = get_account_1();
+// 		let account_2 = get_root_account();
+// 		// fails no controller set
+// 		assert_noop!(Oracle::add_stake(Origin::signed(account_1), 50), Error::<Test>::UnsetSigner);
+//
+// 		assert_ok!(Oracle::set_signer(Origin::signed(account_1), account_2));
+//
+// 		assert_eq!(Balances::free_balance(account_2), 100);
+// 		assert_eq!(Balances::free_balance(account_1), 99);
+// 		assert_ok!(Oracle::add_stake(Origin::signed(account_1), 50));
+// 		assert_eq!(Balances::free_balance(account_1), 49);
+// 		assert_eq!(Balances::total_balance(&account_1), 49);
+// 		// funds were transferred to signer and locked
+// 		assert_eq!(Balances::free_balance(account_2), 100);
+// 		assert_eq!(Balances::total_balance(&account_2), 151);
+//
+// 		assert_eq!(Oracle::oracle_stake(account_2), Some(51));
+// 		assert_eq!(Oracle::oracle_stake(account_1), None);
+//
+// 		assert_ok!(Oracle::add_stake(Origin::signed(account_1), 39));
+// 		assert_eq!(Balances::free_balance(account_1), 10);
+// 		assert_eq!(Balances::total_balance(&account_1), 10);
+// 		assert_eq!(Balances::free_balance(account_2), 100);
+// 		assert_eq!(Balances::total_balance(&account_2), 190);
+//
+// 		assert_eq!(Oracle::oracle_stake(account_2), Some(90));
+// 		assert_eq!(Oracle::oracle_stake(account_1), None);
+//
+// 		assert_noop!(
+// 			Oracle::add_stake(Origin::signed(account_1), 10),
+// 			BalancesError::<Test>::KeepAlive
+// 		);
+// 	});
+// }
 
 #[test]
 fn remove_and_reclaim_stake() {
