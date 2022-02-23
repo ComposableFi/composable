7408d3e48013ebfe6943f34cd93e352e0f092ddd
Ângelo Lovatto <angelolovatto@gmail.com>
2022-02-23 15:30:01 -0300

    chore(frame): create empty clearing-house pallet

---
+++ frame/clearing-house/src/lib.rs
@@ -0,0 +1,99 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
+
+#[cfg(feature = "runtime-benchmarks")]
+mod benchmarking;
+
+#[frame_support::pallet]
+pub mod pallet {
+	// ----------------------------------------------------------------------------------------------------
+	//                                       Imports and Dependencies
+	// ----------------------------------------------------------------------------------------------------
+	use frame_support::{
+		ensure,
+		pallet_prelude::*,
+		traits::{
+			fungibles::{Inspect, Mutate, Transfer},
+			tokens::fungibles::MutateHold,
+		},
+		PalletId,
+	};
+	use frame_system::Config as SystemConfig;
+
+	// ----------------------------------------------------------------------------------------------------
+	//                                    Declaration Of The Pallet Type
+	// ----------------------------------------------------------------------------------------------------
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	// ----------------------------------------------------------------------------------------------------
+	//                                             Config Trait
+	// ----------------------------------------------------------------------------------------------------
+
+	// Configure the pallet by specifying the parameters and types on which it depends.
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		// Because this pallet emits events, it depends on the runtime's definition of an event.
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+	}
+
+	// ----------------------------------------------------------------------------------------------------
+	//                                             Pallet Types
+	// ----------------------------------------------------------------------------------------------------
+
+	// pub type PoolIndex = u64;
+
+	// ----------------------------------------------------------------------------------------------------
+	//                                           Runtime  Storage
+	// ----------------------------------------------------------------------------------------------------
+
+	// ----------------------------------------------------------------------------------------------------
+	//                                            Runtime Events
+	// ----------------------------------------------------------------------------------------------------
+
+	// Pallets use events to inform users when important changes are made.
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {}
+
+	// ----------------------------------------------------------------------------------------------------
+	//                                           Runtime  Errors
+	// ----------------------------------------------------------------------------------------------------
+
+	// Errors inform users that something went wrong.
+	#[pallet::error]
+	pub enum Error<T> {}
+
+	// ----------------------------------------------------------------------------------------------------
+	//                              Extrinsics
+	// ----------------------------------------------------------------------------------------------------
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {}
+
+	// ----------------------------------------------------------------------------------------------------
+	//                              Trait Implementations
+	// ----------------------------------------------------------------------------------------------------
+
+	// ----------------------------------------------------------------------------------------------------
+	//                              Helper Functions
+	// ----------------------------------------------------------------------------------------------------
+
+	// Helper functions - core functionality
+	impl<T: Config> Pallet<T> {}
+
+	// Helper functions - validity checks
+	impl<T: Config> Pallet<T> {}
+
+	// Helper functions - low-level functionality
+	impl<T: Config> Pallet<T> {}
+}
