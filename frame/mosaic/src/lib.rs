b27a31aa846d9a446c755f919555264f9e990fbc
Vivek Pandya <vivekvpandya@gmail.com>
2022-03-04 18:11:31 +0530

    Fix clippy warnings. (#719)

--- frame/mosaic/src/lib.rs
+++ frame/mosaic/src/lib.rs
@@ -173,11 +173,13 @@ pub mod pallet {
 
 	#[pallet::storage]
 	#[pallet::getter(fn time_lock_period)]
+	#[allow(clippy::disallowed_type)]
 	pub type TimeLockPeriod<T: Config> =
 		StorageValue<_, BlockNumberOf<T>, ValueQuery, TimeLockPeriodOnEmpty<T>>;
 
 	#[pallet::storage]
 	#[pallet::getter(fn nonce)]
+	#[allow(clippy::disallowed_type)]
 	pub type Nonce<T: Config> = StorageValue<_, u128, ValueQuery>;
 
 	#[pallet::type_value]
