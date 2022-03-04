28b7c33e1702c50ea3323cc3f6759b1fc86ecb47
benluelo <benluelo@hotmail.com>
2022-03-04 09:01:05 -0500

    formatting

--- frame/lending/src/tests.rs
+++ frame/lending/src/tests.rs
@@ -76,8 +76,8 @@ fn accrue_interest_base_cases() {
 	let error = 25;
 	assert_eq!(
 		accrued_increase,
-		10_000_000_000_000_000_000 * MILLISECS_PER_BLOCK as u128 / SECONDS_PER_YEAR_NAIVE as u128
-			+ error
+		10_000_000_000_000_000_000 * MILLISECS_PER_BLOCK as u128 / SECONDS_PER_YEAR_NAIVE as u128 +
+			error
 	);
 }
 
@@ -628,8 +628,8 @@ fn liquidation() {
 
 		// TODO: check deposit_collateral event
 
-		let usdt_amt = 2 * DEFAULT_COLLATERAL_FACTOR * USDT::ONE * get_price(BTC::ID, collateral)
-			/ get_price(NORMALIZED::ID, NORMALIZED::ONE);
+		let usdt_amt = 2 * DEFAULT_COLLATERAL_FACTOR * USDT::ONE * get_price(BTC::ID, collateral) /
+			get_price(NORMALIZED::ID, NORMALIZED::ONE);
 		assert_ok!(Tokens::mint_into(USDT::ID, &CHARLIE, usdt_amt));
 		assert_ok!(Vault::deposit(Origin::signed(*CHARLIE), vault, usdt_amt));
 
