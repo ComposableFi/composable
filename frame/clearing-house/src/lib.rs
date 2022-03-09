48742cf69b2d73425e5ff45a3f63c4ae37f2d9b9
Ângelo <12701614+0xangelo@users.noreply.github.com>
2022-03-09 09:19:24 -0300

    feat(CH): add storage items to CH pallet (#729)
    * feat(CH): add storage types
    * feat(CH): add global margin ratio parameters
    * test(CH): add mock runtime
    * refactor(CH): remove unnecessary generate_store macro
    
    Tested with `cargo check -p clearing-house --all-targets

--- frame/clearing-house/src/lib.rs
+++ frame/clearing-house/src/lib.rs
@@ -16,14 +16,15 @@ pub mod pallet {
 	// ----------------------------------------------------------------------------------------------------
 	//                                       Imports and Dependencies
 	// ----------------------------------------------------------------------------------------------------
-	use frame_support::pallet_prelude::*;
+	use codec::FullCodec;
+	use frame_support::{pallet_prelude::*, Blake2_128Concat, Twox64Concat};
+	use sp_runtime::FixedPointNumber;
 
 	// ----------------------------------------------------------------------------------------------------
 	//                                    Declaration Of The Pallet Type
 	// ----------------------------------------------------------------------------------------------------
 
 	#[pallet::pallet]
-	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
 	// ----------------------------------------------------------------------------------------------------
@@ -35,15 +36,89 @@ pub mod pallet {
 	pub trait Config: frame_system::Config {
 		// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+		/// The balance type for this pallet. Used for keeping track of nonnegative amounts of
+		/// assets.
+		type Balance: FullCodec + MaxEncodedLen + TypeInfo;
+		/// The asset ID type for this and the oracle pallet
+		type AssetId: FullCodec + MaxEncodedLen + TypeInfo;
+		/// The market ID type for this pallet.
+		type MarketId: FullCodec + MaxEncodedLen + TypeInfo;
+		/// Signed decimal fixed point number.
+		type Decimal: FullCodec + MaxEncodedLen + TypeInfo + FixedPointNumber;
+		/// Timestamp to be used for funding rate updates
+		type Timestamp: FullCodec + MaxEncodedLen + TypeInfo;
+		/// Duration type for funding rate periodicity
+		type Duration: FullCodec + MaxEncodedLen + TypeInfo;
+		/// The virtual AMM ID type for this pallet. `pallet-virtual-amm` should implement a trait
+		/// VAMM with an associated type 'VAMMId' compatible with this one.
+		type VAMMId: FullCodec + MaxEncodedLen + TypeInfo;
 	}
 
 	// ----------------------------------------------------------------------------------------------------
 	//                                             Pallet Types
 	// ----------------------------------------------------------------------------------------------------
 
+	/// Stores the user's position in a particular market
+	#[derive(Encode, Decode, MaxEncodedLen, TypeInfo)]
+	pub struct Position<MarketId, Decimal> {
+		market_id: MarketId,
+		base_asset_amount: Decimal,
+		quote_asset_notional_amount: Decimal,
+		last_cum_funding: Decimal,
+	}
+
+	/// Data relating to a perpetual contracts market
+	#[derive(Encode, Decode, MaxEncodedLen, TypeInfo)]
+	pub struct Market<AssetId, Decimal, Duration, Timestamp, VAMMId> {
+		vamm_id: VAMMId,
+		asset_id: AssetId,
+		cum_funding_rate: Decimal,
+		funding_rate_ts: Timestamp,
+		periodicity: Duration,
+	}
+
+	pub type AssetIdOf<T> = <T as Config>::AssetId;
+	pub type MarketIdOf<T> = <T as Config>::MarketId;
+	pub type DecimalOf<T> = <T as Config>::Decimal;
+	pub type TimestampOf<T> = <T as Config>::Timestamp;
+	pub type DurationOf<T> = <T as Config>::Duration;
+	pub type VAMMIdOf<T> = <T as Config>::VAMMId;
+	pub type PositionOf<T> = Position<MarketIdOf<T>, DecimalOf<T>>;
+	pub type MarketOf<T> =
+		Market<AssetIdOf<T>, DecimalOf<T>, DurationOf<T>, TimestampOf<T>, VAMMIdOf<T>>;
+
 	// ----------------------------------------------------------------------------------------------------
 	//                                           Runtime  Storage
 	// ----------------------------------------------------------------------------------------------------
+
+	#[pallet::storage]
+	#[pallet::getter(fn get_initial_margin_ratio)]
+	/// Minimum margin ratio for opening a new position
+	type InitialMarginRatio<T: Config> = StorageValue<_, T::Decimal, ValueQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn get_maintenance_margin_ratio)]
+	/// Minimum margin ratio, below which liquidations can occur
+	type MaintenanceMarginRatio<T: Config> = StorageValue<_, T::Decimal, ValueQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn get_margin)]
+	pub type AccountsMargin<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, T::Balance>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn get_position)]
+	pub type Positions<T: Config> = StorageDoubleMap<
+		_,
+		Blake2_128Concat,
+		T::AccountId,
+		Twox64Concat,
+		T::MarketId,
+		PositionOf<T>,
+	>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn get_market)]
+	pub type Markets<T: Config> = StorageMap<_, Twox64Concat, T::MarketId, MarketOf<T>>;
 
 	// ----------------------------------------------------------------------------------------------------
 	//                                            Runtime Events
@@ -51,7 +126,7 @@ pub mod pallet {
 
 	// Pallets use events to inform users when important changes are made.
 	#[pallet::event]
-	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	// #[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {}
 
 	// ----------------------------------------------------------------------------------------------------
