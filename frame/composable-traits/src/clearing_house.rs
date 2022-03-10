05cd5ba1aefe6527ceec0a09137a84466e2968b6
Ângelo Lovatto <12701614+0xangelo@users.noreply.github.com>
2022-03-10 14:53:44 -0300

    feat(CH): add and implement MarginAccounts trait

---
+++ frame/composable-traits/src/clearing_house.rs
@@ -0,0 +1,23 @@
+//! # Clearing House
+//!
+//! Common traits for clearing house implementations
+use frame_support::pallet_prelude::DispatchError;
+
+pub trait MarginAccounts {
+	/// The trader's account identifier type
+	type AccountId;
+	/// The asset identifier type
+	type AssetId;
+	/// The balance type for an account
+	type Balance;
+
+	/// Add margin to a user's account
+	///
+	/// Assumes margin account is unique to each wallet address, i.e., there's only one margin
+	/// account per user.
+	fn add_margin(
+		acc: &Self::AccountId,
+		asset: Self::AssetId,
+		amount: Self::Balance,
+	) -> Result<(), DispatchError>;
+}
