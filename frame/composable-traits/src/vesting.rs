e3c6a20bbec2bc03c476ed24b793f1106dfa9aeb
Vim Wickramasinghe <vimukthi@composable.finance>
2022-03-10 15:45:05 +0100

    unit tests: frame/composable-traits/src/vesting.rs

--- frame/composable-traits/src/vesting.rs
+++ frame/composable-traits/src/vesting.rs
@@ -130,6 +130,27 @@ mod tests {
 	use super::*;
 
 	#[test]
+	fn test_is_zero_period() {
+		let mut vesting_schedule_time_based = VestingSchedule::<u32, u64, u64> {
+			window: MomentBased { start: 1u64, period: 10u64 },
+			period_count: 100,
+			per_period: 1u64,
+		};
+		assert!(!vesting_schedule_time_based.is_zero_period());
+		vesting_schedule_time_based.window = MomentBased {start: 1, period: 0};
+		assert!(vesting_schedule_time_based.is_zero_period());
+
+		let mut vesting_schedule_block_number_based = VestingSchedule::<u64, u32, u64> {
+			window: BlockNumberBased { start: 1u64, period: 10u64 },
+			period_count: 100,
+			per_period: 1u64,
+		};
+		assert!(!vesting_schedule_block_number_based.is_zero_period());
+		vesting_schedule_block_number_based.window = BlockNumberBased {start: 1, period: 0};
+		assert!(vesting_schedule_block_number_based.is_zero_period());
+	}
+
+	#[test]
 	fn test_end() {
 		let vesting_schedule_time_based = VestingSchedule::<u32, u64, u64> {
 			window: MomentBased { start: 1u64, period: 10u64 },
@@ -165,5 +186,27 @@ mod tests {
 			per_period: 1u64,
 		};
 		assert_eq!(vesting_schedule.total_amount().unwrap(), 100)
+	}
+
+	/// TODO proptest for exhastive tests
+	#[test]
+	fn test_locked_amount() {
+		let vesting_schedule_time_based = VestingSchedule::<u32, u64, u64> {
+			window: MomentBased { start: 1u64, period: 10u64 },
+			period_count: 100,
+			per_period: 1u64,
+		};
+		assert_eq!(vesting_schedule_time_based.locked_amount(1, 1), 100);
+		assert_eq!(vesting_schedule_time_based.locked_amount(1, 11), 99);
+		assert_eq!(vesting_schedule_time_based.locked_amount(1, 1001), 0);
+
+		let vesting_schedule_block_number_based = VestingSchedule::<u64, u32, u64> {
+			window: BlockNumberBased { start: 1u64, period: 10u64 },
+			period_count: 100,
+			per_period: 1u64,
+		};
+		assert_eq!(vesting_schedule_block_number_based.locked_amount(1, 1), 100);
+		assert_eq!(vesting_schedule_block_number_based.locked_amount(11, 1), 99);
+		assert_eq!(vesting_schedule_block_number_based.locked_amount(1001, 1), 0);
 	}
 }
