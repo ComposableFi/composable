689f241d546e4b0dc0e303c9812762a1bb22e049
cor <cor@pruijs.dev>
2022-03-11 20:51:22 +0100

    define book shell subshell

--- flake.nix
+++ flake.nix
@@ -1,26 +1,21 @@
 {
   description = "Composable Finance";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs, utils }:
+  outputs = { self, nixpkgs }:
     let
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
     in {
       packages = forAllSystems (system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
+        let pkgs = nixpkgsFor.${system}; in
         {
           book = pkgs.stdenv.mkDerivation {
             name = "composable-book";
             src = ./book;
-            buildInputs = [ pkgs.mdbook pkgs.tree ];
+            buildInputs = [ pkgs.mdbook ];
             dontUnpack = true;
             installPhase = ''
               mkdir -p $out/book
@@ -31,10 +26,16 @@
         }
       );
 
+      # Default package is currently the book, but that will change
       defaultPackage = forAllSystems (system: self.packages.${system}.book);
 
-      devShell = forAllSystems(system: with nixpkgsFor.${system}; mkShell {
-        buildInputs = [ mdbook ];
-      });
+      devShells = forAllSystems(system: 
+        let pkgs = nixpkgsFor.${system}; in 
+        {
+          book = pkgs.mkShell {
+            buildInputs = [ pkgs.mdbook ];
+          };
+        }
+      );
     };
 }
