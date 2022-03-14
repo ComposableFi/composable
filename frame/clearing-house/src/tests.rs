a3115da9311d2043a542ecea603c4ffc2a51ac3e
Ângelo Lovatto <12701614+0xangelo@users.noreply.github.com>
2022-03-14 20:05:08 -0300

    test(CH): assert deposit unsupported collateral errors

--- frame/clearing-house/src/tests.rs
+++ frame/clearing-house/src/tests.rs
@@ -1,5 +1,5 @@
 pub use crate::{
-	mock::runtime::{ClearingHouse, ExtBuilder, Origin, Runtime, Tokens, ALICE, USDC},
+	mock::runtime::{ClearingHouse, ExtBuilder, Origin, Runtime, Tokens, ALICE, PICA, USDC},
 	pallet::*,
 };
 use frame_support::assert_err;
@@ -15,5 +15,18 @@ fn test_add_margin_returns_transfer_error() {
 			ClearingHouse::add_margin(origin, USDC, 1_000u32.into()),
 			TokenError::<Runtime>::BalanceTooLow
 		);
+	});
+}
+
+#[test]
+fn test_deposit_unsupported_collateral_returns_error() {
+	ExtBuilder::default().build().execute_with(|| {
+		CollateralTypes::<Runtime>::insert(&USDC, ());
+
+		let origin = Origin::signed(ALICE);
+		assert_err!(
+			ClearingHouse::add_margin(origin, PICA, 1_000u32.into()),
+			Error::<Runtime>::UnsupportedCollateralType
+		)
 	});
 }
