7f9e5ccdf856f0d9a30e40195fbd10c60dac9139
kevin-composable <kevin@composable.finance>
2022-03-14 10:49:22 -0700

    Added pallet skeleton with test extrinsic

--- frame/instrumental/src/lib.rs
+++ frame/instrumental/src/lib.rs
@@ -1,8 +1,110 @@
+#[frame_support::pallet]
+pub mod pallet {
+	// ----------------------------------------------------------------------------------------------------
+	//                                       Imports and Dependencies                                      
+	// ----------------------------------------------------------------------------------------------------
+
+	use frame_support::pallet_prelude::*;
+	use frame_system::{
+		ensure_signed,
+		pallet_prelude::*,
+	};
+
+	// ----------------------------------------------------------------------------------------------------
+	//                                    Declaration Of The Pallet Type                                           
+	// ----------------------------------------------------------------------------------------------------
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	// ----------------------------------------------------------------------------------------------------
+	//                                             Config Trait                                            
+	// ----------------------------------------------------------------------------------------------------
+
+	// Configure the pallet by specifying the parameters and types on which it depends.
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		#[allow(missing_docs)]
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+	}
+
+	// ----------------------------------------------------------------------------------------------------
+    //                                             Pallet Types                                           
+	// ----------------------------------------------------------------------------------------------------
+
+	// ----------------------------------------------------------------------------------------------------
+    //                                            Runtime Events                                          
+	// ----------------------------------------------------------------------------------------------------
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		Test {
+			issuer: T::AccountId
+		},
+
+		LiquidityAdded {
+
+		}
+	}
+
+	// ----------------------------------------------------------------------------------------------------
+    //                                           Runtime  Errors                                           
+	// ----------------------------------------------------------------------------------------------------
+
+	#[pallet::error]
+	pub enum Error<T> {
+	}
+
+	// ----------------------------------------------------------------------------------------------------
+    //                                           Runtime  Storage                                          
+	// ----------------------------------------------------------------------------------------------------
+
+	// ----------------------------------------------------------------------------------------------------
+    //                                                Hooks                                                
+	// ----------------------------------------------------------------------------------------------------
+
+	#[pallet::hooks]
+	impl<T: Config> Hooks<T::BlockNumber> for Pallet<T> {}
+
+	// ----------------------------------------------------------------------------------------------------
+    //                                              Extrinsics                                             
+	// ----------------------------------------------------------------------------------------------------
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+
+		#[pallet::weight(0)]
+		pub fn test(
+			origin: OriginFor<T>,
+		) -> DispatchResultWithPostInfo {
+			// Requirement 0) This extrinsic must be signed 
+			let from = ensure_signed(origin)?;
+
+			Self::deposit_event(Event::Test { issuer: from });
+
+			Ok(().into())
+		}
+
+		// #[pallet::weight(0)]
+		// pub fn add_liquidity(
+		// 	origin: OriginFor<T>,
+		// ) -> DispatchResultWithPostInfo {
+		// 	// Requirement 0) This extrinsic must be signed 
+		// 	let from = ensure_signed(origin)?;
+
+		// 	Self::deposit_event(Event::LiquidityAdded { });
+
+		// 	Ok(().into())
+		// }
+	}
+}
+
+// ----------------------------------------------------------------------------------------------------
+//                                              Unit Tests                                             
+// ----------------------------------------------------------------------------------------------------
+
 #[cfg(test)]
 mod tests {
-	#[test]
-	fn it_works() {
-		let result = 2 + 2;
-		assert_eq!(result, 4);
-	}
 }
