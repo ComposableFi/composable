092292fe9c125697ac660cef19f55021f671f9e4
Ângelo Lovatto <12701614+0xangelo@users.noreply.github.com>
2022-03-14 19:46:22 -0300

    test(CH): add test_add_margin_returns_transfer_error

--- frame/clearing-house/src/tests.rs
+++ frame/clearing-house/src/tests.rs
@@ -1,1 +1,19 @@
+pub use crate::{
+	mock::runtime::{ClearingHouse, ExtBuilder, Origin, Runtime, Tokens, ALICE, USDC},
+	pallet::*,
+};
+use frame_support::assert_err;
+use orml_tokens::Error as TokenError;
 
+#[test]
+fn test_add_margin_returns_transfer_error() {
+	ExtBuilder::default().build().execute_with(|| {
+		CollateralTypes::<Runtime>::insert(&USDC, ());
+
+		let origin = Origin::signed(ALICE);
+		assert_err!(
+			ClearingHouse::add_margin(origin, USDC, 1_000u32.into()),
+			TokenError::<Runtime>::BalanceTooLow
+		);
+	});
+}
