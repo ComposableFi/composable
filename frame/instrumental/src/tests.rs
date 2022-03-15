509bf21c502aa94cfab758a7bf31eaf0c9e6f39b
kevin-composable <kevin@composable.finance>
2022-03-15 13:32:47 -0700

    added a second test to check that the test extrinsic is correctly emits an event

--- frame/instrumental/src/tests.rs
+++ frame/instrumental/src/tests.rs
@@ -1,16 +1,28 @@
 #[allow(unused_imports)]
 
 use crate::mock::{
-    ALICE, ExtBuilder, Instrumental, Origin
+    ALICE, Event, ExtBuilder, Instrumental, Origin, System
 };
+use crate::pallet;
 
-use frame_support::{
-    assert_ok, 
-};
+use frame_support::assert_ok;
 
 #[test]
 fn call_test_extrinsic() {
     ExtBuilder::default().build().execute_with(|| {
         assert_ok!(Instrumental::test(Origin::signed(ALICE)));
+    });
+}
+
+#[test]
+fn test_extrinsic_emits_event() {
+    ExtBuilder::default().build().execute_with(|| {
+        System::set_block_number(1);
+
+        assert_ok!(Instrumental::test(Origin::signed(ALICE)));
+
+        System::assert_last_event(Event::Instrumental(
+            pallet::Event::Test { account: ALICE}
+        ));
     });
 }
