bb9c53937c378ed0418e627ea676873ba046a3ea
kevin-composable <kevin@composable.finance>
2022-03-16 14:02:19 -0700

    Added create() method under WeightInfo trait and removed test() method

--- frame/instrumental/src/weights.rs
+++ frame/instrumental/src/weights.rs
@@ -1,21 +1,23 @@
 use frame_support::weights::Weight;
 use sp_std::marker::PhantomData;
 
+const I_HAVENT_CALCULATED_YET: i32 = 1_000;
+
 pub trait WeightInfo {
-	fn test() -> Weight;
+	fn create() -> Weight;
 }
 
 /// Weights for pallet_instrumental using the Substrate node and recommended hardware.
 pub struct SubstrateWeight<T>(PhantomData<T>);
 impl<T: frame_system::Config> WeightInfo for SubstrateWeight<T> {
-	fn test() -> Weight {
-		1_000 as Weight
+	fn create() -> Weight {
+		I_HAVENT_CALCULATED_YET as Weight
 	}
 }
 
 // For backwards compatibility and tests
 impl WeightInfo for () {
-	fn test() -> Weight {
-        1_000 as Weight
-    }
+	fn create() -> Weight {
+		I_HAVENT_CALCULATED_YET as Weight
+	}
 }
