5f16e6952813c624f584d5f219d00aac8e8ea374
kevin-composable <kevin@composable.finance>
2022-03-16 14:04:56 -0700

    replaced the test extrinsic tests with create extrinsic tests

--- frame/instrumental/src/tests.rs
+++ frame/instrumental/src/tests.rs
@@ -4,25 +4,26 @@ use crate::mock::{
     ALICE, Event, ExtBuilder, Instrumental, Origin, System
 };
 use crate::pallet;
+use crate::currency::PICA;
 
 use frame_support::assert_ok;
 
 #[test]
-fn call_test_extrinsic() {
+fn call_create_extrinsic() {
     ExtBuilder::default().build().execute_with(|| {
-        assert_ok!(Instrumental::test(Origin::signed(ALICE)));
+        assert_ok!(Instrumental::create(Origin::signed(ALICE), PICA::ID));
     });
 }
 
 #[test]
-fn test_extrinsic_emits_event() {
+fn test_create_emits_event() {
     ExtBuilder::default().build().execute_with(|| {
         System::set_block_number(1);
 
-        assert_ok!(Instrumental::test(Origin::signed(ALICE)));
+        assert_ok!(Instrumental::create(Origin::signed(ALICE), PICA::ID));
 
         System::assert_last_event(Event::Instrumental(
-            pallet::Event::Test { account: ALICE}
+            pallet::Event::Create { asset: PICA::ID }
         ));
     });
 }
