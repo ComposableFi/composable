aa81cbb10da33e67edcaa0eed4d10c343829ffa6
kevin-composable <kevin@composable.finance>
2022-03-16 14:29:38 -0700

    added create extrinsic tests to test updating the AssetVault runtime storage object and to test not being able to create more than one vault for a given asset

--- frame/instrumental/src/tests.rs
+++ frame/instrumental/src/tests.rs
@@ -1,22 +1,13 @@
-#[allow(unused_imports)]
-
 use crate::mock::{
-    ALICE, Event, ExtBuilder, Instrumental, Origin, System
+    ALICE, Event, ExtBuilder, Instrumental, MockRuntime, Origin, System
 };
-use crate::pallet;
+use crate::{pallet, pallet::AssetVault, pallet::Error};
 use crate::currency::PICA;
 
-use frame_support::assert_ok;
+use frame_support::{assert_ok, assert_noop};
 
 #[test]
-fn call_create_extrinsic() {
-    ExtBuilder::default().build().execute_with(|| {
-        assert_ok!(Instrumental::create(Origin::signed(ALICE), PICA::ID));
-    });
-}
-
-#[test]
-fn test_create_emits_event() {
+fn create_extrinsic_emits_event() {
     ExtBuilder::default().build().execute_with(|| {
         System::set_block_number(1);
 
@@ -25,5 +16,26 @@ fn test_create_emits_event() {
         System::assert_last_event(Event::Instrumental(
             pallet::Event::Create { asset: PICA::ID }
         ));
+    });
+}
+
+#[test]
+fn create_extrinsic_updates_storage() {
+    ExtBuilder::default().build().execute_with(|| {
+        assert!(!AssetVault::<MockRuntime>::contains_key(PICA::ID));
+        assert_ok!(Instrumental::create(Origin::signed(ALICE), PICA::ID));
+        assert!(AssetVault::<MockRuntime>::contains_key(PICA::ID));
+    });
+}
+
+#[test]
+fn cannot_create_more_than_one_vault_for_an_asset() {
+    ExtBuilder::default().build().execute_with(|| {
+        assert_ok!(Instrumental::create(Origin::signed(ALICE), PICA::ID));
+
+        assert_noop!(
+            Instrumental::create(Origin::signed(ALICE), PICA::ID),
+            Error::<MockRuntime>::VaultAlreadyExists
+        );
     });
 }
