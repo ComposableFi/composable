1532e53e0c42d6860c0037d17be77aa77b627d98
Seun Lanlege <seunlanlege@gmail.com>
2022-03-18 12:00:09 +0100

    fix clippy

--- runtime/composable/src/lib.rs
+++ runtime/composable/src/lib.rs
@@ -977,7 +977,7 @@ impl_runtime_apis! {
 				transaction_payment::ChargeTransactionPayment::<Runtime>::from(0),
 			);
 
-			let signature = MultiSignature::from(sr25519::Signature([0u8;64]));
+			let signature = MultiSignature::from(sr25519::Signature([0_u8;64]));
 			let address = AccountIdLookup::unlookup(signer);
 			let ext = UncheckedExtrinsic::new_signed(call, address, signature, extra);
 
