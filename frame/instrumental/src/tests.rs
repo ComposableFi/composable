cfa2d164a32abd25478ea12d5f31d99a47182469
kevin-composable <kevin@composable.finance>
2022-03-21 14:28:12 -0700

    CU-24j2h9e[DONE] Use Assets Pallet In Tests Over Balances/Tokens Directly

--- frame/instrumental/src/tests.rs
+++ frame/instrumental/src/tests.rs
@@ -1,14 +1,16 @@
+#[cfg(test)]
+
 use crate::mock::{
     ALICE, Event, ExtBuilder, Instrumental, MockRuntime, Origin, System,
 };
 use crate::{pallet, pallet::AssetVault, pallet::Error};
-use crate::currency::PICA;
+use crate::currency::USDC;
 
 use frame_support::{
     assert_ok, assert_noop, assert_storage_noop,
 };
 
-use proptest::prelude::*;
+// use proptest::prelude::*;
 
 // ----------------------------------------------------------------------------------------------------
 //                                                Create                                               
@@ -19,10 +21,10 @@ fn create_extrinsic_emits_event() {
     ExtBuilder::default().build().execute_with(|| {
         System::set_block_number(1);
 
-        assert_ok!(Instrumental::create(Origin::signed(ALICE), PICA::ID));
+        assert_ok!(Instrumental::create(Origin::signed(ALICE), USDC::ID));
 
         System::assert_last_event(Event::Instrumental(
-            pallet::Event::Created { asset: PICA::ID }
+            pallet::Event::Created { asset: USDC::ID }
         ));
     });
 }
@@ -30,19 +32,19 @@ fn create_extrinsic_emits_event() {
 #[test]
 fn create_extrinsic_updates_storage() {
     ExtBuilder::default().build().execute_with(|| {
-        assert!(!AssetVault::<MockRuntime>::contains_key(PICA::ID));
-        assert_ok!(Instrumental::create(Origin::signed(ALICE), PICA::ID));
-        assert!(AssetVault::<MockRuntime>::contains_key(PICA::ID));
+        assert!(!AssetVault::<MockRuntime>::contains_key(USDC::ID));
+        assert_ok!(Instrumental::create(Origin::signed(ALICE), USDC::ID));
+        assert!(AssetVault::<MockRuntime>::contains_key(USDC::ID));
     });
 }
 
 #[test]
 fn cannot_create_more_than_one_vault_for_an_asset() {
     ExtBuilder::default().build().execute_with(|| {
-        assert_ok!(Instrumental::create(Origin::signed(ALICE), PICA::ID));
+        assert_ok!(Instrumental::create(Origin::signed(ALICE), USDC::ID));
 
         assert_noop!(
-            Instrumental::create(Origin::signed(ALICE), PICA::ID),
+            Instrumental::create(Origin::signed(ALICE), USDC::ID),
             Error::<MockRuntime>::VaultAlreadyExists
         );
     });
@@ -54,15 +56,16 @@ fn cannot_create_more_than_one_vault_for_an_asset() {
 
 #[test]
 fn add_liquidity_extrinsic_emits_event() {
-    ExtBuilder::default().initialize_balance(ALICE, PICA::ID, PICA::units(100))
-        .build().execute_with(|| {
+    ExtBuilder::default().initialize_balance(
+        ALICE, USDC::ID, USDC::units(100)
+    ).build().execute_with(|| {
         System::set_block_number(1);
 
-        assert_ok!(Instrumental::create(Origin::signed(ALICE), PICA::ID));
-        assert_ok!(Instrumental::add_liquidity(Origin::signed(ALICE), PICA::ID, PICA::units(100)));
+        assert_ok!(Instrumental::create(Origin::signed(ALICE), USDC::ID));
+        assert_ok!(Instrumental::add_liquidity(Origin::signed(ALICE), USDC::ID, USDC::units(100)));
 
         System::assert_last_event(Event::Instrumental(
-            pallet::Event::AddedLiquidity { asset: PICA::ID , amount: PICA::units(100)}
+            pallet::Event::AddedLiquidity { asset: USDC::ID , amount: USDC::units(100)}
         ));
     });
 }
@@ -73,7 +76,7 @@ fn add_liquidity_asset_must_have_an_associated_vault() {
         System::set_block_number(1);
         
         assert_noop!(
-            Instrumental::add_liquidity(Origin::signed(ALICE), PICA::ID, PICA::units(100)),
+            Instrumental::add_liquidity(Origin::signed(ALICE), USDC::ID, USDC::units(100)),
             Error::<MockRuntime>::AssetDoesNotHaveAnAssociatedVault
         );
     });
@@ -83,10 +86,10 @@ fn add_liquidity_asset_must_have_an_associated_vault() {
 #[allow(unused_must_use)]
 fn add_liquidity_does_not_update_storage_if_user_does_not_have_balance() {
     ExtBuilder::default().build().execute_with(|| {
-        assert_ok!(Instrumental::create(Origin::signed(ALICE), PICA::ID));
+        assert_ok!(Instrumental::create(Origin::signed(ALICE), USDC::ID));
 
         assert_storage_noop!(
-            Instrumental::add_liquidity(Origin::signed(ALICE), PICA::ID, PICA::units(100))
+            Instrumental::add_liquidity(Origin::signed(ALICE), USDC::ID, USDC::units(100))
         );
     });
 }
@@ -98,17 +101,17 @@ fn add_liquidity_does_not_update_storage_if_user_does_not_have_balance() {
 #[test]
 fn remove_liquidity_extrinsic_emits_event() {
     ExtBuilder::default()
-        .initialize_balance(ALICE, PICA::ID, PICA::units(100))
+        .initialize_balance(ALICE, USDC::ID, USDC::units(100))
         .build()
         .execute_with(|| {
             System::set_block_number(1);
 
-            assert_ok!(Instrumental::create(Origin::signed(ALICE), PICA::ID));
-            assert_ok!(Instrumental::add_liquidity(Origin::signed(ALICE), PICA::ID, PICA::units(100)));
-            assert_ok!(Instrumental::remove_liquidity(Origin::signed(ALICE), PICA::ID, PICA::units(100)));
+            assert_ok!(Instrumental::create(Origin::signed(ALICE), USDC::ID));
+            assert_ok!(Instrumental::add_liquidity(Origin::signed(ALICE), USDC::ID, USDC::units(100)));
+            assert_ok!(Instrumental::remove_liquidity(Origin::signed(ALICE), USDC::ID, USDC::units(100)));
 
             System::assert_last_event(Event::Instrumental(
-                pallet::Event::RemovedLiquidity { asset: PICA::ID , amount: PICA::units(100)}
+                pallet::Event::RemovedLiquidity { asset: USDC::ID , amount: USDC::units(100)}
             ));
     });
 }
@@ -119,7 +122,7 @@ fn remove_liquidity_asset_must_have_an_associated_vault() {
         System::set_block_number(1);
         
         assert_noop!(
-            Instrumental::remove_liquidity(Origin::signed(ALICE), PICA::ID, PICA::units(100)),
+            Instrumental::remove_liquidity(Origin::signed(ALICE), USDC::ID, USDC::units(100)),
             Error::<MockRuntime>::AssetDoesNotHaveAnAssociatedVault
         );
     });
