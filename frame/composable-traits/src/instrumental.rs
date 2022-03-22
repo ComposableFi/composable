2311cf172f83e0867d2230931762a2dc3f67cb08
kevin-composable <kevin@composable.finance>
2022-03-21 18:13:41 -0700

    CU-24c93tp[DONE] Added Instrumental::create trait implementation

---
+++ frame/composable-traits/src/instrumental.rs
@@ -0,0 +1,28 @@
+use frame_support::{
+    pallet_prelude::*,
+    sp_std::fmt::Debug,
+};
+use codec::Codec;
+
+pub trait Instrumental {
+    type AccountId: core::cmp::Ord;
+	type AssetId;
+	type Balance;
+	type VaultId: Clone + Codec + Debug + PartialEq + Default + Parameter;
+
+    fn create(
+        asset: &Self::AssetId,
+    ) -> Result<Self::VaultId, DispatchError>;
+
+    fn add_liquidity(
+        issuer: Self::AccountId,
+        asset: Self::AssetId,
+        amount: Self::Balance
+    ) -> Result<(), DispatchError>;
+
+    fn remove_liquidity(
+        issuer: Self::AccountId,
+        asset: Self::AssetId,
+        amount: Self::Balance
+    ) -> Result<(), DispatchError>;
+}
