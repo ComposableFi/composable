6e239aa95137fe4e110b4b1aaa850171f1d16134
Seun Lanlege <seunlanlege@gmail.com>
2022-03-22 13:54:21 +0100

    review runtime versions

--- runtime/dali/src/lib.rs
+++ runtime/dali/src/lib.rs
@@ -110,10 +110,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
 	// This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
 	//   the compatible custom types.
-	spec_version: 1000,
-	impl_version: 8,
+	spec_version: 2000,
+	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 3,
+	transaction_version: 1,
 	state_version: 0,
 };
 
