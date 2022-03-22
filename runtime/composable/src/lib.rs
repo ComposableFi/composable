c7e4116684818cd095ce5ad9b1a198a91bf279b6
Web3 Philosopher <seunlanlege@gmail.com>
2022-03-22 15:52:07 +0100

    Release take 4 (#798)
    * Add missing std features for custom rpcs and pin gcloud action version (#791)
    
    * fix(runtime): missing std for custom rpcs on dali/picasso
    
    * fix(ci): pin ci devnet gcloud version
    
    * review runtime versions (#792)
    
    * fix composable runtime (#795)
    
    Co-authored-by: Hussein Ait-Lahcen <hussein.aitlahcen@gmail.com>

--- runtime/composable/src/lib.rs
+++ runtime/composable/src/lib.rs
@@ -101,9 +101,9 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	//   runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
 	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
 	spec_version: 1000,
-	impl_version: 4,
+	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 2,
+	transaction_version: 1,
 	state_version: 0,
 };
 
