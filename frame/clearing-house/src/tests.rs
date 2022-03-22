72192559d7e0e4cf559466bd21d6a761da083228
Ângelo Lovatto <12701614+0xangelo@users.noreply.github.com>
2022-03-22 16:03:03 -0300

    test(CH): expect event upon succesful add-margin call
    CU-240p63v[IN REVIEW]

--- frame/clearing-house/src/tests.rs
+++ frame/clearing-house/src/tests.rs
@@ -2,7 +2,7 @@ pub use crate::{
 	mock::{
 		accounts::{AccountId, ALICE},
 		assets::{AssetId, PICA, USDC},
-		runtime::{Balance, ClearingHouse, ExtBuilder, Origin, Runtime, Tokens},
+		runtime::{Balance, ClearingHouse, ExtBuilder, Origin, Runtime, System, Tokens},
 	},
 	pallet::*,
 };
@@ -44,13 +44,16 @@ fn test_deposit_supported_collateral_succeeds() {
 	ExtBuilder { balances: vec![(ALICE, USDC, 1_000_000)], ..Default::default() }
 		.build()
 		.execute_with(|| {
-			let before = AccountsMargin::<Runtime>::get(&ALICE).unwrap_or_default();
-
-			let origin = Origin::signed(ALICE);
+			let account = ALICE;
+			let asset = USDC;
 			let amount: Balance = 1_000u32.into();
-			assert_ok!(ClearingHouse::add_margin(origin, USDC, amount));
 
-			let after = AccountsMargin::<Runtime>::get(&ALICE).unwrap_or_default();
+			let before = AccountsMargin::<Runtime>::get(&account).unwrap_or_default();
+			assert_ok!(ClearingHouse::add_margin(Origin::signed(account), asset, amount));
+
+			System::assert_last_event(Event::MarginAdded { account, asset, amount }.into());
+
+			let after = AccountsMargin::<Runtime>::get(&account).unwrap_or_default();
 			assert_eq!(after - before, amount);
 		})
 }
