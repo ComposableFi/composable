1298797a4e1f9f113e321de45726039b9f056bf3
kevin-composable <kevin@composable.finance>
2022-03-23 15:04:04 -0700

    CU-2530qdn[IN PROGRESS] added generate_deposits prop_compose to generate a vector of (AccountId, Asset, Balance) tuples

---
+++ frame/instrumental/src/account_id.rs
@@ -0,0 +1,36 @@
+use sp_runtime::{
+	traits::{IdentifyAccount, Verify}
+};
+use sp_core::{
+	sr25519::{Public, Signature}
+};
+
+use hex_literal::hex;
+
+pub type AccountId = <<Signature as Verify>::Signer as IdentifyAccount>::AccountId;
+
+pub static ADMIN: Public =
+	Public(hex!("0000000000000000000000000000000000000000000000000000000000000000"));
+pub static ALICE: Public =
+	Public(hex!("0000000000000000000000000000000000000000000000000000000000000001"));
+pub static BOB: Public =
+	Public(hex!("0000000000000000000000000000000000000000000000000000000000000002"));
+pub static CHARLIE: Public =
+	Public(hex!("0000000000000000000000000000000000000000000000000000000000000003"));
+pub static DAVE: Public =
+	Public(hex!("0000000000000000000000000000000000000000000000000000000000000004"));
+pub static EVEN: Public =
+	Public(hex!("0000000000000000000000000000000000000000000000000000000000000005"));
+
+use proptest::{prop_oneof, strategy::{Just, Strategy}};
+
+#[allow(dead_code)]
+pub fn pick_account() -> impl Strategy<Value = AccountId> {
+	prop_oneof![
+		Just(ALICE),
+		Just(BOB),
+		Just(CHARLIE),
+		Just(DAVE),
+		Just(EVEN),
+	]
+}
