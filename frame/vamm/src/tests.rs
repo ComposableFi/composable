e29b364fba2251e561bd985389e424f2415bc638
Matheus Cardoso <cardosaum@pm.me>
2022-03-24 17:28:28 -0300

    refactor(vamm): remove unneeded tests

--- frame/vamm/src/tests.rs
+++ frame/vamm/src/tests.rs
@@ -7,22 +7,3 @@ use crate::pallet;
 
 use frame_support::assert_ok;
 
-#[test]
-fn call_test_extrinsic() {
-    ExtBuilder::default().build().execute_with(|| {
-        assert_ok!(Vamm::test(Origin::signed(ALICE)));
-    });
-}
-
-#[test]
-fn test_extrinsic_emits_event() {
-    ExtBuilder::default().build().execute_with(|| {
-        System::set_block_number(1);
-
-        assert_ok!(Vamm::test(Origin::signed(ALICE)));
-
-        System::assert_last_event(Event::Vamm(
-            pallet::Event::Test { account: ALICE}
-        ));
-    });
-}
