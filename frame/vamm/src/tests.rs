99ebbc8e62121c70ecd2983dcd212b4408a2d22f
Matheus Cardoso <cardosaum@pm.me>
2022-04-02 13:03:39 -0300

    chore(vamm): Add imports to test.rs
    CU-22wtyrx[IN PROGRESS]

--- frame/vamm/src/tests.rs
+++ frame/vamm/src/tests.rs
@@ -1,9 +1,28 @@
-#[allow(unused_imports)]
-
-use crate::mock::{
-    ALICE, Event, ExtBuilder, Vamm, Origin, System
+use crate::{
+	mock::{Event, ExtBuilder, Origin, System, Vamm, VammId},
+	pallet,
+	pallet::Error,
 };
-use crate::pallet;
 
-use frame_support::assert_ok;
+use composable_traits::vamm::Vamm as VammTrait;
 
+use proptest::prelude::*;
+
+use frame_support::{
+	assert_noop, assert_ok, assert_storage_noop, sp_std::collections::btree_map::BTreeMap,
+	traits::fungibles::Inspect,
+};
+
+struct VammConfigBuilder {
+	pub vamm_id: VammId,
+}
+
+// #[test]
+// fn create_vamm_emits_event() {
+// 	ExtBuilder::default().build(1).execute_with(|| {
+// 		let config = VaultConfigBuilder::default().build();
+// 		assert_ok!(Vamm::create());
+
+// 		System::assert_last_event(Event::Created(pallet::Event::Created { vamm_id: 1u64, state }));
+// 	});
+// }
