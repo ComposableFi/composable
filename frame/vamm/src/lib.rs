83b7d930813ae6dc7284cff23f8e418ade5b3e50
Matheus Cardoso <cardosaum@pm.me>
2022-04-12 11:17:20 -0300

    style(vamm): Add clippy warnings and denies

--- frame/vamm/src/lib.rs
+++ frame/vamm/src/lib.rs
@@ -61,6 +61,40 @@
 //! - [`Clearing House Pallet`](../clearing_house/index.html)
 //!
 //! <!-- Original author: @Cardosaum -->
+#![cfg_attr(
+	not(test),
+	warn(
+		clippy::disallowed_methods,
+		clippy::disallowed_types,
+		clippy::indexing_slicing,
+		clippy::todo,
+		clippy::unwrap_used,
+		clippy::panic
+	)
+)] // allow in tests
+#![warn(clippy::unseparated_literal_suffix)]
+#![cfg_attr(not(feature = "std"), no_std)]
+#![deny(
+	dead_code,
+	bad_style,
+	bare_trait_objects,
+	const_err,
+	improper_ctypes,
+	non_shorthand_field_patterns,
+	no_mangle_generic_items,
+	overflowing_literals,
+	path_statements,
+	patterns_in_fns_without_body,
+	private_in_public,
+	unconditional_recursion,
+	unused_allocation,
+	unused_comparisons,
+	unused_parens,
+	while_true,
+	trivial_casts,
+	trivial_numeric_casts,
+	unused_extern_crates
+)]
 
 #[cfg(test)]
 mod tests;
