9c6a9488e9f9c06c3a7751d26c0c562b6c7d72b1
Kevin | Composable <kevin@composable.finance>
2022-04-14 13:55:10 -0700

    Added Vault dependency and updated MockRuntime to reflect this change

--- frame/instrumental-strategy-pablo/src/lib.rs
+++ frame/instrumental-strategy-pablo/src/lib.rs
@@ -18,11 +18,21 @@ pub mod pallet {
 	use frame_support::{
 		pallet_prelude::*,
 		storage::bounded_btree_set::BoundedBTreeSet,
-		transactional
+		transactional, dispatch::DispatchResult
 	};
 
+	use sp_runtime::{
+		traits::{
+			AtLeast32BitUnsigned, CheckedAdd, CheckedMul, CheckedSub, Zero,
+		},
+	};
+
+	use composable_traits::vault::StrategicVault;
+
+	use storage::require_transaction;
+
 	use sp_std::fmt::Debug;
-	use codec::FullCodec;
+	use codec::{Codec, FullCodec};
 
 	// -------------------------------------------------------------------------------------------
 	//                                Declaration Of The Pallet Type                              
@@ -44,6 +54,31 @@ pub mod pallet {
 
 		type WeightInfo: WeightInfo;
 
+		/// The Balance type used by the pallet for bookkeeping. `Config::Convert` is used for
+		/// conversions to `u128`, which are used in the computations.
+		type Balance: Default
+			+ Parameter
+			+ Codec
+			+ MaxEncodedLen
+			+ Copy
+			+ Ord
+			+ CheckedAdd
+			+ CheckedSub
+			+ CheckedMul
+			+ AtLeast32BitUnsigned
+			+ Zero;
+
+		/// The `AssetId` used by the pallet. Corresponds to the Ids used by the Currency pallet.
+		type AssetId: FullCodec
+			+ MaxEncodedLen
+			+ Eq
+			+ PartialEq
+			+ Copy
+			+ MaybeSerializeDeserialize
+			+ Debug
+			+ Default
+			+ TypeInfo;
+
 		/// The `VaultId` used by the pallet. Corresponds to the Ids used by the Vault pallet.
 		type VaultId: FullCodec
 			+ MaxEncodedLen
@@ -56,6 +91,13 @@ pub mod pallet {
 			+ Ord
 			+ TypeInfo
 			+ Into<u128>;
+
+		type Vault: StrategicVault<
+			AssetId = Self::AssetId,
+			Balance = Self::Balance,
+			AccountId = Self::AccountId,
+			VaultId = Self::VaultId
+		> ;
 
 		/// The maximum number of vaults that can be associated with this strategy.
 		type MaxAssociatedVaults: Get<u32>;
@@ -82,6 +124,10 @@ pub mod pallet {
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
 		AssociatedVault { vault_id: T::VaultId },
+
+		RebalancedVault { vault_id: T::VaultId },
+
+		UnableToRebalanceVault { vault_id: T::VaultId },
 	}
 
 	// -------------------------------------------------------------------------------------------
@@ -126,12 +172,39 @@ pub mod pallet {
 				vaults.try_insert(*vault_id)
 					.map_err(|_| Error::<T>::TooManyAssociatedStrategies)?;
 
-				Self::deposit_event(Event::AssociatedVault { vault_id: *vault_id });
+				Self::deposit_event(Event::AssociatedVault{ vault_id: *vault_id });
 
 				Ok(*vault_id)
 			})
 		}
 		
+		pub fn rebalance() -> DispatchResult {
+			AssociatedVaults::<T>::try_mutate(|vaults| -> DispatchResult {
+				vaults.iter().for_each(|vault_id| {
+					if let Ok(_) = Self::do_rebalance(vault_id) {
+						Self::deposit_event(Event::RebalancedVault{ vault_id: *vault_id });
+					} else {
+						Self::deposit_event(Event::UnableToRebalanceVault{ vault_id: *vault_id });
+					}
+				});
+
+				Ok(())
+			})
+		}
+
+	}
+
+	// -------------------------------------------------------------------------------------------
+    //                                   Low Level Functionality                                  
+	// -------------------------------------------------------------------------------------------
+	
+	impl<T: Config> Pallet<T> {
+		
+		#[transactional]
+		fn do_rebalance(_vault_id: &T::VaultId) -> DispatchResult {
+			Ok(())
+		}
+
 	}
 }
 
