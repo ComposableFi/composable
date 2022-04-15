d5437cd3809a9086b0b132bf8e5e108f3a2ff8c9
Kevin | Composable <kevin@composable.finance>
2022-04-15 14:09:45 -0700

    Refactored the Instrumental Pallet's create trait implementation to utilize the Validate trait for its validity checks; Removed dead code CU-2td7zmr[Complete] CU-24qhwmy[COMPLETE]

--- frame/instrumental/src/validation.rs
+++ frame/instrumental/src/validation.rs
@@ -6,29 +6,6 @@ use composable_support::validation::Validate;
 use core::marker::PhantomData;
 use sp_runtime::Perquintill;
 
-// #[derive(Clone, Copy)]
-// pub struct ValidateInstrumentalVaultConfig<T> {
-// 	_marker: PhantomData<T>,
-// }
-
-// impl<T: Config> Validate<InstrumentalVaultConfig<T::AssetId, Perquintill>, ValidateInstrumentalVaultConfig<T>>
-// 	for ValidateInstrumentalVaultConfig<T>
-// {
-// 	fn validate(
-// 		input: InstrumentalVaultConfig<T::AssetId, Perquintill>,
-// 	) -> Result<InstrumentalVaultConfig<T::AssetId, Perquintill>, &'static str> {
-// 		if AssetVault::<T>::contains_key(input.asset_id) {
-//             return Err("Vault Already Exists")
-//         }
-
-//         if input.percent_deployable < Perquintill::zero() || Perquintill::one() < input.percent_deployable {
-//             return Err("Invalid Deployable Percent")
-//         }
-
-// 		Ok(input)
-// 	}
-// }
-
 // -----------------------------------------------------------------------------------------------
 //                                    ValidateVaultDoesNotExist                                   
 // -----------------------------------------------------------------------------------------------
@@ -46,30 +23,6 @@ impl<T: Config> Validate<InstrumentalVaultConfigFor<T>, ValidateVaultDoesNotExis
 	) -> Result<InstrumentalVaultConfig<T::AssetId, Perquintill>, &'static str> {
 		if AssetVault::<T>::contains_key(input.asset_id) {
             return Err("Vault Already Exists")
-        }
-
-		Ok(input)
-	}
-}
-
-// -----------------------------------------------------------------------------------------------
-//                                    ValidatePercentDeployable                                   
-// -----------------------------------------------------------------------------------------------
-
-#[derive(Clone, Copy)]
-pub struct ValidatePercentDeployable<T> {
-	_marker: PhantomData<T>,
-}
-
-impl<T: Config> Validate<InstrumentalVaultConfigFor<T>, ValidatePercentDeployable<T>>
-	for ValidatePercentDeployable<T>
-{
-	fn validate(
-		input: InstrumentalVaultConfig<T::AssetId, Perquintill>,
-	) -> Result<InstrumentalVaultConfig<T::AssetId, Perquintill>, &'static str> {
-		if input.percent_deployable < Perquintill::zero() || 
-            Perquintill::one() < input.percent_deployable {
-                return Err("Invalid Deployable Percent")
         }
 
 		Ok(input)
