9a2f2e29920e528e5c9b1a782b5d73f1bf021ea9
Andrey Orlov <aaorlov1@gmail.com>
2022-04-17 21:27:05 +0400

    Update scripts/wait_for_polkadot_launch.sh
    Signed-off-by: Andrey Orlov <aaorlov1@gmail.com>

---
+++ scripts/wait_for_polkadot_launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while IFS="" read -r string; do
+    echo $string
+    matched_string=$(echo "$string" | grep "POLKADOT LAUNCH COMPLETE")
+    if [ -z "${matched_string}" ]; then
+        true
+    else
+        exit 0
+    fi
+done < <(docker-compose logs --follow)
