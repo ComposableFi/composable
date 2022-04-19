8132efab53c9b7c1313667127063fbce7a1b8b0c
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2022-04-19 07:10:23 +0200

    fix(runtime-tests): working assets.balanceOf

--- integration-tests/runtime-tests/test/tests/assets/rpcAssetsTests.ts
+++ integration-tests/runtime-tests/test/tests/assets/rpcAssetsTests.ts
@@ -1,23 +1,28 @@
-import { CurrencyId } from '@composable/types/interfaces';
-import { AnyNumber } from '@polkadot/types-codec/types';
+import { SafeRpcWrapper } from '@composable/types/interfaces';
 import { expect } from 'chai';
 import testConfiguration from './test_configuration.json';
 
-describe('rpc.assets Tests', function () {
-    if (!testConfiguration.enabledTests.rpc.enabled)
-        return;
-    it('rpc.assets.balanceOf Tests', async function () {
-        if (!testConfiguration.enabledTests.rpc.balanceOf__success)
-            this.skip();
-        const asset_id = api.createType('CurrencyId', '123456789123456789');
-        const publicKey = walletAlice.address;
-        const result = await RpcAssetsTests.rpcAssetsTest(asset_id, publicKey);
-        expect(result).to.be.a["bignumber"].that.equals('0');
-    });
+describe('rpc.assets Tests', function() {
+  if (!testConfiguration.enabledTests.rpc.enabled)
+    return;
+  it('rpc.assets.balanceOf Tests', async function() {
+    if (!testConfiguration.enabledTests.rpc.balanceOf__success)
+        this.skip();
+    const publicKey = walletBob.address;
+    const PICA = api.createType('SafeRpcWrapper', 1) as SafeRpcWrapper;
+    const PICA_amount = await RpcAssetsTests.rpcAssetsTest(PICA, publicKey);
+    expect(parseInt(PICA_amount.toString())).to.not.equals(0);
+    const KSM = api.createType('SafeRpcWrapper', 4) as SafeRpcWrapper;
+    const KSM_amount = await RpcAssetsTests.rpcAssetsTest(KSM, publicKey);
+    expect(parseInt(KSM_amount.toString())).to.be.equals(0);
+    const kUSD = api.createType('SafeRpcWrapper', 129) as SafeRpcWrapper;
+    const kUSD_amount = await RpcAssetsTests.rpcAssetsTest(kUSD, publicKey);
+    expect(parseInt(kUSD_amount.toString())).to.be.equals(0);
+  });
 });
 
 export class RpcAssetsTests {
-    public static async rpcAssetsTest(asset_id: CurrencyId | AnyNumber, publicKey: string | Uint8Array) {
-        return await api.rpc.assets.balanceOf(asset_id, publicKey);
-    }
+  public static async rpcAssetsTest(assetId: SafeRpcWrapper, publicKey: string | Uint8Array) {
+    return await api.rpc.assets.balanceOf(assetId, publicKey);
+  }
 }
