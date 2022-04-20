c539cda5de96a5740e49a1e0878b44531eb33200
Dominik Roth <dominik@composable.finance>
2022-04-20 21:54:51 +0200

    Cleanup
    Cleanup
    
    test(Runtime Integration Tests): XCMP Tests are working!
    
    test(Runtime Integration Tests): Minor changes for easier debugging.
    
    XCMP W.I.P.
    
    test(Runtime Integration Tests): Updates from coding workshop.
    
    XCMP Test implementation
    
    test(Runtime Integration Tests): Adding verifications
    
    test(Runtime Integration Tests): Tests working now!
    
    test(Runtime Integration Tests): Fix for new test structure
    
    test(Runtime Integration Tests): Updated types.
    
    Wip. add xTokensPallet test
    
    test(Runtime Integration Tests): Fix for application not exiting due to connected ApiClient.
    
    test(Runtime Integration Tests): Fix for checking the wrong wallet.
    
    test(Runtime Integration Tests): Transaction is running & added basic verification steps.
    
    test(Runtime Integration Tests): Added missing object creation & transaction for first test.
    
    test(Runtime Integration Tests): Some cleanup.
    
    test(Runtime Integration Tests): Some cleanup.
    
    test(Runtime Integration Tests): Added template for Mosaic Tests.

--- integration-tests/runtime-tests/src/types/interfaces/pablo/types.ts
+++ integration-tests/runtime-tests/src/types/interfaces/pablo/types.ts
@@ -1,14 +1,9 @@
 // Auto-generated via `yarn polkadot-types-from-defs`, do not edit
 /* eslint-disable */
 
-import type {
-  ComposableTraitsDefiCurrencyPairCurrencyId,
-  CustomRpcBalance,
-  CustomRpcCurrencyId,
-  SafeRpcWrapper
-} from "@composable/types/interfaces/common";
-import type { Enum, Null, Struct, u128, u16 } from "@polkadot/types-codec";
-import type { AccountId32, BlockNumber, Permill } from "@polkadot/types/interfaces/runtime";
+import type { ComposableTraitsDefiCurrencyPairCurrencyId, CustomRpcBalance, CustomRpcCurrencyId, SafeRpcWrapper } from '@composable/types/interfaces/common';
+import type { Enum, Null, Struct, u128, u16 } from '@polkadot/types-codec';
+import type { AccountId32, BlockNumber, Permill } from '@polkadot/types/interfaces/runtime';
 
 /** @name ComposableTraitsDexFee */
 export interface ComposableTraitsDexFee extends Struct {
@@ -41,14 +36,14 @@ export interface PalletPabloPoolConfiguration extends Enum {
     readonly owner: AccountId32;
     readonly pair: ComposableTraitsDefiCurrencyPairCurrencyId;
     readonly sale: {
-      readonly start: BlockNumber;
-      readonly end: BlockNumber;
-      readonly initial_weight: Permill;
-      readonly final_weight: Permill;
-    } & Struct;
+    readonly start: BlockNumber;
+    readonly end: BlockNumber;
+    readonly initial_weight: Permill;
+    readonly final_weight: Permill;
+  } & Struct;
     readonly fee: Permill;
   } & Struct;
-  readonly type: "StableSwap" | "ConstantProduct" | "LiquidityBootstrapping";
+  readonly type: 'StableSwap' | 'ConstantProduct' | 'LiquidityBootstrapping';
 }
 
 /** @name PalletPabloPoolId */
@@ -71,4 +66,4 @@ export interface PalletPabloPriceCumulative extends Null {}
 /** @name PalletPabloTimeWeightedAveragePrice */
 export interface PalletPabloTimeWeightedAveragePrice extends Null {}
 
-export type PHANTOM_PABLO = "pablo";
+export type PHANTOM_PABLO = 'pablo';
