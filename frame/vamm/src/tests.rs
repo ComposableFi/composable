5ac4784074436436f503d0aa09feb04ca7bf60bc
Matheus Cardoso <cardosaum@pm.me>
2022-04-22 00:27:23 -0300

    test(vamm): Finish swap base test

--- frame/vamm/src/tests.rs
+++ frame/vamm/src/tests.rs
@@ -426,22 +426,34 @@ proptest! {
 	#![proptest_config(ProptestConfig::with_cases(RUN_CASES))]
 	#[test]
 	fn swap_base_remove_insufficient_funds_error(
-		vamm_state in get_vamm_state(Default::default()),
-		swap_config in get_swap_config(
+		mut vamm_state in get_vamm_state(Default::default()),
+		mut swap_config in get_swap_config(
 			TestSwapConfig {
 				direction: Some(Direction::Remove),
 				vamm_id: Some(0),
-				input_amount: Some(balance_range_lower_half()),
+				asset: Some(AssetType::Base),
 				..Default::default()}),
+		input_amount in balance_range_upper_half(),
+		base_asset_reserves in balance_range_lower_half(),
 	) {
-		prop_assume!(swap_config.input_amount <= vamm_state.base_asset_reserves);
+		prop_assume!(input_amount > base_asset_reserves);
+		prop_assume!(swap_config.direction == Direction::Remove);
 
 		ExtBuilder {
 			vamm_count: 1,
 			vamms: vec![(0, vamm_state)]
 		}.build().execute_with(|| {
+			swap_config.input_amount = input_amount;
+			vamm_state.base_asset_reserves = base_asset_reserves;
+			VammMap::<MockRuntime>::mutate(0, |vamm| {
+				*vamm = Some(vamm_state);
+			});
+
 			let swap = Vamm::swap(&swap_config);
-			assert_err!(swap, Error::<MockRuntime>::VammDoesNotExist);
+			assert_err!(swap, Error::<MockRuntime>::InsufficientFundsForTrade);
+		})
+	}
+}
 		})
 	}
 }
