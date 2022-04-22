524b19bdbe933a1725b2fd24fd2bc2da494df5ad
Matheus Cardoso <cardosaum@pm.me>
2022-04-22 00:28:15 -0300

    test(vamm): Add more tests for swap base and quote

--- frame/vamm/src/tests.rs
+++ frame/vamm/src/tests.rs
@@ -454,6 +454,137 @@ proptest! {
 		})
 	}
 }
+
+proptest! {
+	#![proptest_config(ProptestConfig::with_cases(RUN_CASES))]
+	#[test]
+	fn swap_base_remove_succeeds(
+		mut vamm_state in get_vamm_state(Default::default()),
+		mut swap_config in get_swap_config(
+			TestSwapConfig {
+				direction: Some(Direction::Remove),
+				vamm_id: Some(0),
+				asset: Some(AssetType::Base),
+				..Default::default()}),
+		input_amount in balance_range_lower_half(),
+		base_asset_reserves in balance_range_upper_half(),
+	) {
+		prop_assume!(input_amount <= base_asset_reserves);
+		prop_assume!(swap_config.direction == Direction::Remove);
+
+		ExtBuilder {
+			vamm_count: 1,
+			vamms: vec![(0, vamm_state)]
+		}.build().execute_with(|| {
+			swap_config.input_amount = input_amount;
+			vamm_state.base_asset_reserves = base_asset_reserves;
+			VammMap::<MockRuntime>::mutate(0, |vamm| {
+				*vamm = Some(vamm_state);
+			});
+
+			let swap = Vamm::swap(&swap_config);
+			assert_ok!(swap);
+		})
+	}
+}
+
+proptest! {
+	#![proptest_config(ProptestConfig::with_cases(RUN_CASES))]
+	#[test]
+	fn swap_base_add_trade_extrapoles_maximum_supported_amount_error(
+		mut vamm_state in get_vamm_state(Default::default()),
+		mut swap_config in get_swap_config(
+			TestSwapConfig {
+				direction: Some(Direction::Add),
+				vamm_id: Some(0),
+				asset: Some(AssetType::Base),
+				..Default::default()}),
+		input_amount in balance_range_upper_half(),
+		base_asset_reserves in balance_range_upper_half(),
+	) {
+		prop_assume!(swap_config.direction == Direction::Add);
+
+		ExtBuilder {
+			vamm_count: 1,
+			vamms: vec![(0, vamm_state)]
+		}.build().execute_with(|| {
+			swap_config.input_amount = input_amount;
+			vamm_state.base_asset_reserves = base_asset_reserves;
+			VammMap::<MockRuntime>::mutate(0, |vamm| {
+				*vamm = Some(vamm_state);
+			});
+
+			let swap = Vamm::swap(&swap_config);
+			assert_err!(swap, Error::<MockRuntime>::TradeExtrapolatesMaximumSupportedAmount);
+		})
+	}
+}
+
+// ----------------------------------------------------------------------------------------------------
+//                                             Swap Quote Asset
+// ----------------------------------------------------------------------------------------------------
+
+proptest! {
+	#![proptest_config(ProptestConfig::with_cases(RUN_CASES))]
+	#[test]
+	fn swap_quote_remove_insufficient_funds_error(
+		mut vamm_state in get_vamm_state(Default::default()),
+		mut swap_config in get_swap_config(
+			TestSwapConfig {
+				direction: Some(Direction::Remove),
+				vamm_id: Some(0),
+				asset: Some(AssetType::Quote),
+				..Default::default()}),
+		input_amount in balance_range_upper_half(),
+		quote_asset_reserves in balance_range_lower_half(),
+	) {
+		prop_assume!(input_amount > quote_asset_reserves);
+		prop_assume!(swap_config.direction == Direction::Remove);
+
+		ExtBuilder {
+			vamm_count: 1,
+			vamms: vec![(0, vamm_state)]
+		}.build().execute_with(|| {
+			swap_config.input_amount = input_amount;
+			vamm_state.quote_asset_reserves = quote_asset_reserves;
+			VammMap::<MockRuntime>::mutate(0, |vamm| {
+				*vamm = Some(vamm_state);
+			});
+
+			let swap = Vamm::swap(&swap_config);
+			assert_err!(swap, Error::<MockRuntime>::InsufficientFundsForTrade);
+		})
+	}
+}
+
+proptest! {
+	#![proptest_config(ProptestConfig::with_cases(RUN_CASES))]
+	#[test]
+	fn swap_quote_add_trade_extrapoles_maximum_supported_amount_error(
+		mut vamm_state in get_vamm_state(Default::default()),
+		mut swap_config in get_swap_config(
+			TestSwapConfig {
+				direction: Some(Direction::Add),
+				vamm_id: Some(0),
+				asset: Some(AssetType::Quote),
+				..Default::default()}),
+		input_amount in balance_range_upper_half(),
+		quote_asset_reserves in balance_range_upper_half(),
+	) {
+		prop_assume!(swap_config.direction == Direction::Add);
+
+		ExtBuilder {
+			vamm_count: 1,
+			vamms: vec![(0, vamm_state)]
+		}.build().execute_with(|| {
+			swap_config.input_amount = input_amount;
+			vamm_state.quote_asset_reserves = quote_asset_reserves;
+			VammMap::<MockRuntime>::mutate(0, |vamm| {
+				*vamm = Some(vamm_state);
+			});
+
+			let swap = Vamm::swap(&swap_config);
+			assert_err!(swap, Error::<MockRuntime>::TradeExtrapolatesMaximumSupportedAmount);
 		})
 	}
 }
