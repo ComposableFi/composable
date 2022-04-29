3a56d1ab9c30731b2fc74c1b79229463895208f9
Yasin <yasin132@gmail.com>
2022-04-29 14:11:23 +0300

    initial commit for tests
    initial commit for the tests
    
    renamed to Pablo
    
    added cpDex
    
    CPDex and StableSwapDex tests added.
    
    Added LBP Tests
    
    code refactoring
    
    added spotPricePRC in stableswapTests
    
    tags removed
    
    types added
    
    SwapFailure added.
    
    test updated
    
    More test cases added.
    
    Code cleaned

--- integration-tests/runtime-tests/src/types/interfaces/common/definitions.ts
+++ integration-tests/runtime-tests/src/types/interfaces/common/definitions.ts
@@ -12,6 +12,6 @@ export default {
       quote: "CurrencyId"
     },
     ComposableTraitsXcmCumulusMethodId: "Null",
-    ComposableTraitsXcmXcmSellRequest: "Null"
+    ComposableTraitsXcmXcmSellRequest: "Null",
   },
 };
