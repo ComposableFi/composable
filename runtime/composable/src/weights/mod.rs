d2c3845f77c164ff980802c1738baeea2a76de15
“r2st” <sumaninster11@gmail.com>
2022-05-02 07:45:36 -0400

    weights fixed

---
+++ runtime/composable/src/weights/mod.rs
@@ -0,0 +1,15 @@
+#![allow(clippy::unnecessary_cast)]
+pub mod balances;
+pub mod collator_selection;
+pub mod collective;
+pub mod democracy;
+pub mod frame_system;
+pub mod indices;
+pub mod membership;
+// pub mod scheduler;
+pub mod currency_factory;
+pub mod session;
+pub mod timestamp;
+pub mod tokens;
+pub mod treasury;
+pub mod utility;
---
+++ runtime/picasso/src/weights/tokens.rs
@@ -0,0 +1,39 @@
+#![allow(unused_parens)]
+#![allow(unused_imports)]
+#![allow(clippy::unnecessary_cast)]
+
+use frame_support::{
+	traits::Get,
+	weights::{constants::RocksDbWeight, Weight},
+};
+use sp_std::marker::PhantomData;
+
+/// Default weights.
+pub struct WeightInfo<T>(PhantomData<T>);
+impl<T: frame_system::Config> orml_tokens::WeightInfo for WeightInfo<T> {
+	fn transfer() -> Weight {
+		(69_000_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(5 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(4 as Weight))
+	}
+	fn transfer_all() -> Weight {
+		(69_000_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(5 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(4 as Weight))
+	}
+	fn transfer_keep_alive() -> Weight {
+		(38_000_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(3 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(3 as Weight))
+	}
+	fn force_transfer() -> Weight {
+		(45_000_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(4 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(3 as Weight))
+	}
+	fn set_balance() -> Weight {
+		(34_000_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(3 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(3 as Weight))
+	}
+}
