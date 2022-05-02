37e4762005d98a8197ad2413c426dab3f7b3eec3
Ângelo Lovatto <12701614+0xangelo@users.noreply.github.com>
2022-05-02 18:09:08 -0300

    test(CH): expect state changes on funding update

--- frame/clearing-house/src/tests/update_funding.rs
+++ frame/clearing-house/src/tests/update_funding.rs
@@ -3,8 +3,8 @@ use crate::{
 	mock::{
 		accounts::ALICE,
 		runtime::{
-			ExtBuilder, MarketId, Origin, Runtime, System as SystemPallet, TestPallet,
-			Timestamp as TimestampPallet, MINIMUM_PERIOD_SECONDS,
+			ExtBuilder, MarketId, Oracle as OraclePallet, Origin, Runtime, System as SystemPallet,
+			TestPallet, Timestamp as TimestampPallet, Vamm as VammPallet, MINIMUM_PERIOD_SECONDS,
 		},
 	},
 	tests::run_to_block,
@@ -16,6 +16,7 @@ use composable_traits::{
 };
 use frame_support::{assert_noop, assert_ok, pallet_prelude::Hooks};
 use proptest::prelude::*;
+use sp_runtime::FixedI128;
 
 fn run_for_seconds(seconds: DurationSeconds) {
 	if SystemPallet::block_number() > 0 {
@@ -78,4 +79,41 @@ proptest! {
 			assert_ok!(TestPallet::update_funding(Origin::signed(ALICE), market_id));
 		})
 	}
+}
+
+// TODO(0xangelo): what to expect if a lot of time has passed since the last update?
+
+#[test]
+fn updates_market_state() {
+	let funding_frequency = ONE_HOUR;
+
+	ExtBuilder::default().build().execute_with(|| {
+		run_to_block(1);
+		// Set oracle and vamm TWAPs
+		OraclePallet::set_twap(Some(10_000)); // 100 in cents
+		VammPallet::set_twap(Some(100.into()));
+
+		let mut config = valid_market_config();
+		config.funding_frequency = funding_frequency;
+		let market_id = <TestPallet as ClearingHouse>::create_market(&config).unwrap();
+		let old_market = TestPallet::get_market(&market_id).unwrap();
+
+		run_for_seconds(ONE_HOUR);
+		// Set new oracle and vamm TWAPs
+		OraclePallet::set_twap(Some(10_000)); // 100 in cents
+		VammPallet::set_twap(Some(120.into()));
+
+		assert_ok!(TestPallet::update_funding(Origin::signed(ALICE), market_id));
+
+		let new_market = TestPallet::get_market(&market_id).unwrap();
+		assert_eq!(new_market.funding_rate_ts, old_market.funding_rate_ts + ONE_HOUR);
+		assert_eq!(
+			new_market.cum_funding_rate,
+			old_market.cum_funding_rate +
+				FixedI128::from(20) *
+					FixedI128::from((old_market.funding_frequency, old_market.funding_period))
+		);
+
+		// TODO(0xangelo): expect event
+	})
 }
