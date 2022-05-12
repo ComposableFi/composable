880457a4d7ddb08c6a051115a3cd0b2c5346a4e4
Dominik Roth <dominik@composable.finance>
2022-05-12 19:38:16 +0200

    test(Runtime Integration Tests): All test calls implemented

--- integration-tests/runtime-tests/test/tests/assets/txAssetsTests.ts
+++ integration-tests/runtime-tests/test/tests/assets/txAssetsTests.ts
@@ -49,6 +49,7 @@ describe.only("tx.assets Tests", function() {
 
   before("Providing funds for tests", async function() {
     this.timeout(5 * 60 * 1000);
+    await mintAssetsToWallet(api, sudoKey, sudoKey, [1]);
     await mintAssetsToWallet(api, walletBob, sudoKey, [1, 4]);
   });
 
@@ -65,16 +66,17 @@ describe.only("tx.assets Tests", function() {
 
     // it(name, function) describes a single test.
     it("A wallet can `transfer` KSM to another wallet", async function() {
-      const paraAsset = api.createType('u128', 4);
+      this.timeout(2 * 60 * 1000);
+      const paraAsset = api.createType("u128", 4);
       const paraDest = walletBob.derive("/tests/assets/transferTestReceiverWallet1").publicKey;
-      const paraAmount = api.createType('Balance', 100000000000);
-      const paraKeepAlive = api.createType('bool', false);
+      const paraAmount = api.createType("Balance", 100000000000);
+      const paraKeepAlive = api.createType("bool", true);
 
-      const {data: [result]} = await sendAndWaitForSuccess(
+      const { data: [result] } = await sendAndWaitForSuccess(
         api,
         walletBob,
-        api.events.balances.transfer.is,
-        api.tx.assets.transfer(paraAsset, paraDest, paraAsset, paraKeepAlive)
+        api.events.balances.Deposit.is,
+        api.tx.assets.transfer(paraAsset, paraDest, paraAmount, paraKeepAlive)
       );
 
       console.debug(result);
@@ -90,7 +92,19 @@ describe.only("tx.assets Tests", function() {
 
     // it(name, function) describes a single test.
     it("A wallet can `transfer` native asset PICA to another wallet", async function() {
-      // ToDo
+      this.timeout(2 * 60 * 1000);
+      const paraDest = walletBob.derive("/tests/assets/transferTestReceiverWallet1").publicKey;
+      const paraAmount = api.createType("Balance", 100000000000);
+      const paraKeepAlive = api.createType("bool", true);
+
+      const { data: [result] } = await sendAndWaitForSuccess(
+        api,
+        walletBob,
+        api.events.balances.Deposit.is,
+        api.tx.assets.transferNative(paraDest, paraAmount, paraKeepAlive)
+      );
+
+      console.debug(result);
     });
   });
 
@@ -102,13 +116,35 @@ describe.only("tx.assets Tests", function() {
     if (!testConfiguration.enabledTests.tx.transfer__success) return;
 
     // it(name, function) describes a single test.
-    it("A wallet can `transfer` KSM to another wallet", async function() {
-      // ToDo
+    it("A *sudo* wallet can `forceTransfer` KSM to another wallet", async function() {
+      this.timeout(2 * 60 * 1000);
+      const paraAsset = api.createType("u128", 4);
+      const paraSource = walletBob.publicKey;
+      const paraDest = walletBob.derive("/tests/assets/transferTestReceiverWallet1").publicKey;
+      const paraAmount = api.createType("Balance", 100000000000);
+      const paraKeepAlive = api.createType("bool", true);
+
+      const { data: [result] } = await sendAndWaitForSuccess(
+        api,
+        sudoKey,
+        api.events.sudo.Sudid.is,
+        api.tx.sudo.sudo(
+          api.tx.assets.forceTransfer(
+            paraAsset,
+            paraSource,
+            paraDest,
+            paraAmount,
+            paraKeepAlive
+          )
+        )
+      );
+
+      console.debug(result);
     });
   });
 
   /**
-   * The `force_transfer` extrinsic transfers the blockchains native asset (PICA) from `origin` to `dest`
+   * The `force_transfer_native` extrinsic transfers the blockchains native asset (PICA) from `origin` to `dest`
    * with sudo privileges.
    */
   describe("tx.assets.transfer Tests", function() {
@@ -116,8 +152,28 @@ describe.only("tx.assets Tests", function() {
     if (!testConfiguration.enabledTests.tx.transfer__success) return;
 
     // it(name, function) describes a single test.
-    it("A wallet can `transfer` KSM to another wallet", async function() {
-      // ToDo
+    it("A *sudo* wallet can `force_transfer_native` token to another wallet", async function() {
+      this.timeout(2 * 60 * 1000);
+      const paraSource = walletBob.publicKey;
+      const paraDest = walletBob.derive("/tests/assets/transferTestReceiverWallet1").publicKey;
+      const paraAmount = api.createType("Balance", 100000000000);
+      const paraKeepAlive = api.createType("bool", true);
+
+      const { data: [result] } = await sendAndWaitForSuccess(
+        api,
+        sudoKey,
+        api.events.sudo.Sudid.is,
+        api.tx.sudo.sudo(
+          api.tx.assets.forceTransferNative(
+            paraSource,
+            paraDest,
+            paraAmount,
+            paraKeepAlive
+          )
+        )
+      );
+
+      console.debug(result);
     });
   });
 
@@ -129,8 +185,20 @@ describe.only("tx.assets Tests", function() {
     if (!testConfiguration.enabledTests.tx.transfer__success) return;
 
     // it(name, function) describes a single test.
-    it("A wallet can `transfer` KSM to another wallet", async function() {
-      // ToDo
+    it("A wallet can `transfer_all` remaining KSM to another wallet", async function() {
+      this.timeout(2 * 60 * 1000);
+      const paraAsset = api.createType("u128", 4);
+      const paraDest = walletBob.derive("/tests/assets/transferTestReceiverWallet1").publicKey;
+      const paraKeepAlive = api.createType("bool", false);
+
+      const { data: [result] } = await sendAndWaitForSuccess(
+        api,
+        walletBob,
+        api.events.balances.Deposit.is,
+        api.tx.assets.transferAll(paraAsset, paraDest, paraKeepAlive)
+      );
+
+      console.debug(result);
     });
   });
 
@@ -143,8 +211,19 @@ describe.only("tx.assets Tests", function() {
     if (!testConfiguration.enabledTests.tx.transfer__success) return;
 
     // it(name, function) describes a single test.
-    it("A wallet can `transfer` KSM to another wallet", async function() {
-      // ToDo
+    it("A wallet can `transfer_all_native` tokens to another wallet", async function() {
+      this.timeout(2 * 60 * 1000);
+      const paraDest = walletBob.derive("/tests/assets/transferTestReceiverWallet1").publicKey;
+      const paraKeepAlive = api.createType("bool", false);
+
+      const { data: [result] } = await sendAndWaitForSuccess(
+        api,
+        walletBob,
+        api.events.balances.Deposit.is,
+        api.tx.assets.transferAllNative(paraDest, paraKeepAlive)
+      );
+
+      console.debug(result);
     });
   });
 
@@ -156,8 +235,21 @@ describe.only("tx.assets Tests", function() {
     if (!testConfiguration.enabledTests.tx.transfer__success) return;
 
     // it(name, function) describes a single test.
-    it("A wallet can `transfer` KSM to another wallet", async function() {
-      // ToDo
+    it("A *sudo* wallet can `mint_initialize` a new asset to another wallet", async function() {
+      this.timeout(2 * 60 * 1000);
+      const paraAmount = api.createType("u128", 100000000000);
+      const paraDest = walletBob.derive("/tests/assets/transferTestReceiverWallet1").publicKey;
+
+      const { data: [result] } = await sendAndWaitForSuccess(
+        api,
+        sudoKey,
+        api.events.sudo.Sudid.is,
+        api.tx.sudo.sudo(
+          api.tx.assets.mintInitialize(paraAmount, paraDest)
+        )
+      );
+
+      console.debug(result);
     });
   });
 
@@ -172,8 +264,22 @@ describe.only("tx.assets Tests", function() {
     if (!testConfiguration.enabledTests.tx.transfer__success) return;
 
     // it(name, function) describes a single test.
-    it("A wallet can `transfer` KSM to another wallet", async function() {
-      // ToDo
+    it("A *sudo* wallet can `mint_initialize_with_governance` a new asset to another wallet", async function() {
+      this.timeout(2 * 60 * 1000);
+      const paraAmount = api.createType("u128", 100000000000);
+      const paraGovernanceOrigin = walletBob.derive("/tests/assets/transferTestReceiverWallet1").publicKey;
+      const paraDest = walletBob.derive("/tests/assets/transferTestReceiverWallet1").publicKey;
+
+      const { data: [result] } = await sendAndWaitForSuccess(
+        api,
+        sudoKey,
+        api.events.sudo.Sudid.is,
+        api.tx.sudo.sudo(
+          api.tx.assets.mintInitializeWithGovernance(paraAmount, paraGovernanceOrigin, paraDest)
+        )
+      );
+
+      console.debug(result);
     });
   });
 
@@ -185,21 +291,49 @@ describe.only("tx.assets Tests", function() {
     if (!testConfiguration.enabledTests.tx.transfer__success) return;
 
     // it(name, function) describes a single test.
-    it("A wallet can `transfer` KSM to another wallet", async function() {
-      // ToDo
+    it("A *sudo* wallet can `mintInto` KSM to another wallet", async function() {
+      this.timeout(2 * 60 * 1000);
+      const paraAsset = api.createType("u128", 4);
+      const paraAmount = api.createType("u128", 4);
+      const paraDest = walletBob.derive("/tests/assets/transferTestReceiverWallet1").publicKey;
+
+      const { data: [result] } = await sendAndWaitForSuccess(
+        api,
+        sudoKey,
+        api.events.sudo.Sudid.is,
+        api.tx.sudo.sudo(
+          api.tx.assets.mintInto(paraAsset, paraDest, paraAmount)
+        )
+      );
+
+      console.debug(result);
     });
   });
 
   /**
-   * The `mint_into` extrinsic burms `amount` of `asset_id` of `dest` wallet.
+   * The `burn_from` extrinsic burns `amount` of `asset_id` of `dest` wallet.
    */
   describe("tx.assets.burn_from Tests", function() {
     // Check if group of tests are enabled.
     if (!testConfiguration.enabledTests.tx.transfer__success) return;
 
     // it(name, function) describes a single test.
-    it("A wallet can `transfer` KSM to another wallet", async function() {
-      // ToDo
+    it("A *sudo* wallet can `burn_from` KSM to another wallet", async function() {
+      this.timeout(2 * 60 * 1000);
+      const paraAsset = api.createType("u128", 4);
+      const paraAmount = api.createType("u128", 4);
+      const paraDest = walletBob.derive("/tests/assets/transferTestReceiverWallet1").publicKey;
+
+      const { data: [result] } = await sendAndWaitForSuccess(
+        api,
+        sudoKey,
+        api.events.sudo.Sudid.is,
+        api.tx.sudo.sudo(
+          api.tx.assets.burnFrom(paraAsset, paraDest, paraAmount)
+        )
+      );
+
+      console.debug(result);
     });
   });
 });
