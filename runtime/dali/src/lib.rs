97740e297bd22824edf6cfce8c01e7d361942aa8
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2022-05-22 17:15:58 +0200

    chore(upgrade): replace deprecated `match_type` with `match_types`

--- runtime/dali/src/lib.rs
+++ runtime/dali/src/lib.rs
@@ -53,7 +53,7 @@ use sp_version::RuntimeVersion;
 
 // A few exports that help ease life for downstream crates.
 pub use frame_support::{
-	construct_runtime, match_type, parameter_types,
+	construct_runtime, parameter_types,
 	traits::{Contains, Everything, KeyOwnerProofSystem, Nothing, Randomness, StorageInfo},
 	weights::{
 		constants::{BlockExecutionWeight, ExtrinsicBaseWeight, RocksDbWeight, WEIGHT_PER_SECOND},
