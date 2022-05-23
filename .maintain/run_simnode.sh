8c4a19cf13dc07a13a8bed4715bdc9ed5ed12b6e
Adedayo Akinpelu <adedayoakinpelu@gmail.com>
2022-05-23 10:14:27 +0100

    remove hostname from script (#1040)
    * remove hostname from script

--- .maintain/run_simnode.sh
+++ .maintain/run_simnode.sh
@@ -23,9 +23,8 @@ YDATE=$(date -d yesterday +'%m-%d-%Y')
 
 run_simnode() {
   CHAIN="$1"
-  echo "Running simnode for $CHAIN"
-  HOST=$(echo $HOSTNAME)
-  FILENAME=$HOST-$YDATE.zip
+  echo "Running simnode for $CHAIN "
+  FILENAME=$YDATE.zip
   GS_BUCKET="$CHAIN-data-store"
   sudo gsutil cp gs://$GS_BUCKET/"$FILENAME" .
   sudo unzip -o "$FILENAME" -d /tmp/db
