e7eac548f0d537e0bf12af2a18fc04c0c61885d1
Matheus Cardoso <cardosaum@pm.me>
2022-05-27 11:16:57 -0300

    test(vamm): Add quote asset tests in `get_twap`
    CU-24ah0vw
    CU-2mdqe2j
    CU-2mdqe2z
    CU-2mdqe3v

--- frame/vamm/src/tests/get_twap.rs
+++ frame/vamm/src/tests/get_twap.rs
@@ -65,4 +65,61 @@ proptest! {
 			);
 		})
 	}
+
+	// ----------------------------------------------------------------------------------------------------
+	//                              TWAP - Quote Asset
+	// ----------------------------------------------------------------------------------------------------
+	#[test]
+	fn get_twap_quote_asset_succeeds(
+		vamm_state in get_vamm_state(Default::default())
+	) {
+		ExtBuilder {
+			vamm_count: 1,
+			vamms: vec![(0, vamm_state)]
+		}.build().execute_with(|| {
+			assert_ok!(TestPallet::get_twap(&0, AssetType::Quote));
+			assert_storage_noop!(TestPallet::get_twap(&0, AssetType::Quote));
+		})
+	}
+
+	#[test]
+	fn get_twap_quote_asset_fails_if_vamm_does_not_exists(
+		vamm_state in get_vamm_state(Default::default()),
+		vamm_id in 1..=VammId::MAX
+	) {
+		ExtBuilder {
+			vamm_count: 1,
+			vamms: vec![(0, vamm_state)]
+		}.build().execute_with(|| {
+			assert_noop!(
+				TestPallet::get_twap(&vamm_id, AssetType::Quote),
+				Error::<MockRuntime>::VammDoesNotExist
+			);
+		})
+	}
+
+	#[test]
+	fn get_twap_quote_asset_fails_if_vamm_is_closed(
+		mut vamm_state in get_vamm_state(Default::default()),
+		closed in (0..18446744073709551_u64).prop_map(Some),
+	) {
+		// Make sure the market will be closed.
+		vamm_state.closed = closed;
+
+		ExtBuilder {
+			vamm_count: 1,
+			vamms: vec![(0, vamm_state)]
+		}.build().execute_with(|| {
+			// In order to check if vamm is closed or not we must simulate time
+			// passing.
+			run_for_seconds(
+				match vamm_state.closed {Some(t) => t+1, _ => 1}
+			);
+
+			assert_noop!(
+				TestPallet::get_twap(&0, AssetType::Quote),
+				Error::<MockRuntime>::VammIsClosed
+			);
+		})
+	}
 }
