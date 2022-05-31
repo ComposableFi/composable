2877592f2b60edf25f4317ea8b3042e46de34d82
Vim Wickramasinghe <vimukthi@composable.finance>
2022-05-31 17:34:54 +0200

    Regen

--- rfcs/adoc-o-md.sh
+++ rfcs/adoc-o-md.sh
@@ -1,3 +1,4 @@
 INPUT_ADOC=0004-pablo-distribution.adoc
+# asciidoctor -r asciidoctor-diagram -r asciidoctor-mathematical --backend html --out-file - $INPUT_ADOC | \
 asciidoctor -r asciidoctor-diagram --backend html --out-file - $INPUT_ADOC | \
 pandoc --from html --to markdown_strict --output $INPUT_ADOC.md
