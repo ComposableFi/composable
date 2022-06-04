e4bb24a10f2f127b2f52cbcb9381cb934e2c44b7
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-06-04 19:47:51 +0500

    Some more tests.

--- frontend/ui-pablo/app/store/hooks/usePoolTvlChart.ts
+++ frontend/ui-pablo/app/store/hooks/usePoolTvlChart.ts
@@ -6,131 +6,83 @@ import moment from "moment";
 import { useState, useEffect } from "react";
 import { usePoolDetails } from "./usePoolDetails";
 
-const HOUR = 1 * 60 * 60 * 1000;
-const DAY = 24 * HOUR;
-
-function aggragateData(
-  data: [number, number][],
+function getIntervalRangeLimit(
+  timestamp: number,
+  rangeLimit: "start" | "end",
   chartInterval: "24h" | "1w" | "1m"
-): [number, number][] {
-  const processed: [number, number][] = [];
-  if (!data.length) return processed;
-
+): number {
   switch (chartInterval) {
     case "24h":
-      const yesterday = data[0][0] - DAY;
-
-      const toAggregate = data.filter((i: [number, number]) => {
-        return i[0] > yesterday;
-      });
-
-      let startTs = moment(data[0][0]).startOf("h").valueOf();
-
-      while (startTs > yesterday) {
-        let dataPointStart = startTs;
-        const dataPointEnd = startTs + HOUR;
-
-        let maxTvlForThisPoint = toAggregate.reduce((p, c) => {
-          if (c[0] > dataPointStart - HOUR && c[0] < dataPointStart) {
-            return c[1];
-          }
-          return p;
-        }, 0);
-
-        let thisPointTvl = 0;
-        toAggregate.forEach((item) => {
-          if (
-            item[0] > dataPointStart &&
-            item[0] < dataPointEnd &&
-            item[1] > thisPointTvl
-          ) {
-            thisPointTvl = item[1];
-          }
-        });
-
-        if (thisPointTvl !== 0) {
-          maxTvlForThisPoint = thisPointTvl;
-        }
-
-        processed.push([dataPointStart, maxTvlForThisPoint]);
-        startTs = startTs - HOUR;
-      }
-
-      return processed;
+      return rangeLimit == "start"
+        ? moment(timestamp).startOf("h").valueOf()
+        : moment(timestamp).endOf("h").valueOf();
     case "1w":
-    case "1m":
-      let key = chartInterval == "1m" ? "month" : "week";
-      let startOf = moment(data[0][0])
-        .startOf(key as any)
-        .valueOf();
-
-      while (startOf > data[data.length - 1][0]) {
-        const endOf = moment(startOf)
-          .endOf(key as any)
-          .valueOf();
-
-        let lastStartOf =
-          chartInterval === "1w"
-            ? startOf - 7 * DAY
-            : (startOf = moment(startOf - HOUR)
-                .startOf(key as any)
-                .valueOf());
-
-        let maxTvlForThisPoint = data.reduce((p, c) => {
-          if (c[0] > lastStartOf && c[0] < startOf) {
-            return c[1];
-          }
-          return p;
-        }, 0);
-
-        let thisPointTvl = 0;
-        data.forEach((item) => {
-          if (item[0] > startOf && item[0] < endOf && item[1] > thisPointTvl) {
-            thisPointTvl = item[1];
-          }
-        });
-
-        if (thisPointTvl !== 0) {
-          maxTvlForThisPoint = thisPointTvl;
-        }
-
-        processed.push([startOf, maxTvlForThisPoint]);
-
-        if (chartInterval === "1w") {
-          startOf = startOf - 7 * DAY;
-        } else {
-          startOf = moment(startOf - HOUR)
-            .startOf(key as any)
-            .valueOf();
-        }
-      }
+      return rangeLimit == "start"
+        ? moment(timestamp).startOf("week").valueOf()
+        : moment(timestamp).endOf("week").valueOf();
+    case "1w":
+      return rangeLimit == "start"
+        ? moment(timestamp).startOf("month").valueOf()
+        : moment(timestamp).endOf("month").valueOf();
     default:
-      return processed;
+      return timestamp;
   }
 }
 
-function createIntervals (series: [number, number][], chartInterval: ChartInterval): string[] {
+function processTvlChartSeries(
+  data: [number, number][],
+  chartInterval: "24h" | "1w" | "1m"
+): [number, number][] {
+  if (!data.length) return data;
+
+  let rangeStart: Set<number> | Array<number> = new Set(
+    data.map((i) => getIntervalRangeLimit(i[0], "start", chartInterval))
+  );
+  let rangeEnd: Set<number> | Array<number> = new Set(
+    data.map((i) => getIntervalRangeLimit(i[0], "end", chartInterval))
+  );
+
+  rangeStart = Array.from(rangeStart).sort((a, b) => {
+    return b - a;
+  });
+  rangeEnd = Array.from(rangeStart).sort((a, b) => {
+    return b - a;
+  });
+
+  const intervals = rangeStart.reduce((p, c, i) => {
+    p.push([c, (rangeEnd as number[])[i]])
+    return p;
+  }, [] as [number, number][])
+
+  console.log(intervals)
+
+  return [];
+}
+
+function createIntervals(
+  series: [number, number][],
+  chartInterval: ChartInterval
+): string[] {
   let intervals: string[] = [];
   if (!series.length) return intervals;
 
-  switch(chartInterval) {
+  switch (chartInterval) {
     case "24h":
       for (let i = 0; i < series.length; i += 6) {
         if (series[i]) {
-          intervals.push(moment(series[i][0]).format("hh:mm"))
+          intervals.push(moment(series[i][0]).format("hh:mm"));
         }
       }
-      return intervals
+      return intervals;
     default:
-      return intervals
+      return intervals;
   }
-
 }
 
 export const usePoolTvlChart = (poolId: number) => {
   const poolDetails = usePoolDetails(poolId);
 
-  const [seriesIntervals, setSeriesIntervals] = useState<string[]>([])
+  const [seriesIntervals, setSeriesIntervals] = useState<string[]>([]);
   const [chartSeries, setChartSeries] = useState<[number, number][]>([]);
   const [selectedInterval, setSelectedInterval] = useState(
     DEFI_CONFIG.swapChartIntervals[0]
@@ -150,9 +102,14 @@ export const usePoolTvlChart = (poolId: number) => {
             ];
           });
 
-          const series = aggragateData(data, selectedInterval.symbol as any);
+          const series = processTvlChartSeries(
+            data,
+            selectedInterval.symbol as any
+          );
           setChartSeries(series);
-          setSeriesIntervals(createIntervals(series, selectedInterval.symbol as any))
+          setSeriesIntervals(
+            createIntervals(series, selectedInterval.symbol as any)
+          );
         })
         .catch((err) => {
           console.log("Error fetching chart data", err.message);
@@ -167,6 +124,6 @@ export const usePoolTvlChart = (poolId: number) => {
     selectedInterval,
     setSelectedInterval,
     chartSeries,
-    seriesIntervals
+    seriesIntervals,
   };
 };
