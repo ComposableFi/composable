21e114f40f97426e9cabfe3f9a9c29d834512af8
BELOUS Mikhail A <mikhail.belous@raiffeisen.ru>
2022-06-08 16:46:38 +0300

    add first whole MVP of rebalance() function

--- frame/instrumental-strategy-pablo/src/lib.rs
+++ frame/instrumental-strategy-pablo/src/lib.rs
@@ -13,18 +13,23 @@ pub mod pallet {
 	// -------------------------------------------------------------------------------------------
 	//                                   Imports and Dependencies
 	// -------------------------------------------------------------------------------------------
-	use pablo::Pools;
 	use codec::{Codec, FullCodec};
+	use composable_support::math::safe::SafeArithmetic;
 	use composable_traits::{
-		instrumental::InstrumentalProtocolStrategy, 
-		vault::{StrategicVault, Vault},
-		dex::Amm};
+		dex::Amm,
+		instrumental::InstrumentalProtocolStrategy,
+		vault::{FundsAvailability, StrategicVault, Vault},
+	};
 	use frame_support::{
-		dispatch::DispatchResult, pallet_prelude::*, storage::bounded_btree_set::BoundedBTreeSet,
-		transactional, PalletId,
-	}; 
+		dispatch::{DispatchError, DispatchResult},
+		pallet_prelude::*,
+		storage::bounded_btree_set::BoundedBTreeSet,
+		transactional, Blake2_128Concat, PalletId,
+		traits::{fungibles::{Inspect, Mutate, MutateHold, Transfer},
+		},
+	};
 	use sp_runtime::traits::{
-		AccountIdConversion, AtLeast32BitUnsigned, CheckedAdd, CheckedMul, CheckedSub, Zero,
+		AccountIdConversion, AtLeast32BitUnsigned, CheckedAdd, CheckedMul, CheckedSub, One, Zero,
 	};
 	use sp_std::fmt::Debug;
 
@@ -96,6 +101,11 @@ pub mod pallet {
 			VaultId = Self::VaultId,
 		>;
 
+		/// Currency is used for the assets managed by the vaults.
+		type Currency: Transfer<Self::AccountId, Balance = Self::Balance, AssetId = Self::AssetId>
+			+ Mutate<Self::AccountId, Balance = Self::Balance, AssetId = Self::AssetId>
+			+ MutateHold<Self::AccountId, Balance = Self::Balance, AssetId = Self::AssetId>;
+
 		type Pablo: Amm<
 			AssetId = Self::AssetId,
 			Balance = Self::Balance,
@@ -129,8 +139,6 @@ pub mod pallet {
 		type PalletId: Get<PalletId>;
 	}
 
-	type PICA: AssetId; // DELETE(belousm) just for testing MVP
-
 	// -------------------------------------------------------------------------------------------
 	//                                         Pallet Types
 	// -------------------------------------------------------------------------------------------
@@ -145,14 +153,11 @@ pub mod pallet {
 		StorageValue<_, BoundedBTreeSet<T::VaultId, T::MaxAssociatedVaults>, ValueQuery>;
 
 	// TODO(belousm): where pools will be added?
+	// An asset whitelisted by Instrumental
+	// The corresponding Pool to invest the whitelisted asset into
 	#[pallet::storage]
 	#[pallet::getter(fn pools)]
-	pub type InstrumentalPools<T: Config> = StorageMap<_, 
-		Blake2_128Concat,
-		T::AssetId, // An asset whitelisted by Instrumental
-		T::PoolId   // The corresponding Pool to invest the whitelisted asset into
-	>;
-
+	pub type Pools<T: Config> = StorageMap<_, Blake2_128Concat, T::AssetId, T::PoolId>;
 
 	// -------------------------------------------------------------------------------------------
 	//                                        Runtime Events
@@ -177,6 +182,10 @@ pub mod pallet {
 		VaultAlreadyAssociated,
 
 		TooManyAssociatedStrategies,
+
+		// TODO(belousm): only for MVP version we can assume the `pool_id` is already known and
+		// exist. We should remove it in V1.
+		PoolNotFound,
 	}
 
 	// -------------------------------------------------------------------------------------------
@@ -246,55 +255,80 @@ pub mod pallet {
 
 	impl<T: Config> Pallet<T> {
 		#[transactional]
-		fn do_rebalance(_vault_id: &T::VaultId) -> DispatchResult {
-			let pool_id: Option<T::PoolId, _> = Self::get_pool(T::Vault::asset_id(vault_id).unwrap());
+		fn do_rebalance(vault_id: &T::VaultId) -> DispatchResult {
+			let asset_id = T::Vault::asset_id(vault_id)?;
+			let account_id = T::Vault::account_id(vault_id);
+			let pool_id = Self::pools(asset_id).ok_or_else(|| Error::<T>::PoolNotFound)?;
 			let task = T::Vault::available_funds(vault_id, &Self::account_id())?;
 			let action = match task {
 				FundsAvailability::Withdrawable(balance) => {
 					let vault_account = T::Vault::account_id(vault_id);
-					let lp_token_amount = amount_of_lp_token_for_added_liquidity(pool_id, T::Balance.set_zero(), balance);
-					T::Pablo::add_liquidity(&vault_account,
-											pool_id,
-											T::Balance.set_zero(),
-											balance,
-											lp_token_amount,
-											true);	
-
+					let lp_token_amount = T::Pablo::amount_of_lp_token_for_added_liquidity(
+						pool_id,
+						T::Balance::zero(),
+						balance,
+					)?;
+					T::Pablo::add_liquidity(
+						&vault_account,
+						pool_id,
+						T::Balance::zero(),
+						balance,
+						lp_token_amount,
+						bool::default(),
+					)?;
 				},
 				FundsAvailability::Depositable(balance) => {
 					let vault_account = T::Vault::account_id(vault_id);
-					let lp_token_amount = amount_of_lp_token_for_added_liquidity(pool_id, T::Balance.set_zero(), balance);
-					T::Pablo::remove_liquidity(&vault_account,
-											pool_id,
-											T::Balance.set_zero(),
-											balance,
-											lp_token_amount);	
-
+					let lp_token_amount = T::Pablo::amount_of_lp_token_for_added_liquidity(
+						pool_id,
+						T::Balance::zero(),
+						balance,
+					)?;
+					T::Pablo::remove_liquidity(
+						&vault_account,
+						pool_id,
+						lp_token_amount,
+						T::Balance::zero(),
+						balance,
+					)?;
 				},
 				FundsAvailability::MustLiquidate => {
-					// TODO(belousm): should we transfer all assets to Vault from strategy?
-					todo!();
+					let vault_account = T::Vault::account_id(vault_id);
+					let lp_token_id = T::Pablo::lp_token(pool_id)?;
+					let balance_of_lp_token = T::Currency::balance(lp_token_id, &account_id);
+					let balance_of_asset = T::Currency::balance(asset_id, &account_id);
+					T::Pablo::remove_liquidity(
+						&vault_account,
+						pool_id,
+						balance_of_lp_token,
+						T::Balance::zero(),
+						balance_of_asset,
+					)?;
+				}
+				FundsAvailability::Equilibrable => {
+					();
 				},
 			};
 			Ok(())
 		}
 
-		fn get_pool(asset_id: T::AssetId) -> Option<T::PoolId, _> {
-			match InstrumentalPools::<T>::get(asset_id).from_query_to_optional_value() {
-				Some(pool_id) => {pool_id},
-				None => {
-					Pools::<T>::iter_keys().for_each(|pool_id| {
-						if T::Pablo::currency_pair(pool_id).unwrap().quote == asset_id 
-						&& T::Pablo::currency_pair(pool_id).unwrap().base == PICA {
-							InstrumentalPools::<T>::insert(asset_id, pool_id);
-							pool_id
-						}
-					});
-					todo!(); // TODO(belousm): return other strategy pool
-				}
-			}
-
-		}
+		// #[transactional]
+		// fn get_pool(asset_id: &T::AssetId) -> Result<T::PoolId, DispatchError> {
+		// 	let pica: T::AssetId;
+		// 	match InstrumentalPools::<T>::get(asset_id) {
+		// 		Some(pool_id) => Ok(pool_id),
+		// 		None => {
+		// 			Pools::<T>::iter_keys().for_each(|pool_id| {
+		// 				if T::Pablo::currency_pair(pool_id)?.quote == *asset_id &&
+		// 					T::Pablo::currency_pair(pool_id)?.base == pica {
+		// 					InstrumentalPools::<T>::insert(asset_id, pool_id);
+		// 					return Ok(pool_id)
+		// 				}
+		// 			});
+		// 			todo!(); // TODO(belousm): return other strategy pool
+		// 		},
+		// 	}
+		// }
 	}
 }
 
