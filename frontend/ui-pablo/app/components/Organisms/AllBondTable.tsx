e17d095b0344dee4a587676e649fca9e39136ddb
Saad <48211799+saadjhk@users.noreply.github.com>
2022-06-20 17:16:35 +0500

    Update frontend/ui-pablo/app/components/Organisms/AllBondTable.tsx
    Co-authored-by: Karel L. Kubat <k.l.kubat@gmail.com>
    
    Signed-off-by: Saad <48211799+saadjhk@users.noreply.github.com>

--- frontend/ui-pablo/app/components/Organisms/AllBondTable.tsx
+++ frontend/ui-pablo/app/components/Organisms/AllBondTable.tsx
@@ -54,7 +54,7 @@ export const AllBondTable: React.FC = () => {
 
   useEffect(() => {
     dispatch(addNextDataBondPools({ startIndex }));
-    // only called once
+    // Only called once.
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
