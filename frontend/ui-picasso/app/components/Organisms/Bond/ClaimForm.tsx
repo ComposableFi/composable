26f083fa009f04e8d9e6a4597dee59be88ec1b0c
Mamali <isthereanybody@gmail.com>
2022-06-22 17:29:38 +0430

    Add useClaim hook

--- frontend/ui-picasso/app/components/Organisms/Bond/ClaimForm.tsx
+++ frontend/ui-picasso/app/components/Organisms/Bond/ClaimForm.tsx
@@ -1,23 +1,18 @@
 import { alpha, useTheme } from "@mui/material/styles";
 import { Box, Button, InputAdornment, Stack, Typography } from "@mui/material";
 import { BigNumberInput, TokenAsset } from "@/components";
-import {
-  useBlockInterval,
-  usePicassoProvider,
-  useSelectedAccount,
-} from "@/defi/polkadot/hooks";
+import { usePicassoProvider, useSelectedAccount } from "@/defi/polkadot/hooks";
 import { useOpenPositions } from "@/defi/polkadot/hooks/useOpenPositions";
 import { useAppSelector } from "@/hooks/store";
 import PositionDetailsRow from "@/components/Atom/PositionDetailsRow";
-import { claim, fromPica, getROI } from "@/defi/polkadot/pallets/BondedFinance";
+import { claim, getROI } from "@/defi/polkadot/pallets/BondedFinance";
 import BigNumber from "bignumber.js";
 import router from "next/router";
 import { ActiveBond } from "@/stores/defi/polkadot/bonds/slice";
-import { getClaimable } from "@/components/Organisms/Bond/utils";
-import { useCurrentBlockAndTime } from "@/defi/polkadot/utils";
 import { PairAsset } from "@/components/Atom/PairAsset";
 import { useExecutor } from "substrate-react";
-import { humanBalance, humanDate, SHORT_HUMAN_DATE } from "@/utils/formatters";
+import { humanBalance } from "@/utils/formatters";
+import { useClaim } from "@/stores/defi/polkadot/bonds/useClaim";
 
 function findCurrentBond(b: ActiveBond, bond: string): boolean {
   console.log(b);
@@ -28,11 +23,12 @@ export const ClaimForm = () => {
   const theme = useTheme();
   const account = useSelectedAccount();
   const { parachainApi } = usePicassoProvider();
-  const { block } = useCurrentBlockAndTime(parachainApi);
   useOpenPositions(account);
   const executor = useExecutor();
-  const interval = useBlockInterval();
   const { bond } = router.query;
+  const { claimable, vestingTime, vestedTime, pending } = useClaim(
+    bond?.toString() ?? ""
+  );
   const openBonds = useAppSelector<ActiveBond[]>(
     (state) => state.bonding.openPositions
   );
@@ -60,36 +56,7 @@ export const ClaimForm = () => {
       }
     );
   };
-  const { perPeriod, periodCount, window } = activeBond;
-  const lastBlock = window.blockNumberBased.start
-    .plus(window.blockNumberBased.period)
-    .multipliedBy(periodCount);
-  const claimable = getClaimable(
-    block,
-    window,
-    perPeriod,
-    lastBlock,
-    periodCount
-  );
 
-  const total = periodCount.multipliedBy(fromPica(perPeriod));
-
-  const pending = total.minus(claimable);
-  const remainingBlocks = lastBlock.minus(block).lte(0)
-    ? new BigNumber(0)
-    : lastBlock.minus(block);
-  const vesting_time = humanDate(
-    remainingBlocks.multipliedBy(Number(interval) / 1000).toNumber()
-  );
-
-  const sourceBlock = block.gt(lastBlock) ? lastBlock : block;
-  const vested_time = humanDate(
-    sourceBlock
-      .minus(window.blockNumberBased.start)
-      .multipliedBy(Number(interval) / 1000)
-      .toNumber(),
-    SHORT_HUMAN_DATE
-  );
   return (
     <Box
       sx={{
@@ -154,9 +121,9 @@ export const ClaimForm = () => {
         />
         <PositionDetailsRow
           label="Time until fully vested"
-          description={`${vesting_time}`}
+          description={`${vestingTime}`}
         />
-        <PositionDetailsRow label="Vested" description={vested_time} />
+        <PositionDetailsRow label="Vested" description={vestedTime} />
         <PositionDetailsRow
           label="ROI"
           description={`${getROI(
