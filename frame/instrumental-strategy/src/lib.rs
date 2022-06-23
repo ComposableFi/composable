9bd36db16bdd28b605307023b345e70d0d689f84
saruman9 <rum.274.4@gmail.com>
2022-06-23 16:24:28 +0800

    refactor(instrumental-strategy): add clippy's rules and fix it

--- frame/instrumental-strategy/src/lib.rs
+++ frame/instrumental-strategy/src/lib.rs
@@ -1,9 +1,25 @@
-#[cfg(test)]
-mod tests;
+#![cfg_attr(not(feature = "std"), no_std)]
+#![warn(
+	clippy::indexing_slicing,
+	clippy::panic,
+	clippy::todo,
+	clippy::unseparated_literal_suffix,
+	clippy::unwrap_used
+)]
+#![cfg_attr(
+	test,
+	allow(
+		clippy::disallowed_methods,
+		clippy::disallowed_types,
+		clippy::panic,
+		clippy::unwrap_used,
+	)
+)]
 
 #[cfg(test)]
 mod mock;
-
+#[cfg(test)]
+mod tests;
 mod weights;
 
 pub use pallet::*;
@@ -139,6 +155,7 @@ pub mod pallet {
 
 	#[pallet::storage]
 	#[pallet::getter(fn associated_vaults)]
+	#[allow(clippy::disallowed_types)]
 	pub type AssociatedVaults<T: Config> =
 		StorageValue<_, BoundedBTreeSet<T::VaultId, T::MaxAssociatedVaults>, ValueQuery>;
 
