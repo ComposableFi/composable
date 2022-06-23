b4c8cbcbf987c3d6e949d5cb5d6137185d22ea9c
saruman9 <rum.274.4@gmail.com>
2022-06-23 21:48:52 +0800

    test(instrumental-strategy-pablo): use `ConstantProduct` instead `StableSwap` for default pool

--- frame/instrumental-strategy-pablo/src/mock/helpers.rs
+++ frame/instrumental-strategy-pablo/src/mock/helpers.rs
@@ -12,22 +12,22 @@ pub fn create_usdt_usdc_pool() -> PoolId {
 	let usdc_amount = 1_000_000_000 * CurrencyId::unit::<Balance>();
 	let assets = CurrencyPair::new(CurrencyId::USDT, CurrencyId::USDC);
 	let amounts = vec![usdt_amount, usdc_amount];
-	create_pool(assets, amounts, 100, Permill::zero())
+	create_pool(assets, amounts, Permill::zero(), Permill::from_percent(50))
 }
 
 fn create_pool(
 	assets: CurrencyPair<CurrencyId>,
 	// TODO(saruman9): add amount to a pools
 	_amounts: Vec<Balance>,
-	amplification_coefficient: u16,
 	fee: Permill,
+	base_weight: Permill,
 ) -> PoolId {
-	let config = PoolInitConfiguration::<AccountId, CurrencyId, BlockNumber>::StableSwap {
+	let config = PoolInitConfiguration::<AccountId, CurrencyId, BlockNumber>::ConstantProduct {
 		// TODO(saruman9): create users
 		owner: 1,
 		pair: assets,
-		amplification_coefficient,
 		fee,
+		base_weight,
 	};
 	let pool_id = Pablo::do_create_pool(config);
 	assert_ok!(pool_id);
