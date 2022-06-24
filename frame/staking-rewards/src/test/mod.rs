8a8e63fc20e6d1fc8aa1b0bcd77f76c11cd00659
Vim Wickramasinghe <vimukthi@composable.finance>
2022-06-24 12:25:30 +0200

    [cu-313atr5] test

--- frame/staking-rewards/src/test/mod.rs
+++ frame/staking-rewards/src/test/mod.rs
@@ -1,5 +1,5 @@
 use sp_std::collections::btree_map::BTreeMap;
-use frame_support::{assert_noop, assert_ok, BoundedBTreeMap};
+use frame_support::{assert_err, assert_noop, assert_ok, BoundedBTreeMap};
 use frame_system::EventRecord;
 use sp_arithmetic::Perbill;
 use composable_tests_helpers::test::currency::{CurrencyId, PICA, USDT};
@@ -21,7 +21,7 @@ mod benchmarking;
 fn test_create_reward_pool() {
     new_test_ext().execute_with(|| {
         System::set_block_number(1);
-        let pool_init_config = get_default_reward_pool();
+        let mut pool_init_config = get_default_reward_pool();
         assert_ok!(StakingRewards::create_reward_pool(Origin::root(), pool_init_config));
 
         assert_last_event::<Test, _>(|e| {
@@ -29,29 +29,55 @@ fn test_create_reward_pool() {
             Event::StakingRewards(crate::Event::RewardPoolCreated { owner, pool_id, .. })
             if owner == ALICE && pool_id == 1)
         });
+
+        // invalid end block
+        assert_err!(StakingRewards::create_reward_pool(
+            Origin::root(), get_reward_pool_config_invalida_end_block()
+        ), crate::Error::<Test>::InvalidEndBlock);
+
     });
 }
 
 fn get_default_reward_pool() -> RewardPoolConfiguration<
     AccountId, CurrencyId, Balance, BlockNumber, BoundedBTreeMap<u64, Perbill, MaxStakingDurationPresets>> {
-    let mut duration_presets = BTreeMap::new();
-    duration_presets.insert(ONE_HOUR, Perbill::from_percent(1));
-    duration_presets.insert(ONE_MINUTE, Perbill::from_rational(1_u32, 10_u32));
     let pool_init_config = RewardRateBasedIncentive {
         owner: ALICE,
         asset_id: PICA::ID,
         end_block: 5,
-        initial_reward_config: RewardConfig {
-            asset_id: USDT::ID,
-            max_rewards: 100_u128,
-            reward_rate: Perbill::from_percent(10),
-        },
-        lock: LockConfig {
-            duration_presets: BoundedBTreeMap::try_from(duration_presets).unwrap(),
-            unlock_penalty: Perbill::from_percent(5),
-        }
+        initial_reward_config: default_reward_config(),
+        lock: default_lock_config()
     };
     pool_init_config
+}
+
+fn get_reward_pool_config_invalida_end_block() -> RewardPoolConfiguration<
+    AccountId, CurrencyId, Balance, BlockNumber, BoundedBTreeMap<u64, Perbill, MaxStakingDurationPresets>> {
+    let pool_init_config = RewardRateBasedIncentive {
+        owner: ALICE,
+        asset_id: PICA::ID,
+        end_block: 0,
+        initial_reward_config: default_reward_config(),
+        lock: default_lock_config()
+    };
+    pool_init_config
+}
+
+fn default_lock_config() -> LockConfig<BoundedBTreeMap<DurationSeconds, Perbill, MaxStakingDurationPresets>> {
+    let mut duration_presets = BTreeMap::new();
+    duration_presets.insert(ONE_HOUR, Perbill::from_percent(1));
+    duration_presets.insert(ONE_MINUTE, Perbill::from_rational(1_u32, 10_u32));
+    LockConfig {
+        duration_presets: BoundedBTreeMap::try_from(duration_presets).unwrap(),
+        unlock_penalty: Perbill::from_percent(5),
+    }
+}
+
+fn default_reward_config() -> RewardConfig<u128, u128> {
+    RewardConfig {
+        asset_id: USDT::ID,
+        max_rewards: 100_u128,
+        reward_rate: Perbill::from_percent(10),
+    }
 }
 
 pub fn assert_has_event<T, F>(matcher: F)
