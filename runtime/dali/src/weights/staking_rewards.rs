9150dc1ec2bab3a0cbb552341b70fde90e94b74a
Vim Wickramasinghe <vimukthi@composable.finance>
2022-06-28 14:42:23 +0200

    remove file

--- runtime/dali/src/weights/staking_rewards.rs
+++
@@ -1,9 +0,0 @@
-use frame_support::dispatch::Weight;
-use sp_std::marker::PhantomData;
-
-pub struct WeightInfo<T>(PhantomData<T>);
-impl<T: frame_system::Config> pallet_staking_rewards::weights::WeightInfo for WeightInfo<T> {
-	fn create_reward_pool() -> Weight {
-		10_000
-	}
-}
