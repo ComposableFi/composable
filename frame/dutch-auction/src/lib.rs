526153fd589dbf4aac7b3628c08b9ab793843cee
mikolaichuk <mihail@mikolaichuk.com>
2022-06-28 16:26:44 +0600

    Fixed HAL-02

--- frame/dutch-auction/src/lib.rs
+++ frame/dutch-auction/src/lib.rs
@@ -76,6 +76,7 @@ mod mock;
 mod prelude;
 mod support;
 mod types;
+mod validation;
 pub mod weights;
 
 #[frame_support::pallet]
@@ -84,13 +85,13 @@ pub mod pallet {
 	use crate::{prelude::*, types::*};
 	use xcm::latest::{prelude::*, MultiAsset, WeightLimit::Unlimited};
 
-	use crate::{math::*, support::DefiMultiReservableCurrency};
+	use crate::{math::*, support::DefiMultiReservableCurrency, validation::XcmSellRequestValid};
 	use composable_support::{
 		abstractions::{
 			nonce::{Increment, Nonce},
 			utils::{increment::WrappingIncrement, start_at::ZeroInit},
 		},
-		math::wrapping_next::WrappingNext,
+		math::wrapping_next::WrappingNext, validation::Validate,
 	};
 	use composable_traits::{
 		defi::{DeFiComposableConfig, DeFiEngine, OrderIdLike, Sell, SellEngine, Take},
@@ -343,6 +344,8 @@ pub mod pallet {
 			request: XcmSellRequest,
 		) -> DispatchResultWithPostInfo {
 			// TODO: make events/logs from all failed liqudations
+
+            let request = XcmSellRequestValid::validate(request)?;
 
 			// incoming message is generic in representations, so need to map it back to local,
 			let parachain_id = ensure_sibling_para(<T as Config>::XcmOrigin::from(origin))?;
