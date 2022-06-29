1aaa5017c5688d77a5b34cede9b6e6c3fd8db5d0
saruman9 <rum.274.4@gmail.com>
2022-06-29 18:42:21 +0800

    refactor(instrumental-strategy-pablo): move tests in separate modules

--- frame/instrumental-strategy-pablo/src/tests.rs
+++ frame/instrumental-strategy-pablo/src/tests.rs
@@ -20,75 +20,85 @@ use crate::{pallet, pallet::Error};
 //                                          Associate Vault
 // -------------------------------------------------------------------------------------------------
 
-#[test]
-fn add_an_associated_vault() {
-	ExtBuilder::default().build().execute_with(|| {
-		let vault_id: VaultId = 1;
+#[cfg(test)]
+mod associate_vault {
+	use super::*;
 
-		assert_ok!(PabloStrategy::associate_vault(&vault_id));
-	});
-}
+	#[test]
+	fn add_an_associated_vault() {
+		ExtBuilder::default().build().execute_with(|| {
+			let vault_id: VaultId = 1;
 
-#[test]
-fn adding_an_associated_vault_twice_throws_an_error() {
-	ExtBuilder::default().build().execute_with(|| {
-		let vault_id: VaultId = 1;
+			assert_ok!(PabloStrategy::associate_vault(&vault_id));
+		});
+	}
 
-		assert_ok!(PabloStrategy::associate_vault(&vault_id));
-		assert_noop!(
-			PabloStrategy::associate_vault(&vault_id),
-			Error::<MockRuntime>::VaultAlreadyAssociated
-		);
-	});
-}
+	#[test]
+	fn adding_an_associated_vault_twice_throws_an_error() {
+		ExtBuilder::default().build().execute_with(|| {
+			let vault_id: VaultId = 1;
 
-#[test]
-fn associating_too_many_vaults_throws_an_error() {
-	ExtBuilder::default().build().execute_with(|| {
-		for vault_id in 0..MAX_ASSOCIATED_VAULTS {
-			assert_ok!(PabloStrategy::associate_vault(&(vault_id as VaultId)));
-		}
+			assert_ok!(PabloStrategy::associate_vault(&vault_id));
+			assert_noop!(
+				PabloStrategy::associate_vault(&vault_id),
+				Error::<MockRuntime>::VaultAlreadyAssociated
+			);
+		});
+	}
 
-		let vault_id = MAX_ASSOCIATED_VAULTS as VaultId;
-		assert_noop!(
-			PabloStrategy::associate_vault(&vault_id),
-			Error::<MockRuntime>::TooManyAssociatedStrategies
-		);
-	});
+	#[test]
+	fn associating_too_many_vaults_throws_an_error() {
+		ExtBuilder::default().build().execute_with(|| {
+			for vault_id in 0..MAX_ASSOCIATED_VAULTS {
+				assert_ok!(PabloStrategy::associate_vault(&(vault_id as VaultId)));
+			}
+
+			let vault_id = MAX_ASSOCIATED_VAULTS as VaultId;
+			assert_noop!(
+				PabloStrategy::associate_vault(&vault_id),
+				Error::<MockRuntime>::TooManyAssociatedStrategies
+			);
+		});
+	}
 }
 
 // -------------------------------------------------------------------------------------------------
 //                                             Rebalance
 // -------------------------------------------------------------------------------------------------
 
-#[test]
-fn rebalance_emits_event() {
-	ExtBuilder::default().build().execute_with(|| {
-		System::set_block_number(1);
+#[cfg(test)]
+mod rebalance {
+	use super::*;
 
-		let base_asset = CurrencyId::LAYR;
-		let quote_asset = CurrencyId::CROWD_LOAN;
-		let amount = 1_000_000_000 * CurrencyId::unit::<Balance>();
-		let amounts = vec![amount; 2];
+	#[test]
+	fn rebalance_emits_event() {
+		ExtBuilder::default().build().execute_with(|| {
+			System::set_block_number(1);
 
-		// Create Vault (LAYR)
-		let vault_id = create_vault(base_asset, None);
+			let base_asset = CurrencyId::LAYR;
+			let quote_asset = CurrencyId::CROWD_LOAN;
+			let amount = 1_000_000_000 * CurrencyId::unit::<Balance>();
+			let amounts = vec![amount; 2];
 
-		// Create Pool (LAYR/CROWD_LOAN)
-		let pool_id = create_pool_with(CurrencyPair::new(base_asset, quote_asset), amounts);
-		pallet::AdminAccountIds::<MockRuntime>::insert(ADMIN, AccessRights::Full);
-		assert_ok!(PabloStrategy::set_pool_id_for_asset(
-			Origin::signed(ADMIN),
-			base_asset,
-			pool_id
-		));
+			// Create Vault (LAYR)
+			let vault_id = create_vault(base_asset, None);
 
-		assert_ok!(PabloStrategy::associate_vault(&vault_id));
+			// Create Pool (LAYR/CROWD_LOAN)
+			let pool_id = create_pool_with(CurrencyPair::new(base_asset, quote_asset), amounts);
+			pallet::AdminAccountIds::<MockRuntime>::insert(ADMIN, AccessRights::Full);
+			assert_ok!(PabloStrategy::set_pool_id_for_asset(
+				Origin::signed(ADMIN),
+				base_asset,
+				pool_id
+			));
 
-		assert_ok!(PabloStrategy::rebalance());
+			assert_ok!(PabloStrategy::associate_vault(&vault_id));
 
-		System::assert_last_event(Event::PabloStrategy(pallet::Event::RebalancedVault {
-			vault_id,
-		}));
-	});
+			assert_ok!(PabloStrategy::rebalance());
+
+			System::assert_last_event(Event::PabloStrategy(pallet::Event::RebalancedVault {
+				vault_id,
+			}));
+		});
+	}
 }
