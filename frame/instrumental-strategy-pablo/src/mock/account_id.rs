8f40039fabe4522eea464f402a2b075c3ca94607
saruman9 <rum.274.4@gmail.com>
2022-06-29 14:44:35 +0800

    refactor(instrumental-strategy-pablo): omit explicit changing of number of assets for tests

--- frame/instrumental-strategy-pablo/src/mock/account_id.rs
+++ frame/instrumental-strategy-pablo/src/mock/account_id.rs
@@ -1,29 +1,23 @@
 use hex_literal::hex;
+use proptest::strategy::Just;
 use sp_core::sr25519::{Public, Signature};
 use sp_runtime::traits::{IdentifyAccount, Verify};
 
 pub type AccountId = <<Signature as Verify>::Signer as IdentifyAccount>::AccountId;
 
-pub static ADMIN: Public =
+pub const ADMIN: AccountId =
 	Public(hex!("0000000000000000000000000000000000000000000000000000000000000000"));
-pub static ALICE: Public =
+pub const ALICE: AccountId =
 	Public(hex!("0000000000000000000000000000000000000000000000000000000000000001"));
-pub static BOB: Public =
+pub const BOB: AccountId =
 	Public(hex!("0000000000000000000000000000000000000000000000000000000000000002"));
-pub static CHARLIE: Public =
+pub const CHARLIE: AccountId =
 	Public(hex!("0000000000000000000000000000000000000000000000000000000000000003"));
-pub static DAVE: Public =
+pub const DAVE: AccountId =
 	Public(hex!("0000000000000000000000000000000000000000000000000000000000000004"));
-pub static EVEN: Public =
+pub const EVEN: AccountId =
 	Public(hex!("0000000000000000000000000000000000000000000000000000000000000005"));
 
-use proptest::{
-	prop_oneof,
-	strategy::{Just, Strategy},
-};
-
-// TODO(saruman9): will be used in the future
-#[allow(dead_code)]
-pub fn pick_account() -> impl Strategy<Value = AccountId> {
-	prop_oneof![Just(ALICE), Just(BOB), Just(CHARLIE), Just(DAVE), Just(EVEN),]
+pub const fn accounts() -> [Just<AccountId>; 5] {
+	[Just(ALICE), Just(BOB), Just(CHARLIE), Just(DAVE), Just(EVEN)]
 }
