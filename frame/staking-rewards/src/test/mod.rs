2bcb15187b07f1167690c3f1a0afc585c23110b6
Vivek Pandya <vivekvpandya@gmail.com>
2022-07-05 14:04:25 +0530

    Implement split() and add simple test

--- frame/staking-rewards/src/test/mod.rs
+++ frame/staking-rewards/src/test/mod.rs
@@ -1,20 +1,24 @@
 use crate::{
 	test::{prelude::H256, runtime::*},
-	Config,
+	Config, StakeCount, Stakes
 };
 use composable_tests_helpers::test::currency::{PICA, USDT};
 use composable_traits::{
 	staking::{
-		lock::LockConfig, RewardConfig, RewardPoolConfiguration,
-		RewardPoolConfiguration::RewardRateBasedIncentive,
+		lock::{Lock, LockConfig}, RewardConfig, RewardPoolConfiguration,
+		RewardPoolConfiguration::RewardRateBasedIncentive, Rewards,
+        Staking, Stake, 
 	},
 	time::{DurationSeconds, ONE_HOUR, ONE_MINUTE},
 };
+use composable_support::abstractions::utils::increment::Increment;
 use frame_support::{assert_err, assert_ok, BoundedBTreeMap};
 use frame_system::EventRecord;
-use sp_arithmetic::Perbill;
+use sp_arithmetic::{Perbill, Permill};
 use sp_core::sr25519::Public;
+use sp_runtime::PerThing;
 use sp_std::collections::btree_map::BTreeMap;
+use composable_tests_helpers::test::currency::CurrencyId;
 
 mod prelude;
 mod runtime;
@@ -40,6 +44,44 @@ fn test_create_reward_pool() {
 			),
 			crate::Error::<Test>::EndBlockMustBeInTheFuture
 		);
+	});
+}
+
+#[test]
+fn test_split_postion() {
+	new_test_ext().execute_with(|| {
+		System::set_block_number(1);
+		let pool_init_config = get_default_reward_pool();
+		assert_ok!(StakingRewards::create_reward_pool(Origin::root(), pool_init_config));
+        let new_position = StakeCount::<Test>::increment();
+        assert_ok!(new_position);
+        let stake = Stake::<RewardPoolId, Balance, Rewards<CurrencyId, Balance, MaxRewardConfigsPerPool>> {
+            reward_pool_id: 1,
+            stake: 1000_000_000_000_000_u128,
+            share: 1000_000_000_000_000_u128,
+            reductions: Rewards::<_,_,_>::new(),
+            lock: Lock {
+                started_at: 10000_u64,
+                duration: 10000000_u64,
+                unlock_penalty: Perbill::from_percent(2)
+            } 
+        };
+        Stakes::<Test>::insert(1, stake.clone());
+        let ratio =  Permill::from_rational(1_u32,7_u32);
+        let left_from_one_ratio = ratio.left_from_one();
+        let split = <StakingRewards as Staking>::split(&ALICE, &1_u128, ratio);
+        assert_ok!(split);
+        let stake1 = Stakes::<Test>::get(1);
+        let stake2 = Stakes::<Test>::get(2);
+        assert!(stake1.is_some());
+        assert!(stake2.is_some());
+        let stake1 = stake1.unwrap();
+        let stake2 = stake2.unwrap();
+        // validate stake and share as per ratio
+        assert_eq!(stake1.stake, ratio.mul_floor(stake.stake));
+        assert_eq!(stake1.share, ratio.mul_floor(stake.share));
+        assert_eq!(stake2.stake, left_from_one_ratio.mul_floor(stake.stake));
+        assert_eq!(stake2.share, left_from_one_ratio.mul_floor(stake.share));
 	});
 }
 
