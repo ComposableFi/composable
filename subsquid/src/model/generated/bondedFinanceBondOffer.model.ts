eb75c54bb8887603b7e1a6deb8e76dc579b2e86c
Andrés Mechali <andresmechali@gmail.com>
2022-07-06 13:40:50 +0000

    Integrate bonded-finance total purchased bonds into subsquid (#1273)
    ## Issue
    https://app.clickup.com/t/2ecu5r4
    
    ## Description
    Keeps count of total purchased bonds
    
    
    
    Co-authored-by: Andrés Mechali <andrem@composable.finance>

---
+++ subsquid/src/model/generated/bondedFinanceBondOffer.model.ts
@@ -0,0 +1,24 @@
+import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_} from "typeorm"
+import * as marshal from "./marshal"
+
+@Entity_()
+export class BondedFinanceBondOffer {
+  constructor(props?: Partial<BondedFinanceBondOffer>) {
+    Object.assign(this, props)
+  }
+
+  @PrimaryColumn_()
+  id!: string
+
+  /**
+   * Total amount of purchased bonds
+   */
+  @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
+  totalPurchased!: bigint
+
+  /**
+   * Beneficiary account for the bond offer
+   */
+  @Column_("text", {nullable: false})
+  beneficiary!: string
+}
