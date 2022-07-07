a6fd0319929a8432ccb4aee7abb0ad4a3df8ed9a
Andrey Orlov <aaorlov1@gmail.com>
2022-07-07 11:09:58 +0400

    Update tests

--- runtime/dali/src/lib.rs
+++ runtime/dali/src/lib.rs
@@ -878,6 +878,7 @@ impl pallet_staking_rewards::Config for Runtime {
 	type PositionId = u128;
 	type AssetId = CurrencyId;
 	type CurrencyFactory = CurrencyFactory;
+	type Assets = Assets;
 	type UnixTime = Timestamp;
 	type ReleaseRewardsPoolsBatchSize = frame_support::traits::ConstU8<13>;
 	type PalletId = StakingRewardsPalletId;
