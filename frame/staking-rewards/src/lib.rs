355176e816405f25420008ff7cbdf6bcdd958b92
Andrey Orlov <aaorlov1@gmail.com>
2022-07-07 14:04:28 +0400

    Update tests

--- frame/staking-rewards/src/lib.rs
+++ frame/staking-rewards/src/lib.rs
@@ -62,7 +62,7 @@ pub mod pallet {
 	use frame_support::{
 		pallet_prelude::*,
 		traits::{
-			fungibles::{Inspect, Mutate, Transfer},
+			fungibles::{Inspect, Transfer},
 			tokens::WithdrawConsequence,
 			UnixTime,
 		},
@@ -161,9 +161,7 @@ pub mod pallet {
 		type CurrencyFactory: CurrencyFactory<Self::AssetId, Self::Balance>;
 
 		/// Dependency allowing this pallet to transfer funds from one account to another.
-		type Assets: Transfer<AccountIdOf<Self>, Balance = BalanceOf<Self>, AssetId = AssetIdOf<Self>>
-			+ Mutate<AccountIdOf<Self>, Balance = BalanceOf<Self>, AssetId = AssetIdOf<Self>>
-			+ Inspect<AccountIdOf<Self>, Balance = BalanceOf<Self>, AssetId = AssetIdOf<Self>>;
+		type Assets: Transfer<AccountIdOf<Self>, Balance = BalanceOf<Self>, AssetId = AssetIdOf<Self>>;
 
 		/// is used for rate based rewarding and position lock timing
 		type UnixTime: UnixTime;
