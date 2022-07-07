6ce255396b04324a7823af87200d91ca47f356fe
Connor Davis <17688291+PoisonPhang@users.noreply.github.com>
2022-07-07 09:35:01 -0500

    Corrected staking rewards weights

--- runtime/dali/src/weights/pallet_staking_rewards.rs
+++ runtime/dali/src/weights/pallet_staking_rewards.rs
@@ -30,7 +30,8 @@ use sp_std::marker::PhantomData;
 
 /// Weight functions for `pallet_staking_rewards`.
 pub struct WeightInfo<T>(PhantomData<T>);
-impl<T: frame_system::Config> pallet_staking_rewards::WeightInfo for WeightInfo<T> {
+impl<T: frame_system::Config> pallet_staking_rewards::weights::WeightInfo for WeightInfo<T> {
+	// Storage: unknown [0x3a7472616e73616374696f6e5f6c6576656c3a] (r:1 w:1)
 	// Storage: StakingRewards RewardPoolCount (r:1 w:1)
 	// Storage: StakingRewards RewardPools (r:0 w:1)
 	fn create_reward_pool() -> Weight {
