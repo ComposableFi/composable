3272b543a4c5cbcc719ba65f67a7f01c81ee9e28
benluelo <benluelo@hotmail.com>
2022-07-13 14:19:46 +0000

    update style.sh (#1312)
    ## Issue
    
    Prettier was inconsistent before, causing unrelated PRs to touch JS/TS files. This should fix it.

--- integration-tests/runtime-tests/src/types/interfaces/types.ts
+++ integration-tests/runtime-tests/src/types/interfaces/types.ts
@@ -1,8 +1,8 @@
 // Auto-generated via `yarn polkadot-types-from-defs`, do not edit
 /* eslint-disable */
 
-export * from './crowdloanRewards/types';
-export * from './assets/types';
-export * from './lending/types';
-export * from './common/types';
-export * from './pablo/types';
+export * from "./crowdloanRewards/types";
+export * from "./assets/types";
+export * from "./lending/types";
+export * from "./common/types";
+export * from "./pablo/types";
