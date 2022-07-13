ad15ef58b65ca03d23b88ee2caa8516138461af8
Andrés Mechali <andresmechali@gmail.com>
2022-07-13 13:03:01 +0200

    update schema and migrations
    Signed-off-by: Andrés Mechali <andresmechali@gmail.com>

---
+++ subsquid/src/model/generated/vestingSchedule.model.ts
@@ -0,0 +1,25 @@
+import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_} from "typeorm"
+import * as marshal from "./marshal"
+import {Schedule} from "./_schedule"
+
+@Entity_()
+export class VestingSchedule {
+  constructor(props?: Partial<VestingSchedule>) {
+    Object.assign(this, props)
+  }
+
+  @PrimaryColumn_()
+  id!: string
+
+  /**
+   * 'To' account for the vesting schedule
+   */
+  @Column_("text", {nullable: false})
+  beneficiary!: string
+
+  /**
+   * Vesting schedule
+   */
+  @Column_("jsonb", {transformer: {to: obj => obj.toJSON(), from: obj => new Schedule(undefined, marshal.nonNull(obj))}, nullable: false})
+  schedule!: Schedule
+}
