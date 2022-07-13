b0ec5307dff2e309d32840a1e1c4ee7d308cae78
Andrés Mechali <andresmechali@gmail.com>
2022-07-13 13:03:54 +0200

    handle VestingScheduleAdded event
    Signed-off-by: Andrés Mechali <andresmechali@gmail.com>

--- subsquid/src/types/v2300.ts
+++ subsquid/src/types/v2300.ts
@@ -16,3 +16,23 @@ export interface Fee {
   protocolFee: bigint
   assetId: CurrencyId
 }
+
+export interface VestingSchedule {
+  window: VestingWindow
+  periodCount: number
+  perPeriod: bigint
+}
+
+export type VestingWindow = VestingWindow_MomentBased | VestingWindow_BlockNumberBased
+
+export interface VestingWindow_MomentBased {
+  __kind: 'MomentBased'
+  start: bigint
+  period: bigint
+}
+
+export interface VestingWindow_BlockNumberBased {
+  __kind: 'BlockNumberBased'
+  start: number
+  period: number
+}
---
+++ subsquid/src/vestingProcessor.ts
@@ -0,0 +1,71 @@
+import { EventHandlerContext } from "@subsquid/substrate-processor";
+import { randomUUID } from "crypto";
+import { VestingSchedule as VestingScheduleType } from "./types/v2300";
+import { Schedule, ScheduleWindow, VestingSchedule } from "./model";
+import { VestingVestingScheduleAddedEvent } from "./types/events";
+import { encodeAccount } from "./utils";
+
+interface VestingScheduleAddedEvent {
+  from: Uint8Array;
+  to: Uint8Array;
+  asset: bigint;
+  schedule: VestingScheduleType;
+}
+
+/**
+ * Extracts information about a VestingScheduleAdded event
+ * @param event
+ */
+function getVestingScheduleAddedEvent(
+  event: VestingVestingScheduleAddedEvent
+): VestingScheduleAddedEvent {
+  if (event.isV2300) {
+    const { from, to, asset, schedule } = event.asV2300;
+
+    return { from, to, asset, schedule };
+  }
+
+  const { from, to, asset, schedule } = event.asLatest;
+
+  return { from, to, asset, schedule };
+}
+
+/**
+ * Creates Schedule
+ * @param vestingSchedule
+ */
+export function createVestingSchedule(
+  vestingSchedule: VestingScheduleType
+): Schedule {
+  const vestingWindow = new ScheduleWindow();
+  vestingWindow.start = BigInt(vestingSchedule.window.start);
+  vestingWindow.period = BigInt(vestingSchedule.window.period);
+  vestingWindow.kind = vestingSchedule.window.__kind;
+
+  const schedule = new Schedule();
+  schedule.window = vestingWindow;
+  schedule.periodCount = BigInt(vestingSchedule.periodCount);
+  schedule.perPeriod = BigInt(vestingSchedule.perPeriod);
+
+  return schedule;
+}
+
+/**
+ * Updates database with vesting schedule information
+ * @param ctx
+ * @param event
+ */
+export async function processVestingScheduleAddedEvent(
+  ctx: EventHandlerContext,
+  event: VestingVestingScheduleAddedEvent
+) {
+  const { to, schedule } = getVestingScheduleAddedEvent(event);
+
+  const vestingSchedule = new VestingSchedule({
+    id: randomUUID(),
+    beneficiary: encodeAccount(to),
+    schedule: createVestingSchedule(schedule),
+  });
+
+  await ctx.store.save(vestingSchedule);
+}
