da664773675bcc26e405e4a3f2637a1b60a554c7
Dominik Roth <dominik@composable.finance>
2022-07-15 19:21:10 +0200

    test(integration-tests/launch): W.I.P.

--- integration-tests/runtime-tests/test/tests/launch-tests/phase2.ts
+++ integration-tests/runtime-tests/test/tests/launch-tests/phase2.ts
@@ -4,10 +4,10 @@ import testConfiguration from "./test_configuration.json";
 import { KeyringPair } from "@polkadot/keyring/types";
 import { getNewConnection } from "@composable/utils/connectionHelper";
 import { getDevWallets } from "@composable/utils/walletHelper";
-import { removeLiquidityFromPool } from "@composabletests/tests/pablo/testHandlers/pabloTestHelper";
-import { mintAssetsToWallet, Pica } from "@composable/utils/mintingHelper";
+import { mintAssetsToWallet } from "@composable/utils/mintingHelper";
 import * as pablo from "@composable/utils/pablo";
 import { Phase2 } from "@composabletests/tests/launch-tests/testHelper";
+import BN from "bn.js";
 
 /**
  * Test suite for verifying phase 2 of the launch process.
@@ -45,7 +45,8 @@ describe.only("[SHORT] Picasso/Pablo Launch Plan - Phase 2", function() {
   let sudoKey: KeyringPair,
     composableManagerWallet: KeyringPair,
     liquidityProviderWallet1: KeyringPair;
-  let ksmUsdcPoolId: number,
+  let ksmUsdcPoolId: BN,
+    ksmUsdcLpTokenId: BN,
     picaLBPPoolId: number,
     picaUsdcPoolId: number,
     picaKsmPoolId: number;
@@ -88,7 +89,7 @@ describe.only("[SHORT] Picasso/Pablo Launch Plan - Phase 2", function() {
   describe("Picasso/Pablo Launch Plan - Phase 2A", function() {
     if (!testConfiguration.enabledTests.query.account__success.enabled) return;
 
-    describe.only("Test 2A pool creation", function() {
+    describe("Test 2A pool creation", function() {
       it.only("Users can not create a pablo pool.", async function() {
         this.timeout(2 * 60 * 1000);
 
@@ -106,20 +107,28 @@ describe.only("[SHORT] Picasso/Pablo Launch Plan - Phase 2", function() {
           baseWeight
         );
         // ToDo: Update to expect error!
-        await Phase2.verifyLastPoolCreation(
+        const { poolId, lpTokenId } = await Phase2.verifyLastPoolCreation(
           api,
-          {
-            base: baseAsset,
-            quote: quoteAsset
-          },
-          {
-            feeRate: fee,
-            ownerFeeRate: 200000,
-            protocolFeeRate: 1000000
-          },
-          baseWeight,
-          baseWeight
+          api.createType("PalletPabloPoolConfiguration", {
+            ConstantProduct: {
+              owner: composableManagerWallet.publicKey,
+              pair: {
+                base: baseAsset,
+                quote: quoteAsset
+              },
+              lpToken: 100_000_000_000n,
+              feeConfig: {
+                feeRate: fee,
+                ownerFeeRate: 200000,
+                protocolFeeRate: 1000000
+              },
+              baseWeight: baseWeight,
+              quoteWeight: baseWeight
+            }
+          })
         );
+        ksmUsdcPoolId = poolId;
+        ksmUsdcLpTokenId = lpTokenId;
       });
 
       it("Create KSM/USDC uniswap pool by root.", async function() {
@@ -145,28 +154,67 @@ describe.only("[SHORT] Picasso/Pablo Launch Plan - Phase 2", function() {
 
     describe("Test 2A pool liquidity", function() {
       describe("Test 2A pool add liquidity", function() {
-        it("Users can add liquidity to the pool", async function() {
+        it.only("Users can add liquidity to the pool", async function() {
           this.timeout(2 * 60 * 1000);
+          const lpTokenBalanceBefore = await api.rpc.assets.balanceOf(ksmUsdcLpTokenId.toString(), liquidityProviderWallet1.publicKey);
           const { data: [result] } = await pablo.addLiquidity(api, liquidityProviderWallet1, ksmUsdcPoolId, baseAmount, quoteAmount, minMintAmount, true);
+          const lpTokenBalanceAfter = await api.rpc.assets.balanceOf(ksmUsdcLpTokenId.toString(), liquidityProviderWallet1.publicKey);
+          expect(new BN(lpTokenBalanceAfter.toString())).to.be.bignumber
+            .greaterThan(new BN(lpTokenBalanceBefore.toString()));
         });
 
-        it("Pool owner can add liquidity to the pool", async function() {
+        it("Pool owner (root) can add liquidity to the pool", async function() {
           this.timeout(2 * 60 * 1000);
+          const lpTokenBalanceBefore = await api.rpc.assets.balanceOf(ksmUsdcLpTokenId.toString(), liquidityProviderWallet1.publicKey);
           const { data: [result] } = await pablo.sudo.sudoAddLiquidity(api, sudoKey, ksmUsdcPoolId, baseAmount, quoteAmount, minMintAmount, true);
+          const lpTokenBalanceAfter = await api.rpc.assets.balanceOf(ksmUsdcLpTokenId.toString(), liquidityProviderWallet1.publicKey);
+          expect(new BN(lpTokenBalanceAfter.toString())).to.be.bignumber
+            .greaterThan(new BN(lpTokenBalanceBefore.toString()));
         });
       });
 
       describe("Test 2A pool remove liquidity", function() {
-        it("Users can remove liquidity from the pool", async function() {
+        it.only("Users can remove liquidity from the pool", async function() {
           this.timeout(2 * 60 * 1000);
-          const result = await removeLiquidityFromPool(api, ksmUsdcPoolId, liquidityProviderWallet1, Pica(500));
-          expect(result.resultBase.toBn()).to.be.bignumber.closeTo(result.resultQuote.toBn(), Pica(15).toString());
+          const lpTokenBalanceBefore = await api.rpc.assets.balanceOf(ksmUsdcLpTokenId.toString(), liquidityProviderWallet1.publicKey);
+          const lpAmount = new BN(lpTokenBalanceBefore.toString()).div(new BN(2));
+          const baseAmount = 0;
+          const quoteAmount = 0;
+          const { data: [result] } = await pablo.removeLiquidity(api, liquidityProviderWallet1, ksmUsdcPoolId, lpAmount, baseAmount, quoteAmount);
+          const lpTokenBalanceAfter = await api.rpc.assets.balanceOf(ksmUsdcLpTokenId.toString(), liquidityProviderWallet1.publicKey);
+          expect(new BN(lpTokenBalanceAfter.toString())).to.be.bignumber
+            .lessThan(new BN(lpTokenBalanceBefore.toString()));
         });
-
         it("Pool owner can remove liquidity from the pool", async function() {
           this.timeout(2 * 60 * 1000);
-          const result = await removeLiquidityFromPool(api, ksmUsdcPoolId, composableManagerWallet, Pica(500));
-          expect(result.resultBase.toBn()).to.be.bignumber.closeTo(result.resultQuote.toBn(), Pica(15).toString());
+          const lpTokenBalanceBefore = await api.rpc.assets.balanceOf(ksmUsdcLpTokenId.toString(), liquidityProviderWallet1.publicKey);
+          const lpAmount = new BN(lpTokenBalanceBefore.toString()).div(new BN(2));
+          const baseAmount = 0;
+          const quoteAmount = 0;
+          const { data: [result] } = await pablo.sudo.sudoRemoveLiquidity(api, sudoKey, ksmUsdcPoolId, lpAmount, baseAmount, quoteAmount);
+          const lpTokenBalanceAfter = await api.rpc.assets.balanceOf(ksmUsdcLpTokenId.toString(), liquidityProviderWallet1.publicKey);
+          expect(new BN(lpTokenBalanceAfter.toString())).to.be.bignumber
+            .lessThan(new BN(lpTokenBalanceBefore.toString()));
+        });
+      });
+    });
+
+    describe("Test 2A trading", function() {
+      describe("Test 2A buy", function() {
+        it("Users can buy from pool", async function() {
+          // ToDo: Implement when pablo staking is done.
+        });
+      });
+
+      describe("Test 2A sell", function() {
+        it("Users can sell to pool", async function() {
+          // ToDo: Implement when pablo staking is done.
+        });
+      });
+
+      describe("Test 2A swap", function() {
+        it("Users can swap in the pool", async function() {
+          // ToDo: Implement when pablo staking is done.
         });
       });
     });
@@ -174,17 +222,19 @@ describe.only("[SHORT] Picasso/Pablo Launch Plan - Phase 2", function() {
     describe("Test 2A pool stake", function() {
       describe("Test 2A pool stake", function() {
         it("Users can stake LP tokens", async function() {
+          // ToDo: Implement when pablo staking is done.
         });
       });
 
       describe("Test 2A pool unstake", function() {
         it("Users can unstake LP tokens", async function() {
+          // ToDo: Implement when pablo staking is done.
         });
       });
     });
 
     describe("Test 2A pool farming rewards", function() {
-
+      // ToDo: Implement when pablo staking is done.
     });
   });
 
@@ -196,25 +246,53 @@ describe.only("[SHORT] Picasso/Pablo Launch Plan - Phase 2", function() {
   describe("Picasso/Pablo Launch Plan - Phase 2B", function() {
     if (!testConfiguration.enabledTests.query.account__success.enabled) return;
 
-    it("Create PICA LBP w/ USDC", async function() {
+    it.only("Create PICA LBP w/ USDC", async function() {
       if (!testConfiguration.enabledTests.query.account__success.balanceGTZero1) this.skip();
       this.timeout(2 * 60 * 1000);
       const currentBlock = await api.query.system.number();
-
+      const baseAsset = picaAssetId;
+      const quoteAsset = usdcAssetId;
+      const saleStart = currentBlock.toNumber() + 5;
+      const saleEnd = currentBlock.toNumber() + 50;
+      const initialWeight = 980000;
+      const finalWeight = 500000;
+      const feeRate = 0;
+      const ownerFeeRate = 0;
+      const protocolFeeRate = 0;
       const result = await pablo.liquidityBootstrapping.createMarket(
         api,
         composableManagerWallet,
         composableManagerWallet.publicKey,
         picaAssetId,
         usdcAssetId,
-        currentBlock.toNumber() + 5,
-        currentBlock.toNumber() + 50,
-        980000,
-        500000,
-        0,
-        0,
-        0
+        saleStart,
+        saleEnd,
+        initialWeight,
+        finalWeight,
+        feeRate,
+        ownerFeeRate,
+        protocolFeeRate
       );
+      await Phase2.verifyLastPoolCreation(api, api.createType("PalletPabloPoolConfiguration", {
+        LiquidityBootstrapping: {
+          owner: api.createType("AccountId32", composableManagerWallet.publicKey),
+          pair: api.createType("ComposableTraitsDefiCurrencyPairCurrencyId", {
+            base: api.createType("u128", baseAsset),
+            quote: api.createType("u128", quoteAsset)
+          }),
+          sale: api.createType("ComposableTraitsDexSale", {
+            start: api.createType("u32", saleStart),
+            end: api.createType("u32", saleEnd),
+            initialWeight: api.createType("Permill", initialWeight),
+            finalWeight: api.createType("Permill", finalWeight)
+          }),
+          feeConfig: api.createType("ComposableTraitsDexFeeConfig", {
+            feeRate: api.createType("Permill", feeRate),
+            ownerFeeRate: api.createType("Permill", ownerFeeRate),
+            protocolFeeRate: api.createType("Permill", protocolFeeRate)
+          })
+        }
+      }));
     });
   });
 
--- integration-tests/runtime-tests/test/tests/launch-tests/testHelper.ts
+++ integration-tests/runtime-tests/test/tests/launch-tests/testHelper.ts
@@ -1,33 +1,51 @@
 import { ApiPromise } from "@polkadot/api";
 import BN from "bn.js";
-import { u128 } from "@polkadot/types-codec";
-import { Permill } from "@polkadot/types/interfaces/runtime";
 import { expect } from "chai";
+import { PalletPabloPoolConfiguration } from "@composable/types/interfaces";
+import { Option, u128 } from "@polkadot/types-codec";
 
 export class Phase2 {
-
   public static async verifyLastPoolCreation(
     api: ApiPromise,
-    pair: { base: number | u128, quote: number | u128 },
-    feeConfig: {
-      feeRate: number | u128,
-      ownerFeeRate: number | u128,
-      protocolFeeRate: number | u128
-    },
-    baseWeight: number | Permill,
-    quoteWeight: number | Permill
-  ) {
+    poolConfig: PalletPabloPoolConfiguration
+  ): Promise<{ poolId: BN, lpTokenId: BN }> {
     const poolAmount = await api.query.pablo.poolCount();
-    const pools = await api.query.pablo.pools(poolAmount.sub(new BN(1)));
+    const poolId = poolAmount.sub(new BN(1));
+    const pools = await api.query.pablo.pools(poolId);
     console.log(pools.toString());
-    const unwrapped = pools.unwrap();
-    expect(pools.unwrap().asConstantProduct.pair.base).to.be.bignumber.equal(new BN(pair.base));
-    expect(pools.unwrap().asConstantProduct.pair.quote).to.be.bignumber.equal(new BN(pair.quote));
-    expect(pools.unwrap().asConstantProduct.feeConfig.feeRate).to.be.bignumber.equal(new BN(feeConfig.feeRate));
-    expect(pools.unwrap().asConstantProduct.feeConfig.ownerFeeRate).to.be.bignumber.equal(new BN(feeConfig.ownerFeeRate));
-    expect(pools.unwrap().asConstantProduct.feeConfig.protocolFeeRate).to.be.bignumber.equal(new BN(feeConfig.protocolFeeRate));
-    expect(pools.unwrap().asConstantProduct.baseWeight).to.be.bignumber.equal(new BN(baseWeight));
-    expect(pools.unwrap().asConstantProduct.quoteWeight).to.be.bignumber.equal(new BN(quoteWeight));
+    if (poolConfig.isConstantProduct) {
+      return this.verifyConstantProductPool(poolConfig, pools, poolId, poolAmount);
+    } else if (poolConfig.isLiquidityBootstrapping) {
+      // ToDo!
+    } else if (poolConfig.isStableSwap) {
+      // ToDo!
+    }
+  }
+
+  static verifyConstantProductPool(
+    poolConfig: PalletPabloPoolConfiguration,
+    pools: Option<PalletPabloPoolConfiguration>,
+    poolId: BN,
+    poolAmount: u128
+  ) {
+    const lpTokenId = poolConfig.asConstantProduct.lpToken.add(poolAmount);
+    expect(pools.unwrap().asConstantProduct.pair.base).to.be.bignumber
+      .equal(new BN(poolConfig.asConstantProduct.pair.base));
+    expect(pools.unwrap().asConstantProduct.pair.quote).to.be.bignumber
+      .equal(new BN(poolConfig.asConstantProduct.pair.quote));
+    expect(pools.unwrap().asConstantProduct.feeConfig.feeRate).to.be.bignumber
+      .equal(new BN(poolConfig.asConstantProduct.feeConfig.feeRate));
+    expect(pools.unwrap().asConstantProduct.feeConfig.ownerFeeRate).to.be.bignumber
+      .equal(new BN(poolConfig.asConstantProduct.feeConfig.ownerFeeRate));
+    expect(pools.unwrap().asConstantProduct.feeConfig.protocolFeeRate).to.be.bignumber
+      .equal(new BN(poolConfig.asConstantProduct.feeConfig.protocolFeeRate));
+    expect(pools.unwrap().asConstantProduct.baseWeight).to.be.bignumber
+      .equal(new BN(poolConfig.asConstantProduct.baseWeight));
+    expect(pools.unwrap().asConstantProduct.quoteWeight).to.be.bignumber
+      .equal(new BN(poolConfig.asConstantProduct.quoteWeight));
+    expect(pools.unwrap().asConstantProduct.lpToken).to.be.bignumber
+      .equal(lpTokenId);
+    return { poolId, lpTokenId };
   }
 
   public static async verifyPoolLiquidityAdded() {
