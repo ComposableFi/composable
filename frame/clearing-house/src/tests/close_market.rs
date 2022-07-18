5f9a3a0afed7415fc0806013e2983e4b1d00a096
Ângelo <12701614+0xangelo@users.noreply.github.com>
2022-07-18 12:33:46 +0000

    chore(CH): check for market closure in close_position

--- frame/clearing-house/src/tests/close_market.rs
+++ frame/clearing-house/src/tests/close_market.rs
@@ -3,8 +3,11 @@ use crate::{
 		accounts::ALICE,
 		runtime::{MarketId, Origin, Runtime, TestPallet, Vamm as VammPallet},
 	},
-	tests::{as_balance, run_to_time, with_trading_context, ExtBuilder, MarketConfig},
-	Direction::{Long, Short},
+	tests::{
+		as_balance, get_market, run_to_time, with_market_context, with_trading_context, ExtBuilder,
+		MarketConfig,
+	},
+	Direction::Long,
 	Error,
 };
 use composable_traits::time::DurationSeconds;
@@ -18,9 +21,18 @@ use proptest::prelude::*;
 const MAX_DURATION_SECONDS: DurationSeconds = 1_000_000_000;
 
 prop_compose! {
-	fn close_times()(close_market in 2..MAX_DURATION_SECONDS)(
+	fn valid_close_times()(close_market in 2..MAX_DURATION_SECONDS)(
 		close_market in Just(close_market),
 		close_position in 1..close_market,
+	) -> (DurationSeconds, DurationSeconds) {
+		(close_position, close_market)
+	}
+}
+
+prop_compose! {
+	fn invalid_close_times()(close_market in 1..(MAX_DURATION_SECONDS / 2))(
+		close_market in Just(close_market),
+		close_position in (close_market + 1)..=MAX_DURATION_SECONDS,
 	) -> (DurationSeconds, DurationSeconds) {
 		(close_position, close_market)
 	}
@@ -31,6 +43,21 @@ prop_compose! {
 // --------------------------------------------------------------------------------------------------
 
 proptest! {
+	#[test]
+	fn updates_market_state(time in any::<DurationSeconds>()) {
+		let config = MarketConfig::default();
+
+		with_market_context(ExtBuilder::default(), config, |market_id| {
+			let market_before = get_market(&market_id);
+
+			assert_ok!(TestPallet::close_market(Origin::signed(ALICE), market_id, time));
+
+			let market_after = get_market(&market_id);
+
+			assert_ne!(market_before, market_after);
+		});
+	}
+
 	#[test]
 	fn should_fail_if_market_does_not_exist(
 		market_id in any::<MarketId>(),
@@ -46,7 +73,7 @@ proptest! {
 
 	#[test]
 	fn should_allow_closing_positions_before_market_close(
-		(position_time, market_time) in close_times(),
+		(position_time, market_time) in valid_close_times(),
 	) {
 		let config = MarketConfig::default();
 		let collateral = as_balance(100);
@@ -71,6 +98,39 @@ proptest! {
 
 			// Alice closes her position
 			assert_ok!(TestPallet::close_position(Origin::signed(ALICE), market_id));
+		});
+	}
+
+	#[test]
+	fn should_not_allow_closing_positions_after_market_close(
+		(position_time, market_time) in invalid_close_times(),
+	) {
+		let config = MarketConfig::default();
+		let collateral = as_balance(100);
+
+		with_trading_context(config, collateral, |market_id| {
+			// Alice opens a position while market is open
+			VammPallet::set_price(Some(10.into()));
+
+			assert_ok!(TestPallet::open_position(
+				Origin::signed(ALICE),
+				market_id,
+				Long,
+				as_balance(100),
+				as_balance(10)
+			));
+
+			// In the same block, market close is programmed to a future time
+			assert_ok!(TestPallet::close_market(Origin::signed(ALICE), market_id, market_time));
+
+			// Time passes and now it's after the market close
+			run_to_time(position_time);
+
+			// Alice closes her position, but it should fail
+			assert_noop!(
+				TestPallet::close_position(Origin::signed(ALICE), market_id),
+				Error::<Runtime>::MarketClosed,
+			);
 		});
 	}
 }
