a96960e98376ca1a3a90fb1389eaa4603961660b
Ângelo Lovatto <12701614+0xangelo@users.noreply.github.com>
2022-07-18 09:12:41 +0100

    chore(CH): wip

--- frame/clearing-house/src/tests/close_market.rs
+++ frame/clearing-house/src/tests/close_market.rs
@@ -1,14 +1,32 @@
 use crate::{
 	mock::{
 		accounts::ALICE,
-		runtime::{MarketId, Origin, Runtime, TestPallet},
+		runtime::{MarketId, Origin, Runtime, TestPallet, Vamm as VammPallet},
 	},
-	tests::ExtBuilder,
+	tests::{as_balance, run_to_time, with_trading_context, ExtBuilder, MarketConfig},
+	Direction::{Long, Short},
 	Error,
 };
 use composable_traits::time::DurationSeconds;
-use frame_support::assert_noop;
+use frame_support::{assert_noop, assert_ok};
 use proptest::prelude::*;
+
+// ----------------------------------------------------------------------------------------------------
+//                                             Prop Compose
+// ----------------------------------------------------------------------------------------------------
+
+prop_compose! {
+	fn close_times()(close_market in 2..DurationSeconds::MAX)(
+		close_market in Just(close_market),
+		close_position in 1..close_market,
+	) -> (DurationSeconds, DurationSeconds){
+		(close_position, close_market)
+	}
+}
+
+// --------------------------------------------------------------------------------------------------
+//                                         Property Tests
+// --------------------------------------------------------------------------------------------------
 
 proptest! {
 	#[test]
@@ -21,6 +39,36 @@ proptest! {
 			TestPallet::close_market(Origin::signed(ALICE), market_id, time),
 				Error::<Runtime>::MarketIdNotFound
 			);
+		});
+	}
+
+	#[test]
+	fn should_allow_closing_positions_before_market_close(
+		(position_time, market_time) in close_times(),
+	) {
+		let config = MarketConfig::default();
+		let collateral = as_balance(100);
+
+		with_trading_context(config, collateral, |market_id| {
+			// Alice opens a position while market is open
+			VammPallet::set_price(Some(10.into()));
+
+			assert_ok!(TestPallet::open_position(
+				Origin::signed(ALICE),
+				market_id,
+				Long,
+				as_balance(100),
+				as_balance(10)
+			));
+
+			// In the same block, market close is programmed to a future time
+			assert_ok!(TestPallet::close_market(Origin::signed(ALICE), market_id, market_time));
+
+			// Time passes, but it's still earlier than the market close
+			run_to_time(position_time);
+
+			// Alice closes her position
+			assert_ok!(TestPallet::close_position(Origin::signed(ALICE), market_id));
 		});
 	}
 }
