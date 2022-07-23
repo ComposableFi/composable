b3382daf9cc85a6a766f2c332de172163bb981b0
Dzmitry Lahoda <dzmitry@lahoda.pro>
2022-07-23 23:33:27 +0100

    wtf

--- nix-install-vscode-composable.sh
+++ nix-install-vscode-composable.sh
@@ -1,14 +1,22 @@
 # install nix under `vscode` user targeting composable cache
 echo "Installin via $1 and using $2"
 
+# so we avoid using symbols which may not execute well in shells
+# easy to cat what is going on
 curl --location $1 > ./nix-install.sh
-chmod +x ./nix-install.sh
+chmod +x ./nix-install.sh 
 ./nix-install.sh
+
+# force nix upon user
 echo "source ~/.nix-profile/etc/profile.d/nix.sh" >> ~/.bashrc
 echo "source ~/.nix-profile/etc/profile.d/nix.sh" >> ~/.profile
 chmod +x ~/.nix-profile/etc/profile.d/nix.sh
 ~/.nix-profile/etc/profile.d/nix.sh
 
+ls ~/.nix-profile/bin
+export PATH="~/.nix-profile/bin:$PATH"
+
+# ensure user is on same binaries we are
 nix-channel --add $2 nixpkgs
 nix-channel --update                
 nix-env --install --attr nixpkgs.cachix
