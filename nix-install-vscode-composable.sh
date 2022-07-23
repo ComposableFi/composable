6d4abb4319df3e0554a2d6635ef5035e7ae9e73c
Dzmitry Lahoda <dzmitry@lahoda.pro>
2022-07-24 00:04:39 +0100

    wtf

--- nix-install-vscode-composable.sh
+++ nix-install-vscode-composable.sh
@@ -28,6 +28,6 @@ export PATH="~/.nix-profile/bin:$PATH"
     ./nix-env --install --attr nixpkgs.cachix
     find / -name cachix
     chmod +x ~/.nix-profile/bin
-ls ~/.nix-profile/bin
-    cachix use composable-community       
+    ls ~/.nix-profile/bin
+    ./cachix use composable-community       
 )
