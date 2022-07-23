6fc2d1783ae546e530adc85ddb53d44a62bfb20c
Dzmitry Lahoda <dzmitry@lahoda.pro>
2022-07-23 23:06:02 +0100

    wtf

--- nix-install-vscode-composable.sh
+++ nix-install-vscode-composable.sh
@@ -1,9 +1,11 @@
 # install nix under `vscode` user targeting composable cache
+echo $1
 curl --location $1 > ./nix-install.sh
 chmod +x ./nix-install.sh
 echo "source ~/.nix-profile/etc/profile.d/nix.sh" >> ~/.bashrc
 echo "source ~/.nix-profile/etc/profile.d/nix.sh" >> ~/.profile
 
+echo $2
 # nix-channel --add ${{env.NIX_NIXPKGS_CHANNEL}} nixpkgs
 # nix-channel --update                
 # nix-env --install --attr nixpkgs.cachix
