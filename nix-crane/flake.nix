979a1fcc09a8213040f2bc8103818653bd08b3da
Dzmitry Lahoda <dzmitry@lahoda.pro>
2022-07-25 13:02:07 +0100

    some refactorign

--- nix-crane/flake.nix
+++ nix-crane/flake.nix
@@ -169,26 +169,7 @@
           hash = "sha256-k+6mXjAsHbS4gnHljGmEkMcok77zBd8jhyp56mXyKgI=";
         };
 
-        # TODO: move this to dockerTools.Buildx
-        # https://github.com/microsoft/vscode-dev-containers/tree/main/containers/debian
-        # https://github.com/numtide/flake-utils/blob/master/default.nix       
-        supported-nix-to-container-images = { 
-            # NOTE: we peeky container version to make these work version compatible with nix and native packaging/scripts
-            x86_64-linux ={
-                arch = "amd64";
-            };
-            aarch64-linux ={
-                arch = "arm64";
-            };
-            aarch64-darwin ={
-                arch = "arm64";
-            };
-           };
-        nix-to-container-image = system: 
-          if builtins.hasAttr system supported-nix-to-container-images then 
-            supported-nix-to-container-images.${system}
-          else supported-nix-to-container-images.x86_64-linux;
-
+        nix-to-container-image = import ./nix-to-container-image.nix;
       in rec {
         packages = {
           inherit wasm-optimizer;
