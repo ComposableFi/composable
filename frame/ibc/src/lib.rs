6fca1f7ba79378b5e06ab27dce517922b4b4a0cb
Seun Lanlege <seunlanlege@gmail.com>
2022-07-28 16:09:36 +0000

    don't deposit empty events

--- frame/ibc/src/lib.rs
+++ frame/ibc/src/lib.rs
@@ -380,11 +380,14 @@ pub mod pallet {
 			);
 
 			log::trace!(target: "pallet_ibc", "[pallet_ibc_deliver]: logs: {:?}", logs);
-			log::trace!(target: "pallet_ibc", "[pallet_ibc_deliver]: errors: {:?}", errors);
 
 			// todo: consolidate into one.
-			Self::deposit_event(events.into());
-			Self::deposit_event(errors.into());
+			if !events.is_empty() {
+				Self::deposit_event(events.into())
+			};
+			if !errors.is_empty() {
+				Self::deposit_event(errors.into())
+			};
 			Ok(())
 		}
 
