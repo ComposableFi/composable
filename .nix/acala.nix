e1c4e76b5a6f2169d162d667d5aa7f02694f1bee
Dzmitry Lahoda <dzmitry@lahoda.pro>
2022-08-07 17:58:28 +0100

    acala

--- .nix/acala.nix
+++ .nix/acala.nix
@@ -1,20 +1,30 @@
 {pkgs, }:
 with pkgs.stdenv;
+with pkgs;
+
 mkDerivation {
+	name = "acala";
   src = pkgs.fetchFromGitHub {
                 owner = "AcalaNetwork";
                 repo = "Acala";
                 rev = "e9d2b3caa0663c1d3e7d4d6e7d3faef4a569099c";
-                hash = "sha256-ggcyOsA4cyo5l87cZmOMI0w1gCzmWy9NRJiWxjBdB1E=";
+                hash = "sha256-7oQxYDX+hMSrXZdwWbF1tkahSr1olze1HaCOjCvfWsc=";
               };     
+	installPhase = ''
+	  ls
+		mkdir --parents $out/bin && make init
+	'';
+	buildInputs = [ openssl ];
+	nativeBuildInputs = [ clang cargo ];
+	buildPhase = ''
+	  ls
+		make build-release && mv ./target/production/acala $out/bin
+	'';
+	# TODO: moved these to some `cumulus based derivation`
+	LD_LIBRARY_PATH = lib.strings.makeLibraryPath [
+		stdenv.cc.cc.lib
+		llvmPackages.libclang.lib
+	];
+	LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";	
 }
-	# (
-	# 	cd ../../..
-	# 	git clone https://github.com//.git
-	# 	cd Acala
-	# 	make init
-	# 	make build-release
-	# 	./target/production/acala --version
-	# )
-
   
