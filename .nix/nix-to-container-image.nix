eb25bc3c55a2649205f21cd833d34f21f2b34c48
cor <cor@pruijs.dev>
2022-08-09 16:40:17 +0200

    fix: make container SHA dependent on arch target

--- .nix/nix-to-container-image.nix
+++ .nix/nix-to-container-image.nix
@@ -6,15 +6,20 @@ let supported-nix-to-container-images = {
     # NOTE: we peeky container version to make these work version compatible with nix and native packaging/scripts
         x86_64-linux ={
             arch = "amd64";
+            sha256 = "0vraf6iwbddpcy4l9msks6lmi35k7wfgpafikb56k3qinvvcjm9b";
         };
         aarch64-linux ={
             arch = "arm64";
+            sha256 = "sha256-TBbDCKlGb+n5V/IqWPuZBef3dZ//Pt7v3gdOvyoOrjU=";
         };
         aarch64-darwin ={
             arch = "arm64";
+            sha256 = "sha256-TBbDCKlGb+n5V/IqWPuZBef3dZ//Pt7v3gdOvyoOrjU=";
+
         };
         x86_64-darwin ={
             arch = "amd64";
+            sha256 = "0vraf6iwbddpcy4l9msks6lmi35k7wfgpafikb56k3qinvvcjm9b";
         };
     };
 in 
