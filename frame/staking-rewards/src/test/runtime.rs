23c3acedb1c0534d438991d989c26b574a091865
Vim Wickramasinghe <vimukthi@composable.finance>
2022-08-09 12:12:06 +0200

    Fix staking-rewards tests

--- frame/staking-rewards/src/test/runtime.rs
+++ frame/staking-rewards/src/test/runtime.rs
@@ -17,7 +17,7 @@ use sp_runtime::{
 	testing::Header,
 	traits::{BlakeTwo256, IdentityLookup},
 };
-use composable_traits::fnft::FNFTAccountProxyType;
+use composable_traits::fnft::{FNFTAccountProxyType, FNFTAccountProxyTypeSelector};
 
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
