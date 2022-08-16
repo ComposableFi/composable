43cbd5923f0c1fbe378e9d0b2d8d3fe818b4dc92
David Salami <wizdave97@gmail.com>
2022-08-16 16:53:20 +0100

    minor fix

--- frame/ibc/src/lib.rs
+++ frame/ibc/src/lib.rs
@@ -330,8 +330,7 @@ pub mod pallet {
 						type_url.as_str(),
 						conn_open_init::TYPE_URL |
 							conn_open_ack::TYPE_URL | conn_open_try::TYPE_URL |
-							conn_open_confirm::TYPE_URL | conn_open_ack::TYPE_URL |
-							create_client::TYPE_URL
+							conn_open_confirm::TYPE_URL | create_client::TYPE_URL
 					);
 					if is_permissioned {
 						return None
@@ -362,8 +361,7 @@ pub mod pallet {
 						type_url.as_str(),
 						conn_open_init::TYPE_URL |
 							conn_open_ack::TYPE_URL | conn_open_try::TYPE_URL |
-							conn_open_confirm::TYPE_URL | conn_open_ack::TYPE_URL |
-							create_client::TYPE_URL
+							conn_open_confirm::TYPE_URL | create_client::TYPE_URL
 					);
 					if !is_permissioned {
 						return None
