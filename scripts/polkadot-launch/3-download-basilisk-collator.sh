6685a832b8afe938e607553cde89c3b590dc1ee6
Dzmitry Lahoda <dzmitry@lahoda.pro>
2022-08-17 10:23:46 +0000

    #CU-3ajfdfq nix karura without preopened channels (#1448)
    ## Description
    - replaced raw config with builder
    - added acala
    - fixed runtime task failure as reported by @vimukthi-git 
    
    
    acala built
    ````
    dz@dz-pc-11:~/git/blockchain/polkadot/composable$ /nix/store/bfrjvfgqb070wcg3xk3ska00yvavfyf6-acala/bin/acala
    Error: Input("Not specific which chain to run.")
    ````

--- scripts/polkadot-launch/3-download-basilisk-collator.sh
+++
@@ -1,4 +0,0 @@
-	mkdir -p ../../../basilisk-node/target/release
-	curl https://github.com/galacticcouncil/Basilisk-node/releases/download/v8.0.0/basilisk -Lo ../../../basilisk-node/target/release/basilisk
-	chmod +x ../../../basilisk-node/target/release/basilisk
-	../../../basilisk-node/target/release/basilisk --version
