aa74077fd039f9a4e630965ec49f7585c71d765f
Julian <juliansv22@gmail.com>
2022-08-18 06:29:57 -0300

    finish tests for vesting pallet

--- integration-tests/runtime-tests/test/tests/vesting/vestingTests.ts
+++ integration-tests/runtime-tests/test/tests/vesting/vestingTests.ts
@@ -5,16 +5,18 @@ import { getDevWallets } from "@composable/utils/walletHelper";
 import { mintAssetsToWallet } from "@composable/utils/mintingHelper";
 import { sendAndWaitForSuccess, waitForBlocks } from "@composable/utils/polkadotjs";
 import { expect } from "chai";
+import BN from "bn.js";
+
 
 
 
 // Vesting pallet integration test
 
 // In these tests we are testing the following extrinsics:
+//  - vestedTransfer
 //  - claim
 //  - claimFor
 //  - updateVestingSchedules
-//  - vestedTransfer
 
 describe("Vesting Pallet Tests", function () {
   let api: ApiPromise;
@@ -28,8 +30,8 @@ describe("Vesting Pallet Tests", function () {
     api = newClient;
     const { devWalletAlice, devWalletEve, devWalletFerdie } = getDevWallets(newKeyring);
     sudoKey = devWalletAlice;
-    walletEveId = devWalletEve.derive("/test/constantProductDex/walletId1");
-    walletFerdieId = devWalletFerdie.derive("/test/constantProductDex/walletId2");
+    walletEveId = devWalletEve;
+    walletFerdieId = devWalletFerdie;
     eth = 5;
     usdt = 6;
   });
@@ -42,20 +44,8 @@ describe("Vesting Pallet Tests", function () {
   after("Closing the connection", async function () {
     await api.disconnect();
   });
-
-//   it.only("debug", async function () {
-//     const currentBlock = await waitForBlocks(api)
-//     console.log("Number(waitForblocks) returns: ", Number(currentBlock));
-//     console.log("waitForblocks.toString() returns: ", currentBlock.toString());
-//     const from = api.createType("MultiAddress", {
-//         id: walletEveId.address
-//     });
-//     console.log(" from: ",  from);
-//   });
     
-  // vestedTransfer extrinsic
   it("should perform a vested transfer - block number based", async function() {
-    console.log("vested transfer function");
     const from = api.createType("MultiAddress", {
         id: walletEveId.address
     });
@@ -67,7 +57,7 @@ describe("Vesting Pallet Tests", function () {
     const scheduleInfo = api.createType("ComposableTraitsVestingVestingScheduleInfo", {
         window: api.createType("ComposableTraitsVestingVestingWindow", {
             blockNumberBased: {
-                start: api.createType("u32", (Number(currentBlock) + 5)),
+                start: api.createType("u32", (Number(currentBlock) + 2)),
                 period: api.createType("u32", 2)
             }
         }),
@@ -76,78 +66,67 @@ describe("Vesting Pallet Tests", function () {
     });
 
     const { 
-      data: [sender, recipient, assetId, vestingScheduleId, ] 
+      data: [ , currencyId, , amount] 
     } = await sendAndWaitForSuccess(
         api,
         sudoKey,
-        api.events.vesting.VestingScheduleAdded.is,
+        api.events.tokens.LockSet.is,
         api.tx.sudo.sudo(api.tx.vesting.vestedTransfer(from, beneficiary, asset, scheduleInfo))
     );
-
-    vestingScheduleIdSet = Number(vestingScheduleId);
-    beneficiaryWallet = recipient.toString();
     
- 
-    console.log("Number(assetId): ", Number(assetId));
+    const rawAnswer = await api.query.vesting.vestingSchedules(walletFerdieId.address, usdt);
+    const answer = rawAnswer.toJSON();
+    vestingScheduleIdSet = answer[1].vestingScheduleId
+
+    expect(Number(currencyId)).to.be.eq(usdt);
+    expect(Number(amount)).to.be.eq(10*10000000000000);
 
 
-    // console.log("lockId: ", lockId);
-    // console.log("currencyId: ", currencyId);
-    // console.log("who: ", who);
-    // console.log("amount: ", amount);
-    // console.log("usdt: ", usdt);
-    // console.log("beneficiary: ", beneficiary);
-    // console.log("suma : ", (20*10000000000000));
+  });
 
-
-    // expect(sender.toString()).to.be.equal((walletEveId.address).toString());
-    // expect(recipient.toString()).to.be.equal((walletFerdieId.address).toString());
-    // expect(assetId.toString()).to.be.equal(asset.toString());
-
-    // console.log("scheduleInfoInTx: ", scheduleInfoInTx);
-
-    });
-  // claim extrinsic
-  it('should claim USDT', async function () {
-    await waitForBlocks(api, 5);
-    console.log("Claim function");
+  it('beneficiary should be able to claim available funds', async function () {
+    await waitForBlocks(api, 4);
     const asset = api.createType("u128", usdt);
-    const vestingScheduleId = api.createType("ComposableTraitsVestingVestingScheduleIdSet", {
-        One: api.createType("u128", vestingScheduleIdSet)
-    }); // ask
+    const vestingScheduleId = api.createType("ComposableTraitsVestingVestingScheduleIdSet", "All")
 
-    await sendAndWaitForSuccess(
+    const {
+        data: [, assetId, , lockedAmount, claimedAmount]
+    } = await sendAndWaitForSuccess(
         api,
-        sudoKey,
+        walletFerdieId,
         api.events.vesting.Claimed.is,
         api.tx.vesting.claim(asset, vestingScheduleId)
     );
-    
+
+    expect(Number(assetId)).to.be.eq(usdt);
+    expect(Number(lockedAmount)).to.be.gt(0);
+    expect(Number(claimedAmount)).to.be.gt(0);
     
   });
 
-  // claimFor extrinsic
-  it("should use claimFor extrinsic - USDT", async function () {
-    console.log("Claim For function");
-    this.timeout(2 * 60 * 1000);
+  it("any user can claim available funds for beneficiary", async function () {
     const target = api.createType("MultiAddress", {
-        id: beneficiaryWallet
+        id: walletFerdieId.address
     });
     const asset = api.createType("u128", usdt);
     const vestingScheduleIds = api.createType("ComposableTraitsVestingVestingScheduleIdSet", "All");
 
-    await sendAndWaitForSuccess(
+    const {
+        data: [, assetId, , lockedAmount, claimedAmount]
+    } = await sendAndWaitForSuccess(
         api,
         sudoKey,
         api.events.vesting.Claimed.is,
         api.tx.vesting.claimFor(target, asset, vestingScheduleIds)
     );
 
+    expect(Number(assetId)).to.be.eq(usdt);
+    expect(Number(lockedAmount)).to.be.gt(0);
+    expect(Number(claimedAmount)).to.be.gt(0);
+
   });
 
-  // updateVestingSchedules extrinsic
   it("should update vesting schedule - Block number based", async function() {
-    console.log("update function");
     const who = api.createType("MultiAddress", {
         id: walletEveId.address
     });
@@ -167,12 +146,49 @@ describe("Vesting Pallet Tests", function () {
         },
       ]);
 
-    await sendAndWaitForSuccess(
+    const { 
+        data: [ , currencyId, , amount] 
+    } = await sendAndWaitForSuccess(
         api,
         sudoKey,
-        api.events.vesting.VestingSchedulesUpdated.is,
+        api.events.tokens.LockSet.is,
         api.tx.sudo.sudo(api.tx.vesting.updateVestingSchedules(who, asset, vestingSchedules))
     );
+
+    expect(Number(currencyId)).to.be.eq(usdt);
+    expect(Number(amount)).to.be.eq(40*1000000000000);
+
+  });
+
+  it("should perform a vested transfer - moment based", async function() {
+    const from = api.createType("MultiAddress", {
+        id: walletEveId.address
+    });
+    const beneficiary = api.createType("MultiAddress", {
+        id: walletFerdieId.address
+    });
+    const asset = api.createType("u128", usdt);
+    const scheduleInfo = api.createType("ComposableTraitsVestingVestingScheduleInfo", {
+        window: api.createType("ComposableTraitsVestingVestingWindow", {
+            MomentBased: {
+                start: api.createType("u64", 18),
+                period: api.createType("u64", 2)
+            }
+        }),
+        periodCount: 10,
+        perPeriod: api.createType("u128", 10000000000000)
+    });
+
+    const { 
+        data: [ , currencyId, , amount] 
+    } = await sendAndWaitForSuccess(
+        api,
+        sudoKey,
+        api.events.tokens.LockSet.is,
+        api.tx.sudo.sudo(api.tx.vesting.vestedTransfer(from, beneficiary, asset, scheduleInfo))
+    );
+    
+    expect(Number(currencyId)).to.be.eq(usdt);
 
   });
 
