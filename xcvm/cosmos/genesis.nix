bd82bbb6d2990d6d25639f05170373a72d544692
Dzmitry Lahoda <dzmitry@lahoda.pro>
2022-08-23 20:48:38 +0100

    data derivation
    Signed-off-by: Dzmitry Lahoda <dzmitry@lahoda.pro>

--- xcvm/cosmos/genesis.nix
+++ xcvm/cosmos/genesis.nix
@@ -1,9 +1,9 @@
 { pkgs } :
   pkgs.stdenv.mkDerivation {
     name = "genesis";
-    src = ./.junod;
-    installPhase = ''
-    ls $src
-    mkdir --parents $out/data/.junod && cp -r $src/.junod $out/data/.junod
+    src = ./junod/.juno/keyring-file;
+    installPhase = "true";
+   buildPhase = ''    
+    mkdir --parents $out/data/.juno/keyring-file && cp -r $src $out/data/.juno/keyring-file
     '';
 }
