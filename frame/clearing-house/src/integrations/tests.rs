49bf54ecd11138fa7ad1edf72ecf9801653ede24
Ângelo <12701614+0xangelo@users.noreply.github.com>
2022-08-23 12:55:15 +0000

    test(CH): separate tests in modules

--- frame/clearing-house/src/integrations/tests.rs
+++ frame/clearing-house/src/integrations/tests.rs
@@ -155,28 +155,32 @@ pub const UNIT: Balance = UnsignedDecimal::DIV;
 //                                        Create Market
 // -------------------------------------------------------------------------------------------------
 
-proptest! {
-	#[test]
-	fn should_succeed_in_creating_first_market(
-		asset_id in prop_oneof![Just(DOT), Just(PICA)]
-	) {
-		ExtBuilder {
-			native_balances: vec![(ALICE, UNIT), (BOB, UNIT)],
-			balances: vec![(ALICE, PICA, UNIT), (BOB, PICA, UNIT)],
-			..Default::default()
-		}
-		.build()
-		.execute_with(|| {
-			set_oracle_for(asset_id, 1_000); // 10 in cents
-			let config = MarketConfig { asset: asset_id, ..Default::default() };
-			assert_ok!(TestPallet::create_market(Origin::signed(ALICE), config));
+mod create_market {
+	use super::*;
 
-			let market_id = MarketId::zero();
-			let market = TestPallet::get_market(&market_id).unwrap();
-			assert_eq!(market.asset_id, asset_id);
-			assert_eq!(market.last_oracle_price, Decimal::from(10));
-			assert_eq!(market.last_oracle_twap, Decimal::from(10));
-		})
+	proptest! {
+		#[test]
+		fn should_succeed_in_creating_first_market(
+			asset_id in prop_oneof![Just(DOT), Just(PICA)]
+		) {
+			ExtBuilder {
+				native_balances: vec![(ALICE, UNIT), (BOB, UNIT)],
+				balances: vec![(ALICE, PICA, UNIT), (BOB, PICA, UNIT)],
+				..Default::default()
+			}
+			.build()
+			.execute_with(|| {
+				set_oracle_for(asset_id, 1_000); // 10 in cents
+				let config = MarketConfig { asset: asset_id, ..Default::default() };
+				assert_ok!(TestPallet::create_market(Origin::signed(ALICE), config));
+
+				let market_id = MarketId::zero();
+				let market = TestPallet::get_market(&market_id).unwrap();
+				assert_eq!(market.asset_id, asset_id);
+				assert_eq!(market.last_oracle_price, Decimal::from(10));
+				assert_eq!(market.last_oracle_twap, Decimal::from(10));
+			})
+		}
 	}
 }
 
@@ -184,132 +188,191 @@ proptest! {
 //                                     Open/Close Position
 // -------------------------------------------------------------------------------------------------
 
-#[test]
-fn should_succeed_in_opening_first_position() {
-	ExtBuilder {
-		native_balances: vec![(ALICE, UNIT), (BOB, UNIT)],
-		balances: vec![(ALICE, PICA, UNIT), (BOB, PICA, UNIT), (BOB, USDC, UNIT * 100)],
-		..Default::default()
-	}
-	.build()
-	.execute_with(|| {
-		set_oracle_for(DOT, 1_000);
-		let config = MarketConfig {
-			asset: DOT,
-			vamm_config: VammConfig {
-				base_asset_reserves: UNIT * 100,
-				quote_asset_reserves: UNIT * 10_000,
-				peg_multiplier: 10,
-				twap_period: ONE_HOUR,
-			},
-			margin_ratio_initial: Decimal::from_float(0.1),
-			margin_ratio_maintenance: Decimal::from_float(0.02),
-			margin_ratio_partial: Decimal::from_float(0.04),
-			minimum_trade_size: 0.into(),
-			funding_frequency: ONE_HOUR,
-			funding_period: ONE_HOUR * 24,
-			taker_fee: 0,
-			twap_period: ONE_HOUR,
-		};
-		assert_ok!(TestPallet::create_market(Origin::signed(ALICE), config));
+mod open_position {
+	use super::*;
 
-		assert_ok!(TestPallet::deposit_collateral(Origin::signed(BOB), USDC, UNIT * 100));
-
-		let market = get_market(&MarketId::zero());
-		let vamm_state = get_vamm(&market.vamm_id);
-
-		assert_ok!(TestPallet::open_position(
-			Origin::signed(BOB),
-			Zero::zero(),
-			Long,
-			UNIT * 100,
-			0
-		));
-
-		assert_ne!(get_market(&MarketId::zero()), market);
-		assert_ne!(get_vamm(&market.vamm_id), vamm_state);
-	})
-}
-
-#[test]
-fn should_succeed_with_two_traders_in_a_market() {
-	ExtBuilder {
-		native_balances: vec![(ALICE, UNIT), (BOB, UNIT)],
-		balances: vec![
-			(ALICE, PICA, UNIT),
-			(BOB, PICA, UNIT),
-			(ALICE, USDC, UNIT * 100),
-			(BOB, USDC, UNIT * 100),
-		],
-		..Default::default()
-	}
-	.build()
-	.execute_with(|| {
-		let asset_id = PICA;
-		set_oracle_for(asset_id, 1_000);
-
-		let config = MarketConfig {
-			asset: asset_id,
-			vamm_config: VammConfig {
-				base_asset_reserves: UNIT * 100,
-				quote_asset_reserves: UNIT * 100_000,
-				peg_multiplier: 1,
-				twap_period: ONE_HOUR,
-			},
+	#[test]
+	fn should_succeed_in_opening_first_position() {
+		ExtBuilder {
+			native_balances: vec![(ALICE, UNIT), (BOB, UNIT)],
+			balances: vec![(ALICE, PICA, UNIT), (BOB, PICA, UNIT), (BOB, USDC, UNIT * 100)],
 			..Default::default()
-		};
-		assert_ok!(TestPallet::create_market(Origin::signed(ALICE), config));
-		let market_id = Zero::zero();
-		let market = get_market(&market_id);
-		let vamm_state_before = get_vamm(&market.vamm_id);
+		}
+		.build()
+		.execute_with(|| {
+			set_oracle_for(DOT, 1_000);
+			let config = MarketConfig {
+				asset: DOT,
+				vamm_config: VammConfig {
+					base_asset_reserves: UNIT * 100,
+					quote_asset_reserves: UNIT * 10_000,
+					peg_multiplier: 10,
+					twap_period: ONE_HOUR,
+				},
+				margin_ratio_initial: Decimal::from_float(0.1),
+				margin_ratio_maintenance: Decimal::from_float(0.02),
+				margin_ratio_partial: Decimal::from_float(0.04),
+				minimum_trade_size: 0.into(),
+				funding_frequency: ONE_HOUR,
+				funding_period: ONE_HOUR * 24,
+				taker_fee: 0,
+				twap_period: ONE_HOUR,
+			};
+			assert_ok!(TestPallet::create_market(Origin::signed(ALICE), config));
 
-		assert_ok!(TestPallet::deposit_collateral(Origin::signed(ALICE), USDC, UNIT * 100));
-		assert_ok!(TestPallet::deposit_collateral(Origin::signed(BOB), USDC, UNIT * 100));
+			assert_ok!(TestPallet::deposit_collateral(Origin::signed(BOB), USDC, UNIT * 100));
 
-		assert_ok!(TestPallet::open_position(
-			Origin::signed(ALICE),
-			market_id,
-			Long,
-			UNIT * 100,
-			0
-		));
-		assert_ok!(TestPallet::open_position(Origin::signed(BOB), market_id, Long, UNIT * 100, 0));
+			let market = get_market(&MarketId::zero());
+			let vamm_state = get_vamm(&market.vamm_id);
 
-		assert_ok!(TestPallet::close_position(Origin::signed(ALICE), market_id));
-		assert_ok!(TestPallet::close_position(Origin::signed(BOB), market_id));
+			assert_ok!(TestPallet::open_position(
+				Origin::signed(BOB),
+				Zero::zero(),
+				Long,
+				UNIT * 100,
+				0
+			));
 
-		// Alice closes her position in profit, Bob closes his position in loss
-		// However, since Alice closes her position first, there are no realized losses in the
-		// market yet, so her profits are outstanding
-		let alice_col = get_collateral(&ALICE);
-		let alice_outstanding_profits = get_outstanding_profits(&ALICE);
-		let bob_col = get_collateral(&BOB);
-		assert!(alice_col + alice_outstanding_profits > bob_col);
-		assert_eq!(alice_col + alice_outstanding_profits + bob_col, UNIT * 200);
+			assert_ne!(get_market(&MarketId::zero()), market);
+			assert_ne!(get_vamm(&market.vamm_id), vamm_state);
+		})
+	}
 
-		assert_ok!(TestPallet::withdraw_collateral(
-			Origin::signed(ALICE),
-			alice_col + alice_outstanding_profits
-		));
+	#[test]
+	fn should_succeed_with_two_traders_in_a_market() {
+		ExtBuilder {
+			native_balances: vec![(ALICE, UNIT), (BOB, UNIT)],
+			balances: vec![
+				(ALICE, PICA, UNIT),
+				(BOB, PICA, UNIT),
+				(ALICE, USDC, UNIT * 100),
+				(BOB, USDC, UNIT * 100),
+			],
+			..Default::default()
+		}
+		.build()
+		.execute_with(|| {
+			let asset_id = PICA;
+			set_oracle_for(asset_id, 1_000);
 
-		// vAMM is back to its initial state due to path independence
-		let vamm_state_after = get_vamm(&market.vamm_id);
-		assert_eq!(vamm_state_before.base_asset_reserves, vamm_state_after.base_asset_reserves);
-		assert_eq!(vamm_state_before.quote_asset_reserves, vamm_state_after.quote_asset_reserves);
-	})
+			let config = MarketConfig {
+				asset: asset_id,
+				vamm_config: VammConfig {
+					base_asset_reserves: UNIT * 100,
+					quote_asset_reserves: UNIT * 100_000,
+					peg_multiplier: 1,
+					twap_period: ONE_HOUR,
+				},
+				..Default::default()
+			};
+			assert_ok!(TestPallet::create_market(Origin::signed(ALICE), config));
+			let market_id = Zero::zero();
+			let market = get_market(&market_id);
+			let vamm_state_before = get_vamm(&market.vamm_id);
+
+			assert_ok!(TestPallet::deposit_collateral(Origin::signed(ALICE), USDC, UNIT * 100));
+			assert_ok!(TestPallet::deposit_collateral(Origin::signed(BOB), USDC, UNIT * 100));
+
+			assert_ok!(TestPallet::open_position(
+				Origin::signed(ALICE),
+				market_id,
+				Long,
+				UNIT * 100,
+				0
+			));
+			assert_ok!(TestPallet::open_position(
+				Origin::signed(BOB),
+				market_id,
+				Long,
+				UNIT * 100,
+				0
+			));
+
+			assert_ok!(TestPallet::close_position(Origin::signed(ALICE), market_id));
+			assert_ok!(TestPallet::close_position(Origin::signed(BOB), market_id));
+
+			// Alice closes her position in profit, Bob closes his position in loss
+			// However, since Alice closes her position first, there are no realized losses in the
+			// market yet, so her profits are outstanding
+			let alice_col = get_collateral(&ALICE);
+			let alice_outstanding_profits = get_outstanding_profits(&ALICE);
+			let bob_col = get_collateral(&BOB);
+			assert!(alice_col + alice_outstanding_profits > bob_col);
+			assert_eq!(alice_col + alice_outstanding_profits + bob_col, UNIT * 200);
+
+			assert_ok!(TestPallet::withdraw_collateral(
+				Origin::signed(ALICE),
+				alice_col + alice_outstanding_profits
+			));
+
+			// vAMM is back to its initial state due to path independence
+			let vamm_state_after = get_vamm(&market.vamm_id);
+			assert_eq!(vamm_state_before.base_asset_reserves, vamm_state_after.base_asset_reserves);
+			assert_eq!(
+				vamm_state_before.quote_asset_reserves,
+				vamm_state_after.quote_asset_reserves
+			);
+		})
+	}
 }
 
 // -------------------------------------------------------------------------------------------------
 //                                     Update Funding
 // -------------------------------------------------------------------------------------------------
 
-#[test]
-fn should_update_oracle_twap() {
-	ExtBuilder { native_balances: vec![(ALICE, UNIT), (BOB, UNIT)], ..Default::default() }
+mod update_funding {
+	use super::*;
+
+	#[test]
+	fn should_update_oracle_twap() {
+		ExtBuilder { native_balances: vec![(ALICE, UNIT), (BOB, UNIT)], ..Default::default() }
+			.build()
+			.execute_with(|| {
+				let asset_id = DOT;
+				set_oracle_for(asset_id, 1_000); // Index price = 10.0
+
+				let config = MarketConfig {
+					asset: asset_id,
+					vamm_config: VammConfig {
+						// Mark price = 10.0
+						base_asset_reserves: UNIT * 10_000,
+						quote_asset_reserves: UNIT * 100_000,
+						peg_multiplier: 1,
+						twap_period: ONE_HOUR,
+					},
+					..Default::default()
+				};
+				assert_ok!(TestPallet::create_market(Origin::signed(ALICE), config.clone()));
+
+				let market_id = Zero::zero();
+				let market = get_market(&market_id);
+				let vamm = get_vamm(&market.vamm_id);
+
+				assert_eq!(market.last_oracle_price, 10.into());
+				assert_eq!(market.last_oracle_twap, 10.into());
+				assert_eq!(vamm.base_asset_twap, 10.into());
+
+				update_oracle_for(asset_id, 1_100); //  Index price = 11.0
+				run_to_time(market.last_oracle_ts + config.twap_period);
+				assert_ok!(TestPallet::update_funding(Origin::signed(ALICE), market_id));
+				let market = get_market(&market_id);
+				// Oracle price updates are clipped at 10bps from the previous recorded price
+				assert_eq!(market.last_oracle_price, (1001, 100).into());
+				assert!(market.last_oracle_twap > 10.into());
+			})
+	}
+
+	#[test]
+	fn should_update_vamm_twap() {
+		ExtBuilder {
+			native_balances: vec![(ALICE, UNIT), (BOB, UNIT)],
+			balances: vec![(ALICE, USDC, UNIT * 100)],
+			..Default::default()
+		}
 		.build()
 		.execute_with(|| {
 			let asset_id = DOT;
-			set_oracle_for(asset_id, 1_000); // Index price = 10.0
+			set_oracle_for(asset_id, 1_000);
 
 			let config = MarketConfig {
 				asset: asset_id,
@@ -324,74 +387,33 @@ fn should_update_oracle_twap() {
 			};
 			assert_ok!(TestPallet::create_market(Origin::signed(ALICE), config.clone()));
 
+			assert_ok!(TestPallet::deposit_collateral(Origin::signed(ALICE), USDC, UNIT * 100));
+
 			let market_id = Zero::zero();
 			let market = get_market(&market_id);
-			let vamm = get_vamm(&market.vamm_id);
+			let vamm_before = get_vamm(&market.vamm_id);
 
 			assert_eq!(market.last_oracle_price, 10.into());
 			assert_eq!(market.last_oracle_twap, 10.into());
-			assert_eq!(vamm.base_asset_twap, 10.into());
+			assert_eq!(vamm_before.base_asset_twap, 10.into());
 
-			update_oracle_for(asset_id, 1_100); //  Index price = 11.0
+			assert_ok!(TestPallet::open_position(
+				Origin::signed(ALICE),
+				market_id,
+				Long,
+				UNIT * 100,
+				0
+			));
+			let vamm_after = get_vamm(&market.vamm_id);
+			// open_position should update TWAP before swapping, therefore not changing the mark
+			// TWAP
+			assert_eq!(vamm_before.base_asset_twap, vamm_after.base_asset_twap);
+			let vamm_before = vamm_after;
+
 			run_to_time(market.last_oracle_ts + config.twap_period);
 			assert_ok!(TestPallet::update_funding(Origin::signed(ALICE), market_id));
-			let market = get_market(&market_id);
-			// Oracle price updates are clipped at 10bps from the previous recorded price
-			assert_eq!(market.last_oracle_price, (1001, 100).into());
-			assert!(market.last_oracle_twap > 10.into());
+			let vamm_after = get_vamm(&market.vamm_id);
+			assert!(vamm_before.base_asset_twap < vamm_after.base_asset_twap);
 		})
-}
-
-#[test]
-fn should_update_vamm_twap() {
-	ExtBuilder {
-		native_balances: vec![(ALICE, UNIT), (BOB, UNIT)],
-		balances: vec![(ALICE, USDC, UNIT * 100)],
-		..Default::default()
 	}
-	.build()
-	.execute_with(|| {
-		let asset_id = DOT;
-		set_oracle_for(asset_id, 1_000);
-
-		let config = MarketConfig {
-			asset: asset_id,
-			vamm_config: VammConfig {
-				// Mark price = 10.0
-				base_asset_reserves: UNIT * 10_000,
-				quote_asset_reserves: UNIT * 100_000,
-				peg_multiplier: 1,
-				twap_period: ONE_HOUR,
-			},
-			..Default::default()
-		};
-		assert_ok!(TestPallet::create_market(Origin::signed(ALICE), config.clone()));
-
-		assert_ok!(TestPallet::deposit_collateral(Origin::signed(ALICE), USDC, UNIT * 100));
-
-		let market_id = Zero::zero();
-		let market = get_market(&market_id);
-		let vamm_before = get_vamm(&market.vamm_id);
-
-		assert_eq!(market.last_oracle_price, 10.into());
-		assert_eq!(market.last_oracle_twap, 10.into());
-		assert_eq!(vamm_before.base_asset_twap, 10.into());
-
-		assert_ok!(TestPallet::open_position(
-			Origin::signed(ALICE),
-			market_id,
-			Long,
-			UNIT * 100,
-			0
-		));
-		let vamm_after = get_vamm(&market.vamm_id);
-		// open_position should update TWAP before swapping, therefore not changing the mark TWAP
-		assert_eq!(vamm_before.base_asset_twap, vamm_after.base_asset_twap);
-		let vamm_before = vamm_after;
-
-		run_to_time(market.last_oracle_ts + config.twap_period);
-		assert_ok!(TestPallet::update_funding(Origin::signed(ALICE), market_id));
-		let vamm_after = get_vamm(&market.vamm_id);
-		assert!(vamm_before.base_asset_twap < vamm_after.base_asset_twap);
-	})
 }
