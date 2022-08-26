01407c45714140105d0ccbf5ffa9435af2ab6538
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-08-26 14:56:12 +0500

    import query augmentation

--- frontend/apps/pablo/pages/_app.tsx
+++ frontend/apps/pablo/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "defi-interfaces/types-lookup.d";
 import "defi-interfaces/augment-api-tx";
 import "defi-interfaces/augment-api-rpc";
+import "defi-interfaces/augment-api-query";
 import "defi-interfaces/augment-types";
 
 import * as React from "react";
