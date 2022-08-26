a2454e85f32013784a96b3442d850298a142ec1f
cor <cor@pruijs.dev>
2022-08-27 01:24:01 +0200

    move devnet-xcvm-up to services system

--- flake.nix
+++ flake.nix
@@ -723,11 +723,7 @@
           # https://github.com/NixOS/nix/issues/5560
           apps =
             let
-              arion-pure = import ./.nix/arion-pure.nix {
-                inherit pkgs;
-                inherit packages;
-              };
-
+              arion-pure = import ./.nix/arion-pure.nix { inherit pkgs; inherit packages; };
               arion-up-program = pkgs.writeShellApplication {
                 name = "devnet-up";
                 runtimeInputs = [ pkgs.arion pkgs.docker pkgs.coreutils pkgs.bash ];
@@ -735,66 +731,26 @@
                   arion --prebuilt-file ${arion-pure} up --build --force-recreate -V --always-recreate-deps --remove-orphans
                 '';
               };
-
+          
+              devnet-xcvm = import ./.nix/arion-xcvm-pure.nix { inherit pkgs; inherit packages; };
+              devnet-xcvm-up-program = pkgs.writeShellApplication {
+                name = "devnet-xcvm-up";
+                runtimeInputs = [ pkgs.arion pkgs.docker pkgs.coreutils pkgs.bash ];
+                text = ''
+                  arion --prebuilt-file ${devnet-xcvm} up --build --force-recreate -V --always-recreate-deps --remove-orphans
+                '';
+              };
             in
             rec
-            {
-              devnet-xcvm-up =
-                let
-                  devnet-xcvm =
-                    pkgs.arion.build
-                      {
-                        modules = [
-                          ({ pkgs, ... }: {
-                            config = {
-                              project = {
-                                name = "devnet-xcvm";
-                              };
-                              services = {
-                                junod-testing-local = {
-                                  service = {
-                                    name = "junod-testing-local";
-                                    # NOTE: the do not release git hash tags, so not clear how to share client and docker image
-                                    image = "ghcr.io/cosmoscontracts/juno:v9.0.0";
-                                    environment = {
-                                      STAKE_TOKEN = "ujunox";
-                                      UNSAFE_CORS = "true";
-                                      USER = "juno16g2rahf5846rxzp3fwlswy08fz8ccuwk03k57y";
-                                      GAS_LIMIT = 100000000;
-                                    };
-                                    # TODO: mount proper genesis here as per
-                                    # "clip hire initial neck maid actor venue client foam budget lock catalog sweet steak waste crater broccoli pipe steak sister coyote moment obvious choose" > junod keys add alice --recover
-                                    # `"wasm":{"codes":[],"contracts":[],"gen_msgs":[],"params":{"code_upload_access":{"address":"","permission":"Everybody"},`
-                                    #network_mode 
-                                    command = ''
-                                      ./setup_and_run.sh juno16g2rahf5846rxzp3fwlswy08fz8ccuwk03k57y
-                                    '';
-                                    #network_mode = "host";
-                                    # these ports are open by default
-                                    ports = [
-                                      "1317:1317" # rest openapi
-                                      "26656:26656" # p2p
-                                      "26657:26657" # rpc json-rpc
-                                    ];
-                                  };
-                                };
-                              };
-                            };
-                          })
-                        ];
-                        inherit pkgs;
-                      }
-                  ;
-                in
-                {
-                  type = "app";
-                  program = "${pkgs.writeShellScript "arion-up" ''
-                  ${pkgs.arion}/bin/arion --prebuilt-file ${devnet-xcvm} up --build --force-recreate -V --always-recreate-deps --remove-orphans
-              ''}";
-              };
+            {              
               devnet-up = {
                 type = "app";
                 program = "${arion-up-program}/bin/devnet-up";
+              };
+
+              devnet-xcvm-up = {
+                type = "app";
+                program = "${devnet-xcvm-up-program}/bin/devnet-xcvm-up";
               };
 
               devnet-dali = {
@@ -808,7 +764,6 @@
               };
 
               kusama-picasso-karura-devnet = {
-                # nix run .#devnet
                 type = "app";
                 program =
                   "${packages.kusama-picasso-karura-devnet}/bin/kusama-picasso-karura";
