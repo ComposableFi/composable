9f2b893d30cd4153ec771f1d50fe93a53f60fec7
Dzmitry Lahoda <dzmitry@lahoda.pro>
2022-08-27 13:51:31 +0100

    try:

--- .devcontainer/devcontainer-base-image-per-system.nix
+++ .devcontainer/devcontainer-base-image-per-system.nix
@@ -3,13 +3,13 @@ let
   base-images = {
     x86_64-linux = {
       arch = "amd64";
-      sha256 = "65fde405acf4a8e9c228c62ebd677b3a817669c5d759ef2c8a95b0b7069285d3";
-      imageDigest = "sha256:65fde405acf4a8e9c228c62ebd677b3a817669c5d759ef2c8a95b0b7069285d3";
+      imageDigest = "sha256:61d1944c51e3b8bbbe3e08bf2fb580cb620951d7d4f6b01fdb3697ef40e4802b";
+      sha256 = "0lxdncasmrh07mcgg2nrkmzd6m3slbic36w9x0cxppii2496cq8q";
     };
     x86_64-darwin = {
       arch = "amd64";
-      sha256 = "65fde405acf4a8e9c228c62ebd677b3a817669c5d759ef2c8a95b0b7069285d3";
-      imageDigest = "sha256:65fde405acf4a8e9c228c62ebd677b3a817669c5d759ef2c8a95b0b7069285d3";
+      imageDigest = "sha256:61d1944c51e3b8bbbe3e08bf2fb580cb620951d7d4f6b01fdb3697ef40e4802b";
+      sha256 = "0lxdncasmrh07mcgg2nrkmzd6m3slbic36w9x0cxppii2496cq8q";
     };
     aarch64-linux = {
       arch = "arm64";
