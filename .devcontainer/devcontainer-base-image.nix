c518a6b4e2f70d8c4673c7fd06b1caa6718e78c5
Dzmitry Lahoda <dzmitry@lahoda.pro>
2022-08-28 12:48:59 +0100

    fmt

--- .devcontainer/devcontainer-base-image.nix
+++ .devcontainer/devcontainer-base-image.nix
@@ -1,7 +1,6 @@
 { pkgs ? import <nixpkgs>, system ? builtins.defaultSystem }:
 with pkgs;
-let
-  nix-to-container-image = import ./devcontainer-base-image-per-system.nix;
+let nix-to-container-image = import ./devcontainer-base-image-per-system.nix;
 in dockerTools.pullImage ((nix-to-container-image system) // {
   imageName = "mcr.microsoft.com/vscode/devcontainers/base";
   os = "linux";
