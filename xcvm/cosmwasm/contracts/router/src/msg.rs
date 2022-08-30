f8121382808e44b7a023c12e3dcf3e2e0dd08623
aeryz <abdullaheryz@protonmail.com>
2022-08-30 16:45:52 +0200

    feat(cosmwasm): router contract
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

--- xcvm/cosmwasm/contracts/router/src/msg.rs
+++ xcvm/cosmwasm/contracts/router/src/msg.rs
@@ -1,28 +1,22 @@
 use crate::state::UserId;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-use xcvm_core::NetworkId;
+use xcvm_core::{Funds, NetworkId};
+use xcvm_interpreter::msg::ExecuteMsg as InterpreterExecuteMsg;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
 	pub registry_address: String,
+	pub interpreter_code_id: u64,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-	NewInterpreter(NetworkId, UserId),
+	Run {
+		network_id: NetworkId,
+		user_id: UserId,
+		interpreter_execute_msg: InterpreterExecuteMsg,
+		funds: Funds,
+	},
 }
-
-/*
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
-pub enum QueryMsg {
-	GetAssetContract(XcvmAssetId),
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct GetAssetContractResponse {
-	pub addr: Addr,
-}
-*/
