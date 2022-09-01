66cbb6930d85901408a4ca8c1dfb1ce91b4d6cea
Dzmitry Lahoda <dzmitry@lahoda.pro>
2022-09-01 17:57:39 +0100

    fmt to pass ci
    Signed-off-by: Dzmitry Lahoda <dzmitry@lahoda.pro>

--- flake.nix
+++ flake.nix
@@ -350,6 +350,7 @@
             '';
           docs-renders = [ mdbook plantuml graphviz pandoc ];
 
+<<<<<<< HEAD
           mkFrontendStatic = { kusamaEndpoint, picassoEndpoint, karuraEndpoint
             , subsquidEndpoint }:
             let bp = pkgs.callPackage npm-buildpackage { };
@@ -403,10 +404,12 @@
               --execution=wasm
             '';
 
-          mk-xcvm-contract = name: crane-nightly.buildPackage (common-attrs // {
+          mk-xcvm-contract = name:
+            crane-nightly.buildPackage (common-attrs // {
               pnameSuffix = name;
-              cargoBuildCommand = "cargo build --target wasm32-unknown-unknown --profile cosmwasm-contracts -p ${name}";
-              RUSTFLAGS="-C link-arg=-s";
+              cargoBuildCommand =
+                "cargo build --target wasm32-unknown-unknown --profile cosmwasm-contracts -p ${name}";
+              RUSTFLAGS = "-C link-arg=-s";
             });
 
           subwasm = let
@@ -470,7 +473,8 @@
             inherit subwasm;
             inherit subwasm-release-body;
 
-            xcvm-contract-asset-registry = mk-xcvm-contract "xcvm-asset-registry";
+            xcvm-contract-asset-registry =
+              mk-xcvm-contract "xcvm-asset-registry";
             xcvm-contract-router = mk-xcvm-contract "xcvm-router";
             xcvm-contract-interpreter = mk-xcvm-contract "xcvm-interpreter";
 
