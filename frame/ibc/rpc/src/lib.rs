a1daea61cc59c7f8e253acdc11a0e368bc2258d0
David Salami <wizdave97@gmail.com>
2022-09-02 14:54:10 +0100

    make rpc export public

--- frame/ibc/rpc/src/lib.rs
+++ frame/ibc/rpc/src/lib.rs
@@ -96,9 +96,9 @@ pub enum BlockNumberOrHash<Hash> {
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct HeightAndTimestamp {
 	/// Height
-	height: Height,
+	pub height: Height,
 	/// Timestamp nanso seconds
-	timestamp: u64,
+	pub timestamp: u64,
 }
 
 impl<Hash: std::fmt::Debug> Display for BlockNumberOrHash<Hash> {
