c8832b98a30f8b899596634c6a8760f1834aceee
Mamali <isthereanybody@gmail.com>
2022-09-05 20:11:40 +0430

    fix typo on component name

--- frontend/apps/picasso-storybook/stories/molecules/myBondingsTable.stories.tsx
+++ frontend/apps/picasso-storybook/stories/molecules/myBondingsTable.stories.tsx
@@ -1,32 +1,32 @@
-import { MyBondingsTable } from "picasso/components";
-import { ComponentStory, Story } from "@storybook/react";
+import { MyBondsTable } from "picasso/components";
+import { ComponentStory } from "@storybook/react";
 import { TOKENS } from "tokens";
 
 export default {
   title: "molecules/MyBondsTable",
-  component: MyBondingsTable
+  component: MyBondsTable,
 };
 
-const Template: ComponentStory<typeof MyBondingsTable> = args => (
-  <MyBondingsTable {...args} />
+const Template: ComponentStory<typeof MyBondsTable> = (args) => (
+  <MyBondsTable {...args} />
 );
 
-export const MyBondingsTableStory = Template.bind({});
-MyBondingsTableStory.args = {
+export const MyBondsTableStory = Template.bind({});
+MyBondsTableStory.args = {
   assets: [
     {
       token: TOKENS["ksm"],
       toToken: TOKENS["pica"],
       claimable: 543,
       pending: 123,
-      vesting_time: "4D 2H 43M"
+      vesting_time: "4D 2H 43M",
     },
     {
       token: TOKENS["pica"],
       toToken: TOKENS["ksm"],
       claimable: 543,
       pending: 123,
-      vesting_time: "4D 2H 43M"
-    }
-  ]
+      vesting_time: "4D 2H 43M",
+    },
+  ],
 };
