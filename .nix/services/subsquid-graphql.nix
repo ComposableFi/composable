b52bd9119cfed3b8ad7234b138c27b20b28f1cd4
cor <cor@pruijs.dev>
2022-09-06 12:59:37 +0200

    feat: nixify subsquid-graphql

--- .nix/services/subsquid-graphql.nix
+++ .nix/services/subsquid-graphql.nix
@@ -1,7 +1,26 @@
-{ database, pkgs, ...}:
+{ database, pkgs, graphqlPort, ...}:
 {
   service = {
-    build.context = ../../../subsquid;
-    build.dockerfile = ../../../subsquid/graphql.Dockerfile;
+    build.context = "${../../subsquid}";
+    depends_on = {
+      "${database.name}" = {
+        condition = "service_healthy";
+      };
+    
+    };
+    ports = [
+      "${toString graphqlPort}:4000"
+    ];
+    environment = {
+      DB_NAME = database.name; 
+      DB_HOST = database.host;
+      DB_PASS = database.password;
+      DB_PORT_PG = database.port;
+    };
   };
+
+  # Unfortunately, arion does not model this field yet.
+  # We can add a PR that adds it right below this option 
+  # https://github.com/hercules-ci/arion/blob/e5fb978143240f8d293e6e5acc9691acf472928d/src/nix/modules/service/docker-compose-service.nix#L66  
+  out.service.build.dockerfile = "graphql.Dockerfile";
 }
