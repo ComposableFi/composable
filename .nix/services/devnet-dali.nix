2856110c52980bbf5d5e2ef6737509aa5367ce40
cor <cor@pruijs.dev>
2022-09-06 02:12:29 +0200

    fmt ports

--- .nix/services/devnet-dali.nix
+++ .nix/services/devnet-dali.nix
@@ -12,8 +12,12 @@
         ${packages.devnet-dali}/bin/run-devnet-dali-dev
       ''
     ];
-    ports =
-      [ "${toString relaychainPort}:9944" "${toString parachainPort}:9988" ];
+    ports = [
+      "${toString relaychainPort}:9944"
+      "${toString parachainPort}:9988"
+    ];
     stop_signal = "SIGINT";
   };
 }
+
+
