fe16614448813ad05d348663a6853fee03160031
Andrés Mechali <andresm@composable.finance>
2022-09-12 10:48:47 +0200

    ignore spellchecking on generated folders

--- subsquid/scripts/reset-db.sh
+++ subsquid/scripts/reset-db.sh
@@ -1,9 +1,8 @@
 set -e
-npm run typegen
 npx squid-typeorm-codegen
 npm run build
 #rm -rf db/migrations/*.js
 npx squid-typeorm-migration
-npx squid-typeorm-migration create
+npx squid-typeorm-migration generate
 #npx sqd db create-migration Init
 npx squid-typeorm-migration migrate
