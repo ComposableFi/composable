3dbc517b6a4e5c1514ba218965db0f1342130a50
belousm <mike.belous@gmail.com>
2022-09-14 13:51:28 +0300

    fix(instrumental): fix `runtime`

--- frame/instrumental/src/mock/runtime.rs
+++ frame/instrumental/src/mock/runtime.rs
@@ -1,6 +1,7 @@
+use composable_traits::{account_proxy::ProxyType, fnft::FnftAccountProxyType};
 use frame_support::{
 	ord_parameter_types, parameter_types,
-	traits::{Everything, GenesisBuild},
+	traits::{Everything, GenesisBuild, InstanceFilter},
 	PalletId,
 };
 use frame_system::{EnsureRoot, EnsureSigned, EnsureSignedBy};
@@ -11,7 +12,7 @@ use primitives::currency::{CurrencyId, ValidateCurrencyId};
 use sp_core::H256;
 use sp_runtime::{
 	testing::Header,
-	traits::{ConvertInto, IdentityLookup},
+	traits::{BlakeTwo256, ConstU32, ConvertInto, IdentityLookup},
 	Permill,
 };
 
@@ -23,6 +24,9 @@ pub type PoolId = u128;
 pub type VaultId = u64;
 pub type Moment = composable_traits::time::Timestamp;
 pub type Amount = i128;
+pub type RewardPoolId = u16;
+pub type PositionId = u128;
+pub type FinancialNftInstanceId = u64;
 // These time units are defined in number of blocks.
 pub const MILLISECS_PER_BLOCK: Moment = 3000;
 pub const SECS_PER_BLOCK: Moment = MILLISECS_PER_BLOCK / 1000;
@@ -213,6 +217,79 @@ impl pallet_timestamp::Config for MockRuntime {
 }
 
 // -------------------------------------------------------------------------------------------------
+//                                             Proxy
+// -------------------------------------------------------------------------------------------------
+
+parameter_types! {
+	pub MaxProxies : u32 = 4;
+	pub MaxPending : u32 = 32;
+	// just make dali simple to proxy
+	pub ProxyPrice: u32 = 0;
+}
+
+impl pallet_account_proxy::Config for MockRuntime {
+	type Event = Event;
+	type Call = Call;
+	type Currency = ();
+	type ProxyType = ProxyType;
+	type ProxyDepositBase = ProxyPrice;
+	type ProxyDepositFactor = ProxyPrice;
+	type MaxProxies = MaxProxies;
+	type WeightInfo = ();
+	type MaxPending = MaxPending;
+	type CallHasher = BlakeTwo256;
+	type AnnouncementDepositBase = ProxyPrice;
+	type AnnouncementDepositFactor = ProxyPrice;
+}
+
+// -------------------------------------------------------------------------------------------------
+//                                             FNFT
+// -------------------------------------------------------------------------------------------------
+
+parameter_types! {
+	pub const FnftPalletId: PalletId = PalletId(*b"pal_fnft");
+}
+
+impl pallet_fnft::Config for MockRuntime {
+	type Event = Event;
+
+	type MaxProperties = ConstU32<16>;
+	type FinancialNftCollectionId = CurrencyId;
+	type FinancialNftInstanceId = FinancialNftInstanceId;
+	type ProxyType = ProxyType;
+	type AccountProxy = Proxy;
+	type ProxyTypeSelector = FnftAccountProxyType;
+	type PalletId = FnftPalletId;
+}
+
+// -------------------------------------------------------------------------------------------------
+//                                             Staking rewards
+// -------------------------------------------------------------------------------------------------
+
+parameter_types! {
+	pub const StakingRewardsPalletId: PalletId = PalletId(*b"stk_rwrd");
+}
+impl pallet_staking_rewards::Config for MockRuntime {
+	type Event = Event;
+	type Balance = Balance;
+	type RewardPoolId = RewardPoolId;
+	type PositionId = PositionId;
+	type AssetId = CurrencyId;
+	type Assets = Tokens;
+	type CurrencyFactory = LpTokenFactory;
+	type UnixTime = Timestamp;
+	type ReleaseRewardsPoolsBatchSize = frame_support::traits::ConstU8<13>;
+	type PalletId = StakingRewardsPalletId;
+	type MaxStakingDurationPresets = MaxStakingDurationPresets;
+	type MaxRewardConfigsPerPool = MaxRewardConfigsPerPool;
+	type RewardPoolCreationOrigin = EnsureRoot<Self::AccountId>;
+	type WeightInfo = ();
+	type RewardPoolUpdateOrigin = EnsureRoot<Self::AccountId>;
+	type FinancialNftInstanceId = u64;
+	type FinancialNft = FinancialNft;
+}
+
+// -------------------------------------------------------------------------------------------------
 //                                            Pablo (AMM)
 // -------------------------------------------------------------------------------------------------
 
@@ -223,6 +300,10 @@ parameter_types! {
 	pub const MaxInitialWeight: Permill = Permill::from_percent(95);
 	pub const MinFinalWeight: Permill = Permill::from_percent(5);
 	pub const TWAPInterval: Moment = MILLISECS_PER_BLOCK * 10;
+	pub const MaxStakingRewardPools: u32 = 10;
+	pub const MaxRewardConfigsPerPool: u32 = 10;
+	pub const MaxStakingDurationPresets: u32 = 10;
+	pub const MillisecsPerBlock: u32 = 12000;
 }
 
 impl pallet_pablo::Config for MockRuntime {
@@ -233,14 +314,21 @@ impl pallet_pablo::Config for MockRuntime {
 	type CurrencyFactory = LpTokenFactory;
 	type Assets = Assets;
 	type PoolId = PoolId;
+	type RewardPoolId = RewardPoolId;
 	type PalletId = PabloPalletId;
 	type LocalAssets = LpTokenFactory;
 	type LbpMinSaleDuration = MinSaleDuration;
+	type MaxStakingDurationPresets = MaxStakingDurationPresets;
 	type LbpMaxSaleDuration = MaxSaleDuration;
+	type MaxStakingRewardPools = MaxStakingRewardPools;
 	type LbpMaxInitialWeight = MaxInitialWeight;
+	type ManageStaking = StakingRewards;
+	type MaxRewardConfigsPerPool = MaxRewardConfigsPerPool;
 	type LbpMinFinalWeight = MinFinalWeight;
 	type PoolCreationOrigin = EnsureSigned<Self::AccountId>;
 	type EnableTwapOrigin = EnsureRoot<AccountId>;
+	type ProtocolStaking = StakingRewards;
+	type MsPerBlock = MillisecsPerBlock;
 	type Time = Timestamp;
 	type TWAPInterval = TWAPInterval;
 	type WeightInfo = ();
@@ -282,6 +370,7 @@ impl instrumental_strategy_pablo::Config for MockRuntime {
 	type WeightInfo = ();
 	type AssetId = CurrencyId;
 	type Balance = Balance;
+	type Convert = ConvertInto;
 	type VaultId = VaultId;
 	type Vault = Vault;
 	type MaxAssociatedVaults = MaxAssociatedVaults;
@@ -352,6 +441,9 @@ frame_support::construct_runtime!(
 		GovernanceRegistry: pallet_governance_registry::{Pallet, Call, Storage, Event<T>},
 		Timestamp: pallet_timestamp::{Pallet, Call, Storage},
 		CollectiveInstrumental: pallet_collective::<Instance1>::{Pallet, Call, Event<T>, Origin<T>, Config<T>},
+		StakingRewards: pallet_staking_rewards::{Pallet, Storage, Call, Event<T>},
+		FinancialNft: pallet_fnft::{Pallet, Storage, Event<T>},
+		Proxy: pallet_account_proxy::{Pallet, Storage, Call, Event<T>},
 
 		LpTokenFactory: pallet_currency_factory::{Pallet, Storage, Event<T>},
 
@@ -410,6 +502,42 @@ impl ExtBuilder {
 			self.native_balances.push((user, balance));
 		} else {
 			self.balances.push((user, asset, balance));
+		}
+	}
+}
+
+impl InstanceFilter<Call> for ProxyType {
+	fn filter(&self, c: &Call) -> bool {
+		match self {
+			ProxyType::Any => true,
+			ProxyType::Governance => matches!(
+				c,
+				// TODO democracy
+				Call::System(..)
+			),
+			// ProxyType::Staking => {
+			// 	matches!(c, Call::Staking(..) | Call::Session(..) | Call::Utility(..))
+			// },
+			// ProxyType::IdentityJudgement => matches!(
+			// 	c,
+			// 	Call::Identity(pallet_identity::Call::provide_judgement { .. }) | Call::Utility(..)
+			// ),
+			// ProxyType::CancelProxy => {
+			// 	matches!(c, Call::Proxy(pallet_proxy::Call::reject_announcement { .. }))
+			// },
+			// ProxyType::Auction => matches!(
+			// 	c,
+			// 	Call::Auctions(..) | Call::Crowdloan(..) | Call::Registrar(..) | Call::Slots(..)
+			// ),
+			_ => false,
+		}
+	}
+	fn is_superset(&self, o: &Self) -> bool {
+		match (self, o) {
+			(x, y) if x == y => true,
+			(ProxyType::Any, _) => true,
+			(_, ProxyType::Any) => false,
+			_ => false,
 		}
 	}
 }
