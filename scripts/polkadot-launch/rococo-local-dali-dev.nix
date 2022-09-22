81e6e0b5f746aed692300aae0a281310298ee5f1
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2022-09-22 15:12:22 +0200

    feat(nix): fixup `composable` node name and integrate revision

--- scripts/polkadot-launch/rococo-local-dali-dev.nix
+++ scripts/polkadot-launch/rococo-local-dali-dev.nix
@@ -29,7 +29,7 @@ in {
       wsPort = 9988;
       count = 3;
       chain = "dali-dev";
-      bin = "${composable-bin}/bin/composable-node";
+      bin = "${composable-bin}/bin/composable";
       flags = [
         "--unsafe-ws-external"
         "--unsafe-rpc-external"
