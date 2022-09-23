1c26cdf745c7507ecefc61a3e078992c190f6ccb
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-09-23 16:46:37 +0500

    Revert FE change

--- frontend/apps/picasso/stores/defi/polkadot/crowdloanRewards/CrowdloanRewardsUpdater.tsx
+++ frontend/apps/picasso/stores/defi/polkadot/crowdloanRewards/CrowdloanRewardsUpdater.tsx
@@ -169,11 +169,10 @@ const CrowdloanRewardsUpdater = () => {
         netVestedPICA = (rewards as any)[addr];
       }
       // dev
-      if (process.env.NODE_ENV !== "production") {
+      if (process.env.VERCEL_ENV !== "production") {
         if (addr && (devRewards as any)[addr]) {
           netVestedPICA = (devRewards as any)[addr];
         }
-        console.log(addr, devRewards)
       }
 
       if (addr && (contributions.contributedAmountsWithoutBoost as any)[addr]) {
