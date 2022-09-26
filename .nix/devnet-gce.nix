31dff4416f8102866f288638352e1fa6219dc202
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2022-09-26 11:48:19 -0500

    feat(devnet): persistent devnet with dispatch workflow

--- .nix/devnet-gce.nix
+++ .nix/devnet-gce.nix
@@ -1,8 +1,4 @@
-{ gce-input, book, devnet }:
-let
-  description = "What machines we will deploy on GCE";
-  machine-name = "composable-devnet-${devnet.chain-spec}";
-in {
+{ gce-input, book, devnet, disk-size, machine-name, domain }: {
   resources.gceNetworks.composable-devnet = gce-input // {
     name = "composable-devnet-network";
     firewall = {
@@ -24,7 +20,7 @@ in {
         network = resources.gceNetworks.composable-devnet;
         region = "europe-central2-c";
         instanceType = "n2-standard-4";
-        rootDiskSize = 200;
+        rootDiskSize = disk-size;
         tags = [ "http" "https" ];
       };
     };
@@ -50,8 +46,6 @@ in {
       defaults = { email = "hussein@composable.finance"; };
     };
     services.nginx = let
-      runtimeName = pkgs.lib.removeSuffix "-dev" devnet.chain-spec;
-      domain = "${runtimeName}.devnets.composablefinance.ninja";
       virtualConfig = let
         routify-nodes = prefix:
           map (node: (node // { name = prefix + node.name; }));
@@ -64,26 +58,20 @@ in {
       in {
         enableACME = true;
         forceSSL = true;
-        locations = builtins.foldl' (x: y: x // y) {
-          # TODO: make sure we build doc
-          # "= /doc/" = {
-          #   return = "301 https://${domain}/doc/composable/index.html";
-          # };
-          # "= /doc" = {
-          #   return = "301 https://${domain}/doc/composable/index.html";
-          # };
-          # "/doc/" = { root = devnet.documentation; };
-          "/" = { root = "${book}/book"; };
-        } (map (node: {
-          "/${node.name}" = {
-            proxyPass = "http://127.0.0.1:${builtins.toString node.wsPort}";
-            proxyWebsockets = true;
-            extraConfig = ''
-              proxy_set_header Origin "";
-              proxy_set_header Host 127.0.0.1:${builtins.toString node.wsPort};
-            '';
-          };
-        }) routified-nodes);
+        locations =
+          builtins.foldl' (x: y: x // y) { "/" = { root = "${book}/book"; }; }
+          (map (node: {
+            "/${node.name}" = {
+              proxyPass = "http://127.0.0.1:${builtins.toString node.wsPort}";
+              proxyWebsockets = true;
+              extraConfig = ''
+                proxy_set_header Origin "";
+                proxy_set_header Host 127.0.0.1:${
+                  builtins.toString node.wsPort
+                };
+              '';
+            };
+          }) routified-nodes);
       };
     in {
       enable = true;
--- .nix/devnet.nix
+++ .nix/devnet.nix
@@ -1,18 +1,28 @@
 { nixpkgs, devnet-dali, devnet-picasso, book, gce-input }:
 let
-  description =
-    "Derives Dali and Picasso releases from remote branches with relevant remote depoyments and documentation";
+  persistent-machine = import ./devnet-gce.nix {
+    inherit gce-input;
+    inherit book;
+    devnet = devnet-dali;
+    disk-size = 200;
+    machine-name = "composable-persistent-devnet";
+    domain = "persistent.devnets.composablefinance.ninja";
+  };
 in builtins.foldl' (machines: devnet:
   let
     machine = import ./devnet-gce.nix {
       inherit gce-input;
       inherit devnet;
       inherit book;
+      disk-size = 200;
+      machine-name = "composable-devnet-${devnet.chain-spec}";
+      domain = let prefix = nixpkgs.lib.removeSuffix "-dev" devnet.chain-spec;
+      in "${prefix}.devnets.composablefinance.ninja";
     };
-  in machines // machine) {
+  in machines // machine) ({
     inherit nixpkgs;
     network = {
       description = "Composable Devnet";
       storage.legacy = { };
     };
-  } [ devnet-dali devnet-picasso ]
+  } // persistent-machine) [ devnet-dali devnet-picasso ]
