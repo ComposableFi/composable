2bfe45cde9bb56f78caa4b7132b163ed2f12a25a
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2022-09-26 09:48:32 -0500

    feat(legacy-build): nightly prefix for rustup

--- legacy-build.sh
+++ legacy-build.sh
@@ -2,14 +2,14 @@
 #set -e
 
 cd code && \
-cargo build --release -p wasm-optimizer && \
-cargo build --release -p composable-runtime-wasm --target wasm32-unknown-unknown && \
-cargo build --release -p picasso-runtime-wasm --target wasm32-unknown-unknown && \
-cargo build --release -p dali-runtime-wasm --target wasm32-unknown-unknown && \
+cargo +nightly build --release -p wasm-optimizer && \
+cargo +nightly build --release -p composable-runtime-wasm --target wasm32-unknown-unknown && \
+cargo +nightly build --release -p picasso-runtime-wasm --target wasm32-unknown-unknown && \
+cargo +nightly build --release -p dali-runtime-wasm --target wasm32-unknown-unknown && \
 ./target/release/wasm-optimizer --input ./target/wasm32-unknown-unknown/release/dali_runtime.wasm --output ./target/wasm32-unknown-unknown/release/dali_runtime.optimized.wasm && \
 ./target/release/wasm-optimizer --input ./target/wasm32-unknown-unknown/release/picasso_runtime.wasm --output ./target/wasm32-unknown-unknown/release/picasso_runtime.optimized.wasm && \
 ./target/release/wasm-optimizer --input ./target/wasm32-unknown-unknown/release/composable_runtime.wasm --output ./target/wasm32-unknown-unknown/release/composable_runtime.optimized.wasm && \
 export DALI_RUNTIME=$(realpath ./target/wasm32-unknown-unknown/release/dali_runtime.optimized.wasm)
 export PICASSO_RUNTIME=$(realpath ./target/wasm32-unknown-unknown/release/picasso_runtime.optimized.wasm)
 export COMPOSABLE_RUNTIME=$(realpath ./target/wasm32-unknown-unknown/release/composable_runtime.optimized.wasm)
-cargo build --release --package composable --features=builtin-wasm
+cargo +nightly build --release --package composable --features=builtin-wasm
