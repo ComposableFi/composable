88380271ce1d134a4d59f6adcc85ba35ec098edc
hussein <hussein.aitlahcen@gmail.com>
2022-10-10 15:05:30 +0200

    chore: format

--- .nix/services/devnet.nix
+++ .nix/services/devnet.nix
@@ -5,11 +5,7 @@
   };
   service = {
     restart = "always";
-    command = [
-      "sh"
-      "-c"
-      "${pkgs.lib.meta.getExe devnet}"
-    ];
+    command = [ "sh" "-c" "${pkgs.lib.meta.getExe devnet}" ];
     ports = builtins.map
       ({ host, container }: "${toString host}:${toString container}") ports;
     stop_signal = "SIGINT";
