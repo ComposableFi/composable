e53a19fc3c0abc292a48b342bf3918d06fbc7289
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-10-10 23:18:43 +0500

    Update 1665425909288-Data.js

--- subsquid/db/migrations/1665425909288-Data.js
+++ subsquid/db/migrations/1665425909288-Data.js
@@ -1,5 +1,5 @@
-module.exports = class Data1664782175476 {
-  name = 'Data1664782175476'
+module.exports = class Data1665425909288 {
+  name = 'Data1665425909288'
 
   async up(db) {
     await db.query(`CREATE TABLE "account" ("id" character varying NOT NULL, "event_id" text NOT NULL, CONSTRAINT "PK_54115ee388cdb6d86bb4bf5b2ea" PRIMARY KEY ("id"))`)
