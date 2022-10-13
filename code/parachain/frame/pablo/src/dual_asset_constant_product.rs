e0c2153ad311ed1ada403e623eb1656bb2c3dee6
Vim Wickramasinghe <vimukthi@composable.finance>
2022-10-14 01:06:24 +0200

    [cu-30xvvc2] Remove CurrencyPair use in pool data structure

--- code/parachain/frame/pablo/src/dual_asset_constant_product.rs
+++ code/parachain/frame/pablo/src/dual_asset_constant_product.rs
@@ -6,7 +6,7 @@ use composable_support::math::safe::{SafeAdd, SafeSub};
 use composable_traits::{
 	currency::{CurrencyFactory, RangeId},
 	defi::CurrencyPair,
-	dex::{ConstantProductPoolInfo, Fee, FeeConfig},
+	dex::{BasicPoolInfo, Fee, FeeConfig},
 };
 use frame_support::{
 	pallet_prelude::*,
@@ -14,28 +14,29 @@ use frame_support::{
 };
 use sp_runtime::{
 	traits::{Convert, One, Zero},
-	Permill,
+	BoundedBTreeMap, Permill,
 };
 
-// Uniswap
-pub(crate) struct Uniswap<T>(PhantomData<T>);
+// Balancer V1 Constant Product Pool
+pub(crate) struct DualAssetConstantProduct<T>(PhantomData<T>);
 
-impl<T: Config> Uniswap<T> {
+impl<T: Config> DualAssetConstantProduct<T> {
 	pub(crate) fn do_create_pool(
 		who: &T::AccountId,
-		pair: CurrencyPair<T::AssetId>,
 		fee_config: FeeConfig,
-		base_weight: Permill,
+		assets_weights: BoundedBTreeMap<T::AssetId, Permill, ConstU32<2>>,
 	) -> Result<T::PoolId, DispatchError> {
 		// TODO(hussein-aitlahcen): refactor all those checks using Validated
-		ensure!(base_weight != Permill::zero(), Error::<T>::WeightsMustBeNonZero);
-		ensure!(base_weight < Permill::one(), Error::<T>::WeightsMustSumToOne);
-		ensure!(pair.base != pair.quote, Error::<T>::InvalidPair);
+		ensure!(assets_weights.len() == 2, Error::<T>::InvalidPair);
+		let weights = assets_weights.iter().map(|(_, w)| w).copied().collect::<Vec<_>>();
+		ensure!(
+			weights[0] != Permill::zero() && weights[1] != Permill::zero(),
+			Error::<T>::WeightsMustBeNonZero
+		);
+		ensure!(weights[0] + weights[1] == Permill::one(), Error::<T>::WeightsMustSumToOne);
 		ensure!(fee_config.fee_rate < Permill::one(), Error::<T>::InvalidFees);
 
 		let lp_token = T::CurrencyFactory::create(RangeId::LP_TOKENS, T::Balance::default())?;
-
-		let quote_weight = Permill::one().safe_sub(&base_weight)?;
 
 		// Add new pool
 		let pool_id =
@@ -43,13 +44,11 @@ impl<T: Config> Uniswap<T> {
 				let pool_id = *pool_count;
 				Pools::<T>::insert(
 					pool_id,
-					PoolConfiguration::ConstantProduct(ConstantProductPoolInfo {
+					PoolConfiguration::DualAssetConstantProduct(BasicPoolInfo {
 						owner: who.clone(),
-						pair,
+						assets_weights,
 						lp_token,
 						fee_config,
-						base_weight,
-						quote_weight,
 					}),
 				);
 				*pool_count = pool_id.safe_add(&T::PoolId::one())?;
@@ -60,44 +59,53 @@ impl<T: Config> Uniswap<T> {
 	}
 
 	pub(crate) fn get_exchange_value(
-		pool: &ConstantProductPoolInfo<T::AccountId, T::AssetId>,
+		pool: &BasicPoolInfo<T::AccountId, T::AssetId, ConstU32<2>>,
 		pool_account: &T::AccountId,
-		asset_id: T::AssetId,
-		amount: T::Balance,
+		in_asset_id: T::AssetId,
+		in_asset_amount: T::Balance,
 	) -> Result<T::Balance, DispatchError> {
-		ensure!(pool.pair.contains(asset_id), Error::<T>::InvalidAsset);
-		let amount = T::Convert::convert(amount);
-		let pool_base_aum = T::Convert::convert(T::Assets::balance(pool.pair.base, pool_account));
-		let pool_quote_aum = T::Convert::convert(T::Assets::balance(pool.pair.quote, pool_account));
+		ensure!(pool.assets_weights.contains_key(&in_asset_id), Error::<T>::InvalidAsset);
+		let amount = T::Convert::convert(in_asset_amount);
+		let pool_assets = Self::get_pool_balances(pool, pool_account);
+		// TODO (vim): We have no way of knowing which amount is for which asset (fixed in a later
+		//  stage). For now we assume the pool defined order
+		let base_asset = pool_assets[0];
+		let quote_asset = pool_assets[1];
 		ensure!(
-			!pool_base_aum.is_zero() && !pool_quote_aum.is_zero(),
+			pool_assets.iter().find(|(_, _, balance)| balance.is_zero()).is_none(),
 			Error::<T>::NotEnoughLiquidity
 		);
+
 		// TODO (vim): Following does not work for "buy" as this causes "out_given_in" to be used in
 		//  case user wanting to buy the quote asset of the pool.
-		let exchange_amount = if asset_id == pool.pair.quote {
-			compute_out_given_in(
-				pool.quote_weight,
-				pool.base_weight,
-				pool_quote_aum,
-				pool_base_aum,
-				amount,
-			)
+		let exchange_amount = if in_asset_id == quote_asset.0 {
+			compute_out_given_in(quote_asset.1, base_asset.1, quote_asset.2, base_asset.2, amount)
 		} else {
-			compute_in_given_out(
-				pool.quote_weight,
-				pool.base_weight,
-				pool_quote_aum,
-				pool_base_aum,
-				amount,
-			)
+			compute_in_given_out(quote_asset.1, base_asset.1, quote_asset.2, base_asset.2, amount)
 		}?;
 		Ok(T::Convert::convert(exchange_amount))
 	}
 
+	fn get_pool_balances(
+		pool: &BasicPoolInfo<T::AccountId, T::AssetId, ConstU32<2>>,
+		pool_account: &T::AccountId,
+	) -> Vec<(T::AssetId, Permill, u128)> {
+		pool.assets_weights
+			.iter()
+			.map(|(asset_id, weight)| {
+				(
+					asset_id.clone(),
+					weight.clone(),
+					T::Convert::convert(T::Assets::balance(asset_id.clone(), pool_account)),
+				)
+			})
+			// TODO (vim): Make a map here later when the out_asset is provided.
+			.collect::<Vec<_>>()
+	}
+
 	pub(crate) fn add_liquidity(
 		who: &T::AccountId,
-		pool: ConstantProductPoolInfo<T::AccountId, T::AssetId>,
+		pool: BasicPoolInfo<T::AccountId, T::AssetId, ConstU32<2>>,
 		pool_account: T::AccountId,
 		base_amount: T::Balance,
 		quote_amount: T::Balance,
@@ -108,17 +116,19 @@ impl<T: Config> Uniswap<T> {
 		//  to be higher than expected if the base token has more than what is allowed by the pool
 		//  weight.
 		ensure!(base_amount > T::Balance::zero(), Error::<T>::InvalidAmount);
-		let pool_base_aum = T::Convert::convert(T::Assets::balance(pool.pair.base, &pool_account));
-		let pool_quote_aum =
-			T::Convert::convert(T::Assets::balance(pool.pair.quote, &pool_account));
+		let pool_assets = Self::get_pool_balances(&pool, &pool_account);
+		// TODO (vim): We have no way of knowing which amount is for which asset (fixed in a later
+		// stage). For now we assume the pool defined order
+		let base_asset = pool_assets[0];
+		let quote_asset = pool_assets[1];
 
 		let lp_total_issuance = T::Convert::convert(T::Assets::total_issuance(pool.lp_token));
 		let (quote_amount, amount_of_lp_token_to_mint) = compute_deposit_lp(
 			lp_total_issuance,
 			T::Convert::convert(base_amount),
 			T::Convert::convert(quote_amount),
-			pool_base_aum,
-			pool_quote_aum,
+			base_asset.2,
+			quote_asset.2,
 		)?;
 		let quote_amount = T::Convert::convert(quote_amount);
 		let amount_of_lp_token_to_mint = T::Convert::convert(amount_of_lp_token_to_mint);
@@ -129,41 +139,52 @@ impl<T: Config> Uniswap<T> {
 			Error::<T>::CannotRespectMinimumRequested
 		);
 
-		T::Assets::transfer(pool.pair.base, who, &pool_account, base_amount, keep_alive)?;
-		T::Assets::transfer(pool.pair.quote, who, &pool_account, quote_amount, keep_alive)?;
+		T::Assets::transfer(base_asset.0, who, &pool_account, base_amount, keep_alive)?;
+		T::Assets::transfer(quote_asset.0, who, &pool_account, quote_amount, keep_alive)?;
 		T::Assets::mint_into(pool.lp_token, who, amount_of_lp_token_to_mint)?;
 		Ok((base_amount, quote_amount, amount_of_lp_token_to_mint))
 	}
 
 	pub(crate) fn remove_liquidity(
 		who: &T::AccountId,
-		pool: ConstantProductPoolInfo<T::AccountId, T::AssetId>,
+		pool: BasicPoolInfo<T::AccountId, T::AssetId, ConstU32<2>>,
 		pool_account: T::AccountId,
 		lp_amount: T::Balance,
 		base_amount: T::Balance,
 		quote_amount: T::Balance,
 	) -> Result<(T::Balance, T::Balance, T::Balance), DispatchError> {
 		let lp_issued = T::Assets::total_issuance(pool.lp_token);
-		// NOTE(hussein-aitlahcen): no need to keep alive the pool account
-		T::Assets::transfer(pool.pair.base, &pool_account, who, base_amount, false)?;
-		T::Assets::transfer(pool.pair.quote, &pool_account, who, quote_amount, false)?;
+		let pool_assets = Self::get_pool_balances(&pool, &pool_account);
+		// TODO (vim): We have no way of knowing which amount is for which asset (fixed in a later
+		// stage). For now we assume the pool defined order
+		let base_asset = pool_assets[0];
+		let quote_asset = pool_assets[1];
+		T::Assets::transfer(base_asset.0, &pool_account, who, base_amount, false)?;
+		T::Assets::transfer(quote_asset.0, &pool_account, who, quote_amount, false)?;
 		T::Assets::burn_from(pool.lp_token, who, lp_amount)?;
 
 		Ok((base_amount, quote_amount, lp_issued.safe_sub(&lp_amount)?))
 	}
 
 	pub(crate) fn do_compute_swap(
-		pool: &ConstantProductPoolInfo<T::AccountId, T::AssetId>,
+		pool: &BasicPoolInfo<T::AccountId, T::AssetId, ConstU32<2>>,
 		pool_account: &T::AccountId,
 		pair: CurrencyPair<T::AssetId>,
 		quote_amount: T::Balance,
 		apply_fees: bool,
 	) -> Result<(T::Balance, T::Balance, Fee<T::AssetId, T::Balance>), DispatchError> {
-		// TODO (vim): pair implements PartialEq where equivalence holds even if base = quote and
-		//  quote = base. Confusing!!!
-		ensure!(pair == pool.pair, Error::<T>::PairMismatch);
-		let pool_base_aum = T::Convert::convert(T::Assets::balance(pair.base, pool_account));
-		let pool_quote_aum = T::Convert::convert(T::Assets::balance(pair.quote, pool_account));
+		let pool_assets = Self::get_pool_balances(&pool, pool_account);
+		// TODO (vim): We have no way of knowing which amount is for which asset (fixed in a later
+		// stage). For now we assume the pool defined order
+		let base_asset = pool_assets[0];
+		let quote_asset = pool_assets[1];
+		ensure!(
+			pool_assets
+				.iter()
+				.find(|(asset_id, _, _)| pair.contains(asset_id.clone()))
+				.is_some(),
+			Error::<T>::PairMismatch
+		);
 
 		let fee = if apply_fees {
 			pool.fee_config.calculate_fees(pair.quote, quote_amount)
@@ -175,10 +196,10 @@ impl<T: Config> Uniswap<T> {
 		let quote_amount_excluding_lp_fee = T::Convert::convert(quote_amount.safe_sub(&fee.fee)?);
 		let base_amount = compute_out_given_in(
 			// TODO bug (possibly the cause of https://app.clickup.com/t/39ny0d0) weights are swapped when the pair sent is different from the pool pair
-			pool.quote_weight,
-			pool.base_weight,
-			pool_quote_aum,
-			pool_base_aum,
+			quote_asset.1,
+			base_asset.1,
+			quote_asset.2,
+			base_asset.2,
 			quote_amount_excluding_lp_fee,
 		)?;
 		ensure!(base_amount > 0 && quote_amount_excluding_lp_fee > 0, Error::<T>::InvalidAmount);
