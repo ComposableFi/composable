ccb4724502d60353c4f5e73764f305d82a12552b
Mamali <isthereanybody@gmail.com>
2022-10-19 11:56:00 +0330

    Disable/Mock bonds and staking pages
    With the removal of this commit, we can resurrect these pages

--- frontend/apps/picasso/pages/staking/index.tsx
+++ frontend/apps/picasso/pages/staking/index.tsx
@@ -1,31 +1,18 @@
-import { StakingConnected } from "@/components/Organisms/Staking/StakingConnected";
-import { StakingDisconnected } from "@/components/Organisms/Staking/StakingDisconnected";
+import { UpcomingFeature } from "@/components/Molecules/UpcomingFeature";
+import { MockStaking } from "@/components/Organisms/Staking/MockStaking";
 import { StakingPageHeading } from "@/components/Organisms/Staking/StakingPageHeading";
 import Default from "@/components/Templates/Default";
-import { Box, useTheme } from "@mui/material";
+import { Box } from "@mui/material";
 import type { NextPage } from "next";
-import { useDotSamaContext } from "substrate-react";
 
 const Staking: NextPage = () => {
-  const theme = useTheme();
-  const { extensionStatus } = useDotSamaContext();
-  const isDisconnected = extensionStatus !== "connected";
-  const standardPageSize = {
-    xs: 12,
-  };
-
   return (
     <Default>
       <Box flexGrow={1} sx={{ mx: "auto" }} maxWidth={1032} mt={9}>
         <StakingPageHeading />
-        {isDisconnected && (
-          <StakingDisconnected gridSize={standardPageSize} theme={theme} />
-        )}
-        {!isDisconnected && (
-          <Box>
-            <StakingConnected />
-          </Box>
-        )}
+        <UpcomingFeature>
+          <MockStaking />
+        </UpcomingFeature>
       </Box>
     </Default>
   );
