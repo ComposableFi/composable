87a16c813a823439f120aab6774e9be5a52c0171
aeryz <abdullaheryz@protonmail.com>
2022-10-26 15:35:46 +0200

    chore(cosmwasm): move xcvm_program.proto to xcvm library
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

--- code/xcvm/cosmwasm/contracts/interpreter/build.rs
+++ code/xcvm/cosmwasm/contracts/interpreter/build.rs
@@ -1,5 +1,9 @@
 extern crate prost_build;
 
 fn main() {
-	prost_build::compile_protos(&["src/interpreter.proto"], &["src/"]).unwrap();
+	prost_build::compile_protos(
+		&["../../../lib/protos/xcvm_program.proto"],
+		&["../../../lib/protos/"],
+	)
+	.unwrap();
 }
