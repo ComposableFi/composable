9e6f500c8397c77124d2eff5de998f4faf020b7b
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-10-27 02:16:07 +0500

    Fetch Karura Assets

--- frontend/apps/picasso/defi/polkadot/pallets/Assets/picasso.ts
+++ frontend/apps/picasso/defi/polkadot/pallets/Assets/picasso.ts
@@ -1,4 +1,3 @@
-import { ComposableTraitsXcmAssetsXcmAssetLocation } from "defi-interfaces";
 import { ApiPromise } from "@polkadot/api";
 import BigNumber from "bignumber.js";
 
@@ -6,7 +5,6 @@ export type PicassoRpcAsset = {
     name: string;
     id: BigNumber;
     decimals?: number;
-    foreignId: ComposableTraitsXcmAssetsXcmAssetLocation
 }
 
 export async function picassoAssetsList(api: ApiPromise): Promise<PicassoRpcAsset[]> {
@@ -15,9 +13,7 @@ export async function picassoAssetsList(api: ApiPromise): Promise<PicassoRpcAsse
         return assetsList.map((asset) => {
             return {
                 name: asset.name.toUtf8(),
-                id: new BigNumber(asset.id.toString()),
-                decimals: asset.id.toNumber(),
-                foreignId: asset.foreignId
+                id: new BigNumber(asset.id.toString())
             }
         });
     } catch (err) {
