c917f75891df7b40c3343fccc92efb8c7c37ce38
Obsessed Cake <aaaoxxx@gmail.com>
2022-10-27 14:25:13 +0400

    Update polkadot-bin sha256

--- .nix/polkadot/polkadot-bin.nix
+++ .nix/polkadot/polkadot-bin.nix
@@ -7,5 +7,5 @@ pkgs.callPackage ./polkadot-tmpl.nix rec {
   owner = "paritytech";
   rev = "v${version}";
   hash = "sha256-LEz3OrVgdFTCnVwzU8C6GeEougaOl2qo7jS9qIdMqAM=";
-  cargoSha256 = "sha256-6y+WK2k1rhqMxMjEJhzJ26WDMKZjXQ+q3ca2hbbeLvA=";
+  cargoSha256 = "sha256-eSZGz7HXjLxQ584OMTVQNBOWPVFBtGWQfuJid489Xm4=";
 }
