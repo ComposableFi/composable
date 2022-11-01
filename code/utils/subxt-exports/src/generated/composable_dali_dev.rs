5a6459f0f25c3180ec7f22c512f614d4b29d85f0
Obsessed Cake <aaaoxxx@gmail.com>
2022-11-01 18:56:46 +0400

    Update dictionaries

--- code/utils/subxt-exports/src/generated/composable_dali_dev.rs
+++ code/utils/subxt-exports/src/generated/composable_dali_dev.rs
@@ -1544,7 +1544,7 @@ pub mod api {
                 #[doc = " A fee multiplier for `Operational` extrinsics to compute \"virtual tip\" to boost their"]
                 #[doc = " `priority`"]
                 #[doc = ""]
-                #[doc = " This value is multiplied by the `final_fee` to obtain a \"virtual tip\" that is later"]
+                #[doc = " This value is multipled by the `final_fee` to obtain a \"virtual tip\" that is later"]
                 #[doc = " added to a tip component in regular `priority` calculations."]
                 #[doc = " It means that a `Normal` transaction can front-run a similarly-sized `Operational`"]
                 #[doc = " extrinsic (with no tip), by including a tip value greater than the virtual tip."]
