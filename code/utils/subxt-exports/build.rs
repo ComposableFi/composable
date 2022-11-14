6f118a0a0284e108f7da71855218bed490e51b9f
Obsessed Cake <aaaoxxx@gmail.com>
2022-11-14 21:01:45 +0400

    Add possibility to change relaychain and parachain hosts for the build script of subxt-exports crate

--- code/utils/subxt-exports/build.rs
+++ code/utils/subxt-exports/build.rs
@@ -13,14 +13,26 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use once_cell::sync::Lazy;
 use std::env;
+
+static RELAY_URL: Lazy<String> = Lazy::new(|| {
+	let host = env::var("RELAY_HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
+	format!("ws://{}:9944", host)
+});
+
+static PARA_URL: Lazy<String> = Lazy::new(|| {
+	let host = env::var("PARA_HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
+	format!("ws://{}:9188", host)
+});
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
 	// TODO(Dzmitry): add conditional env var
 	if env::var("SUBXT_ENABLED").is_ok() {
-		subxt_codegen::build_script("ws://127.0.0.1:9944", "polkadot").await?;
-		subxt_codegen::build_script("ws://127.0.0.1:9188", "parachain").await?;
+		println!("relay_url: {}, para_url: {}", RELAY_URL.as_str(), &PARA_URL.as_str());
+		subxt_codegen::build_script(&PARA_URL, "polkadot").await?;
+		subxt_codegen::build_script(&RELAY_URL, "parachain").await?;
 	}
 	Ok(())
 }
