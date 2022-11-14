9cd611704734d6a83bfc5d8ffea75789b8d7896b
Mamali <isthereanybody@gmail.com>
2022-11-14 15:47:58 +0330

    Update overview stats to pull every one minute

--- frontend/apps/picasso/apollo/hooks/useOverviewStats.ts
+++ frontend/apps/picasso/apollo/hooks/useOverviewStats.ts
@@ -2,7 +2,9 @@ import { useQuery } from "@apollo/client";
 import { OVERVIEW_STATS, OverviewStats } from "@/apollo/queries/overviewStats";
 
 export const useOverviewStats = () => {
-  const { data, error, loading } = useQuery<OverviewStats>(OVERVIEW_STATS);
+  const { data, error, loading } = useQuery<OverviewStats>(OVERVIEW_STATS, {
+    pollInterval: 60000, // Refreshing every 1 minute.
+  });
 
   return { data, error, loading };
 };
