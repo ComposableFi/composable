15a005102fc01c2ae9abbfadb6434e1fb19a1004
kaiserkarel <k.l.kubat@gmail.com>
2022-11-14 16:50:14 +0100

    Add upgrader package

--- .tools/runtime-upgrade/README.md
+++ .tools/runtime-upgrade/README.md
@@ -11,3 +11,9 @@ The tool performs the following steps in sequence:
 2. Performs a `sudo`-based runtime upgrade against the local chain to verify that the upgrade will succeed.
 
 3. If 2 is succesful, perform either a `sudo`-based upgrade or `democracy` proposal, depending on the provided flags.
+
+## Usage
+
+```bash
+docker run --mount type=bind,source=/home/user/runtime.wasm,target=/upgrader/runtime.wasm upgrader upgrade -w wss://kusama.api.onfinality.io/public-ws -k "december suit acoustic cruise crystal tunnel butter piece winner crunch language engine" -r "/upgrader/runtime.wasm" -m "sudo"
+```
---
+++ .tools/runtime-upgrade/upgrader.mjs
@@ -0,0 +1,75 @@
+import { program } from 'commander'
+import { ApiPromise, WsProvider, Keyring } from '@polkadot/api';
+import fs from 'fs';
+import { cryptoWaitReady } from '@polkadot/util-crypto';
+
+program
+    .command('upgrade')
+        .description('Performs a runtime upgrade')
+        .option('-w --wss <wss>', 'wss url to a node')
+        .option('-k --keyring <keyring>', 'path to keyring or seed to use')
+        .option('-r --runtime <runtime>', 'path to runtime wasm')
+        .option('-m --mode <mode>', 'either sudo or democracy', )
+        .action(async function (options) { await upgrade(options)} );
+
+
+async function upgrade(options) {
+  console.log("Starting runtime upgrade");  
+
+  await cryptoWaitReady()
+  console.log("crypto intialized")
+
+  // Initialise the provider to connect to the local node
+  const provider = new WsProvider(options.wss, { timeout: 30 });
+  console.log(options.wss);  
+
+  // Create the API and wait until ready (optional provider passed through)
+  const api = await new ApiPromise({ provider }).isReady;
+  console.log("2");  
+
+  // Find the actual keypair in the keyring (if this is a changed value, the key
+  // needs to be added to the keyring before - this assumes we have defaults, i.e.
+  // Alice as the key - and this already exists on the test keyring)
+  const keyring = new Keyring({ type: 'sr25519' });
+
+  console.log("creating keyring")
+  const adminPair = keyring.addFromUri(options.keyring);
+  console.log(`Using ${adminPair.address}`);  
+
+  // Retrieve the runtime to upgrade
+  const code = fs.readFileSync(options.runtime).toString('hex');
+
+  const proposal = api.tx.system && api.tx.system.setCode
+    ? api.tx.system.setCode(`0x${code}`) // For newer versions of Substrate
+    : api.tx.consensus.setCode(`0x${code}`); // For previous versions
+
+  if (options.mode == 'sudo') {
+    console.log(`Upgrading from ${adminPair.address} using sudo, ${code.length / 2} bytes`);
+
+    // Perform the actual chain upgrade via the sudo module
+    api.tx.sudo
+        .sudo(proposal)
+        .signAndSend(adminPair, ({ events = [], status }) => {
+        console.log('Proposal status:', status.type);
+
+        if (status.isInBlock) {
+            console.error('You have just upgraded your chain');
+
+            console.log('Included at block hash', status.asInBlock.toHex());
+            console.log('Events:');
+
+            console.log(JSON.stringify(events.toHuman(), null, 2));
+        } else if (status.isFinalized) {
+            console.log('Finalized block hash', status.asFinalized.toHex());
+            process.exit(0);
+        }
+    });
+  } else if (options.mode == "democracy") {
+        console.error('democracy is currently still unsupported')
+  } else {
+    console.error("unknown options")
+  }
+  console.log("finished runtime upgrade")
+}
+
+program.parse()
