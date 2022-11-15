ba98b6118599a0a2b90509386b0b31a47b62f81b
Joon <joon@composable.finance>
2022-11-16 03:15:05 +0900

    fix: build issues due to default ssr

--- docs/src/pages/test-vm.tsx
+++ docs/src/pages/test-vm.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-import { vmMethods } from '@site/src/utils/cosmwasm-vm/vm/vmMethods';
-
-vmMethods.safeSingleRunVmSetup();
+import BrowserOnly from '@docusaurus/BrowserOnly';
 export default function TestVM() {
 	return (
 		<Layout title="Hello" description="Hallo">
@@ -11,6 +9,13 @@ export default function TestVM() {
 					Edit <code>src/pages/test-vm/index.tsx</code> and save to reload.
 				</p>
 			</div>
+			<BrowserOnly>
+				{() => {
+					const vmMethods = require('@site/src/utils/cosmwasm-vm/vm/vmMethods').vmMethods;
+					vmMethods.safeSingleRunVmSetup();
+					return <></>;
+				}}
+			</BrowserOnly>
 		</Layout>
 	);
 }
