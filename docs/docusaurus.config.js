02b85e80ef30e1c564d592211d90ee2d71815a0c
hussein <hussein.aitlahcen@gmail.com>
2022-11-16 21:17:24 +0100

    feat(doc): better theme for code and add support for `nix`

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require('prism-react-renderer/themes/duotoneLight');
+const darkCodeTheme = require('prism-react-renderer/themes/duotoneDark');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -120,7 +120,7 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} Composable Finance, Ltd.`,
       },
       prism: {
-        additionalLanguages: ['rust', 'haskell'],
+        additionalLanguages: ['rust', 'haskell', 'nix'],
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
