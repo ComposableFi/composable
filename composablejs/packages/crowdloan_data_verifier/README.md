8b623f75929cfde59782efc87ace9a56a825d6b3
Dominik Roth <dominik@composable.finance>
2022-11-17 14:25:08 +0100

    test(ComposableJS): Added crowdloan pallet data verifier script

---
+++ composablejs/packages/crowdloan_data_verifier/README.md
@@ -0,0 +1,20 @@
+# Crowdloan Pallet Data Verifier
+
+#### @composable/crowdloan_data_verifier
+
+This tool automatically verifies the data populated on the crowdloan pallet,
+based on the provided contributors list in json format.
+
+## How to run
+
+### Installation
+
+```bash
+$ yarn
+```
+
+### Start
+
+```bash
+$ yarn start
+```
---
+++ composablejs/packages/crowdloan_data_verifier/src/main.ts
@@ -0,0 +1,37 @@
+#!/usr/bin/env ts-node
+import "@composable/types/augment-api";
+import "@composable/types/augment-types";
+import { getNewConnection } from "@composable/utils";
+import { verifyCrowdloanData } from "@composable/crowdloan_data_verifier/handler";
+
+
+const CONTRIBUTOR_LIST_URL =
+  "https://raw.githubusercontent.com/ComposableFi/composable/main/frontend/apps/picasso/defi/polkadot/constants/pica-rewards-contributions.json";
+
+const main = async () => {
+  console.log("Crowdloan Pallet Verifier");
+
+  console.log("Connecting...");
+  // Establish connection to the node.
+  const endpoint = process.env.ENDPOINT ?? "ws://127.0.0.1:9988";
+  const { newClient } = await getNewConnection(endpoint);
+
+  const contributorsUrl = process.env.CONTRIBUTOR_LIST_URL ?? CONTRIBUTOR_LIST_URL;
+
+  // Here the actual magic happens
+  await verifyCrowdloanData(newClient, contributorsUrl);
+
+  // Disconnecting from the node.
+  console.debug("disconnecting...");
+  await newClient.disconnect();
+};
+
+main()
+  .then(() => {
+    console.log("Crowdloan data verification finished!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err.toString());
+    process.exit(1);
+  });
