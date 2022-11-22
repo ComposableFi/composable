477b9caba84e10eabd4d89666f1ce84885ec1dc1
Mamali <isthereanybody@gmail.com>
2022-11-22 17:47:32 +0330

    Updated subheaders for stats and transfers

--- frontend/apps/picasso/components/Organisms/Transfer/Header.tsx
+++ frontend/apps/picasso/components/Organisms/Transfer/Header.tsx
@@ -6,7 +6,7 @@ export const Header = () => (
   <>
     <PageTitle
       title="Transfers"
-      subtitle="You will be able to move assets across available Kusama chains."
+      subtitle="Move assets across available Kusama chains."
       textAlign="center"
     />
     <UnavailableFeature pageTitle={"Transfers"} />
