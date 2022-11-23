d3ea77d619d12e8871415d9c2442173652b9e8c1
Obsessed Cake <aaaoxxx@gmail.com>
2022-11-23 15:30:46 +0400

    Update scripts/dali-dev-scale-export.sh

--- scripts/dali-dev-scale-export.sh
+++ scripts/dali-dev-scale-export.sh
@@ -1,13 +1,22 @@
+#!/bin/bash
+
+PARA_PORT=9188
+
 # to be nixified way to generate subxt from local node
 ../../../paritytech/polkadot/target/release/polkadot build-spec --chain=rococo-local --raw > rococo-local-raw.json
 
-../target/release/composable --tmp --force-authoring  --chain=dali-dev  --discover-local --  --chain=./rococo-local-raw.json &
+result/bin/polkadot --chain=./rococo-local-raw.json --tmp &
+polkadot_pid=$(echo $!)
 
-sleep 30
+../target/release/composable --tmp --force-authoring  --chain=dali-dev  --discover-local --ws-port $PARA_PORT --  --chain=./rococo-local-raw.json &
+composable_pid=$(echo $!)
 
-# dz@dz-pc-11:~/github.com/ComposableFi/composable/scripts$ subxt codegen --url  ws://127.0.0.1:9944 > dali.rs
-# The application panicked (crashed).
-# Message:  Unknown prelude type 'NonZeroU64'
-# Location: /home/dz/.cargo/registry/src/github.com-1ecc6299db9ec823/subxt-codegen-0.24.0/src/types/type_path.rs:126
+# wait for TCP port opening
+while ! nc -z 127.0.0.1 $PARA_PORT; do
+  sleep 0.5
+done
 
-subxt codegen --url  ws://127.0.0.1:9944 > dali.rs
+# required subxt v0.25.0
+subxt codegen --url "ws://127.0.0.1:${PARA_PORT}" > dali.rs
+
+kill -9 $polkadot_pid $composable_pid
