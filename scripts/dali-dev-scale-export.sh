1a1a160d146cff1c8e1292d9920970ce14a7c16e
Obsessed Cake <aaaoxxx@gmail.com>
2022-11-25 21:03:40 +0400

    Renaming

--- scripts/dali-dev-scale-export.sh
+++ scripts/dali-dev-scale-export.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-PARA_PORT=9188
+HOST="127.0.0.1"
+PORT=9188
 
 # to be nixified way to generate subxt from local node
 ../../../paritytech/polkadot/target/release/polkadot build-spec --chain=rococo-local --raw > rococo-local-raw.json
@@ -8,16 +9,16 @@ PARA_PORT=9188
 result/bin/polkadot --chain=./rococo-local-raw.json --tmp &
 polkadot_pid=$(echo $!)
 
-../target/release/composable --tmp --force-authoring  --chain=dali-dev  --discover-local --ws-port $PARA_PORT --  --chain=./rococo-local-raw.json &
+../target/release/composable --tmp --force-authoring  --chain=dali-dev  --discover-local --ws-port $PORT --  --chain=./rococo-local-raw.json &
 composable_pid=$(echo $!)
 
 # wait for TCP port opening
-while ! nc -z 127.0.0.1 $PARA_PORT; do
+while ! nc -z $HOST $PORT; do
   sleep 1
-  echo "Trying to connect to 127.0.0.1:${PARA_PORT}..."
+  echo "Trying to connect to ${HOST}:${PORT}..."
 done
 
 # required subxt v0.25.0
-subxt codegen --url "ws://127.0.0.1:${PARA_PORT}" > dali.rs
+subxt codegen --url "ws://${HOST}:${PORT}" > dali.rs
 
 kill -9 $polkadot_pid $composable_pid
