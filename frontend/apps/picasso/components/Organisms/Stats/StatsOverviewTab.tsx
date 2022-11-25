658bc0f2d607a8bd6e25bd83ea432f5a4c825b44
Mamali <isthereanybody@gmail.com>
2022-11-25 18:54:24 +0330

    Conditional -Disable Stats page in case of no subsquid (#2425)

--- frontend/apps/picasso/components/Organisms/Stats/StatsOverviewTab.tsx
+++ frontend/apps/picasso/components/Organisms/Stats/StatsOverviewTab.tsx
@@ -17,7 +17,7 @@ export const StatsOverviewTab: FC = () => {
   return (
     <Grid container spacing={4}>
       <Grid item xs={12} sm={6} md={4}>
-        {!loading && data?.overviewStats && (
+        {!loading && (
           <FeaturedBox
             TextAboveProps={{
               color: theme.palette.common.darkWhite,
@@ -25,39 +25,40 @@ export const StatsOverviewTab: FC = () => {
             TooltipProps={{
               title:
                 "The total value of PICA in USD deposited in Picasso's smart contracts.",
+              disableHoverListener: true,
             }}
             textAbove="Total value locked"
-            title={data?.overviewStats.totalValueLocked.toString()}
+            title={"123,456,789"}
           />
         )}
       </Grid>
       <Grid item xs={12} sm={6} md={4}>
-        {!loading && data?.overviewStats && (
+        {!loading && (
           <FeaturedBox
             TextAboveProps={{
               color: theme.palette.common.darkWhite,
             }}
             TooltipProps={{
               title: "The number of people possessing a Picasso account.",
+              disableHoverListener: true,
             }}
             textAbove="Account holders"
-            title={data?.overviewStats.accountHoldersCount.toString()}
+            title={"1234"}
           />
         )}
       </Grid>
       <Grid item xs={12} sm={6} md={4}>
-        {!loading && data?.overviewStats && (
-          <FeaturedBox
-            TextAboveProps={{
-              color: theme.palette.common.darkWhite,
-            }}
-            TooltipProps={{
-              title: "The total number of finalized transactions.",
-            }}
-            textAbove="Total transactions"
-            title={formatNumber(data?.overviewStats.transactionsCount)}
-          />
-        )}
+        <FeaturedBox
+          TextAboveProps={{
+            color: theme.palette.common.darkWhite,
+          }}
+          TooltipProps={{
+            title: "The total number of finalized transactions.",
+            disableHoverListener: true,
+          }}
+          textAbove="Total transactions"
+          title={"123,456,789"}
+        />
       </Grid>
       <Grid item xs={12} sm={6} md={4}>
         <FeaturedBox
@@ -67,9 +68,10 @@ export const StatsOverviewTab: FC = () => {
           TooltipProps={{
             title:
               "The total value of all minted PICA in USD. (total supply * current market price)",
+            disableHoverListener: true,
           }}
           textAbove="Picasso market cap"
-          title={`$${marketCap.toFormat(2)}`}
+          title={`$0.00`}
         />
       </Grid>
       <Grid item xs={12} sm={6} md={4}>
@@ -79,25 +81,25 @@ export const StatsOverviewTab: FC = () => {
           }}
           TooltipProps={{
             title: "The number of coins publicly available in the market.",
+            disableHoverListener: true,
           }}
           textAbove="Picasso circulating supply"
           title={`${circulatingSupply.toFormat(0)} PICA`}
         />
       </Grid>
       <Grid item xs={12} sm={6} md={4}>
-        {!loading && data?.overviewStats && (
-          <FeaturedBox
-            TextAboveProps={{
-              color: theme.palette.common.darkWhite,
-            }}
-            TooltipProps={{
-              title:
-                "The number of people interacting with Picasso in the last 24 hours.",
-            }}
-            textAbove="Active users"
-            title={data?.overviewStats.activeUsersCount.toString()}
-          />
-        )}
+        <FeaturedBox
+          TextAboveProps={{
+            color: theme.palette.common.darkWhite,
+          }}
+          TooltipProps={{
+            title:
+              "The number of people interacting with Picasso in the last 24 hours.",
+            disableHoverListener: true,
+          }}
+          textAbove="Active users"
+          title={"123,456"}
+        />
       </Grid>
       <Grid item xs={12}>
         <TotalValueLockedChart />
