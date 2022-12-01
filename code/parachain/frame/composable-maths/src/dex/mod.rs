5b0a3dde0f6a11219edf7f9abb8deb50089a1e01
Connor Davis <17688291+PoisonPhang@users.noreply.github.com>
2022-12-01 12:57:28 -0600

    Pass IntoIter<Self>

--- code/parachain/frame/composable-maths/src/dex/mod.rs
+++ code/parachain/frame/composable-maths/src/dex/mod.rs
@@ -5,23 +5,23 @@ pub mod price;
 #[cfg(test)]
 pub mod tests;
 
-pub trait WeightMath: PerThing {
+pub trait WeightMath<'t>: PerThing + 't {
 	/// Sums all weights in an array. If weights total greater than `Perthing::one()`, returns
 	/// `None`.
-	fn sum_weights(weights: &[Self]) -> Option<Self>;
+	fn sum_weights(weights: impl IntoIterator<Item = &'t Self>) -> Option<Self>;
 
 	/// Returns `true` if all weights in array are non-zero.
-	fn non_zero_weights(weights: &[Self]) -> bool;
+	fn non_zero_weights(weights: impl IntoIterator<Item = &'t Self>) -> bool;
 }
 
-impl<T: PerThing + CheckedAdd> WeightMath for T {
-	fn sum_weights(weights: &[Self]) -> Option<Self> {
+impl<'t, T: PerThing + CheckedAdd + 't> WeightMath<'t> for T {
+	fn sum_weights(weights: impl IntoIterator<Item = &'t Self>) -> Option<Self> {
 		weights
-			.iter()
+			.into_iter()
 			.try_fold(Self::zero(), |total_weight, weight_n| total_weight.checked_add(weight_n))
 	}
 
-	fn non_zero_weights(weights: &[Self]) -> bool {
-		weights.iter().all(|weight| !weight.is_zero())
+	fn non_zero_weights(weights: impl IntoIterator<Item = &'t Self>) -> bool {
+		weights.into_iter().all(|weight| !weight.is_zero())
 	}
 }
