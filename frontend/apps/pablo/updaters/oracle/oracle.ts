add8eaa0b0303a8e2276a0b640781bb460ba2dbc
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-12-06 12:27:19 +0500

    Update coingecko client pro API key.

--- frontend/apps/pablo/updaters/oracle/oracle.ts
+++ frontend/apps/pablo/updaters/oracle/oracle.ts
@@ -5,8 +5,40 @@ import {
   setOraclePrice,
 } from "@/store/oracle/slice";
 import useStore from "@/store/useStore";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import BigNumber from "bignumber.js";
+
+function coinGeckoApiUrl(): string {
+  switch (process.env.NODE_ENV) {
+    case "production":
+      return "https://pro-api.coingecko.com/api/v3/simple/price";
+    default:
+      return "https://api.coingecko.com/api/v3/simple/price";
+  }
+}
+
+function coinGeckoHeaders():
+  | { headers: { x_cg_pro_api_key: string } }
+  | undefined {
+  switch (process.env.NODE_ENV) {
+    case "production":
+      return { headers: { x_cg_pro_api_key: process.env.COINGECKO_KEY! } };
+    default:
+      return undefined;
+  }
+}
+
+function coingeckoRequest(
+  targetTokens: string[],
+  fiatCurrencies: string[]
+): Promise<AxiosResponse<any, any>> {
+  return axios.get(
+    `${coinGeckoApiUrl()}?ids=${targetTokens.join(
+      ","
+    )}&vs_currencies=${fiatCurrencies.join(",")}`,
+    coinGeckoHeaders()
+  );
+}
 
 export const subscription = useStore.subscribe(
   (state) => state.substrateTokens,
@@ -14,7 +46,7 @@ export const subscription = useStore.subscribe(
     const { hasFetchedTokens, tokens } = state;
 
     if (hasFetchedTokens) {
-      let vs_currencies = oracleCurrencies;
+      let vs_currencies = [...oracleCurrencies];
       let app_supported: TokenId[] = [];
 
       for (const [id, asset] of Object.entries(tokens)) {
@@ -24,39 +56,49 @@ export const subscription = useStore.subscribe(
       }
 
       if (app_supported.length > 0) {
-        const url = `https://api.coingecko.com/api/v3/simple/price?ids=${app_supported
+        const tokensToFetch = app_supported
           .filter((k) => !!TOKENS[k].coinGeckoId)
           .map((k) => {
             return TOKENS[k].coinGeckoId;
-          })
-          .join(",")}&vs_currencies=${vs_currencies.join(",")}`;
+          });
 
         const allTokenMetadata = Object.values(TOKENS);
-        axios.get(url).then((response: any) => {
-          const tokenIdPrice = Object.entries(response.data) as [
-            string,
-            Record<OracleCurrency, number>
-          ][];
+        coingeckoRequest(tokensToFetch as string[], vs_currencies).then(
+          (response: any) => {
+            const tokenIdPrice = Object.entries(response.data) as [
+              string,
+              Record<OracleCurrency, number>
+            ][];
 
-          for (const tokenAndPrice of tokenIdPrice) {
-            const token = allTokenMetadata.find((x) => (x.coinGeckoId === tokenAndPrice[0]));
+            for (const tokenAndPrice of tokenIdPrice) {
+              const token = allTokenMetadata.find(
+                (x) => x.coinGeckoId === tokenAndPrice[0]
+              );
 
-            if (token) {
-              const baseCurrencies = Object.keys(tokenAndPrice[1]);
+              if (token) {
+                const baseCurrencies = Object.keys(tokenAndPrice[1]);
 
-              for (const baseCurrency of baseCurrencies) {
-                console.log('[CoinGecko Subscription] adding price of ', token.symbol, ' base currency ', baseCurrency);
-                setOraclePrice(
-                  token.symbol,
-                  "coingecko",
-                  baseCurrency as OracleCurrency,
-                  new BigNumber(
-                    tokenAndPrice[1][baseCurrency as OracleCurrency]
-                  )
-                );
+                for (const baseCurrency of baseCurrencies) {
+                  console.log(
+                    "[CoinGecko Subscription] adding price of ",
+                    token.symbol,
+                    " base currency ",
+                    baseCurrency
+                  );
+                  setOraclePrice(
+                    token.symbol,
+                    "coingecko",
+                    baseCurrency as OracleCurrency,
+                    new BigNumber(
+                      tokenAndPrice[1][baseCurrency as OracleCurrency]
+                    )
+                  );
+                }
               }
             }
           }
+        ).catch(err => {
+          console.log('[Coingecko] Oracle Subscription error', err.message);
         });
       }
     }
