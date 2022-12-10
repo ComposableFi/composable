e011ec7a8a2e5a781cd612d4dd8315fb4b5bf9fe
cor <cor@pruijs.dev>
2022-12-10 14:32:42 +0100

    chore: fmt

--- dev-shells.nix
+++ dev-shells.nix
@@ -2,17 +2,16 @@
   perSystem = { config, self', inputs', pkgs, system, systemCommonRust, ... }: {
     devShells = rec {
       minimal = pkgs.mkShell {
-          buildInputs = with pkgs; [ clang nodejs python3 yarn ] ++ (with self'.packages; [ rust-nightly ]);
-          LD_LIBRARY_PATH = pkgs.lib.strings.makeLibraryPath (with pkgs; [
-            stdenv.cc.cc.lib
-            llvmPackages.libclang.lib
-            pkgs.zlib
-          ]);
-          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-          PROTOC = "${pkgs.protobuf}/bin/protoc";
-          ROCKSDB_LIB_DIR = "${pkgs.rocksdb}/lib";
-          NIX_PATH = "nixpkgs=${pkgs.path}";
-        };
+        buildInputs = with pkgs;
+          [ clang nodejs python3 yarn ]
+          ++ (with self'.packages; [ rust-nightly ]);
+        LD_LIBRARY_PATH = pkgs.lib.strings.makeLibraryPath
+          (with pkgs; [ stdenv.cc.cc.lib llvmPackages.libclang.lib pkgs.zlib ]);
+        LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+        PROTOC = "${pkgs.protobuf}/bin/protoc";
+        ROCKSDB_LIB_DIR = "${pkgs.rocksdb}/lib";
+        NIX_PATH = "nixpkgs=${pkgs.path}";
+      };
 
       default = minimal.overrideAttrs (base: {
         buildInputs = base.buildInputs ++ (with pkgs; [
