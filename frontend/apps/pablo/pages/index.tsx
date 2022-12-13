e28b327fe4a80dfda48efe69d8b2a13e6ba2133a
Mamali <isthereanybody@gmail.com>
2022-12-13 12:07:35 +0330

    CU-391cdb7 - Remove Your Bonds and your XPablos

--- frontend/apps/pablo/pages/index.tsx
+++ frontend/apps/pablo/pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import { Container, Box, Grid } from "@mui/material";
+import { Box, Container, Grid } from "@mui/material";
 import Default from "@/components/Templates/Default";
 import { PageTitle } from "@/components";
 import { Statistics } from "@/components/Organisms/overview/Statistics";
@@ -7,8 +7,6 @@ import { useDotSamaContext } from "substrate-react";
 import { ConnectWalletFeaturedBox } from "@/components/Organisms/ConnectWalletFeaturedBox";
 import { WalletBreakdownBox } from "@/components/Organisms/overview/WalletBreakdownBox";
 import { LiquidityProvidersBox } from "@/components/Organisms/overview/LiquidityProvidersBox";
-import { YourBondsBox } from "@/components/Organisms/overview/YourBondsBox";
-import { XPablosBox } from "@/components/Organisms/XPablosBox";
 import { TVLChart } from "@/components/Organisms/overview/TVLChart";
 import { VolumeChart } from "@/components/Organisms/overview/VolumeChart";
 
@@ -25,14 +23,16 @@ const Home: NextPage = () => {
           </Box>
 
           <Grid container>
-            {!connected && <Grid item xs={12}>
-              <ConnectWalletFeaturedBox
-                mt={8}
-                title="Connect wallet"
-                textBelow="To see your portfolio, wallet needs to be connected."
-                ButtonProps={{ label: "Connect Wallet", size: "small" }}
-              />
-            </Grid>}
+            {!connected && (
+              <Grid item xs={12}>
+                <ConnectWalletFeaturedBox
+                  mt={8}
+                  title="Connect wallet"
+                  textBelow="To see your portfolio, wallet needs to be connected."
+                  ButtonProps={{ label: "Connect Wallet", size: "small" }}
+                />
+              </Grid>
+            )}
             <Grid item xs={12} mt={8}>
               <Statistics />
             </Grid>
@@ -50,8 +50,6 @@ const Home: NextPage = () => {
             <>
               <WalletBreakdownBox mt={8} key="wallet-breakdown" />
               <LiquidityProvidersBox mt={8} key="liquidity-provider-box" />
-              <YourBondsBox mt={8} key="your-bond-box" />
-              <XPablosBox financialNftCollectionId="-" mt={8} key="xpablos-box" />
             </>
           )}
         </Box>
