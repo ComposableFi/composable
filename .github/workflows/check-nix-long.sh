31b3443bf60571fad609a2b045c067337dbbac1f
Dzmitry Lahoda <dzmitry@lahoda.pro>
2022-12-15 13:19:12 +0000

    works great
    Signed-off-by: Dzmitry Lahoda <dzmitry@lahoda.pro>

--- .github/workflows/check-nix-long.sh
+++ .github/workflows/check-nix-long.sh
@@ -1,9 +1,4 @@
-NIX_DEBUG_COMMAND="" && [ $ACTIONS_RUNNER_DEBUG == "true" ] && NIX_DEBUG_COMMAND='--print-build-logs --debug --show-trace --verbose'
-NIXPKGS_ALLOW_BROKEN=1 nix flake check --keep-going --no-build --allow-import-from-derivation  --no-update-lock-file --fallback -L ${NIX_DEBUG_COMMAND} --impure --option sandbox relaxed --impure 2>&1 | tee "nix.check.log"
-
+NIX_DEBUG_COMMAND="" && [ $ACTIONS_RUNNER_DEBUG -eq "true" ] && NIX_DEBUG_COMMAND='--print-build-logs --debug --show-trace --verbose'
+NIXPKGS_ALLOW_BROKEN=1 nix flake check --keep-going --no-build --allow-import-from-derivation  --no-update-lock-file --fallback -L ${NIX_DEBUG_COMMAND} --impure --option sandbox relaxed --impure 2>&1 | tee "nix.check.log"  || true
 EXIT=$(cat "nix.check.log" | grep --invert-match '^error (ignored): error:' | grep --invert-match '^error: some errors were encountered during the evaluation' | grep -c 'error:')
-
-if [ $EXIT != 0 ];
-then
-  exit 42
-fi
+exit $EXIT
