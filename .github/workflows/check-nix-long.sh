480942b713dc78bdc83ff867b985b7eadf1d036c
Dzmitry Lahoda <dzmitry@lahoda.pro>
2022-12-15 11:49:36 +0000

    nice
    Signed-off-by: Dzmitry Lahoda <dzmitry@lahoda.pro>

--- .github/workflows/check-nix-long.sh
+++ .github/workflows/check-nix-long.sh
@@ -1,2 +1,9 @@
-NIX_DEBUG_COMMAND="" && [ $ACTIONS_RUNNER_DEBUG == "true" ] && NIX_DEBUG_COMMAND='--print-build-log --debug --show-trace'
-NIXPKGS_ALLOW_BROKEN=1 nix flake check --keep-going --no-build --allow-import-from-derivation  --no-update-lock-file --fallback -L ${NIX_DEBUG_COMMAND} --impure --option sandbox relaxed
+NIX_DEBUG_COMMAND="" && [ $ACTIONS_RUNNER_DEBUG == "true" ] && NIX_DEBUG_COMMAND='--print-build-logs --debug --show-trace --verbose'
+NIXPKGS_ALLOW_BROKEN=1 nix flake check --keep-going --no-build --allow-import-from-derivation  --no-update-lock-file --fallback -L ${NIX_DEBUG_COMMAND} --impure --option sandbox relaxed --impure 2>&1 | tee "nix.check.log"
+
+EXIT=$(cat "nix.check.log" | grep --invert-match '^error (ignored): error:' | grep --invert-match '^error: some errors were encountered during the evaluation' | grep -c 'error:')
+
+if [ $EXIT != 0 ];
+then
+  exit 42
+fi
