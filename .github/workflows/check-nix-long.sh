993660247833e1ba5cd0337866114806eeca95d5
Dzmitry Lahoda <dzmitry@lahoda.pro>
2022-12-15 13:45:37 +0000

    comment bug
    Signed-off-by: Dzmitry Lahoda <dzmitry@lahoda.pro>

--- .github/workflows/check-nix-long.sh
+++ .github/workflows/check-nix-long.sh
@@ -1,4 +1,5 @@
 NIX_DEBUG_COMMAND="" && [ $ACTIONS_RUNNER_DEBUG -eq "true" ] && NIX_DEBUG_COMMAND='--print-build-logs --debug --show-trace --verbose'
 NIXPKGS_ALLOW_BROKEN=1 nix flake check --keep-going --no-build --allow-import-from-derivation  --no-update-lock-file --fallback -L ${NIX_DEBUG_COMMAND} --impure --option sandbox relaxed --impure 2>&1 | tee "nix.check.log"  || true
+# https://github.com/NixOS/nix/issues/7464
 EXIT=$(cat "nix.check.log" | grep --invert-match '^error (ignored): error:' | grep --invert-match '^error: some errors were encountered during the evaluation' | grep -c 'error:')
 exit $EXIT
