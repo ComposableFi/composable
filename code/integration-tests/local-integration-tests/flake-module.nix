1e70eab92354a718c621ed32fc33431b8c49d983
Dzmitry Lahoda <dzmitry@lahoda.pro>
2022-12-16 20:10:51 +0000

    Update code/integration-tests/local-integration-tests/flake-module.nix
    Co-authored-by: obsessed-cake <116171426+obsessed-cake@users.noreply.github.com>
    Signed-off-by: Dzmitry Lahoda <dzmitry@lahoda.pro>

--- code/integration-tests/local-integration-tests/flake-module.nix
+++ code/integration-tests/local-integration-tests/flake-module.nix
@@ -1,27 +1,20 @@
 { self, ... }: {
   perSystem = { config, self', inputs', pkgs, system, crane, systemCommonRust
     , cargoTools, ... }: {
-      packages = {
-        check-picasso-integration-tests = crane.nightly.cargoBuild
+      packages = let 
+        checkIntegrationTests = { chainName }: crane.nightly.cargoBuild
           (systemCommonRust.common-attrs // {
             src = cargoTools.rustSrc;
-            pname = "picasso-local-integration-tests";
+            pname = "${chainName}-local-integration-tests";
             doInstallCargoArtifacts = false;
             cargoArtifacts = self'.packages.common-test-deps;
             cargoBuildCommand = "cargo test --package local-integration-tests";
             cargoExtraArgs =
-              "--features=local-integration-tests,picasso,std --no-default-features --verbose";
+              "--features=local-integration-tests,${chainName},std --no-default-features --verbose";
           });
-        check-dali-integration-tests = crane.nightly.cargoBuild
-          (systemCommonRust.common-attrs // {
-            src = cargoTools.rustSrc;
-            pname = "dali-local-integration-tests";
-            doInstallCargoArtifacts = false;
-            cargoArtifacts = self'.packages.common-test-deps;
-            cargoBuildCommand = "cargo test --package local-integration-tests";
-            cargoExtraArgs =
-              "--features=local-integration-tests,dali,std --no-default-features --verbose";
-          });
+      in {
+        check-dali-integration-tests = checkIntegrationTests { chainName = "dali"; };
+        check-picasso-integration-tests = checkIntegrationTests { chainName = "picasso"; };
       };
     };
 }
