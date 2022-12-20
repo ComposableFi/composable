4073c40e567d4b40df569475c07762b6e0845e29
fl-y <thedigitalimg@gmail.com>
2022-12-21 02:14:08 +0900

    feat: privacy buttons

--- frontend/apps/pablo/components/Organisms/NavBar.tsx
+++ frontend/apps/pablo/components/Organisms/NavBar.tsx
@@ -19,8 +19,8 @@ import { NextRouter, useRouter } from "next/router";
 import { Logo } from "../Atoms";
 import { MenuItemType } from "../types";
 import { FC } from "react";
-import { Link } from "../Molecules/Link";
 import config from "@/constants/config";
+import { Link } from "../Molecules/Link";
 
 const MENU_ITEMS: MenuItemType[] = [
   {
@@ -122,51 +122,93 @@ export const NavBar: FC = () => {
   const isTablet = useTablet();
   const router = useRouter();
   return (
-    <Box pl={isTablet ? 0 : 4}>
-      <Box
-        sx={{
-          padding: theme.spacing(6, 3),
-          mb: theme.spacing(6),
+    <div
+      style={{
+        minHeight: "100vh",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+      }}
+    >
+      <Box pl={isTablet ? 0 : 4}>
+        <Box
+          sx={{
+            padding: theme.spacing(6, 3),
+            mb: theme.spacing(6),
+          }}
+        >
+          <Logo />
+        </Box>
+        <List>
+          {Object.entries(MENU_ITEMS).map(([key, config]) => {
+            return config.subItems ? (
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={
+                    <ExpandMore sx={{ color: theme.palette.primary.light }} />
+                  }
+                  aria-controls={`${config.label}-content`}
+                  id={`${config.label}-header`}
+                >
+                  <ListItem key={key} disabled={config.status === "inactive"}>
+                    <ListItemIcon>
+                      <config.icon />
+                    </ListItemIcon>
+                    <ListItemText primary={config.label} />
+                    {config.endAdornment && (
+                      <ListItemIcon
+                        sx={{ "> a": { color: theme.palette.primary.light } }}
+                      >
+                        {config.endAdornment}
+                      </ListItemIcon>
+                    )}
+                  </ListItem>
+                </AccordionSummary>
+                <AccordionDetails>
+                  {Object.entries(config.subItems).map(([key, config]) =>
+                    MenuItem(router, config, key, theme, true)
+                  )}
+                </AccordionDetails>
+              </Accordion>
+            ) : (
+              MenuItem(router, config, key, theme, false)
+            );
+          })}
+        </List>
+      </Box>
+      <div
+        style={{
+          padding: "0px 65px 87px 57px",
+          height: "16px",
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          gap: "24px",
         }}
       >
-        <Logo />
-      </Box>
-      <List>
-        {Object.entries(MENU_ITEMS).map(([key, config]) => {
-          return config.subItems ? (
-            <Accordion>
-              <AccordionSummary
-                expandIcon={
-                  <ExpandMore sx={{ color: theme.palette.primary.light }} />
-                }
-                aria-controls={`${config.label}-content`}
-                id={`${config.label}-header`}
-              >
-                <ListItem key={key} disabled={config.status === "inactive"}>
-                  <ListItemIcon>
-                    <config.icon />
-                  </ListItemIcon>
-                  <ListItemText primary={config.label} />
-                  {config.endAdornment && (
-                    <ListItemIcon
-                      sx={{ "> a": { color: theme.palette.primary.light } }}
-                    >
-                      {config.endAdornment}
-                    </ListItemIcon>
-                  )}
-                </ListItem>
-              </AccordionSummary>
-              <AccordionDetails>
-                {Object.entries(config.subItems).map(([key, config]) =>
-                  MenuItem(router, config, key, theme, true)
-                )}
-              </AccordionDetails>
-            </Accordion>
-          ) : (
-            MenuItem(router, config, key, theme, false)
-          );
-        })}
-      </List>
-    </Box>
+        <a
+          style={{
+            fontSize: "10px",
+            color: "rgba(255,255,255,0.6)",
+            textDecoration: "unset",
+          }}
+          href="/legal/privacy-policy"
+        >
+          Privacy Policy
+        </a>
+        <a
+          style={{
+            fontSize: "10px",
+            color: "rgba(255,255,255,0.6)",
+            textDecoration: "unset",
+          }}
+          href="/legal/terms-of-use"
+        >
+          Terms of use
+        </a>
+      </div>
+    </div>
   );
 };
