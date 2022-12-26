2b36d785b0e2cb9227a1bc4eadb5e380957e3b54
Mamali <isthereanybody@gmail.com>
2022-12-26 19:26:17 +0330

    Add math library for swap

--- frontend/apps/pablo/store/pools/subscribePoolAmount.ts
+++ frontend/apps/pablo/store/pools/subscribePoolAmount.ts
@@ -1,16 +1,7 @@
 import { ApiPromise } from "@polkadot/api";
 import useStore from "@/store/useStore";
 import { fromChainUnits } from "@/defi/utils";
-
-// const POOL_4_130_ADDRESS = "5w3oyasYQg6xWPRnTBT5A2zLnRDBngFZQP2ify51JjUfKCDD";
-// const POOL_1_130_ADDRESS =
-//   "5w3oyasYQg6xWPRnTBTLu4XvtutPFEMS93yEDukqmZMPaznS";
-// const POOL_1_4_ADDRESS = "";
-//
-// // This should be fetched from RPC or inside
-// const hardcodedPoolWalletAddress = {
-//
-// }
+import { getSubAccount } from "@/defi/utils/pablo/getSubAccount";
 
 async function fetchInPool(
   api: ApiPromise,
@@ -18,7 +9,13 @@ async function fetchInPool(
   assetOut: string,
   wallet: string
 ) {
-  const inPoolAssetIn = await api.query.tokens.accounts(wallet, assetIn);
+  let inPoolAssetIn: any;
+  if (assetIn === "1") {
+    const out = await api.query.system.account(wallet);
+    inPoolAssetIn = out.data;
+  } else {
+    inPoolAssetIn = await api.query.tokens.accounts(wallet, assetIn);
+  }
   const inPoolAssetOut = await api.query.tokens.accounts(wallet, assetOut);
 
   return {
@@ -42,13 +39,13 @@ export function subscribePoolAmount(api: ApiPromise | undefined) {
       for (const pool of pools) {
         const assetIn = pool.config.assets[0].getPicassoAssetId() as string;
         const assetOut = pool.config.assets[1].getPicassoAssetId() as string;
+        const ownerWalletAddress = getSubAccount(api, pool.poolId.toString());
         const amount = await fetchInPool(
           api,
           assetIn,
           assetOut,
-          pool.config.owner
+          ownerWalletAddress
         );
-
         setPoolAmount(pool.poolId.toString(), amount);
         api.query.tokens
           .totalIssuance(pool.config.lpToken.toString())
