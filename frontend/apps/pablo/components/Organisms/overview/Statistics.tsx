0f553fdeacc61083aa722f8e17ac1d9b33197304
Mamali <isthereanybody@gmail.com>
2022-12-26 16:24:48 +0330

    remove pblo price from overview

--- frontend/apps/pablo/components/Organisms/overview/Statistics.tsx
+++ frontend/apps/pablo/components/Organisms/overview/Statistics.tsx
@@ -1,21 +1,19 @@
-import { Grid, Typography, useTheme } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { GridProps } from "@mui/system";
 import { HighlightBox } from "@/components/Atoms/HighlightBox";
-import { useAssetIdOraclePrice } from "@/defi/hooks";
-import { PBLO_ASSET_ID } from "@/defi/utils";
 import BigNumber from "bignumber.js";
+import { FC } from "react";
 
-const threeColumnPageSize = {
+const twoColumns = {
   xs: 12,
-  md: 4,
+  sm: 6,
 };
 
 type ItemProps = {
   label: string;
   value: string;
 };
-const Item: React.FC<ItemProps> = ({ label, value }) => {
-  const theme = useTheme();
+const Item: FC<ItemProps> = ({ label, value }) => {
   return (
     <HighlightBox
       variant="contained"
@@ -33,29 +31,23 @@ const Item: React.FC<ItemProps> = ({ label, value }) => {
   );
 };
 
-export const Statistics: React.FC<GridProps> = ({ ...gridProps }) => {
-  const theme = useTheme();
-
-  const PBLO_PRICE = useAssetIdOraclePrice(PBLO_ASSET_ID);
+export const Statistics: FC<GridProps> = ({ ...gridProps }) => {
   const totalValueLocked = new BigNumber(0);
   const tradingVolume24hrs = new BigNumber(0);
 
   return (
     <Grid container spacing={3} {...gridProps}>
-      <Grid item {...threeColumnPageSize}>
+      <Grid item {...twoColumns}>
         <Item
           label="Total value locked"
           value={`$${totalValueLocked.toFormat()}`}
         />
       </Grid>
-      <Grid item {...threeColumnPageSize}>
+      <Grid item {...twoColumns}>
         <Item
           label="24h trading volume"
           value={`$${tradingVolume24hrs.toFormat()}`}
         />
-      </Grid>
-      <Grid item {...threeColumnPageSize}>
-        <Item label="PBLO price" value={`$${PBLO_PRICE.toFormat()}`} />
       </Grid>
     </Grid>
   );
