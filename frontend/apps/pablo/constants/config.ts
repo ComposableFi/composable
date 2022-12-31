d186871758036e5be40519fcc11e53b86dba8e96
Joon <thedigitalimg@gmail.com>
2022-12-31 23:08:45 +0900

    feat: content change

--- frontend/apps/pablo/constants/config.ts
+++ frontend/apps/pablo/constants/config.ts
@@ -1,7 +1,7 @@
 const config = {
   picassoAppUrl: "https://app.picasso.xyz",
   applicationName: "Pablo | Composable Finance",
-  appDescription: "The community owned substrate-native DEX on Kusama",
+  appDescription: "The community owned substrate-native DEX on Kusama.",
   appUrl: "app.pablo.finance",
   twitterHandle: "@picasso_network",
 } as const;
