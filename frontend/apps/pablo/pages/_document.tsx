c83990b46a95b15a17b635e5259f40e8fe09e0a6
Joon <thedigitalimg@gmail.com>
2022-12-31 22:36:46 +0900

    feat: edit to large image summary

--- frontend/apps/pablo/pages/_document.tsx
+++ frontend/apps/pablo/pages/_document.tsx
@@ -54,7 +54,7 @@ export default class MyDocument extends Document {
           />
           <link rel="manifest" href="/manifest.json" />
           <link rel="shortcut icon" href="/favicon.ico" />
-          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:url" content={config.appUrl} />
           <meta name="twitter:title" content={config.applicationName} />
           <meta name="twitter:description" content={config.appDescription} />
