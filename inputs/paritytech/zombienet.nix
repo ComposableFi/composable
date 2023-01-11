fb23823eb45d5f130fbe64275d958482adb3892f
cor <cor@pruijs.dev>
2023-01-11 12:10:01 +0100

    Update inputs/paritytech/zombienet.nix
    Co-authored-by: obsessed-cake <116171426+obsessed-cake@users.noreply.github.com>
    Signed-off-by: cor <cor@pruijs.dev>

--- inputs/paritytech/zombienet.nix
+++ inputs/paritytech/zombienet.nix
@@ -29,7 +29,6 @@
             python3
             pkg-config
             vips
-            python3
             nodePackages.node-gyp-build
             nodePackages.node-gyp
             nodePackages.typescript
