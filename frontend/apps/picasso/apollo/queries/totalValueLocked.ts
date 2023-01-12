12bcff14dc9a0c55ba9766c55f70ff86e644e91a
Mamali <isthereanybody@gmail.com>
2023-01-12 16:35:46 +0330

    PICA-67: Fix Subsquid changes in totalValueLocked and overviewStats chart (#2908)

--- frontend/apps/picasso/apollo/queries/totalValueLocked.ts
+++ frontend/apps/picasso/apollo/queries/totalValueLocked.ts
@@ -1,19 +1,23 @@
 import { gql } from "@apollo/client";
+import { AssetAmountPair } from "@/apollo/queries/types";
 
 export type TVL = {
-  totalValueLocked: number,
-  date: string
-}
+  lockedValues: Array<AssetAmountPair>;
+  date: string;
+};
 
 export type TotalValueLocked = {
-  totalValueLocked: TVL[]
-}
+  totalValueLocked: TVL[];
+};
 
 export const GET_TOTAL_VALUE_LOCKED = gql`
-    query getTotalValueLocked($range: String! ) {
-        totalValueLocked(params: {range: $range}) {
-            date
-            totalValueLocked
-        }
+  query getTotalValueLocked($range: String!, $source: String!) {
+    totalValueLocked(params: { range: $range, source: $source }) {
+      date
+      lockedValues {
+        assetId
+        amount
+      }
     }
+  }
 `;
