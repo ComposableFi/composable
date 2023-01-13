b4f3bac9428941bd2b823850f099c9744906179e
cor <cor@pruijs.dev>
2023-01-13 15:53:52 +0100

    chore: fmt

--- flake/check.nix
+++ flake/check.nix
@@ -1,14 +1,20 @@
 { self, ... }: {
   perSystem = { config, self', inputs', pkgs, system, ... }: {
-		packages = {
+    packages = {
       check = let
-				checks = ["spell-check" "nixfmt-check" "hadolint-check" "deadnix-check" "docs-static"];
-				toCommand = check: ''
-	        echo "Checking ${check}..."
-					nix build .\#${check} --no-warn-dirty
-				'';
-				script = pkgs.lib.concatMapStrings toCommand checks;
-	    in pkgs.writeShellScriptBin "check" script;
-		};
+        checks = [
+          "spell-check"
+          "nixfmt-check"
+          "hadolint-check"
+          "deadnix-check"
+          "docs-static"
+        ];
+        toCommand = check: ''
+                  echo "Checking ${check}..."
+          				nix build .\#${check} --no-warn-dirty
+          			'';
+        script = pkgs.lib.concatMapStrings toCommand checks;
+      in pkgs.writeShellScriptBin "check" script;
+    };
   };
 }
