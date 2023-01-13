0213058f6faf8acf422eb9562360d671eb0a420b
cor <cor@pruijs.dev>
2023-01-13 15:52:41 +0100

    feat: nix run .\#check local check runner

---
+++ flake/check.nix
@@ -0,0 +1,14 @@
+{ self, ... }: {
+  perSystem = { config, self', inputs', pkgs, system, ... }: {
+		packages = {
+      check = let
+				checks = ["spell-check" "nixfmt-check" "hadolint-check" "deadnix-check" "docs-static"];
+				toCommand = check: ''
+	        echo "Checking ${check}..."
+					nix build .\#${check} --no-warn-dirty
+				'';
+				script = pkgs.lib.concatMapStrings toCommand checks;
+	    in pkgs.writeShellScriptBin "check" script;
+		};
+  };
+}
