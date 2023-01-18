4c3902669817ca0260d96fed302f48d4ae64ebd1
Vim Wickramasinghe <vimukthi@composable.finance>
2023-01-18 17:43:21 +0100

    Release v2.10006.0 to upgrade to Polkadot v0.9.30

--- code/parachain/runtime/composable/src/lib.rs
+++ code/parachain/runtime/composable/src/lib.rs
@@ -115,7 +115,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// The version of the runtime specification. A full node will not attempt to use its native
 	//   runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
 	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
-	spec_version: 10_005,
+	spec_version: 10_006,
 	impl_version: 3,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
