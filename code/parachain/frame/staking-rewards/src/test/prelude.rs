f7c6fe9f9f96d3262e67f8e126cfe994aed7ad88
Kanstantsin Kastsevich <kkast@users.noreply.github.com>
2023-01-27 00:07:03 +0300

    [PICA-102] add claimed_amount to Claimed event (#3001)
    https://app.clickup.com/t/86775ywct
    
    - add claimed_amount to Claimed event
    - removed logic for slashing rewards for early unstakes
    - fixed tests

--- code/parachain/frame/staking-rewards/src/test/prelude.rs
+++ code/parachain/frame/staking-rewards/src/test/prelude.rs
@@ -6,9 +6,6 @@ pub use sp_core::{
 	sr25519::{Public, Signature},
 	H256,
 };
-use sp_runtime::traits::{IdentifyAccount, Verify};
-
-pub type AccountId = <<Signature as Verify>::Signer as IdentifyAccount>::AccountId;
 
 #[cfg(test)]
 pub use composable_tests_helpers::test::currency::*;
