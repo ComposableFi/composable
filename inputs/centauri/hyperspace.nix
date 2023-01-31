91147a4c1fc63029b9f36aa4e7cddc8519e5e246
Obsessed Cake <aaaoxxx@gmail.com>
2023-01-31 19:10:46 +0400

    Nixify hyperspace-composable and hyperspace-picasso

--- inputs/centauri/hyperspace.nix
+++ inputs/centauri/hyperspace.nix
@@ -1,17 +1,11 @@
 { self, ... }: {
   perSystem = { config, self', inputs', pkgs, system, crane, ... }: {
-    packages = rec {
-      hyperspace-dali-container = pkgs.dockerTools.buildImage {
-        tag = "latest";
-        name = "hyperspace-dali";
-        config = { Entrypoint = [ "${hyperspace-dali}/bin/hyperspace" ]; };
-      };
-
-      hyperspace-dali = let
-        src = pkgs.stdenv.mkDerivation rec {
-          name = "centauri";
+    packages = let
+      hyperspace-src = chainName: subxtClient:
+        pkgs.stdenv.mkDerivation rec {
+          name = "centauri-${chainName}";
           pname = "${name}";
-          buildInputs = [ self'.packages.dali-subxt-client ];
+          buildInputs = [ subxtClient ];
           src = pkgs.fetchFromGitHub {
             owner = "obsessed-cake";
             repo = "centauri";
@@ -23,33 +17,72 @@
             mkdir $out
             cp --archive $src/. $out/
             chmod u+w $out/utils/subxt/generated/src/{parachain.rs,relaychain.rs}
-            cp ${self'.packages.dali-subxt-client}/* $out/utils/subxt/generated/src/
+            cp ${subxtClient}/* $out/utils/subxt/generated/src/
           '';
           dontFixup = true;
           dontStrip = true;
         };
-      in crane.stable.buildPackage rec {
-        name = "hyperspace-dali";
-        pname = "${name}";
-        cargoArtifacts = crane.stable.buildDepsOnly {
-          inherit src;
-          doCheck = false;
-          cargoExtraArgs = "-p hyperspace --features dali";
-          cargoTestCommand = "";
+
+      hyperspace = { chainName, subxtClient }:
+        let
+          crateFeatures = if chainName == "dali" then "--features dali" else "";
+        in crane.stable.buildPackage rec {
+          name = "hyperspace-${chainName}";
+          pname = "${name}";
+          cargoArtifacts = crane.stable.buildDepsOnly {
+            src = hyperspace-src chainName subxtClient;
+            doCheck = false;
+            cargoExtraArgs = "-p hyperspace ${crateFeatures}";
+            cargoTestCommand = "";
+            BuildInputs = [ pkgs.protobuf ];
+            PROTOC = "${pkgs.protobuf}/bin/protoc";
+            PROTOC_INCLUDE = "${pkgs.protobuf}/include";
+            PROTOC_NO_VENDOR = "1";
+          };
+          src = hyperspace-src chainName subxtClient;
           BuildInputs = [ pkgs.protobuf ];
           PROTOC = "${pkgs.protobuf}/bin/protoc";
           PROTOC_INCLUDE = "${pkgs.protobuf}/include";
           PROTOC_NO_VENDOR = "1";
+          doCheck = false;
+          cargoExtraArgs = "-p hyperspace ${crateFeatures}";
+          cargoTestCommand = "";
+          meta = { mainProgram = "hyperspace"; };
         };
-        inherit src;
-        BuildInputs = [ pkgs.protobuf ];
-        PROTOC = "${pkgs.protobuf}/bin/protoc";
-        PROTOC_INCLUDE = "${pkgs.protobuf}/include";
-        PROTOC_NO_VENDOR = "1";
-        doCheck = false;
-        cargoExtraArgs = "-p hyperspace --features dali";
-        cargoTestCommand = "";
-        meta = { mainProgram = "hyperspace"; };
+    in rec {
+      hyperspace-composable = hyperspace {
+        chainName = "composable";
+        subxtClient = self'.packages.composable-subxt-client;
+      };
+
+      hyperspace-picasso = hyperspace {
+        chainName = "picasso";
+        subxtClient = self'.packages.picasso-subxt-client;
+      };
+
+      hyperspace-dali = hyperspace {
+        chainName = "dali";
+        subxtClient = self'.packages.dali-subxt-client;
+      };
+
+      hyperspace-composable-container = pkgs.dockerTools.buildImage {
+        tag = "latest";
+        name = "hyperspace-composable";
+        config = {
+          Entrypoint = [ "${hyperspace-composable}/bin/hyperspace" ];
+        };
+      };
+
+      hyperspace-picasso-container = pkgs.dockerTools.buildImage {
+        tag = "latest";
+        name = "hyperspace-dali";
+        config = { Entrypoint = [ "${hyperspace-picasso}/bin/hyperspace" ]; };
+      };
+
+      hyperspace-dali-container = pkgs.dockerTools.buildImage {
+        tag = "latest";
+        name = "hyperspace-dali";
+        config = { Entrypoint = [ "${hyperspace-dali}/bin/hyperspace" ]; };
       };
     };
   };
--- inputs/composable/dali-subxt-client.nix
+++ inputs/composable/dali-subxt-client.nix
@@ -1,6 +1,48 @@
 { ... }: {
   perSystem = { self', pkgs, ... }: {
     packages = {
+      composable-subxt-client = pkgs.stdenv.mkDerivation {
+        name = "composable-subxt-client";
+        dontUnpack = true;
+        buildInputs = [
+          self'.packages.centauri-codegen
+          self'.packages.composable-runtime
+          self'.packages.rococo-wasm-runtime
+          pkgs.rustfmt
+        ];
+        installPhase = ''
+          mkdir -p $out
+
+          ${pkgs.lib.meta.getExe self'.packages.centauri-codegen} \
+            --path $out \
+            --parachain-wasm=${self'.packages.composable-runtime}/lib/runtime.optimized.wasm \
+            --relaychain-wasm=${self'.packages.rococo-wasm-runtime}/lib/rococo_runtime.compact.compressed.wasm
+
+          rustfmt --edition=2018 $out/*
+        '';
+      };
+
+      picasso-subxt-client = pkgs.stdenv.mkDerivation {
+        name = "picasso-subxt-client";
+        dontUnpack = true;
+        buildInputs = [
+          self'.packages.centauri-codegen
+          self'.packages.picasso-runtime
+          self'.packages.rococo-wasm-runtime
+          pkgs.rustfmt
+        ];
+        installPhase = ''
+          mkdir -p $out
+
+          ${pkgs.lib.meta.getExe self'.packages.centauri-codegen} \
+            --path $out \
+            --parachain-wasm=${self'.packages.picasso-runtime}/lib/runtime.optimized.wasm \
+            --relaychain-wasm=${self'.packages.rococo-wasm-runtime}/lib/rococo_runtime.compact.compressed.wasm
+
+          rustfmt --edition=2018 $out/*
+        '';
+      };
+
       dali-subxt-client = pkgs.stdenv.mkDerivation {
         name = "dali-subxt-client";
         dontUnpack = true;
