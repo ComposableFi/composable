76ec38009a0b436342de7efbfe113f9a1ee2b46f
cor <cor@pruijs.dev>
2023-02-01 16:44:17 +0100

    refactor(ci): move ci packages/checks into a single derivation

--- flake/check.nix
+++ flake/check.nix
@@ -1,6 +1,62 @@
 { self, ... }: {
   perSystem = { config, self', inputs', pkgs, system, ... }: {
     packages = {
+      all-ci-packages = pkgs.linkFarmFromDrvs "all-ci-packages" (with self'.packages; [
+        # TODO: I think these can be removed since they are dependencies of the other derivations
+        common-deps
+        common-test-deps
+
+        cargo-fmt-check
+        cargo-clippy-check
+        cargo-deny-check
+        cargo-udeps-check
+        taplo-check
+        prettier-check
+        nixfmt-check
+        deadnix-check
+        spell-check
+        docs-static # todo: deployment
+        frontend-static
+        hadolint-check
+        benchmarks-check
+        unit-tests
+        composable-node
+        composable-bench-node
+        polkadot-node
+        statemine-node
+        bifrost-node
+        acala-node
+        dali-subxt-client
+        zombienet
+        price-feed
+        devnet-container
+        bridge-devnet-dali-container
+        devnet-initialize-script-picasso-persistent
+
+
+        # not needed as part of effects-gate
+        devnet-dali-complete
+        devnet-picasso-complete
+        devnet-dali
+        devnet-centauri
+        # hyperspace-dali
+
+
+        # needs to be pushed to docker
+        cmc-api
+        cmc-api-image
+        
+
+        # TODO: benchmarks-once-dali, benchmarks-once-picasso, benchmarks-once-composable
+        
+        
+        # unsure about these
+        check-dali-integration-tests
+        check-picasso-integration-tests
+        simnode-tests
+        devnet-integration-tests
+      ]);
+
       check = let
         # The order of these checks can still be improved.
         # In general, the shorter the check takes, the higher up it should be.
