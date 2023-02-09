589ada886a8038d6752a4837d8e740d4de16d36f
Mamali <isthereanybody@gmail.com>
2023-02-09 14:54:27 +0000

    Generate labels from seconds

--- frontend/apps/picasso/components/Organisms/Staking/utils.ts
+++ frontend/apps/picasso/components/Organisms/Staking/utils.ts
@@ -25,6 +25,29 @@ export function getMinDuration(
     : 0;
 }
 
+function getLabelFromDurationInSeconds(duration: string | number) {
+  const value = Number(duration);
+  if (value === 0) return "No Lock period";
+  const SECONDS_IN_WEEK = 604800;
+  const numberOfWeeks = value / SECONDS_IN_WEEK;
+  const numberOfMonths = numberOfWeeks / 4;
+  const andWeeks = numberOfWeeks % 4;
+  let output = "";
+  if (numberOfMonths === 0 && numberOfWeeks === 0) {
+    output += "No lock period";
+  } else if (Math.floor(numberOfMonths) === 0) {
+    output += `${numberOfWeeks} weeks`;
+  } else if (numberOfMonths === 1) {
+    output += "1 month";
+  } else if (Math.floor(numberOfMonths) !== numberOfMonths) {
+    output += `${Math.floor(numberOfMonths)} months and ${andWeeks} weeks`;
+  } else {
+    output += `${numberOfMonths} months`;
+  }
+
+  return output;
+}
+
 export function getOptions(
   hasRewardPools: boolean,
   picaRewardPool: RewardPool
@@ -36,7 +59,7 @@ export function getOptions(
         (acc, [duration, _]) => [
           ...acc,
           {
-            label: "",
+            label: getLabelFromDurationInSeconds(duration),
             value: Number(duration),
           },
         ],
