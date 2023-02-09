1d9923a3d21149983a24273d8fd33bfc8abdfc9d
Mamali <isthereanybody@gmail.com>
2023-02-09 10:27:16 +0000

    Add Loading Spinner

--- frontend/apps/picasso/pages/feedback.tsx
+++ frontend/apps/picasso/pages/feedback.tsx
@@ -1,13 +1,26 @@
 import Default from "@/components/Templates/Default";
 import Script from "next/script";
 import * as React from "react";
-import { Grid } from "@mui/material";
+import { useState } from "react";
+import { CircularProgress, Grid, Stack } from "@mui/material";
 
 export default function Feedback() {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <Default>
-      <Grid container mt={9} maxWidth={1032} mx="auto">
+      <Grid container mt={10} maxWidth={1032} mx="auto">
         <Grid item xs={12}>
+          {isLoading && (
+            <Stack
+              alignItems="center"
+              justifyContent="center"
+              width="100%"
+              height="100%"
+            >
+              <CircularProgress />
+            </Stack>
+          )}
           <div data-upvoty=""></div>
         </Grid>
       </Grid>
@@ -22,6 +35,7 @@ export default function Feedback() {
             ssoToken: null,
             baseUrl: "composable-fi.upvoty.com",
           });
+          setIsLoading(false);
         }}
         src={"https://composable-fi.upvoty.com/javascript/upvoty.embed.js"}
       />
