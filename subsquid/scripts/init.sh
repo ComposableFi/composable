ff7c26a071de27e8f564c4c08e8a90e621047cea
Radu Popa <radupopa21@gmail.com>
2023-02-09 15:57:17 +0200

    define shell in the first line of the script

--- subsquid/scripts/init.sh
+++ subsquid/scripts/init.sh
@@ -1,7 +1,47 @@
 #!/bin/sh
 
+set -eou pipefail
 
-function log {
+# main entry point for the script
+main() {
+  #  ${foo:="default value if unset"}
+  if ${TRIGGER_REPROCESSING:=false} ; then
+    run_steps_to_trigger_reprocessing
+    export TRIGGER_REPROCESSING=false
+  fi
+
+  msg="subsquid-processor image version is"
+  log INFO "${msg} $(yq '.services.processor.image'  docker-compose.yml)"
+
+  log INFO "Running db migration"
+  npx squid-typeorm-migration apply || log DIE "DB migration step failed"
+
+  log INFO "Running sqlinit "
+  node lib/helperInit.js || log DIE  "Sqlinit step failed"
+
+  log INFO "Starting the processor" 
+  node -r dotenv/config lib/processor.js || log DIE "Starting the processor step failed"
+}
+
+run_steps_to_trigger_reprocessing() {
+  log INFO "Reprocessing triggered"
+  set_db_connection_details
+  psql -c "DROP SCHEMA public CASCADE ;
+           DROP SCHEMA squid_processor cascade ;
+           create SCHEMA public  ;
+           create schema squid_processor ;"
+}
+
+set_db_connection_details() {
+  # https://www.postgresql.org/docs/current/libpq-envars.html
+  export PGHOST="${DB_HOST}"
+  export PGDATABASE="${DB_NAME}"
+  export PGPASSWORD="${DB_PASS}"
+  export PGUSER="${DB_USER}"
+}
+
+
+log() {
   lvl=$1 msg=$2
   fmt='+%Y-%m-%d %H:%M:%S'
   lg_date=$(date "${fmt}")
@@ -14,15 +54,4 @@ function log {
   fi
 }
 
-
-log INFO "Running db migration"
-npx squid-typeorm-migration apply || log DIE "DB migration step failed"
-
-
-log INFO "Running sqlinit "
-node lib/helperInit.js || log DIE  "Sqlinit step failed"
-
-log INFO "Starting the processor" 
-node -r dotenv/config lib/processor.js || log DIE "Starting the processor step failed"
-
-
+main
