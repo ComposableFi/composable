3e6e931fba25cdd75084134304e3d3873b959706
Vim Wickramasinghe <vimukthi@composable.finance>
2023-02-14 10:52:38 +0100

    release v2.1008.0

--- code/parachain/runtime/dali/src/versions.rs
+++ code/parachain/runtime/dali/src/versions.rs
@@ -16,7 +16,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
 	// This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
 	//   the compatible custom types.
-	spec_version: 10_007,
+	spec_version: 10_008,
 	impl_version: 3,
 	apis: crate::RUNTIME_API_VERSIONS,
 	transaction_version: 1,
