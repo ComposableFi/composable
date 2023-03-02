e6bf5854222b9574d7f9323a07d91a310773c7f0
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-03-02 10:55:36 +0000

    nopd de of  9.38

--- inputs/paritytech/polkadot.nix
+++ inputs/paritytech/polkadot.nix
@@ -2,9 +2,27 @@
   perSystem =
     { config, self', inputs', pkgs, lib, system, crane, systemCommonRust, ... }:
     let
+      substrate = {
+        doCheck = false;
+        buildInputs = with pkgs; [ openssl zstd ];
+        nativeBuildInputs = with pkgs;
+          [ clang pkg-config ] ++ [ self'.packages.rust-nightly ]
+          ++ lib.optional stdenv.isDarwin
+          (with pkgs.darwin.apple_sdk.frameworks; [
+            Security
+            SystemConfiguration
+          ]);
+        LD_LIBRARY_PATH = lib.strings.makeLibraryPath [
+          pkgs.stdenv.cc.cc.lib
+          pkgs.llvmPackages.libclang.lib
+        ];
+        LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+        PROTOC = "${pkgs.protobuf}/bin/protoc";
+        ROCKSDB_LIB_DIR = "${pkgs.rocksdb}/lib";
+      };
       buildPolkadotNode =
         { name, version, repo, owner, rev, hash, cargoSha256 }:
-        pkgs.rustPlatform.buildRustPackage rec {
+        pkgs.rustPlatform.buildRustPackage (rec {
           inherit name version cargoSha256;
           src = pkgs.fetchgit {
             url = "https://github.com/${owner}/${repo}.git";
@@ -16,26 +34,11 @@
           meta = { mainProgram = "polkadot"; };
 
           __noChroot = true;
-          doCheck = false;
-          buildInputs = with pkgs; [ openssl zstd ];
-          nativeBuildInputs = with pkgs;
-            [ clang pkg-config ] ++ [ self'.packages.rust-nightly ]
-            ++ lib.optional stdenv.isDarwin
-            (with pkgs.darwin.apple_sdk.frameworks; [
-              Security
-              SystemConfiguration
-            ]);
-          LD_LIBRARY_PATH = lib.strings.makeLibraryPath [
-            pkgs.stdenv.cc.cc.lib
-            pkgs.llvmPackages.libclang.lib
-          ];
-          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-          PROTOC = "${pkgs.protobuf}/bin/protoc";
-          ROCKSDB_LIB_DIR = "${pkgs.rocksdb}/lib";
-        };
+
+        } // substrate);
     in {
       packages = {
-        polkadot-node = let version = "v0.9.36";
+        polkadot-node = let version = "v0.9.38";
         in buildPolkadotNode rec {
           name = "polkadot-node";
           inherit version;
