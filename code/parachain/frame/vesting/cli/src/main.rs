4a8c7d8a0f06903ac71f71a1992fa7e89c0b51db
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-03-08 19:05:20 +0000

    clean

--- code/parachain/frame/vesting/cli/src/main.rs
+++ code/parachain/frame/vesting/cli/src/main.rs
@@ -1,13 +1,11 @@
 mod input;
 
 use clap::Parser;
-use csv::DeserializeRecordsIntoIter;
 use input::*;
-use sp_core::{hexdisplay::AsBytesRef, Pair};
-use sp_keyring::AccountKeyring;
-use tracing::info;
-use std::{io::Read, str::FromStr};
+use sp_core::Pair;
+use std::str::FromStr;
 use subxt::{dynamic::Value, tx::PairSigner, utils::AccountId32, OnlineClient, SubstrateConfig};
+use tracing::info;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -18,10 +16,9 @@ async fn main() -> anyhow::Result<()> {
 		String::from_utf8(std::fs::read(args.schedule).expect("file")).expect("string");
 	let mut rdr = csv::Reader::from_reader(csv_file.as_bytes());
 	let all: Vec<_> = rdr.deserialize::<Record>().map(|x| x.expect("record")).collect();
-	
 
 	let key = sp_core::sr25519::Pair::from_string(&args.key, None).expect("secret");
-	
+
 	let signer = PairSigner::new(key.clone());
 
 	let api = OnlineClient::<SubstrateConfig>::from_url(args.client).await?;
@@ -42,23 +39,27 @@ async fn main() -> anyhow::Result<()> {
 						"window",
 						Value::named_variant(
 							"MomentBased",
-							vec![("start", Value::u128(1)), ("period", Value::u128(1))],
+							vec![
+								("start", Value::u128(record.window_moment_start as u128)),
+								("period", Value::u128(record.window_moment_period as u128)),
+							],
 						),
 					),
-					("period_count", Value::u128(1)),
-					("per_period", Value::u128(1)),
+					("period_count", Value::u128(record.period_count as u128)),
+					("per_period", Value::u128(record.per_period)),
 				]),
 			),
 		];
 		let tx = subxt::dynamic::tx("Vesting", "vested_transfer", data);
 		let signed = api.tx().create_signed(&tx, &signer, <_>::default()).await.expect("offline");
-		signed.dry_run(None).await.expect("dry_run");
+		let result = signed.dry_run(None).await.expect("dry_run");
+		info!("dry_run {:?}", result);
 		let signed = hex::encode(signed.into_encoded());
 		info!("Signed Vesting::vested_transfer `0x{:?}`", signed);
 
 		// just for testing, do not really submit
 		// let hash = api.tx().sign_and_submit_then_watch_default(&tx, &signer).await?;
-		// let result = hash.wait_for_finalized_success().await.expect("block");		
+		// let result = hash.wait_for_finalized_success().await.expect("block");
 		// info!("Vesting schedule_info submitted {:?}", result);
 	}
 
