c6845b2165940c5a12d59d89a5a2cc7053797c58
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-03-08 14:57:32 +0000

    incompatible

--- code/parachain/frame/vesting/cli/src/args.rs
+++ code/parachain/frame/vesting/cli/src/args.rs
@@ -1,15 +1,17 @@
 use clap::Parser;
 
-use clap::Parser;
-
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
    /// WS url to node
    #[arg(long)]
-   client : String,
+   pub client : String,
 
    /// Link to CSV file with schedule
    #[arg(long)]
-   vesting_schedule: String,
+   pub schedule: String,
+
+   /// Private sudo key
+   #[arg(long)]
+   pub key: String,
 }
--- code/parachain/frame/vesting/cli/src/main.rs
+++ code/parachain/frame/vesting/cli/src/main.rs
@@ -2,6 +2,36 @@ mod args;
 
 use args::*;
 use clap::Parser;
-fn main() {
+use subxt::SubstrateConfig;
+use sp_keyring::AccountKeyring;
+use subxt::{
+    dynamic::Value,
+    tx::PairSigner,
+    OnlineClient,
+};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
 	let args = Args::parse();
+    println!("{:?}", &args);
+	let api = OnlineClient::<SubstrateConfig>::from_url(args.client).await?;
+    // 1. Dynamic Balance Transfer (the dynamic equivalent to the balance_transfer example).
+    //sp_core::{sr25519::Pair
+        let signer = PairSigner::new(AccountKeyring::Alice.pair());
+        
+        let dest = AccountKeyring::Bob.to_account_id();
+        let tx = subxt::dynamic::tx(
+        "System",
+        "remark",
+        vec![
+            Value::from_bytes([0,1]),
+        ],
+    );
+    
+    println!("submit the transaction with default params");
+    println!("{:?}", &tx);
+    let hash = api.tx().sign_and_submit_default(&tx, &signer).await?;
+    println!("Balance transfer extrinsic submitted: {hash}");
+    Ok(())
 }
