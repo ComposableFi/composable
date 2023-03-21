29f04c9d84a30283acc1038bce8d148317072561
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-03-21 23:13:31 +0000

    fixing comma

--- inputs/paritytech/zombienet/flake-module.nix
+++ inputs/paritytech/zombienet/flake-module.nix
@@ -1,37 +1,6 @@
 { self, ... }: {
   perSystem = { config, self', inputs', pkgs, system, devnetTools, ... }:
     let
-      # paritytech-zombienet = pkgs.stdenv.mkDerivation {
-      #   name = "zombienet";
-      #   inherit version;
-      #   src = "${paritytech-zombienet-src}/javascript";
-      #   buildInputs = with pkgs; [ nodejs ];
-      #   nativeBuildInputs = with pkgs;
-      #     [
-      #       yarn
-      #       nodejs
-      #       python3
-      #       nodePackages.node-gyp-build
-      #       nodePackages.node-gyp
-      #       nodePackages.typescript
-
-      #       vips
-      #       pkg-config
-      #     ] ++ devnetTools.withBaseContainerTools;
-      #   buildPhase = ''
-      #     mkdir home
-      #     export HOME=$PWD/home
-      #     npm install
-      #     npm run build
-      #   '';
-      #   installPhase = ''
-      #     mkdir --parents $out
-      #     cp . $out --recursive
-      #   '';
-      #   # https://app.clickup.com/t/3w8y83f
-      #   __noChroot = true;
-      # };
-
       prelude = pkgs.callPackage ./default.nix { };
       runtimeDeps = with pkgs;
         [ git git-lfs ] ++ devnetTools.withBaseContainerTools
