b3d82a32bb6359e1ecc87b475a4494c4ecd86222
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-03-28 10:32:49 +0100

    polkadots for dev and releease

--- inputs/paritytech/polkadot.nix
+++ inputs/paritytech/polkadot.nix
@@ -1,16 +1,6 @@
 { self, ... }: {
-  perSystem =
-    { config
-    , self'
-    , inputs'
-    , pkgs
-    , lib
-    , system
-    , crane
-    , systemCommonRust
-    , subTools
-    , ...
-    }:
+  perSystem = { config, self', inputs', pkgs, lib, system, crane
+    , systemCommonRust, subTools, ... }:
     let
       buildPolkadotNode =
         { name, version, repo, owner, rev, hash, cargoSha256 }:
@@ -28,31 +18,30 @@
           __noChroot = true;
 
         } // subTools.subenv);
-    in
-    {
+    in {
       packages = rec {
-        polkadot-node-9370 =
-          let version = "v0.9.37";
-          in buildPolkadotNode rec {
-            name = "polkadot-node";
-            inherit version;
-            repo = "polkadot";
-            owner = "paritytech";
-            rev = "refs/tags/${version}";
-            hash = "sha256-TTi4cKqQT/2ZZ/acGvcilqTlh2D9t4cfAtQQyVZWdmg=";
-            cargoSha256 = "sha256-3EcGLJNsbFl9ITJuVIrItp1e6weO9/NMNnMOJngsCP0=";
-          };
-        polkadot-node-9390 =
-          let version = "v0.9.39";
-          in buildPolkadotNode rec {
-            name = "polkadot-node";
-            inherit version;
-            repo = "polkadot";
-            owner = "paritytech";
-            rev = "refs/tags/${version}";
-            hash = "sha256-++aSGovKRE4+1hRoDqo6lSO4aenNrdvkVqaIXz4s0bk=";
-            cargoSha256 = "sha256-3EcGLJNsbFl9ITJuVIrItp1e6weO1/NMNnMOJngsCP0=";
-          };
+        # current version for centauri
+        polkadot-node-9370 = let version = "v0.9.37";
+        in buildPolkadotNode rec {
+          name = "polkadot-node";
+          inherit version;
+          repo = "polkadot";
+          owner = "paritytech";
+          rev = "refs/tags/${version}";
+          hash = "sha256-TTi4cKqQT/2ZZ/acGvcilqTlh2D9t4cfAtQQyVZWdmg=";
+          cargoSha256 = "sha256-3EcGLJNsbFl9ITJuVIrItp1e6weO9/NMNnMOJngsCP0=";
+        };
+        # for xcmv3 release and centauri client asap they upgrade
+        polkadot-node-9390 = let version = "v0.9.39";
+        in buildPolkadotNode rec {
+          name = "polkadot-node";
+          inherit version;
+          repo = "polkadot";
+          owner = "paritytech";
+          rev = "refs/tags/${version}";
+          hash = "sha256-++aSGovKRE4+1hRoDqo6lSO4aenNrdvkVqaIXz4s0bk=";
+          cargoSha256 = "sha256-RG/FvtrMCJB1BbMosSPlGJCKmIbRJT7ZUDkj1dVKWKg=";
+        };
 
         polkadot-node-on-parity-kusama = polkadot-node-9370;
         polkadot-node-on-parity-polkadot = polkadot-node-9370;
