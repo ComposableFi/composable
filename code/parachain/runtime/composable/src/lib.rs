20b8d73fe24b660dfac1d4ba498b4ef2ffaf97e3
Kostya Kastsevich <kkastsevich@gmail.com>
2023-03-28 18:05:02 +0300

    change native token id to 2

--- code/parachain/runtime/composable/src/lib.rs
+++ code/parachain/runtime/composable/src/lib.rs
@@ -648,7 +648,7 @@ impl crowdloan_rewards::Config for Runtime {
 
 parameter_types! {
 	pub const MaxStrategies: usize = 255;
-	pub NativeAssetId: CurrencyId = CurrencyId::PICA;
+	pub NativeAssetId: CurrencyId = CurrencyId::LAYR;
 	pub CreationDeposit: Balance = 10 * CurrencyId::unit::<Balance>();
 	pub VaultExistentialDeposit: Balance = 1000 * CurrencyId::unit::<Balance>();
 	pub RentPerBlock: Balance = CurrencyId::milli::<Balance>();
