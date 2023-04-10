15eb5029ca32fb3ded71f47d2544285c9695a6b0
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-04-10 20:08:11 +0100

    Revert "env varrs"
    This reverts commit 61e27cb78937f8ef78c3d0f8710796d2d6df22ac.

--- flake/home-configurations.nix
+++ flake/home-configurations.nix
@@ -4,61 +4,38 @@
     homeConfigurations = let user = "vscode";
     in (withSystem "x86_64-linux"
       ({ config, self', inputs', pkgs, devnetTools, this, subnix, ... }: {
-        "${user}" = let
-          codespace = with pkgs; [ cachix acl direnv ];
-          env-vars = subnix.subattrs;
-
+        vscode = let codespace = with pkgs; [ cachix acl direnv ];
         in self.inputs.home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
-          modules = [
-            ({ config, ... }: {
-              home = let sessionVarsStr = config.lib.shell.exportAll env-vars;
-              in {
-                username = user;
-                sessionVariables = env-vars;
-                homeDirectory = "/home/${user}";
-                stateVersion = "22.11";
-                packages = with pkgs;
-                  with self'.packages;
-                  [
-                    clang
-                    nodejs
-                    python3
-                    yarn
-                    sad
-                    git
-                    git-lfs
-                    subwasm
-                    zombienet
+          modules = [{
+            home = {
+              username = user;
+              sessionVariables = subnix.subattrs;
+              homeDirectory = "/home/${user}";
+              stateVersion = "22.11";
+              packages = with pkgs;
+                with self'.packages;
+                [
+                  clang
+                  nodejs
+                  python3
+                  yarn
+                  sad
+                  git
+                  git-lfs
+                  subwasm
+                  zombienet
 
-                  ] ++ (with self'.packages; [ rust-nightly ]) ++ codespace;
+                ] ++ (with self'.packages; [ rust-nightly ]) ++ codespace;
+            };
+            programs = {
+              home-manager.enable = true;
+              direnv = {
+                enable = true;
+                nix-direnv = { enable = true; };
               };
-              programs =
-                let sessionVarsStr = config.lib.shell.exportAll env-vars;
-                in {
-                  home-manager.enable = true;
-                  bash = {
-                    enable = true;
-                    enableCompletion = true;
-                    sessionVariables = env-vars;
-                    bashrcExtra = ''
-                      ${sessionVarsStr}
-                    '';
-                    initExtra = ''
-                      ${sessionVarsStr}
-                    '';
-                    profileExtra = ''
-                      ${sessionVarsStr}
-                    '';
-                  };
-                  direnv = {
-                    enable = true;
-                    nix-direnv = { enable = true; };
-                  };
-                };
-            })
-
-          ];
+            };
+          }];
         };
       })) // { };
   };
