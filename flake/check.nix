7881218ce4217cbf07e997a75da1e61b40eb6db6
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-04-16 16:42:58 +0100

    fixes ci

--- flake/check.nix
+++ flake/check.nix
@@ -9,13 +9,13 @@
           text = ''
             NIXPKGS_ALLOW_UNSUPPORTED_SYSTEM=1
             export NIXPKGS_ALLOW_UNSUPPORTED_SYSTEM 
-            NIX_DEBUG_COMMAND=""        
+            NIX_DEBUG_ARGS=""        
             if [[ ''${ACTIONS_RUNNER_DEBUG-"false"} = "true" ]]; then
-              NIX_DEBUG_COMMAND=' --print-build-logs --debug --show-trace --verbose'
+              NIX_DEBUG_ARGS=' --print-build-logs --debug --show-trace --verbose'
             fi
             set -o pipefail -o errexit
             # shellcheck disable=SC2086
-            NIXPKGS_ALLOW_BROKEN=1 nix flake check --keep-going --no-build --allow-import-from-derivation --no-update-lock-file --accept-flake-config --fallback --print-build-logs $NIX_DEBUG_COMMAND --impure --option sandbox relaxed 2>&1 | tee "nix.check.log"  || true
+            NIXPKGS_ALLOW_BROKEN=1 nix flake check --keep-going --no-build --allow-import-from-derivation --no-update-lock-file --accept-flake-config --fallback $NIX_DEBUG_ARGS --impure --option sandbox relaxed 2>&1 | tee "nix.check.log"  || true
             set +o pipefail +o errexit
             echo "exited with(https://github.com/NixOS/nix/issues/7464) $?" 
             grep --invert-match  "error: path [']/nix/store/[a-zA-Z0-9]\+-[a-zA-Z0-9\.-]\+['] is not valid" < "nix.check.log" \
@@ -33,7 +33,7 @@
           name = "run-in-docker";
           runtimeInputs = [  ];
           text = ''
-          docker run --rm --volume /var/run/docker.sock:/var/run/docker.sock --volume nix:/nix -it nixos/nix bash -c "nix run composable#''${1-} --print-build-logs --extra-experimental-features nix-command --extra-experimental-features flakes --print-build-logs --option sandbox relaxed --show-trace --accept-flake-config" 
+          docker run --rm --volume /var/run/docker.sock:/var/run/docker.sock --volume nix:/nix -it nixos/nix bash -c "nix run composable#''${1-} --print-build-logs --extra-experimental-features nix-command --extra-experimental-features flakes --option sandbox relaxed --show-trace --accept-flake-config" 
           '';
         };
       };
