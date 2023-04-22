fc5e90a731329ca4dc277755052bf3c46629bc76
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-04-22 10:26:28 +0100

    one more attempt

--- code/runtimes.nix
+++ code/runtimes.nix
@@ -43,8 +43,10 @@
             + pkgs.lib.strings.optionalString (features != "")
             (" --features=${features}");
           # From parity/wasm-builder
+          #ASD
           RUSTFLAGS =
             "-C target-cpu=mvp -C target-feature=-sign-ext -C link-arg=--export-table -Clink-arg=--export=__heap_base -C link-arg=--import-memory";
+            #"-C link-arg=--export-table -C link-arg=--export=__heap_base -C link-arg=--import-memory";
         });
 
       # Derive an optimized wasm runtime from a prebuilt one, garbage collection + compression
@@ -57,6 +59,7 @@
           nativeBuildInputs = [ pkgs.binaryen ];
           installPhase = ''
             mkdir --parents $out/lib
+            #ASD
             # https://github.com/paritytech/substrate/blob/30cb4d10b3118d1b3aa5b2ae7fa8429b2c4f28de/utils/wasm-builder/src/wasm_project.rs#L694
             wasm-opt ${runtime}/lib/${name}_runtime.wasm -o $out/lib/runtime.optimized.wasm -Os --strip-dwarf --debuginfo --mvp-features            
             ${self'.packages.subwasm}/bin/subwasm compress $out/lib/runtime.optimized.wasm $out/lib/runtime.optimized.wasm
