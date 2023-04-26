bc99f09a776207116ba19107d00a02fb2dd93ac7
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-04-26 17:30:52 +0100

    hoho

--- terraform/github.com/team.tf
+++ terraform/github.com/team.tf
@@ -3,11 +3,36 @@ data "github_user" "docs" {
   username = "JafarAz"
 }
 
-resource "github_repository_collaborators" "roles" {
-  repository = "composable"
 
-  user {
-    permission = "write"
-    username   = data.github_user.docs.name
-  }
+data "github_user" "ops" {
+  username = "dzmitry-lahoda"
 }
+
+data "github_user" "bot" {
+  username = "g-la-d-os"
+}
+
+data "github_team" "sre" {
+  slug = "@ComposableFi/sre" 
+}
+
+data "github_team" "devs" {
+  slug = "@ComposableFi/developers" 
+}
+
+data "github_team" "product" {
+  slug = "@ComposableFi/product-mgmt" 
+}
+
+# resource "github_repository_collaborators" "roles" {
+#   repository = "composable"
+
+#   user {
+#     permission = "write"
+#     username   = data.github_user.docs.name
+#   }
+#   user {
+#     permission = "admin"
+#     username   = data.github_user.ops.name
+#   }
+# }
