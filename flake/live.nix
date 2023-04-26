e4b216e241671142d8a97b9cb34414dba98b0501
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-04-26 14:19:49 +0100

    warp

--- flake/live.nix
+++ flake/live.nix
@@ -7,7 +7,7 @@
           name = "composable-follow";
           runtimeInputs = [ self'.packages.composable-node ];
           text = ''
-            nixcomposable  -- --chain=composable --listen-addr=/ip4/0.0.0.0/tcp/30334 --prometheus-port 9615 --base-path data/composable-follow --execution=wasm --ws-external --state-pruning=1024 --blocks-pruning=1024 --rpc-external --rpc-cors=all --unsafe-rpc-external --rpc-methods=unsafe --ws-port 9988 --unsafe-ws-external --rpc-port 39988 --in-peers 1000 --out-peers 1000 --ws-max-connections 10000  --sync=fast-unsafe -- --execution=wasm --listen-addr=/ip4/0.0.0.0/tcp/30333 --sync=fast-unsafe  --state-pruning=1024 --blocks-pruning=1024
+            composable  -- --chain=composable --listen-addr=/ip4/0.0.0.0/tcp/30334 --prometheus-port 9615 --base-path data/composable-follow --execution=wasm --ws-external --state-pruning=1024 --blocks-pruning=1024 --rpc-external --rpc-cors=all --unsafe-rpc-external --rpc-methods=unsafe --ws-port 9988 --unsafe-ws-external --rpc-port 39988 --in-peers 1000 --out-peers 1000 --ws-max-connections 10000  --sync=warp -- --execution=wasm --listen-addr=/ip4/0.0.0.0/tcp/30333 --sync=warp  --state-pruning=1024 --blocks-pruning=1024
           '';
         };
       };
