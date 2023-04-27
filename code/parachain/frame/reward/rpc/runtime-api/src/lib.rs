feeea35b00795e6800013c9219409088b10e227f
cr4pt0 <placex.com@gmail.com>
2023-04-27 21:22:28 +0100

    update RewardApi for reward pallet

--- code/parachain/frame/reward/rpc/runtime-api/src/lib.rs
+++ code/parachain/frame/reward/rpc/runtime-api/src/lib.rs
@@ -35,30 +35,17 @@ fn deserialize_from_string<'de, D: Deserializer<'de>, T: std::str::FromStr>(dese
 }
 
 sp_api::decl_runtime_apis! {
-    pub trait RewardApi<AccountId, VaultId, CurrencyId, Balance, BlockNumber, UnsignedFixedPoint> where
+    pub trait RewardApi<AccountId, CurrencyId, Balance, BlockNumber, UnsignedFixedPoint> where
         AccountId: Codec,
-        VaultId: Codec,
         CurrencyId: Codec,
         Balance: Codec,
         BlockNumber: Codec,
         UnsignedFixedPoint: Codec,
     {
-        /// Calculate the number of escrow rewards accrued
-        fn compute_escrow_reward(account_id: AccountId, currency_id: CurrencyId) -> Result<BalanceWrapper<Balance>, DispatchError>;
-
         /// Calculate the number of farming rewards accrued
         fn compute_farming_reward(account_id: AccountId, pool_currency_id: CurrencyId, reward_currency_id: CurrencyId) -> Result<BalanceWrapper<Balance>, DispatchError>;
 
-        /// Calculate the number of vault rewards accrued
-        fn compute_vault_reward(vault_id: VaultId, currency_id: CurrencyId) -> Result<BalanceWrapper<Balance>, DispatchError>;
-
-        /// Estimate staking reward rate for a one year period
-        fn estimate_escrow_reward_rate(account_id: AccountId, amount: Option<Balance>, lock_time: Option<BlockNumber>) -> Result<UnsignedFixedPoint, DispatchError>;
-
         /// Estimate farming rewards for remaining incentives
         fn estimate_farming_reward(account_id: AccountId, pool_currency_id: CurrencyId, reward_currency_id: CurrencyId) -> Result<BalanceWrapper<Balance>, DispatchError>;
-
-        /// Estimate vault reward rate for a one year period
-        fn estimate_vault_reward_rate(vault_id: VaultId) -> Result<UnsignedFixedPoint, DispatchError>;
     }
 }
