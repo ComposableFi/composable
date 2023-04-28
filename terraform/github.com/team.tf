3af241a6858d1938658faa48e42b962e5cb02420
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-04-28 18:00:06 +0100

    made draft release from each permissioned commit and finalized IaC for repository governance (#3533)
    mostly CI changes
    - now full relevant github managed by repo admins in terraform
    - moved back `dev` team to `push` as it was before (was moved to
    maintaines when tried to fix docs publish with Jafar)
    - finalized PR template
    - Dzmitry is maintainer now, he can admin via terraform
    
    - [x] PR title is my best effort to provide summary of changes and has
    clear text to be part of release notes
    - [x] I marked PR by `misc` label if it should not be in release notes
    - [x] I have linked Zenhub/Github issue if one exists
    - [x] I was clear on what type of deployment required to release my
    changes (node, runtime, contract, indexer, on chain operation, frontend,
    infrastructure) if any in PR title or description
    - [x] I waited and did best effort for `Effect gate, automatically
    merged if passed`(effects-job) to finish with success(green check mark)
    with my changes
    - [x] I have added at least one reviewer in reviewers list
    - [x] I tagged(@) or used other form of notification of one person who I
    think can handle best review of this PR

--- terraform/github.com/team.tf
+++ terraform/github.com/team.tf
@@ -25,12 +25,12 @@ resource "github_repository_collaborators" "roles" {
   repository = "composable"
 
   team {
-    permission = "maintain"
+    permission = "push"
     team_id    = data.github_team.devs.slug
   }
 
   user {
-    permission = "admin"
+    permission = "maintain"
     username   = data.github_user.ops.name
   }
 
