f384718d0ce9490fbe5534ea3d0899b5f574a153
cr4pt0 <placex.com@gmail.com>
2023-05-10 10:55:39 +0100

    add amount parameter to farming pallet for deposit extrinsic

--- code/parachain/frame/farming/src/lib.rs
+++ code/parachain/frame/farming/src/lib.rs
@@ -323,10 +323,13 @@ pub mod pallet {
 		#[pallet::call_index(2)]
 		#[pallet::weight(T::WeightInfo::deposit())]
 		#[transactional]
-		pub fn deposit(origin: OriginFor<T>, pool_currency_id: AssetIdOf<T>) -> DispatchResult {
+		pub fn deposit(
+			origin: OriginFor<T>,
+			pool_currency_id: AssetIdOf<T>,
+			amount: BalanceOf<T>,
+		) -> DispatchResult {
 			let who = ensure_signed(origin)?;
 			// reserve lp tokens to prevent spending
-			let amount = T::MultiCurrency::free_balance(pool_currency_id, &who);
 			T::MultiCurrency::reserve(pool_currency_id, &who, amount)?;
 
 			// deposit lp tokens as stake
