74f6994bb9a8af781afcc6831e8caaa23f113b32
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-05-10 21:01:36 +0100

    works

--- terraform/github.com/branches.tf
+++ terraform/github.com/branches.tf
@@ -11,7 +11,7 @@ resource "github_branch_protection" "main" {
     # Require branches to be up to date before merging
     strict   = false
     # Status checks that are required.
-    contexts = ["pr-workflow-check"]
+    contexts = ["pr-workflow-check / draft-release-check"]
   }
 
   # Require a pull request before merging
