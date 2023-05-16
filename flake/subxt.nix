a36dd1d7319a526e752adc1a3490ff4656e6aa71
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-05-16 20:11:27 +0100

    fmt

--- flake/subxt.nix
+++ flake/subxt.nix
@@ -1,57 +1,53 @@
 { ... }: {
   perSystem = { self', pkgs, systemCommonRust, subnix, ... }: {
-    packages =
-      let
-        mkSubxtClient = name: parachain-runtime: relay-runtime:
-          pkgs.stdenv.mkDerivation (subnix.subenv // {
-            inherit name;
-            dontUnpack = true;
-            buildInputs =
-              [ self'.packages.centauri-codegen parachain-runtime relay-runtime ];
+    packages = let
+      mkSubxtClient = name: parachain-runtime: relay-runtime:
+        pkgs.stdenv.mkDerivation (subnix.subenv // {
+          inherit name;
+          dontUnpack = true;
+          buildInputs =
+            [ self'.packages.centauri-codegen parachain-runtime relay-runtime ];
 
-            installPhase = ''
-              mkdir --parents $out
-              ${pkgs.lib.meta.getExe self'.packages.centauri-codegen} \
-                --path $out \
-                --parachain-wasm=${parachain-runtime}/lib/runtime.optimized.wasm \
-                --relaychain-wasm=${relay-runtime}/lib/relay_runtime.compact.compressed.wasm
-            '';
-          });
-      in
-      {
-        subxt-codegen-picasso = pkgs.writeShellApplication {
-          name = "subxt-codegen-picasso";
-          runtimeInputs = [ pkgs.git pkgs.yq ];
-          text = ''
-            subxt codegen --url=wss://rpc.composable.finance:443  > $1
+          installPhase = ''
+            mkdir --parents $out
+            ${pkgs.lib.meta.getExe self'.packages.centauri-codegen} \
+              --path $out \
+              --parachain-wasm=${parachain-runtime}/lib/runtime.optimized.wasm \
+              --relaychain-wasm=${relay-runtime}/lib/relay_runtime.compact.compressed.wasm
           '';
-        };
-
-        subxt-codegen-composable = pkgs.writeShellApplication {
-          name = "subxt-codegen-composable";
-          runtimeInputs = [ pkgs.git pkgs.yq ];
-          text = ''
-            subxt codegen --url=wss://picasso-rpc.composable.finance:443 > $1
-          '';
-        };
-
-        composable-rococo-subxt-client =
-          mkSubxtClient "composable-rococo-subxt-client"
-            self'.packages.composable-runtime
-            self'.packages.rococo-runtime-from-dep;
-
-        composable-polkadot-subxt-client =
-          mkSubxtClient "composable-polkadot-subxt-client"
-            self'.packages.composable-runtime
-            self'.packages.polkadot-runtime-on-parity;
-
-        picasso-rococo-subxt-client = mkSubxtClient "picasso-rococo-subxt-client"
-          self'.packages.picasso-runtime
-          self'.packages.rococo-runtime-from-dep;
-
-        picasso-kusama-subxt-client = mkSubxtClient "picasso-rococo-subxt-client"
-          self'.packages.picasso-runtime
-          self'.packages.kusama-runtime-on-parity;
+        });
+    in {
+      subxt-codegen-picasso = pkgs.writeShellApplication {
+        name = "subxt-codegen-picasso";
+        runtimeInputs = [ pkgs.git pkgs.yq ];
+        text = ''
+          subxt codegen --url=wss://rpc.composable.finance:443  > $1
+        '';
       };
+
+      subxt-codegen-composable = pkgs.writeShellApplication {
+        name = "subxt-codegen-composable";
+        runtimeInputs = [ pkgs.git pkgs.yq ];
+        text = ''
+          subxt codegen --url=wss://picasso-rpc.composable.finance:443 > $1
+        '';
+      };
+
+      composable-rococo-subxt-client =
+        mkSubxtClient "composable-rococo-subxt-client"
+        self'.packages.composable-runtime
+        self'.packages.rococo-runtime-from-dep;
+
+      composable-polkadot-subxt-client =
+        mkSubxtClient "composable-polkadot-subxt-client"
+        self'.packages.composable-runtime
+        self'.packages.polkadot-runtime-on-parity;
+
+      picasso-rococo-subxt-client = mkSubxtClient "picasso-rococo-subxt-client"
+        self'.packages.picasso-runtime self'.packages.rococo-runtime-from-dep;
+
+      picasso-kusama-subxt-client = mkSubxtClient "picasso-rococo-subxt-client"
+        self'.packages.picasso-runtime self'.packages.kusama-runtime-on-parity;
+    };
   };
 }
--- submetrics/flake-module.nix
+++ submetrics/flake-module.nix
@@ -1,32 +1,18 @@
 { self, ... }: {
-  perSystem =
-    { config
-    , self'
-    , inputs'
-    , pkgs
-    , system
-    , crane
-    , systemCommonRust
-    , cargoTools
-    , ...
-    }: {
-      packages =
-        let
-          pkgs-latest = self.inputs.nixpkgs-latest.legacyPackages.${system};
-          config = ./agent-config.yaml;
-          grafana-observe =
-                pkgs.writeShellApplication {
-                name = "grafana-observe";
-                runtimeInputs = [ pkgs-latest.grafana-agent ];
-                text =
-                  "grafana-agent --config.file=${config}";
-              };
-        in
-        {
-          grafana-agent = pkgs-latest.grafana-agent;
-          inherit grafana-observe;
+  perSystem = { config, self', inputs', pkgs, system, crane, systemCommonRust
+    , cargoTools, ... }: {
+      packages = let
+        pkgs-latest = self.inputs.nixpkgs-latest.legacyPackages.${system};
+        config = ./agent-config.yaml;
+        grafana-observe = pkgs.writeShellApplication {
+          name = "grafana-observe";
+          runtimeInputs = [ pkgs-latest.grafana-agent ];
+          text = "grafana-agent --config.file=${config}";
         };
-
+      in {
+        grafana-agent = pkgs-latest.grafana-agent;
+        inherit grafana-observe;
+      };
 
       apps = { };
 
