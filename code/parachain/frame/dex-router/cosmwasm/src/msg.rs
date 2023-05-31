78c3d68b83d1089e62591f116cf7f0d613e9a30e
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-05-31 10:35:21 +0100

    clean

--- code/parachain/frame/dex-router/cosmwasm/src/msg.rs
+++ code/parachain/frame/dex-router/cosmwasm/src/msg.rs
@@ -13,23 +13,3 @@ pub enum ExecuteMsg {
 pub enum QueryMsg {
 	Price { in_asset: Coin, output_denom: String, pool_id: Uint128 },
 }
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
-#[serde(rename_all = "snake_case")]
-pub struct GetRouteResponse {
-	pub pool_route: Vec<SwapAmountInRoute>,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
-#[serde(rename_all = "snake_case")]
-pub struct SwapAmountInRoute {
-	pub pool_id: Vec<Uint64>,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
-#[serde(rename_all = "snake_case")]
-pub struct SwapResponse {
-	pub original_sender: String,
-	pub token_out_denom: String,
-	pub amount: Uint128,
-}
