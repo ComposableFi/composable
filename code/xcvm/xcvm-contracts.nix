e270a6657ab864b2ef44ab5c86cc5c3770a76692
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-06-05 23:06:20 +0100

    nix

--- code/xcvm/xcvm-contracts.nix
+++ code/xcvm/xcvm-contracts.nix
@@ -18,12 +18,12 @@
     in {
       packages = rec {
         xcvm-contract-asset-registry = mkXcvmContract "cw-xc-asset-registry";
-        xcvm-contract-router = mkXcvmContract "cw-xcvm-router";
-        xcvm-contract-interpreter = mkXcvmContract "cw-xcvm-interpreter";
-        xcvm-contract-gateway = mkXcvmContract "cw-xcvm-gateway";
-        xcvm-contract-pingpong = mkXcvmContract "cw-xcvm-pingpong";
+        xcvm-contract-router = mkXcvmContract "cw-xc-router";
+        xcvm-contract-interpreter = mkXcvmContract "cw-xc-interpreter";
+        xcvm-contract-gateway = mkXcvmContract "cw-xc-gateway";
+        xcvm-contract-pingpong = mkXcvmContract "cw-xc-pingpong";
         xcvm-contracts = pkgs.symlinkJoin {
-          name = "xcvm-contracts";
+          name = "xc-contracts";
           paths = [
             xcvm-contract-asset-registry
             xcvm-contract-router
@@ -39,14 +39,14 @@
           // {
             src = systemCommonRust.mkRustSrc ./.;
             cargoArtifacts = xcvm-deps;
-            buildPhase = "cargo test --release -p xcvm-tests";
+            buildPhase = "cargo test --release --package xc-tests";
             installPhase = "mkdir -p $out";
             CW_XCVM_ASSET_REGISTRY =
-              "${xcvm-contracts}/lib/cw_xcvm_asset_registry.wasm";
-            cw_xc_interpreter =
+              "${xcvm-contracts}/lib/cw_xc_asset_registry.wasm";
+            CW_XCVM_INTERPRETER =
               "${xcvm-contracts}/lib/cw_xc_interpreter.wasm";
-            CW_XCVM_ROUTER = "${xcvm-contracts}/lib/cw_xcvm_router.wasm";
-            CW_XCVM_GATEWAY = "${xcvm-contracts}/lib/cw_xcvm_gateway.wasm";
+            CW_XCVM_ROUTER = "${xcvm-contracts}/lib/cw_xc_router.wasm";
+            CW_XCVM_GATEWAY = "${xcvm-contracts}/lib/cw_xc_gateway.wasm";
             CW20 = pkgs.fetchurl {
               url =
                 "https://github.com/CosmWasm/cw-plus/releases/download/v1.0.1/cw20_base.wasm";
