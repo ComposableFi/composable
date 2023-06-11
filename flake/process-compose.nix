804fa0199896c72f90af701b14a2e137d677634f
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-06-11 16:33:51 +0100

    ultimate

---
+++ flake/process-compose.nix
@@ -0,0 +1,25 @@
+{ ... }: {
+  perSystem = { self', pkgs, systemCommonRust, subnix, lib, ... }: {
+    # 1. run hyperspae in process compose
+    # 2. run centaurid in process compose
+    # 3. run zombienet in process compose
+    # 4. run run hyperspace with some configuraiton
+    # 5. build hyperspace wasm
+    # 6. upload hyperspace wasm
+    # 7. run hyperspace with connected configuraiton
+    process-compose.devnet-cosmos = {
+      settings = {
+        processes = {
+          centauri = {
+            command = ''
+              while true; do
+               echo 42
+               sleep 2
+              done
+            '';
+          };
+        };
+      };
+    };
+  };
+}
