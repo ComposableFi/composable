329a8e6963860ef8b53eb461ba86cd1f94cb4403
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-06-15 17:01:45 +0100

    almost

--- flake/process-compose.nix
+++ flake/process-compose.nix
@@ -1,47 +1,35 @@
 { ... }: {
   perSystem = { self', pkgs, systemCommonRust, subnix, lib, ... }: {
-    # 1. run hyperspae in process compose
-    # 2. run centaurid in process compose
-    # 3. run zombienet in process compose
-    # 4. run run hyperspace with some configuraiton
-    # 5. build hyperspace wasm
-    # 6. upload hyperspace wasm
-    # 7. run hyperspace with connected configuraiton
-
-
-
-    process-compose.devnet-cosmos =
-      let
- a = 1;
-      in
-
+    process-compose.devnet-xc =
       {
         settings = {
           processes = {
             sleep = {
-              command =''
-              sleep 100
+              command = ''
+                sleep 20
               '';
             };
-            # centauri = {
-            #   command =self'.packages.centaurid-gen;
-            # };
-            # picasso = {
-            #   command =self'.packages.zombienet-rococo-local-picasso-dev;
-            # };
+            centauri = {
+              command = self'.packages.centaurid-gen;
+            };
+            picasso = {
+              command = self'.packages.zombienet-rococo-local-picasso-dev;
+            };
             hyperspace = {
               command = ''
                 COMPOSABLE_DATA=/tmp/composable-devnet/
                 HYPERSPACE_DATA="$COMPOSABLE_DATA/hyperspace"
+                RUST_LOG="trace,jsonrpsee_client_transport::ws=debug,soketto=debug,tracing::span=debug,mio::poll=debug,trie=debug,jsonrpsee_core::client::async_client=debug"
+                export RUST_LOG
                 mkdir --parents "$COMPOSABLE_DATA"
                 mkdir --parents "$HYPERSPACE_DATA"
                 
                 cp -f ${self'.packages.hyperspace-config-chain-a} $HYPERSPACE_DATA/config-chain-a.toml  
                 cp -f ${self'.packages.hyperspace-config-chain-b} $HYPERSPACE_DATA/config-chain-b.toml  
-                cp -f ${self'.packages.hyperspace-config-chain-2} $HYPERSPACE_DATA/config-chain-c.toml  
+                cp -f ${self'.packages.hyperspace-config-chain-2} $HYPERSPACE_DATA/config-chain-2.toml  
                 cp -f ${self'.packages.hyperspace-config-core} $HYPERSPACE_DATA/config-core.toml                
-                ${self'.packages.hyperspace-composable-rococo-picasso-rococo}/bin/hyperspace create-clients --
-            '';
+                ${self'.packages.hyperspace-composable-rococo-picasso-rococo}/bin/hyperspace create-clients --config-a $HYPERSPACE_DATA/config-chain-a.toml --config-b $HYPERSPACE_DATA/config-chain-2.toml --config-core $HYPERSPACE_DATA/config-core.toml --delay-period 10
+              '';
             };
           };
         };
