6c9a7f437b5d9b2e992ec4fd4744156ff2feb172
cr4pt0 <placex.com@gmail.com>
2023-06-19 10:57:06 -0300

    configure composable runtime with pallet ibc xcm

--- code/parachain/frame/pallet-xcm-ibc/src/lib.rs
+++ code/parachain/frame/pallet-xcm-ibc/src/lib.rs
@@ -1,38 +1,53 @@
+pub use pallet::*;
+pub use pallet::Error;
+
 #[frame_support::pallet]
 pub mod pallet {
 	use super::*;
-	use frame_support::{pallet_prelude::*, BoundedBTreeSet};
+    use frame_support::pallet_prelude::*;
 
 	/// ## Configuration
 	/// The pallet's configuration trait.
 	#[pallet::config]
-	pub trait Config<I: 'static = ()>: frame_system::Config {
-		/// The overarching event type.
-		type RuntimeEvent: From<Event<Self, I>>
-			+ IsType<<Self as frame_system::Config>::RuntimeEvent>;
-	}
+	pub trait Config: frame_system::Config {
+		#[allow(missing_docs)]
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+    }
 
 	// The pallet's events
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(crate) fn deposit_event)]
-	pub enum Event<T: Config<I>, I: 'static = ()> {
+	pub enum Event<T: Config> {
 		
 	}
 
 	#[pallet::error]
-	pub enum Error<T, I = ()> {
-		
+	pub enum Error<T> {
+		Error1,
 	}
 
 	#[pallet::hooks]
-	impl<T: Config<I>, I: 'static> Hooks<T::BlockNumber> for Pallet<T, I> {}
+	impl<T: Config> Hooks<T::BlockNumber> for Pallet<T> {}
 
 
 	#[pallet::pallet]
-	#[pallet::without_storage_info]
-	pub struct Pallet<T, I = ()>(_);
+	#[pallet::generate_store(pub (super) trait Store)]
+	pub struct Pallet<T>(_);
 
 	// The pallet's dispatchable functions.
 	#[pallet::call]
-	impl<T: Config<I>, I: 'static> Pallet<T, I> {}
+	impl<T: Config> Pallet<T> {
+    }
+
+    impl<T> MultiCurrencyCallback for Pallet<T> {
+
+    }
+}
+
+
+use xcm::v3::*;
+pub trait MultiCurrencyCallback{
+	fn deposit_asset(asset: &MultiAsset, location: &MultiLocation, context: &XcmContext, deposit_result : Result){
+		//check result, unwrap memo if exists and execute ibc packet
+	}
 }
