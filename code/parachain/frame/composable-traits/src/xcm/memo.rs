b5cf61d3988cd8a8dd324b1da24a6355d3bf670d
cr4pt0 <placex.com@gmail.com>
2023-07-01 23:19:05 +0100

    add memo to composable trait xcm memo module

--- code/parachain/frame/composable-traits/src/xcm/memo.rs
+++ code/parachain/frame/composable-traits/src/xcm/memo.rs
@@ -36,53 +36,53 @@ pub struct ChainInfo {
     pub is_substrate_ibc: bool,
 }
 
-// #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
-// pub struct MemoForward {
-//     receiver: String,
-//     port: String,
-//     channel: String,
-//     timeout: String,
-//     retries: u64,
-//     // next: Option<Box<MemoForward>>,
-// }
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+pub struct MemoForward {
+    receiver: String,
+    port: String,
+    channel: String,
+    timeout: String,
+    retries: u64,
+    // next: Option<Box<MemoForward>>,
+}
 
-// #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
-// pub struct MemoData {
-//     forward: MemoForward,
-// }
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+pub struct MemoData {
+    forward: MemoForward,
+}
 
-// impl MemoData {
-//     /// Support only addresses from cosmos ecosystem based on bech32.
-//     fn new(
-//         mut vec: Vec<(ChainInfo, Vec<u8>, [u8; 32])>,
-//     ) -> Option<MemoData> {
-//         // vec.reverse();
-//         let mut memo_data: Option<MemoData> = None;
-//         for (i, name, address) in vec {
-//             // let result: core::result::Result<Vec<bech32::u5>, bech32::Error> =
-//             // 	address.into_iter().map(bech32::u5::try_from_u8).collect();
-//             // let data =
-//             // 	result.map_err(|_| Error::<T>::IncorrectAddress { chain_id: i.chain_id as u8 })?;
+impl MemoData {
+    /// Support only addresses from cosmos ecosystem based on bech32.
+    fn new(
+        mut vec: Vec<(ChainInfo, Vec<u8>, [u8; 32])>,
+    ) -> Option<MemoData> {
+        // vec.reverse();
+        let mut memo_data: Option<MemoData> = None;
+        for (i, name, address) in vec {
+            // let result: core::result::Result<Vec<bech32::u5>, bech32::Error> =
+            // 	address.into_iter().map(bech32::u5::try_from_u8).collect();
+            // let data =
+            // 	result.map_err(|_| Error::<T>::IncorrectAddress { chain_id: i.chain_id as u8 })?;
 
-//             // let name = String::from_utf8(name.into())
-//             // 	.map_err(|_| Error::<T>::IncorrectChainName { chain_id: i.chain_id as u8 })?;
-//             // let result_address = bech32::encode(&name, data.clone()).map_err(|_| {
-//             // 	Error::<T>::FailedToEncodeBech32Address { chain_id: i.chain_id as u8 }
-//             // })?;
+            // let name = String::from_utf8(name.into())
+            // 	.map_err(|_| Error::<T>::IncorrectChainName { chain_id: i.chain_id as u8 })?;
+            // let result_address = bech32::encode(&name, data.clone()).map_err(|_| {
+            // 	Error::<T>::FailedToEncodeBech32Address { chain_id: i.chain_id as u8 }
+            // })?;
 
-//             // let new_memo = MemoData {
-//             // 	forward: MemoForward {
-//             // 		receiver: result_address,
-//             // 		port: String::from("transfer"),
-//             // 		channel: String::from(format!("channel-{}", i.channel_id)),
-//             // 		timeout: String::from(i.timeout.unwrap_or_default().to_string()),
-//             // 		retries: i.retries.unwrap_or_default(),
-//             // 		// next: memo_data.map(|x| Box::new(x.forward)), // memo_data is boxed here
-//             // 	},
-//             // };
-//             // memo_data = Some(new_memo);
-//         }
-//         // Ok(memo_data)
-//         memo_data
-//     }
-// }
+            // let new_memo = MemoData {
+            // 	forward: MemoForward {
+            // 		receiver: result_address,
+            // 		port: String::from("transfer"),
+            // 		channel: String::from(format!("channel-{}", i.channel_id)),
+            // 		timeout: String::from(i.timeout.unwrap_or_default().to_string()),
+            // 		retries: i.retries.unwrap_or_default(),
+            // 		// next: memo_data.map(|x| Box::new(x.forward)), // memo_data is boxed here
+            // 	},
+            // };
+            // memo_data = Some(new_memo);
+        }
+        // Ok(memo_data)
+        memo_data
+    }
+}
