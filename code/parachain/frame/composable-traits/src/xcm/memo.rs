748083ace39ea3471867203e5a8136e8b3622351
cr4pt0 <placex.com@gmail.com>
2023-07-02 16:48:00 +0100

    use #[serde(skip_serializing_if = "Option::is_none")]

--- code/parachain/frame/composable-traits/src/xcm/memo.rs
+++ code/parachain/frame/composable-traits/src/xcm/memo.rs
@@ -36,6 +36,7 @@ pub struct MemoForward {
     pub channel: String,
     pub timeout: String,
     pub retries: u64,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub next: Option<Box<MemoForward>>,
 }
 
@@ -43,51 +44,3 @@ pub struct MemoForward {
 pub struct MemoData {
     pub forward: MemoForward,
 }
-
-// impl MemoData {
-//     /// Support only addresses from cosmos ecosystem based on bech32.
-//     pub fn new(
-//         mut vec: Vec<(ChainInfo, Vec<u8>, [u8; 32])>,
-//     ) -> Result<Option<MemoData>, DispatchError> {
-//         vec.reverse();
-//         let mut memo_data: Option<MemoData> = None;
-//         for (i, name, address) in vec {
-            
-//             // let mut v = sp_std::vec::Vec::new();
-//             // bech32
-//             // use bech32_no_std::{FromBase32, ToBase32};
-//             //iterate over address and use bech32::u5::try_from_u8 to convert to bech32::u5 and save into vec
-//             // for item in address{
-//             //     let x = bech32_no_std::u5::try_from_u8(item).unwrap();
-//             //     v.push(item);
-//             // }
-//             let result: core::result::Result<Vec<bech32_no_std::u5>, bech32_no_std::Error> =
-//             	address.into_iter().map(bech32_no_std::u5::try_from_u8).collect();
-//             let data =
-//             	// result.map_err(|_| Error::<T>::IncorrectAddress { chain_id: i.chain_id as u8 })?;
-//             	result.map_err(|_| DispatchError::Other("()"))?;
-
-//             let name = String::from_utf8(name.into())
-//             	// .map_err(|_| Error::<T>::IncorrectChainName { chain_id: i.chain_id as u8 })?;
-//                 .map_err(|_| DispatchError::Other("()"))?;
-//             let result_address = bech32_no_std::encode(&name, data.clone()).map_err(|_| {
-//             	// Error::<T>::FailedToEncodeBech32Address { chain_id: i.chain_id as u8 }
-//                 DispatchError::Other("()")
-//             })?;
-
-//             let new_memo = MemoData {
-//             	forward: MemoForward {
-//             		receiver: result_address,
-//             		port: String::from("transfer"),
-//             		channel: String::from(scale_info::prelude::format!("channel-{}", i.channel_id)),
-//             		timeout: String::from(i.timeout.unwrap_or_default().to_string()),
-//             		retries: i.retries.unwrap_or_default(),
-//             		// next: memo_data.map(|x| Box::new(x.forward)), // memo_data is boxed here
-//             	},
-//             };
-//             memo_data = Some(new_memo);
-//         }
-//         // Ok(memo_data)
-//         Ok(memo_data)
-//     }
-// }
