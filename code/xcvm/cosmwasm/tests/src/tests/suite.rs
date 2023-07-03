164156a3033d203e09666688391b965decd9dec1
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-07-03 16:52:30 +0100

    Update suite.rs
    Signed-off-by: dzmitry-lahoda <dzmitry@lahoda.pro>

--- code/xcvm/cosmwasm/tests/src/tests/suite.rs
+++ code/xcvm/cosmwasm/tests/src/tests/suite.rs
@@ -327,7 +327,7 @@ fn load_contracts() -> XCVMContracts {
 
 	let code_interpreter = read_contract("cw_xc_interpreter.wasm");
 	let code_gateway = read_contract("cw_xc_gateway.wasm");
-	let cw20_base_path =
+	let out_dir =
 		[option_env!("NIX_CARGO_OUT_DIR").unwrap_or(env!("OUT_DIR")), "/cw20_base.wasm"].concat();
 	// When running the test locally, the cw20_base.wasm file is downloaded by
 	// the Build script (see build.rs).
