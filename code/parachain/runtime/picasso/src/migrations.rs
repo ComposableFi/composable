7ebce4fa3da744f785aa13a866a488784c455744
Kanstantsin Kastsevich <kkastsevich@gmail.com>
2023-07-05 02:24:56 +0300

    migrate picasso asset ids

--- code/parachain/runtime/picasso/src/migrations.rs
+++ code/parachain/runtime/picasso/src/migrations.rs
@@ -1,9 +1,11 @@
 use crate::{prelude::*, *};
 use frame_support::traits::{GetStorageVersion, StorageVersion};
+use migrate_asset_ids::MigratePicassoAssetIds;
 
 pub type Migrations = (
 	SchedulerMigrationV1toV4,
 	TechCollectiveRenameMigration,
+	MigratePicassoAssetIds,
 	preimage::migration::v1::Migration<Runtime>,
 	scheduler::migration::v3::MigrateToV4<Runtime>,
 	democracy::migrations::v1::Migration<Runtime>,
@@ -51,6 +53,385 @@ impl OnRuntimeUpgrade for TechCollectiveRenameMigration {
 	fn on_runtime_upgrade() -> Weight {
 		move_runtime_pallet::<"TechnicalCollective", 1, TechnicalCommittee>() +
 			move_runtime_pallet::<"TechnicalMembership", 1, TechnicalCommitteeMembership>()
+	}
+}
+
+pub mod migrate_asset_ids {
+	use super::*;
+
+	use frame_support::traits::{GetStorageVersion, StorageVersion};
+
+	use common::AccountId;
+	use pablo::PoolConfiguration;
+	use sp_std::{collections::btree_set::BTreeSet, vec::Vec};
+
+	pub struct MigratePicassoAssetIds;
+
+	fn get_new_asset_id(old_asset_id: CurrencyId) -> CurrencyId {
+		let zero_array: [u8; 8] = 0_u64.to_be_bytes();
+		let nonce = (u128::from_be_bytes(
+			<Runtime as pablo::Config>::PalletId::get()
+				.0
+				.into_iter()
+				.chain(zero_array)
+				.collect::<Vec<u8>>()
+				.try_into()
+				.expect("[u8; 8] + bytes(u64) = [u8; 16]"),
+		) ^ old_asset_id.0) as u64;
+		let pablo_index = (Pablo::index() as u32).to_be_bytes();
+		CurrencyId(u128::from_be_bytes(
+			[0, 0, 0, 0]
+				.into_iter()
+				.chain(pablo_index)
+				.chain(nonce.to_be_bytes())
+				.collect::<Vec<u8>>()
+				.try_into()
+				.expect("[u8; 8] + bytes(u64) = [u8; 16]"),
+		))
+	}
+
+	const ASSETS_REGISTRY_V2: StorageVersion = StorageVersion::new(2);
+	// ids to change
+	const DOT_PICA_LPT: CurrencyId = CurrencyId(149379386384882397174193330044887105538);
+	const DOT_USDT_LPT: CurrencyId = CurrencyId(149379386384882397174193330044887105539);
+	const DOT_KSM_LPT: CurrencyId = CurrencyId(149379386384882397174193330044887105540);
+	const DOT_OSMO_LPT: CurrencyId = CurrencyId(149379386384882397174193330044887105541);
+	const KSM_OSMO_LPT: CurrencyId = CurrencyId(149379386384882397174193330044887105542);
+	const USDT_OSMO_LPT: CurrencyId = CurrencyId(149379386384882397174193330044887105543);
+
+	/// migrate orml_tokens storage
+	fn migrate_orml_tokens(old_asset_ids: Vec<CurrencyId>) -> Weight {
+		// orml_tokens TokenIssuance
+		for old_asset_id in old_asset_ids.clone() {
+			let new_asset_id = get_new_asset_id(old_asset_id);
+			orml_tokens::module::TotalIssuance::<Runtime>::swap(old_asset_id, new_asset_id);
+			orml_tokens::module::TotalIssuance::<Runtime>::remove(old_asset_id);
+		}
+
+		// orml_tokens Accounts storage
+		let mut account_ids: BTreeSet<AccountId> = BTreeSet::new();
+		for (account_id, _) in orml_tokens::module::Accounts::<Runtime>::iter_keys() {
+			account_ids.insert(account_id);
+		}
+		for account_id in account_ids {
+			for old_asset_id in old_asset_ids.clone() {
+				let new_asset_id = get_new_asset_id(old_asset_id);
+				orml_tokens::module::Accounts::<Runtime>::swap(
+					account_id.clone(),
+					old_asset_id,
+					account_id.clone(),
+					new_asset_id,
+				);
+				orml_tokens::module::Accounts::<Runtime>::remove(account_id.clone(), old_asset_id);
+			}
+		}
+
+		// orml_tokens Locks storage
+		let mut account_ids: BTreeSet<AccountId> = BTreeSet::new();
+		for (account_id, _) in orml_tokens::module::Locks::<Runtime>::iter_keys() {
+			account_ids.insert(account_id);
+		}
+		for account_id in account_ids {
+			for old_asset_id in old_asset_ids.clone() {
+				let new_asset_id = get_new_asset_id(old_asset_id);
+				orml_tokens::module::Locks::<Runtime>::swap(
+					account_id.clone(),
+					old_asset_id,
+					account_id.clone(),
+					new_asset_id,
+				);
+				orml_tokens::module::Locks::<Runtime>::remove(account_id.clone(), old_asset_id);
+			}
+		}
+
+		// orml_tokens Reserves storage
+		let mut account_ids: BTreeSet<AccountId> = BTreeSet::new();
+		for (account_id, _) in orml_tokens::module::Reserves::<Runtime>::iter_keys() {
+			account_ids.insert(account_id);
+		}
+		for account_id in account_ids {
+			for old_asset_id in old_asset_ids.clone() {
+				let new_asset_id = get_new_asset_id(old_asset_id);
+				orml_tokens::module::Reserves::<Runtime>::swap(
+					account_id.clone(),
+					old_asset_id,
+					account_id.clone(),
+					new_asset_id,
+				);
+				orml_tokens::module::Reserves::<Runtime>::remove(account_id.clone(), old_asset_id);
+			}
+		}
+		Weight::from_ref_time(100_000)
+	}
+
+	/// change some of the pools' lp token ids
+	fn migrate_pablo_pools(old_asset_ids: Vec<CurrencyId>) -> Weight {
+		pablo::pallet::Pools::<Runtime>::translate_values(|pool| match pool {
+			PoolConfiguration::DualAssetConstantProduct(mut config) => {
+				if old_asset_ids.contains(&config.lp_token) {
+					config.lp_token = get_new_asset_id(config.lp_token);
+				}
+				Some(PoolConfiguration::DualAssetConstantProduct(config))
+			},
+		});
+		Weight::from_ref_time(100_000)
+	}
+
+	/// change staking storage for staked lp tokens
+	fn migrate_staking(old_asset_ids: Vec<CurrencyId>) -> Weight {
+		// farming pallet RewardSchedules
+		let mut asset_ids: BTreeSet<(CurrencyId, CurrencyId)> = BTreeSet::new();
+		for (asset_id, reward_id) in farming::pallet::RewardSchedules::<Runtime>::iter_keys() {
+			asset_ids.insert((asset_id, reward_id));
+		}
+		for (old_asset_id, reward_id) in asset_ids {
+			if old_asset_ids.clone().contains(&old_asset_id) {
+				let new_asset_id = get_new_asset_id(old_asset_id);
+				farming::pallet::RewardSchedules::<Runtime>::swap(
+					old_asset_id.clone(),
+					reward_id.clone(),
+					new_asset_id.clone(),
+					reward_id.clone(),
+				);
+				farming::pallet::RewardSchedules::<Runtime>::remove(
+					old_asset_id.clone(),
+					reward_id.clone(),
+				);
+			}
+		}
+
+		// reward pallet RewardCurrencies
+		let mut asset_ids: BTreeSet<CurrencyId> = BTreeSet::new();
+		for asset_id in
+			reward::pallet::RewardCurrencies::<Runtime, FarmingRewardsInstance>::iter_keys()
+		{
+			asset_ids.insert(asset_id);
+		}
+		for old_asset_id in asset_ids {
+			if old_asset_ids.clone().contains(&old_asset_id) {
+				let new_asset_id = get_new_asset_id(old_asset_id);
+				reward::pallet::RewardCurrencies::<Runtime, FarmingRewardsInstance>::swap(
+					old_asset_id.clone(),
+					new_asset_id.clone(),
+				);
+				reward::pallet::RewardCurrencies::<Runtime, FarmingRewardsInstance>::remove(
+					old_asset_id.clone(),
+				);
+			}
+		}
+
+		// reward pallet TotalStake
+		let mut asset_ids: BTreeSet<CurrencyId> = BTreeSet::new();
+		for asset_id in reward::pallet::TotalStake::<Runtime, FarmingRewardsInstance>::iter_keys() {
+			asset_ids.insert(asset_id);
+		}
+		for old_asset_id in asset_ids {
+			if old_asset_ids.clone().contains(&old_asset_id) {
+				let new_asset_id = get_new_asset_id(old_asset_id);
+				reward::pallet::TotalStake::<Runtime, FarmingRewardsInstance>::swap(
+					old_asset_id.clone(),
+					new_asset_id.clone(),
+				);
+				reward::pallet::TotalStake::<Runtime, FarmingRewardsInstance>::remove(
+					old_asset_id.clone(),
+				);
+			}
+		}
+
+		// reward pallet Stake
+		let mut asset_ids: BTreeSet<(CurrencyId, AccountId)> = BTreeSet::new();
+		for (asset_id, user_id) in
+			reward::pallet::Stake::<Runtime, FarmingRewardsInstance>::iter_keys()
+		{
+			asset_ids.insert((asset_id, user_id));
+		}
+		for (old_asset_id, user_id) in asset_ids {
+			if old_asset_ids.clone().contains(&old_asset_id) {
+				let new_asset_id = get_new_asset_id(old_asset_id);
+				reward::pallet::Stake::<Runtime, FarmingRewardsInstance>::swap(
+					(old_asset_id.clone(), user_id.clone()),
+					(new_asset_id.clone(), user_id.clone()),
+				);
+				reward::pallet::Stake::<Runtime, FarmingRewardsInstance>::remove((
+					old_asset_id.clone(),
+					user_id.clone(),
+				));
+			}
+		}
+
+		// reward pallet RewardPerToken
+		let mut keys: BTreeSet<(CurrencyId, CurrencyId)> = BTreeSet::new();
+		for (reward_id, asset_id) in
+			reward::pallet::RewardPerToken::<Runtime, FarmingRewardsInstance>::iter_keys()
+		{
+			keys.insert((reward_id, asset_id));
+		}
+		for (reward_id, old_asset_id) in keys {
+			if old_asset_ids.clone().contains(&old_asset_id) {
+				let new_asset_id: CurrencyId = get_new_asset_id(old_asset_id);
+				reward::pallet::RewardPerToken::<Runtime, FarmingRewardsInstance>::swap(
+					reward_id.clone(),
+					old_asset_id.clone(),
+					reward_id.clone(),
+					new_asset_id.clone(),
+				);
+				reward::pallet::RewardPerToken::<Runtime, FarmingRewardsInstance>::remove(
+					reward_id.clone(),
+					old_asset_id.clone(),
+				);
+			}
+		}
+
+		// reward pallet RewardTally
+		let mut keys: BTreeSet<(CurrencyId, (CurrencyId, AccountId))> = BTreeSet::new();
+		for (reward_id, (asset_id, account_id)) in
+			reward::pallet::RewardTally::<Runtime, FarmingRewardsInstance>::iter_keys()
+		{
+			keys.insert((reward_id, (asset_id, account_id)));
+		}
+		for (reward_id, (old_asset_id, account_id)) in keys {
+			if old_asset_ids.clone().contains(&old_asset_id) {
+				let new_asset_id: CurrencyId = get_new_asset_id(old_asset_id);
+				reward::pallet::RewardTally::<Runtime, FarmingRewardsInstance>::swap(
+					reward_id.clone(),
+					(old_asset_id.clone(), account_id.clone()),
+					reward_id.clone(),
+					(new_asset_id.clone(), account_id.clone()),
+				);
+				reward::pallet::RewardTally::<Runtime, FarmingRewardsInstance>::remove(
+					reward_id.clone(),
+					(old_asset_id.clone(), account_id.clone()),
+				);
+			}
+		}
+
+		Weight::from_ref_time(100_000)
+	}
+
+	/// change some of the pools' lp token ids
+	fn migrate_assets_registry(old_asset_ids: Vec<CurrencyId>) -> Weight {
+		for old_asset_id in old_asset_ids.clone() {
+			let new_asset_id = get_new_asset_id(old_asset_id);
+			assets_registry::pallet::AssetRatio::<Runtime>::swap(old_asset_id, new_asset_id);
+			assets_registry::pallet::ExistentialDeposit::<Runtime>::swap(
+				old_asset_id,
+				new_asset_id,
+			);
+			assets_registry::pallet::AssetName::<Runtime>::swap(old_asset_id, new_asset_id);
+			assets_registry::pallet::AssetName::<Runtime>::remove(old_asset_id);
+			assets_registry::pallet::AssetSymbol::<Runtime>::swap(old_asset_id, new_asset_id);
+			assets_registry::pallet::AssetSymbol::<Runtime>::remove(old_asset_id);
+			assets_registry::pallet::AssetDecimals::<Runtime>::swap(old_asset_id, new_asset_id);
+			assets_registry::pallet::AssetDecimals::<Runtime>::remove(old_asset_id);
+		}
+		Weight::from_ref_time(100_000)
+	}
+
+	fn migrate_asset_ids(old_asset_ids: Vec<CurrencyId>) -> Weight {
+		let mut total_weight = Weight::from_ref_time(0);
+		total_weight += migrate_orml_tokens(old_asset_ids.clone());
+		total_weight += migrate_pablo_pools(old_asset_ids.clone());
+		total_weight += migrate_staking(old_asset_ids.clone());
+		total_weight += migrate_assets_registry(old_asset_ids.clone());
+		total_weight
+	}
+
+	impl OnRuntimeUpgrade for MigratePicassoAssetIds {
+		fn on_runtime_upgrade() -> Weight {
+			let on_chain_version =
+				<AssetsRegistry as GetStorageVersion>::on_chain_storage_version();
+			if on_chain_version < ASSETS_REGISTRY_V2 {
+				let asset_ids = vec![
+					DOT_PICA_LPT,
+					DOT_USDT_LPT,
+					DOT_KSM_LPT,
+					DOT_OSMO_LPT,
+					KSM_OSMO_LPT,
+					USDT_OSMO_LPT,
+				];
+
+				StorageVersion::new(2).put::<AssetsRegistry>();
+				migrate_asset_ids(asset_ids)
+			} else {
+				<Runtime as system::Config>::DbWeight::get().reads(1)
+			}
+		}
+	}
+
+	#[cfg(test)]
+	mod tests {
+		use frame_support::sp_io;
+
+		use super::*;
+
+		pub fn new_test_ext() -> sp_io::TestExternalities {
+			let storage = frame_system::GenesisConfig::default()
+				.build_storage::<Runtime>()
+				.expect("in memory test");
+			let mut externalities = sp_io::TestExternalities::new(storage);
+			externalities.execute_with(|| System::set_block_number(1));
+			externalities
+		}
+
+		mod migrate_asset {
+
+			use super::*;
+
+			#[test]
+			fn check_ids_correct() {
+				assert_eq!(
+					get_new_asset_id(DOT_PICA_LPT),
+					CurrencyId(u128::from_be_bytes([
+						0, 0, 0, 0, 0, 0, 0, 59, 0, 0, 0, 0, 0, 0, 0, 2
+					]))
+				);
+				assert_eq!(
+					get_new_asset_id(DOT_USDT_LPT),
+					CurrencyId(u128::from_be_bytes([
+						0, 0, 0, 0, 0, 0, 0, 59, 0, 0, 0, 0, 0, 0, 0, 3
+					]))
+				);
+				assert_eq!(
+					get_new_asset_id(DOT_KSM_LPT),
+					CurrencyId(u128::from_be_bytes([
+						0, 0, 0, 0, 0, 0, 0, 59, 0, 0, 0, 0, 0, 0, 0, 4
+					]))
+				);
+				assert_eq!(
+					get_new_asset_id(DOT_OSMO_LPT),
+					CurrencyId(u128::from_be_bytes([
+						0, 0, 0, 0, 0, 0, 0, 59, 0, 0, 0, 0, 0, 0, 0, 5
+					]))
+				);
+				assert_eq!(
+					get_new_asset_id(KSM_OSMO_LPT),
+					CurrencyId(u128::from_be_bytes([
+						0, 0, 0, 0, 0, 0, 0, 59, 0, 0, 0, 0, 0, 0, 0, 6
+					]))
+				);
+				assert_eq!(
+					get_new_asset_id(USDT_OSMO_LPT),
+					CurrencyId(u128::from_be_bytes([
+						0, 0, 0, 0, 0, 0, 0, 59, 0, 0, 0, 0, 0, 0, 0, 7
+					]))
+				);
+			}
+
+			#[test]
+			fn run_scenarios() {
+				new_test_ext().execute_with(|| {
+					// todo
+					// create & mint asset 1
+					// create & mint asset 2
+					// create pool
+					// add liquidity
+					// stake some of lp tokens
+					// wait for rewards
+					// execute migration functions and check that old values have new keys and old
+					// keys are removed
+				})
+			}
+		}
 	}
 }
 
