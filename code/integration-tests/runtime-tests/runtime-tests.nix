31ae1271b044d86e0d883a8baa8f53d1825303d0
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-07-10 21:42:31 +0100

    Adding IBCed Composable to local DevNet (nix run .#devnet-xc-run-fresh) (#3775)
    ![image](https://github.com/ComposableFi/composable/assets/757125/2998b89e-302f-40d7-9259-0be335efe829)
    
    
    - made 2 parity relays not conflict with each other
    - removed old devnet
    - while waiting for hyperspaces inits, was writing and fixing some docs
    
    next items may be:
    - run sudo on centauri to make pica ibc denom to be just pica
    - more assets into genesis
    - speed up things (start from old genesis? how to run hyperspace in
    parallel? less nodes/logs usage? )
    - bake single docker from devnet-xc
    
    - [x] PR title is my best effort to provide summary of changes and has
    clear text to be part of release notes
    - [x] I marked PR by `misc` label if it should not be in release notes
    - [x] I have linked Zenhub/Github or any other reference item if one
    exists
    - [x] I was clear on what type of deployment required to release my
    changes (node, runtime, contract, indexer, on chain operation, frontend,
    infrastructure) if any in PR title or description
    - [ ] I waited and did best effort for `pr-workflow-check /
    draft-release-check` to finish with success(green check mark) with my
    changes
    - [x] I have added at least one reviewer in reviewers list
    - [x] I tagged(@) or used other form of notification of one person who I
    think can handle best review of this PR
    - [x] I have proved that PR has no general regressions of relevant
    features and processes required to release into production

--- code/integration-tests/runtime-tests/runtime-tests.nix
+++ code/integration-tests/runtime-tests/runtime-tests.nix
@@ -28,41 +28,43 @@
         '';
       };
 
-      devnet-integration-tests =
-        pkgs.writeShellScriptBin "devnet-integration-tests" ''
+      devnet-integration-tests = pkgs.writeShellApplication {
+        runtimeInputs = with pkgs; [
+          curl
+          dasel
+          nodejs
+          coreutils
+          process-compose
+        ];
+        name = "devnet-integration-tests";
+        text = ''
+          # shellcheck disable=SC2069
           ( ${
             pkgs.lib.meta.getExe self'.packages.default
-          } 2>&1 & ) | tee devnet-picasso.log &
-          wait_for_log () {
-           until test -f "$1"; do
-              sleep 1 
-              echo "$2"
-            done
+          } 2>&1 & ) | tee devnet-xc.log &
+
+          process-compose-stop() {
+            for i in $(process-compose process list); do process-compose process stop "$i"; done
           }
 
-          wait_for_log "devnet-picasso.log" "waiting network start"
-          TIMEOUT=300
-          COMMAND="( tail --follow --lines=0  devnet-picasso.log & ) | grep --max-count=1 \"Network launched 🚀🚀\""
-          set +o errexit
-          timeout $TIMEOUT bash -c "$COMMAND"
-          START_RESULT="$?"
-          set -o errexit
-          if [[ $START_RESULT -ne 0 ]] ; then 
-            printf "failed to start devnet within %s with exit code %s" "$TIMEOUT" "$START_RESULT"
-            exit $START_RESULT
-          fi
-
-          cd code/integration-tests/runtime-tests || exit
-          npm install -q
-          export ENDPOINT=127.0.0.1:9988 ENDPOINT_RELAYCHAIN=127.0.0.1:9944 && npm run test_basic 2>&1>runtime-tests.log &
-          RUNTIME_TESTS_PID=$!
-          wait_for_log "runtime-tests.log" "waiting tests start"
-          tail --follow runtime-tests.log &
-          ( tail --follow --lines=0 runtime-tests.log & ) | ( grep --max-count=5 "API-WS: disconnected from" >stop.log & )
-          ( while : ; do if test $( wc --lines stop.log | cut --delimiter " " --fields 1 ) -gt 4; then kill -s SIGKILL $RUNTIME_TESTS_PID && echo "Failed" && exit 42; fi; sleep 1; done ) &
-          wait $RUNTIME_TESTS_PID
-          exit $?
+          TRIES=0
+          START_RESULT=1
+          while test $TRIES -le 64; do
+            set +o errexit
+            curl --header "Content-Type: application/json" --data '{"id":1, "jsonrpc":"2.0", "method" : "assets_listAssets"}' http://127.0.0.1:32201
+            START_RESULT=$?
+            set -o errexit
+            if test $START_RESULT -eq 0; then
+              process-compose-stop
+              break
+            fi
+            ((TRIES=TRIES+1))
+            sleep 4
+          done
+          process-compose-stop
+          exit $START_RESULT              
         '';
+      };
     };
     apps = {
       devnet-integration-tests = self.inputs.flake-utils.lib.mkApp {
