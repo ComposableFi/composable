05a0e32fa9b8123a42ba5eea6a234e0af68b8718
rust.dev <102041955+RustNinja@users.noreply.github.com>
2023-07-21 13:44:46 +0100

    Pallet Multihop XCM-IBC to move DOT from Polkadot to Cosmos in one transaction. (#3766)
    - [x] PR title is my best effort to provide summary of changes and has
    clear text to be part of release notes
    - [x] I marked PR by `misc` label if it should not be in release notes
    - [x] I have linked Zenhub/Github or any other reference item if one
    exists
    - [x] I was clear on what type of deployment required to release my
    changes (node, runtime, contract, indexer, on chain operation, frontend,
    infrastructure) if any in PR title or description
    - [x] I waited and did best effort for `pr-workflow-check /
    draft-release-check` to finish with success(green check mark) with my
    changes
    - [x] I have added at least one reviewer in reviewers list
    - [x] I tagged(@) or used other form of notification of one person who I
    think can handle best review of this PR
    - [x] I have proved that PR has no general regressions of relevant
    features and processes required to release into production

--- code/parachain/frame/composable-traits/src/xcm/memo.rs
+++ code/parachain/frame/composable-traits/src/xcm/memo.rs
@@ -16,10 +16,13 @@ use crate::prelude::*;
 )]
 pub struct ChainInfo {
 	pub chain_id: u128,
+	pub order: u8,
 	pub channel_id: u64,        //for packet or memo
 	pub timestamp: Option<u64>, //for packet
 	pub height: Option<u64>,    //for memo packet message forwarding
 	pub retries: Option<u64>,   //for memo packet message forwarding
 	pub timeout: Option<u64>,   //for memo packet message forwarding
 	pub is_substrate_ibc: bool,
+	pub is_substrate_xcm: bool,
+	pub para_id: Option<u32>,
 }
