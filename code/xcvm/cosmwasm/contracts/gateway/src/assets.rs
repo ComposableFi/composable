7097cdff6ebd43ad69dacd4698b7dd7f49ec0f3f
Michal Nazarewicz <mina86@mina86.com>
2023-08-09 20:02:37 +0200

    wip

--- code/xcvm/cosmwasm/contracts/gateway/src/assets.rs
+++ code/xcvm/cosmwasm/contracts/gateway/src/assets.rs
@@ -10,8 +10,11 @@ use xc_core::AssetId;
 
 /// Adds a new asset to the registry; errors out if asset already exists.
 pub(crate) fn force_asset(_: auth::Admin, deps: DepsMut, msg: AssetItem) -> Result {
+	let config = crate::state::load(deps.storage)?;
 	ASSETS.save(deps.storage, msg.asset_id, &msg)?;
-	LOCAL_ASSETS.save(deps.storage, msg.local.clone(), &msg)?;
+	if msg.from_network_id == config.here_id {
+		LOCAL_ASSETS.save(deps.storage, msg.local.clone(), &msg)?;
+	}
 	Ok(Response::new().add_event(
 		make_event("assets.forced")
 			.add_attribute("asset_id", msg.asset_id.to_string())
@@ -21,7 +24,7 @@ pub(crate) fn force_asset(_: auth::Admin, deps: DepsMut, msg: AssetItem) -> Resu
 
 /// Fetches information about given asset.
 pub(crate) fn get_asset_by_id(deps: Deps, asset_id: AssetId) -> Result<AssetItem> {
-	ASSETS.may_load(deps.storage, asset_id)?.ok_or(ContractError::UnsupportedAsset)
+	ASSETS.may_load(deps.storage, asset_id)?.ok_or(ContractError::AssetNotFound)
 }
 
 /// Fetches information about given asset by its local reference.
@@ -31,7 +34,7 @@ pub(crate) fn get_local_asset_by_reference(
 ) -> Result<AssetItem> {
 	LOCAL_ASSETS
 		.may_load(deps.storage, reference)?
-		.ok_or(ContractError::UnsupportedAsset)
+		.ok_or(ContractError::AssetNotFound)
 }
 
 /// Removes an existing asset from the registry; errors out if asset doesn’t
@@ -41,12 +44,11 @@ pub(crate) fn force_remove_asset(
 	deps: DepsMut<'_>,
 	asset_id: AssetId,
 ) -> std::result::Result<Response, ContractError> {
-	let mut resp = Response::new();
-	if let Some(asset) = ASSETS.may_load(deps.storage, asset_id)? {
-		ASSETS.remove(deps.storage, asset_id);
+	let config = crate::state::load(deps.storage)?;
+	let asset = ASSETS.load(deps.storage, asset_id)?;
+	ASSETS.remove(deps.storage, asset_id);
+	if asset.from_network_id == config.here_id {
 		LOCAL_ASSETS.remove(deps.storage, asset.local);
-		resp = resp
-			.add_event(make_event("assets.removed").add_attribute("asset_id", asset_id.to_string()))
 	}
-	Ok(resp)
+	Ok(Response::new().add_event(make_event("assets.removed").add_attribute("asset_id", asset_id.to_string())))
 }
