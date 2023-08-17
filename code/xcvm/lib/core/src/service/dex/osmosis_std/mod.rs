b7ee05a360b90b12bfdbf1024fe30b9f4a83851f
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-08-17 23:22:09 +0100

    osmosis swap

---
+++ code/xcvm/lib/core/src/service/dex/osmosis_std/mod.rs
@@ -0,0 +1,2 @@
+// basically copy https://github.com/osmosis-labs/osmosis-rust/tree/main/packages/osmosis-std
+// until they handle all deps to be no_std
---
+++ code/xcvm/lib/core/src/service/dex/osmosis_std/types/osmosis/poolmanager/v1beta1.rs
@@ -0,0 +1,25 @@
+use osmosis_std::types::osmosis::gamm::v1beta1::{MsgSwapExactAmountInResponse, SwapAmountInRoute};
+
+
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    Eq,
+    ::prost::Message,
+    ::serde::Serialize,
+    ::serde::Deserialize,
+    ::schemars::JsonSchema,
+    CosmwasmExt,
+)]
+#[proto_message(type_url = "/osmosis.poolmanager.v1beta1.MsgSwapExactAmountIn")]
+pub struct MsgSwapExactAmountIn {
+    #[prost(string, tag = "1")]
+    pub sender: ::prost::alloc::string::String,
+    #[prost(message, repeated, tag = "2")]
+    pub routes: ::prost::alloc::vec::Vec<SwapAmountInRoute>,
+    #[prost(message, optional, tag = "3")]
+    pub token_in: ::core::option::Option<super::super::super::cosmos::base::v1beta1::Coin>,
+    #[prost(string, tag = "4")]
+    pub token_out_min_amount: ::prost::alloc::string::String,
+}
