4d1181a3c7d69af84d96b7ac5b68af7577b78047
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-09-08 20:02:16 +0100

    fuzzer init

--- code/parachain/frame/cosmwasm/fuzz/fuzz_targets/fuzz_target_1.rs
+++ code/parachain/frame/cosmwasm/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -2,6 +2,9 @@
 
 use libfuzzer_sys::fuzz_target;
 
+extern crate common;
+extern crate pallet_cosmwasm;
+
 fuzz_target!(|data: &[u8]| {
-    // fuzzed code goes here
+	// fuzzed code goes here
 });
