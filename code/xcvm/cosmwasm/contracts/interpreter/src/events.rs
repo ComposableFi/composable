b75a47401fa19afea22f3901192fe7d3e845dcac
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-09-12 10:59:27 +0100

    events

--- code/xcvm/cosmwasm/contracts/interpreter/src/events.rs
+++ code/xcvm/cosmwasm/contracts/interpreter/src/events.rs
@@ -1,4 +1,5 @@
-use cosmwasm_std::Event;
+use cosmwasm_std::{Event, Addr};
+use serde::{Serialize, Deserialize};
 use xc_core::{service::dex::ExchangeId, InterpreterOrigin, shared};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
@@ -10,14 +11,53 @@ pub struct CvmInterpreterExchanged {
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 #[cfg_attr(feature = "std", derive(schemars::JsonSchema))]
+#[serde(rename = "cvm.interpreter.execution_started")]
+pub struct CvmInterpreterExecutionStarted {
+}
+
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+#[cfg_attr(feature = "std", derive(schemars::JsonSchema))]
 #[serde(rename = "cvm.interpreter.instantiated")]
 pub struct CvmInterpreterInstantiated {
 	pub interpreter_origin: String,
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+#[cfg_attr(feature = "std", derive(schemars::JsonSchema))]
+#[serde(rename = "cvm.interpreter.transferred")]
+pub struct CvmInterpreterTransferred {
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+#[cfg_attr(feature = "std", derive(schemars::JsonSchema))]
+#[serde(rename = "cvm.interpreter.owner.added")]
+pub struct CvmInterpreterOwnerAdded {
+    pub owner : Addr,
+}
+
 
 
 // beneath is something to be generate by macro
+
+impl CvmInterpreterOwnerAdded {
+        pub fn new(owner: &[Addr]) -> Event {
+        Event::new("cvm.interpreter.owner.added")
+            .add_attribute("owner", owner.to_string())
+    }
+}
+
+impl CvmInterpreterExecutionStarted {
+    pub fn new() -> Event {
+        Event::new("cvm.interpreter.execution_started")
+    }
+}
+
+impl CvmInterpreterTransferred {
+    pub fn new() -> Event {
+        Event::new("cvm.interpreter.transferred")
+    }
+}
 
 impl CvmInterpreterInstantiated {
 	pub fn new(interpreter_origin: &InterpreterOrigin) -> Event {
