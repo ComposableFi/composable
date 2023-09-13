301ef469acfb22b2386b34b502c918a44f64fb31
Kostya Kastsevich <kkastsevich@gmail.com>
2023-09-13 22:20:27 +0300

    version 10038.1

--- code/parachain/runtime/composable/src/version.rs
+++ code/parachain/runtime/composable/src/version.rs
@@ -14,7 +14,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	//   runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
 	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
 	spec_version: 10038,
-	impl_version: 0,
+	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
 	state_version: 0,
