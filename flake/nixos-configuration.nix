cd4c43769b2e0142d462e0450d8e441b80e77d14
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-09-19 15:43:21 +0100

    clean up

--- flake/nixos-configuration.nix
+++ flake/nixos-configuration.nix
@@ -60,10 +60,6 @@
                   Restart = "always";
                 };
               };
-              users.users.root.openssh.authorizedKeys.keys = [
-                "ssh-rsaAAAAB3NzaC1yc2EAAAADAQABAAABgQCXsAg4FHGlI7YNSM86z + nLpoLKEP8pln4HoqP7GcCYYScoq7OduiYw2uvedYJU2jA91i + Ep6l + mbzh + qxMkAyte80bIHeQbo5f47JXUJblKrveGaVb3mPKQJ7MYVgvw + WySwZcqQrEKbTo + bp6DAIYrCBvWkIdFss // DDMGbcyX3oF5gqZ5DJsiD4q89chY1uOtwIWdjLHe+9LMud7/OetRWwHpbk5i3BFPa3hQiixu48/TynPlzMk4tYSXnmelhzybtrI0j40/UQnpb8nHj/U0+ZAKG2OcNo+wBRuYWUdkoGZh3p5diy7oaPap+AmA+d25qrBnB9Xr2t1QIUoSP7I4+Bq7S4AVKLPQhU1vo8mA4TWzhuHQ4tz5f3iN5XOMKxoxG7wMgL5Y3xF60sqXWlsNJ4VDb285WxuFvEhtPzsRHtdkPMHLjDxePpwASxTkTgoha1FCapvuQOjE73B66urmB5vhpVE5MYb9S5tALEwX95oUJhscwjCcr+9oZFOn5k="
-                "ssh-ed25519AAAAC3NzaC1lZDI1NTE5AAAAIM1+8YaFQCOY4D52kpPs8sgsDdfHFqjHIdWopedt4P7x"
-              ];
             })
 
           ];
