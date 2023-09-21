d51573fc30197c6076e9cb3bb38f59f9d04cd7f0
Kostya Kastsevich <kkastsevich@gmail.com>
2023-09-21 03:44:13 +0300

    version bump

--- code/parachain/runtime/composable/src/version.rs
+++ code/parachain/runtime/composable/src/version.rs
@@ -13,8 +13,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// The version of the runtime specification. A full node will not attempt to use its native
 	//   runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
 	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
-	spec_version: 10038,
-	impl_version: 2,
+	spec_version: 10039,
+	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
 	state_version: 0,
