579069e92c1e7ef95a7c08291a71b37c86075d3e
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-09-25 15:00:23 +0100

    spelling

--- flake/ethereum.nix
+++ flake/ethereum.nix
@@ -2,7 +2,6 @@
   perSystem =
     { self', pkgs, systemCommonRust, subnix, lib, system, devnetTools, ... }: {
       apps = rec {
-
         forge = self.inputs.flake-utils.lib.mkApp {
           name = "forge";
           drv = self.inputs.ethereum.packages.${system}.foundry;
