0b7b7f27726083fcfcd4a2445fdaceaf2a502c9b
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-09-26 23:29:30 +0100

    grow!

--- flake/ethereum.nix
+++ flake/ethereum.nix
@@ -10,6 +10,25 @@
             cp $src/bin/forge $out/bin/forge
           '';
         };
+        eth-executor = pkgs.writeShellApplication {
+          name = "eth-executor";
+          runtimeInputs = [ self.inputs.ethereum.packages.${system}.geth ];
+          text = ''
+            DATADIR=./flake/eth-pos-devnet/execution/
+            
+            geth --http --http.api=eth --http.addr=0.0.0.0 --authrpc.vhosts=* --authrpc.addr=0.0.0.0 --authrpc.jwtsecret=./eth-pos-devnet/jwtsecret --datadir="$DATADIR" --allow-insecure-unlock --unlock=0x123463a4b065722e99115d6c222f267d9cabb524 --password="$DATADIR" --nodiscover --syncmode=full
+          '';
+        };
+        eth-executor = pkgs.writeShellApplication {
+          name = "eth-executor";
+          runtimeInputs = [ self.inputs.ethereum.packages.${system}.geth ];
+          text = ''
+            DATADIR=./flake/eth-pos-devnet/execution/
+            
+            geth --http --http.api=eth --http.addr=0.0.0.0 --authrpc.vhosts=* --authrpc.addr=0.0.0.0 --authrpc.jwtsecret=./eth-pos-devnet/jwtsecret --datadir="$DATADIR" --allow-insecure-unlock --unlock=0x123463a4b065722e99115d6c222f267d9cabb524 --password="$DATADIR" --nodiscover --syncmode=full
+          '';
+        };
+
       };
     };
 }
