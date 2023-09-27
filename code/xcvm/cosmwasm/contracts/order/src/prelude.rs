888f771016675a253ed5817ba022a28fe36dcc8f
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-09-27 16:13:38 +0100

    solver

--- code/xcvm/cosmwasm/contracts/order/src/prelude.rs
+++
@@ -1,9 +0,0 @@
-pub use alloc::{string::String, vec::Vec};
-pub use cosmwasm_std::Addr;
-pub use serde::{Deserialize, Serialize};
-
-#[cfg(feature = "std")]
-pub use cosmwasm_schema::{cw_serde, QueryResponses};
-
-#[cfg(feature = "std")]
-pub use schemars::JsonSchema;
