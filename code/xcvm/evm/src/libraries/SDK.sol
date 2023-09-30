ca37cc1d0168a785848bb4dccd4c712c56737ab8
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-09-30 15:03:04 +0100

    copy pasting idiots

--- code/xcvm/evm/src/libraries/SDK.sol
+++ code/xcvm/evm/src/libraries/SDK.sol
@@ -5,7 +5,7 @@ import "protobuf3-solidity-lib/ProtobufLib.sol";
 import "openzeppelin-contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "../interfaces/IRouter.sol";
-import "./BytesLib.sol";
+import "bytes-utils/BytesLib.sol";
 
 
 library SDK {
