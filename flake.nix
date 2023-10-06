a33984a3330f8cfdcc92137a2c0b09d6887a4afb
Kostya Kastsevich <kkastsevich@gmail.com>
2023-10-06 06:26:56 +0300

    t

--- flake.nix
+++ flake.nix
@@ -61,14 +61,14 @@
     centauri-old-src = {
       flake = false;
       url =
-        "github:ComposableFi/composable-ibc/629eb0da075350ae514bacfde70e15a1c93debd7";
+        "github:ComposableFi/composable-ibc/e53e412ca3c69b652e4ac097771ea9a4df34d67c";
     };
 
     # after https://github.com/ComposableFi/centauri/pull/397 update and check devnet cosmos and dotsama work relaying
     centauri-src = {
       flake = false;
       url =
-        "github:ComposableFi/composable-ibc/629eb0da075350ae514bacfde70e15a1c93debd7";
+        "github:ComposableFi/composable-ibc/e53e412ca3c69b652e4ac097771ea9a4df34d67c";
     };
 
     eth-pos-devnet-src = {
