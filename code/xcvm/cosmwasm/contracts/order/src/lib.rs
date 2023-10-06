8c9e3842a084f8a20123169c2aeaddf3f45a9ce5
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-10-06 17:05:53 +0100

    contract

--- code/xcvm/cosmwasm/contracts/order/src/lib.rs
+++ code/xcvm/cosmwasm/contracts/order/src/lib.rs
@@ -7,7 +7,12 @@ use sylvia::{
 	entry_points,
 	types::{ExecCtx, InstantiateCtx, QueryCtx},
 };
+use cosmwasm_std::Addr;
+use xc_core::{service::dex::ExchangeId, NetworkId};
 
+
+pub type Amount = u128;
+pub type OrderId = u128;
 pub type Blocks = u32;
 
 /// parts of a whole, numerator / denominator
@@ -27,10 +32,65 @@ pub struct OrderSubMsg {
 
 #[cw_serde]
 pub struct OrderItem {
+	pub owner : Addr,
 	pub msg: OrderSubMsg,
 	pub coin: Coin,
-order_id: u128,
+	pub order_id: u128,
 }
+
+/// price information will not be used on chain or deciding.
+/// it will fill orders on chain as instructed
+/// and check that max/min from orders respected
+/// and sum all into volume. and compare solutions.
+/// on chain cares each user gets what it wants and largest volume solution selected.
+#[cw_serde]
+pub struct SolutionSubMsg {
+    pub cows: Vec<Cow>,
+    pub fill: Vec<Fill>,
+    /// must adhere Connection.fork_join_supported, for now it is always false (it restrict set of routes possible)
+    pub routes: Vec<Route>,	
+}
+
+/// how much of order to be solved by CoW.
+/// difference with `Fill` to be solved by cross chain exchange
+/// aggregate pool of all orders in solution is used to give user amount he wants.
+#[cw_serde]
+pub struct Cow {
+    pub order_id: OrderId,
+    pub amount: u128,
+}
+
+
+#[cw_serde]
+pub struct Route {
+    // on this chain
+    pub exchange: Vec<Exchange>,
+    pub spawn: Vec<Spawn>,
+}
+
+#[cw_serde]
+pub struct Spawn {
+    pub to_chain: NetworkId,
+    pub carry: Vec<Amount>,
+    pub execute: Option<Route>,
+}
+
+#[cw_serde]
+pub struct Exchange {
+    pub pool_id: ExchangeId,
+    pub give: Amount,
+    pub want_min: Amount,
+}
+
+#[cw_serde]
+pub struct Fill {
+    pub order_id: OrderId,
+    /// amount of order to be taken (100% in case of full fill, can be less in case of partial)
+    pub taken: u128,
+    /// amount user should get after order executed
+    pub given: u128,
+}
+
 
 pub struct OrderContract<'a> {
 	pub orders: Map<'a, u128, OrderItem>,
@@ -62,11 +122,18 @@ impl OrderContract<'_> {
 
 		/// just save order under incremented id
 		let order_id = self.next_order_id.load(ctx.deps.storage).unwrap_or_default();
-		let order = OrderItem { msg, coin: funds.clone(), order_id, };
+		let order = OrderItem { msg, coin: funds.clone(), order_id, owner: ctx.info.sender };
 		self.orders.save(ctx.deps.storage, order_id, &order)?;
 		self.next_order_id.save(ctx.deps.storage, &(order_id + 1))?;
 		
 		Ok(Response::default())
+	}
+
+	/// Provides solution for set of orders.
+	/// All fully
+	#[msg(exec)]
+	pub fn solve(&self, ctx: ExecCtx, msg: OrderSubMsg) -> StdResult<Response> {
+		todo!()
 	}
 
 	/// Simple get all orders
