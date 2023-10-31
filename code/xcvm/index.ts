76f11f0051f7e3f402c91e8356db34b1f6285d13
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-10-31 17:55:01 +0100

    using sdk

--- code/xcvm/index.ts
+++ code/xcvm/index.ts
@@ -1,2 +1,22 @@
 
-console.error("This is set of examples which teaches you basics of CW and CVM")
+import { Addr  } from "./dist/cw-mantis-order/CwMantisOrder.types"
+import { CwXcCoreClient  } from "./dist/cw-xc-core/CwXcCore.client"
+import { CosmWasmClient, SigningCosmWasmClient, ExecuteResult } from "@cosmjs/cosmwasm-stargate"
+
+import { decodeTxRaw, DirectSecp256k1HdWallet, Registry } from "@cosmjs/proto-signing";
+
+const print = console.info
+
+// replace with RPC use really use, this RPC may not work at point you call it"
+const CENTAURI_MAINNET = "https://centauri-mainnet.concordium.software"
+
+print("creating RPC client")
+
+const rawClient = new SigningCosmWasmClient(CENTAURI_MAINNET, "some key")
+const client = new CwXcCoreClient(CENTAURI_MAINNET)
+
+print("This is set of examples which teaches you basics of CW and CVM")
+
+print("Let start from simple CVM program to transfer PICA from one account to another")
+
+
