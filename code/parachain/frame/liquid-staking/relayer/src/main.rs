b5d18bc8891ebe87ab576f794e976f37acac1d60
rustdev <placex.com@gmail.com>
2023-10-31 20:30:14 +0000

    submit ledger proof each 5 h

--- code/parachain/frame/liquid-staking/relayer/src/main.rs
+++ code/parachain/frame/liquid-staking/relayer/src/main.rs
@@ -39,7 +39,6 @@ async fn main() {
     let sovereign_account_id_index_5 = "14tDkT3U93Pc1wLrHEjfYuhPPnFpMwDr7o8phPCTwTRj5wfE";
 
     let para_storage_key = parachain_header_storage_key(2019);
-    println!("Hello, world!");
 
     let p0 = "5f3e4907f716ac89b6347d15ececedca422adb579f1dbf4f3886c5cfa3bb8cc456d08aea5b028f73699523ae21709a815640ec97748f5b5da9a2298e830e8971df7908861e1710b957fe06f0703bca7d";
     let p1 = "5f3e4907f716ac89b6347d15ececedca422adb579f1dbf4f3886c5cfa3bb8cc491af1d8906a21795a98d84506b4216828886ca7474c66c027a9dc5d73901481568d551d01f13d0eb3bd36dd20ed2f13e";
@@ -48,12 +47,12 @@ async fn main() {
     let p4 = "5f3e4907f716ac89b6347d15ececedca422adb579f1dbf4f3886c5cfa3bb8cc4b8ac2e6f27924e06455d8e5c75b6a775542eca539ca4e92c4b4e0496bef934595597645e21eb29b5ce94bc38b7e45181";
     let p5 = "5f3e4907f716ac89b6347d15ececedca422adb579f1dbf4f3886c5cfa3bb8cc4232647f67d6923193db6aedafe0d5ea3abc522231610ec74f6391785db08c68f3c07c1bb45e2193b413f66926dec5072";
 
-    let s0 = StorageKey(hex::decode(p0).expect("Failed to decode hex string"));
-    let s1 = StorageKey(hex::decode(p1).expect("Failed to decode hex string"));
-    let s2 = StorageKey(hex::decode(p2).expect("Failed to decode hex string"));
-    let s3 = StorageKey(hex::decode(p3).expect("Failed to decode hex string"));
-    let s4 = StorageKey(hex::decode(p4).expect("Failed to decode hex string"));
-    let s5 = StorageKey(hex::decode(p5).expect("Failed to decode hex string"));
+    let s0 = StorageKey(hex::decode(p0).expect("Failed to decode hex string p0"));
+    let s1 = StorageKey(hex::decode(p1).expect("Failed to decode hex string p1"));
+    let s2 = StorageKey(hex::decode(p2).expect("Failed to decode hex string p2"));
+    let s3 = StorageKey(hex::decode(p3).expect("Failed to decode hex string p3"));
+    let s4 = StorageKey(hex::decode(p4).expect("Failed to decode hex string p4"));
+    let s5 = StorageKey(hex::decode(p5).expect("Failed to decode hex string p5"));
 
     let tuple = vec![
         (sovereign_account_id_index_0, s0, 0), 
@@ -76,69 +75,69 @@ async fn main() {
     let para_client = OnlineClient::<ComposableConfig>::from_rpc_client(para_ws_client.clone()).await.unwrap();
 
     
+    while true{
+
+        for i in tuple{
+
+            let keys = vec![i.1.as_ref()];
+
+            let state_proof: Vec<Vec<u8>> = vec![]; //TODO uncomment when rpc method is available
+                            // relay_client
+                            // .rpc()
+                            // .read_proof(keys.iter().map(AsRef::as_ref), None)
+                            // .await.unwrap()
+                            // .proof
+                            // .into_iter()
+                            // .map(|p| p.0)
+                            // .collect();
+            // assert!(state_proof.len() > 0);
+
+            let block_hash =
+                        relay_client.rpc().block_hash(None).await.unwrap().unwrap();
+            println!("block_hash: {:?}", block_hash);
+
+            use subxt::utils::AccountId32;
+            let account_id = AccountId32::from_str(i.0).unwrap();
+            let staking = relaychain::api::storage().staking().ledger(account_id);
+
+            let Some(ledger) = relay_client
+                .storage()
+                .at(block_hash)
+                .fetch(&staking)
+                .await.unwrap() else {
+                    println!("ledger not found");
+                    continue;
+                };
+                
 
 
-    for i in tuple{
+            let sl = hyperspace_core::substrate::composable::relaychain::api::runtime_types::pallet_staking::StakingLedger::try_from(ledger).unwrap();
+            println!("sl: {:?}", sl);
 
-        let keys = vec![i.1.as_ref()];
-
-        let state_proof: Vec<Vec<u8>> = vec![]; //TODO uncomment when rpc method is available
-                        // relay_client
-						// .rpc()
-						// .read_proof(keys.iter().map(AsRef::as_ref), None)
-						// .await.unwrap()
-						// .proof
-						// .into_iter()
-						// .map(|p| p.0)
-						// .collect();
-        // assert!(state_proof.len() > 0);
-
-        let block_hash =
-                    relay_client.rpc().block_hash(None).await.unwrap().unwrap();
-        println!("block_hash: {:?}", block_hash);
-
-        use subxt::utils::AccountId32;
-        let account_id = AccountId32::from_str(i.0).unwrap();
-        let staking = relaychain::api::storage().staking().ledger(account_id);
-
-        let Some(ledger) = relay_client
-            .storage()
-            .at(block_hash)
-            .fetch(&staking)
-            .await.unwrap() else {
-                println!("ledger not found");
-                continue;
+            let xxx = StakingLedger::<AccountId32, u128> {
+                stash: AccountId32::from_str(i.0).unwrap(),
+                total: sl.total,
+                active: sl.active,
+                unlocking: vec![],  //TODO
+                claimed_rewards: vec![], //TODO
             };
-            
+            let tx_set_staking_ledger = parachain_subxt::api::tx().pallet_liquid_staking().set_staking_ledger(i.2, xxx, state_proof);
+            let tx_value = parachain_subxt::api::tx().pallet_liquid_staking().initiate_exchange_rate();
 
+            let api = OnlineClient::<subxt::SubstrateConfig>::from_url("ws://127.0.0.1:8000").await.unwrap();
+            let v : Vec::<Value<()>> = vec![];
 
-        let sl = hyperspace_core::substrate::composable::relaychain::api::runtime_types::pallet_staking::StakingLedger::try_from(ledger).unwrap();
-        println!("sl: {:?}", sl);
+            use subxt::ext::sp_core::Pair;
+            //test wallet for lsd testing 5DPqUqEfnp3buHaqiVnPt8ryykJEQRgdqAjbscnrZG2qDADa
+            let key = sp_keyring::sr25519::sr25519::Pair::from_string(&"private sentence hip meadow place say issue winner express edge royal aerobic", None).expect("secret");
+            let signer: PairSigner<SubstrateConfig, sp_keyring::sr25519::sr25519::Pair> = PairSigner::new(key.clone());
 
-        let xxx = StakingLedger::<AccountId32, u128> {
-            stash: AccountId32::from_str(i.0).unwrap(),
-            total: sl.total,
-            active: sl.active,
-            unlocking: vec![],  //TODO
-            claimed_rewards: vec![], //TODO
-        };
-        let tx_set_staking_ledger = parachain_subxt::api::tx().pallet_liquid_staking().set_staking_ledger(i.2, xxx, state_proof);
-        let tx_value = parachain_subxt::api::tx().pallet_liquid_staking().initiate_exchange_rate();
+            sign_and_submit_staking_ledger_update(&api, tx_set_staking_ledger, signer).await;
+            tokio::time::sleep(std::time::Duration::from_secs(10)).await;
+        }
 
-        let api = OnlineClient::<subxt::SubstrateConfig>::from_url("ws://127.0.0.1:8000").await.unwrap();
-        let v : Vec::<Value<()>> = vec![];
-
-        use subxt::ext::sp_core::Pair;
-        //test wallet for lsd testing 5DPqUqEfnp3buHaqiVnPt8ryykJEQRgdqAjbscnrZG2qDADa
-        let key = sp_keyring::sr25519::sr25519::Pair::from_string(&"private sentence hip meadow place say issue winner express edge royal aerobic", None).expect("secret");
-        let signer: PairSigner<SubstrateConfig, sp_keyring::sr25519::sr25519::Pair> = PairSigner::new(key.clone());
-
-        sign_and_submit_staking_ledger_update(&api, tx_set_staking_ledger, signer).await;
-        tokio::time::sleep(std::time::Duration::from_secs(10)).await;
-
-
-
-
+        //sleep 5 hours before next submit of ledger proof to lsd pallet on composable
+        tokio::time::sleep(std::time::Duration::from_secs(60 * 50 * 5)).await;
     }
 
     todo!();
@@ -152,7 +151,7 @@ async fn sign_and_submit_staking_ledger_update(api: &OnlineClient<SubstrateConfi
         println!("signed: {:?}", signed);
         i -= 1;
         match signed {
-            Ok(progress) => {
+            Ok(_) => {
                 i = 0;
             },
             Err(e) => {
