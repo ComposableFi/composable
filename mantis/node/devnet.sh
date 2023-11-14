f4716067005f067d2721508cadb8fac90c0e81d4
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-11-14 16:03:45 +0000

    mantis

--- mantis/node/devnet.sh
+++ mantis/node/devnet.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-clear && cargo run --bin mantis -- --centauri <CENTAURI> --osmosis <OSMOSIS> --neutron <NEUTRON> --cvm-contract <CVM_CONTRACT> --wallet <WALLET> --order-contract <ORDER_CONTRACT>
+clear && cargo run --bin mantis -- --centauri "http://localhost:26657" --osmosis "localhost:36657" --neutron "localhost:46657" --cvm-contract "centauri1" --wallet "mnemonic" --order-contract "centauri1"
