30091bbf5f4a15ff12001149094d08f9c2aa6afc
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-11-16 18:19:00 +0000

    chore(ci): fix repo key

--- terraform/github.com/keys.tf
+++ terraform/github.com/keys.tf
@@ -4,6 +4,6 @@ resource "tls_private_key" "CI_SSH_KEY" {
 
 resource "github_actions_environment_secret" "CI_SSH_KEY" {
     environment = tls_private_key.CI_SSH_KEY.public_key_openssh
-    repository = data.github_repository.composable
+    repository = data.github_repository.composable.name
     secret_name = "CI_SSH_KEY"   
 }
