e0b27fd43902d9afbe99fc74fba9724ec5476072
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-11-16 18:23:33 +0000

    chore(ci): fixing keys more

--- terraform/github.com/keys.tf
+++ terraform/github.com/keys.tf
@@ -1,9 +1,3 @@
 resource "tls_private_key" "CI_SSH_KEY" {
   algorithm = "ECDSA"
 }
-
-resource "github_actions_environment_secret" "CI_SSH_KEY" {
-    environment = tls_private_key.CI_SSH_KEY.public_key_openssh
-    repository = data.github_repository.composable.name
-    secret_name = "CI_SSH_KEY"   
-}
