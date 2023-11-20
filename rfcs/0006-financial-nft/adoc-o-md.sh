79acd7349132e69c57b1d62cb1c47c19eb940875
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-11-20 14:08:45 +0000

    chore: clean up old docs and add ATOM to CVM registry, better MANTIS doc (#4334)

--- rfcs/0006-financial-nft/adoc-o-md.sh
+++
@@ -1,4 +0,0 @@
-INPUT_ADOC=0006-financial-nft.adoc
-asciidoctor -r asciidoctor-diagram -r asciidoctor-mathematical --backend html --out-file - $INPUT_ADOC | \
-# asciidoctor -r asciidoctor-diagram --backend html --out-file - $INPUT_ADOC | \
-pandoc --from html --to markdown_strict --output $INPUT_ADOC.md
